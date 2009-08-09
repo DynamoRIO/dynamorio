d34c2347eb9588bb6afdefa8b27f5d91eeb6ae73
Derek Bruening <derek.bruening@gmail.com>
2009-08-09 19:11:00 +0000

    * support running nightly.cmake from a copy in parent dir * change night source snapshot to 4am * for now, the CDash nightly test results are not publicly visible: I   created a user for the devs email list that we can share, and set up   emails to be sent on build failure (not test failure since suite isn't   clean enough for that).  I added a pointer to nightly.cmake in the docs.
    SVN-Revision: 200

--- suite/nightly.cmake
+++ suite/nightly.cmake
@@ -40,6 +40,7 @@ cmake_minimum_required (VERSION 2.2)
 #    name and change the site= string below in $nightly/nightly.cmake.  The
 #    convention is {Linux,Windows}.OSVersion.Machine/Location.
 #    E.g., Windows.Vista64.CSAIL
+#    The site name should not contain spaces or quotes.
 # 4) Set up a cron job to run:
 #    cd $nightly/run; ctest -V -S ../nightly.cmake > ctest.log 2>&1
 #
@@ -47,5 +48,7 @@ cmake_minimum_required (VERSION 2.2)
 # space, but the xml file results will be recorded on the CDash server.
 # The run directory needs approximately 1.5GB of space.
 
+# We point at src/suite/runsuite.cmake, assuming this file will be executed
+# from two dirs below.
 # Site name should not have spaces or quotes.
-ctest_run_script(${CTEST_SCRIPT_DIRECTORY}/runsuite.cmake,nightly\;long\;site=Linux.Fedora10.Glaurung)
+ctest_run_script(${CTEST_SCRIPT_DIRECTORY}/src/suite/runsuite.cmake,nightly\;long\;site=Linux.Fedora10.Glaurung)
