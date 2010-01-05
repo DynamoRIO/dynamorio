be5e07ce75db8be0b65026fd4c1a410dff53fbf0
Derek Bruening <derek.bruening@gmail.com>
2010-01-05 21:41:11 +0000

    replace package.sh with package.cmake
    SVN-Revision: 259

--- make/package.cmake
+++ make/package.cmake
@@ -1,5 +1,5 @@
 # **********************************************************
-# Copyright (c) 2009 VMware, Inc.    All rights reserved.
+# Copyright (c) 2009-2010 VMware, Inc.    All rights reserved.
 # **********************************************************
 
 # Redistribution and use in source and binary forms, with or without
@@ -33,12 +33,55 @@
 # package.bat: though we assume can use Unix Makefiles on Windows,
 # in order to build in parallel.
 
-# Usage: invoke via "ctest -S package.cmake,<build#>"
+# Usage: invoke via "ctest -S package.cmake,build=<build#>"
+# See other available args below
 
 cmake_minimum_required (VERSION 2.2)
 
-if ("${CTEST_SCRIPT_ARG}" STREQUAL "")
-  message(FATAL_ERROR "build number not set")
+# arguments are a ;-separated list (must escape as \; from ctest_run_script())
+# required args:
+set(arg_build "")      # build #
+# optional args:
+set(arg_ubuild "")     # unique build #: if not specified, set to arg_build
+set(arg_version "")    # version #
+set(arg_outdir ".")    # directory in which to place deliverables
+set(arg_preload "")    # cmake file to include prior to each 32-bit build
+set(arg_preload64 "")  # cmake file to include prior to each 64-bit build
+set(arg_cacheappend "")# string to append to every build's cache
+set(arg_no64 OFF)      # skip the 64-bit builds?
+
+foreach (arg ${CTEST_SCRIPT_ARG})
+  if (${arg} MATCHES "^build=")
+    string(REGEX REPLACE "^build=" "" arg_build "${arg}")
+  endif ()
+  if (${arg} MATCHES "^ubuild=")
+    string(REGEX REPLACE "^ubuild=" "" arg_ubuild "${arg}")
+  endif ()
+  if (${arg} MATCHES "^version=")
+    string(REGEX REPLACE "^version=" "" arg_version "${arg}")
+  endif ()
+  if (${arg} MATCHES "^outdir=")
+    string(REGEX REPLACE "^outdir=" "" arg_outdir "${arg}")
+  endif ()
+  if (${arg} MATCHES "^preload=")
+    string(REGEX REPLACE "^preload=" "" arg_preload "${arg}")
+  endif ()
+  if (${arg} MATCHES "^preload64=")
+    string(REGEX REPLACE "^preload64=" "" arg_preload64 "${arg}")
+  endif ()
+  if (${arg} MATCHES "^cacheappend=")
+    string(REGEX REPLACE "^cacheappend=" "" arg_cacheappend "${arg}")
+  endif ()
+  if (${arg} MATCHES "^no64")
+    set(arg_no64 ON)
+  endif ()
+endforeach (arg)
+
+if ("${arg_build}" STREQUAL "")
+  message(FATAL_ERROR "build number not set: pass as build= arg")
+endif()
+if ("${arg_ubuild}" STREQUAL "")
+  set(arg_ubuild "${arg_build}")
 endif()
 
 get_filename_component(BINARY_BASE "." ABSOLUTE)
@@ -62,10 +105,33 @@ set(CTEST_COMMAND "${CTEST_EXECUTABLE_NAME}")
 
 function(dobuild name is64 initial_cache)
   set(CTEST_BUILD_NAME "${name}")
+
+  # preload file can set base_cache
+  set(base_cache "")
+  if (is64)
+    set(preload_file "${arg_preload64}")
+  else (is64)
+    set(preload_file "${arg_preload}")
+  endif (is64)
+  if (preload_file)
+    # Command-style CTest (i.e., using ctest_configure(), etc.) does
+    # not support giving args to CTEST_CMAKE_COMMAND so we are forced
+    # to do an include() instead of -C
+    include("${preload_file}")
+  endif (preload_file)
+
+  # version is optional
+  if (arg_version)
+    set(base_cache "${base_cache}
+      VERSION_NUMBER:STRING=${arg_version}")
+  endif (arg_version)
+
   set(CTEST_BINARY_DIRECTORY "${BINARY_BASE}/build_${CTEST_BUILD_NAME}")
   set(CTEST_INITIAL_CACHE "${initial_cache}
-    BUILD_NUMBER:STRING=${CTEST_SCRIPT_ARG}
-    UNIQUE_BUILD_NUMBER:STRING=${CTEST_SCRIPT_ARG}
+    BUILD_NUMBER:STRING=${arg_build}
+    UNIQUE_BUILD_NUMBER:STRING=${arg_ubuild}
+    ${arg_cacheappend}
+    ${base_cache}
     ")
   ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
   file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "${CTEST_INITIAL_CACHE}")
@@ -141,15 +207,16 @@ dobuild("debug-32" OFF "
   BUILD_DRGUI:BOOL=OFF
   BUILD_SAMPLES:BOOL=OFF
   ")
-dobuild("release-64" ON "")
-dobuild("debug-64" ON "
-  DEBUG:BOOL=ON
-  BUILD_TOOLS:BOOL=OFF
-  BUILD_DOCS:BOOL=OFF
-  BUILD_DRGUI:BOOL=OFF
-  BUILD_SAMPLES:BOOL=OFF
-  ")
-
+if (NOT arg_no64)
+  dobuild("release-64" ON "")
+  dobuild("debug-64" ON "
+    DEBUG:BOOL=ON
+    BUILD_TOOLS:BOOL=OFF
+    BUILD_DOCS:BOOL=OFF
+    BUILD_DRGUI:BOOL=OFF
+    BUILD_SAMPLES:BOOL=OFF
+    ")
+endif (NOT arg_no64)
 
 # now package up all the builds
 file(APPEND "${last_build}/CPackConfig.cmake"
@@ -162,4 +229,11 @@ ctest_build(BUILD "${last_build}")
 # copy the final archive into cur dir
 # "cmake -E copy" only takes one file so use 'z' => .tar.gz or .zip
 file(GLOB results ${last_build}/DynamoRIO-*z*)
-execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${results} .)
+execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${results} "${arg_outdir}")
+
+# workaround for http://www.cmake.org/Bug/view.php?id=9647
+# it complains and returns error if CTEST_BINARY_DIRECTORY not set
+set(CTEST_BINARY_DIRECTORY "${BINARY_BASE}/")
+# it tries to configure+build, but with a start command it does nothing,
+# which is what we want:
+ctest_start(${SUITE_TYPE})
