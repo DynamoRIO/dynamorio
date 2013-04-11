ee0012b1459e4d7d123269301a0dc23d78573482
Qin Zhao <zhaoqin@google.com>
2013-04-11 18:42:36 +0000

    replace dr_sscanf with sscanf to handle module path with white-space
    SVN-Revision: 2052

--- clients/bbcov/bbcov2lcov.c
+++ clients/bbcov/bbcov2lcov.c
@@ -524,13 +524,15 @@ read_module_list(char *buf, void ***tables, uint *num_mods)
     PRINT(4, "Reading Module Lists\n");
     *tables = calloc(*num_mods, sizeof(*tables));
     for (i = 0; i < *num_mods; i++) {
-        ushort mod_id;
-        uint64 mod_size;
-        void  *bb_table;
-        if (dr_sscanf(buf, " %u, %llu, %s\n", &mod_id, &mod_size, path) != 3)
+        uint  mod_id;
+        long long unsigned int mod_size; /* for %llu on sscanf */
+        void *bb_table;
+        /* assuming the string is something like:  "0, 2207744, /bin/ls" */
+        /* XXX: we do not use dr_sscanf since it does not support %[] */
+        if (sscanf(buf, " %u, %llu, %[^\n\r]", &mod_id, &mod_size, path) != 3)
             ASSERT(false, "Failed to read module table");
         buf = move_to_next_line(buf);
-        PRINT(5, "Module: %d, "PFX", %s\n", mod_id, (ptr_uint_t)mod_size, path);
+        PRINT(5, "Module: %u, "PFX", %s\n", mod_id, (ptr_uint_t)mod_size, path);
         bb_table = hashtable_lookup(&module_htable, path);
         if (bb_table == NULL) {
             if (mod_size >= UINT_MAX)
