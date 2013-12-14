93730606b6fcde28ee140abfd1f638f5239018db
Derek Bruening <bruening@google.com>
2013-12-14 16:05:01 +0000

    Fixes issue 1343
    Extend the size of the 32-bit interception buffer, as it can be exceeded by
    a client requesting extra syscalls be intercepted while native.
    
    SVN-Revision: 2444

--- core/win32/os_private.h
+++ core/win32/os_private.h
@@ -312,7 +312,7 @@ os_rename_file_in_directory(IN HANDLE rootdir,
 /* thread-shared only needs 4 pages on 32-bit but -thread_private needs 5
  * in case we hook the image entry on an early cbret
  */
-#define INTERCEPTION_CODE_SIZE IF_X64_ELSE(8*4096,6*4096)
+#define INTERCEPTION_CODE_SIZE IF_X64_ELSE(8*4096,7*4096)
 
 /* see notes in intercept_new_thread() about these values */
 #define THREAD_START_ADDR IF_X64_ELSE(CXT_XCX, CXT_XAX)
