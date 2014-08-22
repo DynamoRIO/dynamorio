5cde3aa2f2196409c9a6253e91c1a7b62819cd17
Derek Bruening <bruening@google.com>
2014-08-22 21:11:21 +0000

    Fix warning in Mac build
    SVN-Revision: 2791

--- clients/drcov/drcov2lcov.c
+++ clients/drcov/drcov2lcov.c
@@ -637,7 +637,7 @@ search_cb(drsym_info_t *info, drsym_error_t status, void *data)
         /* strdup is deprecated on Windows */
         strncpy(name, info->name, strlen(info->name) + 1);
         PRINT(5, "function %s: "PFX"-"PFX"\n",
-              name, info->start_offs, info->end_offs);
+              name, (ptr_uint_t)info->start_offs, (ptr_uint_t)info->end_offs);
         ASSERT(info->start_offs <= table->size, "wrong offset");
         hashtable_add(&table->test_htable, (void *)info->start_offs, name);
     }
