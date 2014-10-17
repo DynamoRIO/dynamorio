d442dfe328fd30fa304b78d814e41b1dbd152f96
Derek Bruening <bruening@google.com>
2014-10-17 05:02:54 +0000

    i#1551 port to ARM: initial A32 decoder types and algorithm
    + Add decode table entry flags, extensions, tables, and types for ARM A32
      predicated instrs
    + Add some types we'll need for Thumb and A64 as well
    + Add initial A32 decoder algorithm for predicated instrs
    
    The actual tables, and operand decoding, will come in separately.
    
    SVN-Revision: 2874

---
+++ core/arch/arm/decode.c
@@ -0,0 +1,455 @@
+/* **********************************************************
+ * Copyright (c) 2014 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#include "../globals.h"
+#include "instr.h"
+#include "decode.h"
+#include "decode_private.h"
+#include <string.h> /* for memcpy */
+
+/* ARM decoder.
+ * General strategy:
+ * + We use a data-driven table-based approach, as we need to both encode and
+ *   decode and a central source of data lets us move in both directions.
+ */
+/* FIXME i#1551: add Thumb support: for now just A32 */
+/* FIXME i#1551: add A64 support: for now just A32 */
+
+/* We assume little-endian */
+static inline int
+decode_predicate(uint instr_word)
+{
+    return instr_word >> 28; /* bits 31:28 */
+}
+
+/* We often take bits 27:20 as an 8-bit opcode */
+static inline int
+decode_opc8(uint instr_word)
+{
+    return (instr_word >> 20) & 0xff;
+}
+
+/* We often take bits 7:4 as a 4-bit auxiliary opcode */
+static inline int
+decode_opc4(uint instr_word)
+{
+    return (instr_word >> 4) & 0xf;
+}
+
+static bool
+decode_operand(decode_info_t *di, byte optype, opnd_size_t opsize, opnd_t *opnd)
+{
+    switch (optype) {
+    case TYPE_NONE:
+        *opnd = opnd_create_null();
+        return true;
+    /* FIXME i#1551: add decoding of each operand type */
+    default:
+        /* ok to assert, types coming only from instr_info_t */
+        CLIENT_ASSERT(false, "decode error: unknown operand type");
+    }
+    return false;
+}
+
+/* Disassembles the instruction at pc into the data structures ret_info
+ * and di.  Returns a pointer to the pc of the next instruction.
+ * Returns NULL on an invalid instruction.
+ */
+static byte *
+read_instruction(byte *pc, byte *orig_pc,
+                 const instr_info_t **ret_info, decode_info_t *di
+                 _IF_DEBUG(bool report_invalid))
+{
+    uint instr_word;
+    const instr_info_t *info;
+    uint pred;
+    uint idx;
+
+    /* Read instr bytes and initialize di */
+    di->start_pc = pc;
+    di->orig_pc = orig_pc;
+    instr_word = *(uint *)pc;
+    pc += sizeof(instr_word);
+    di->instr_word = instr_word;
+
+    pred = decode_predicate(instr_word);
+    if (pred == PRED_OP) {
+        uint opc8 = decode_opc8(instr_word);
+        info = &A32_nopred_opc8[opc8];
+    } else {
+        uint opc8 = decode_opc8(instr_word);
+        info = &A32_pred_opc8[opc8];
+    }
+
+    /* If an extension, discard the old info and get a new one */
+    if (info->type == EXT_OPC4X) {
+        if ((instr_word & 0x10 /*bit 4*/) == 0)
+            idx = 0;
+        else if ((instr_word & 0x80 /*bit 7*/) == 0)
+            idx = 1;
+        else
+            idx = 2 + ((instr_word >> 5) & 0x3) /*bits 6:5*/;
+        info = &A32_ext_opc4x[info->code][idx];
+    } else if (info->type == EXT_OPC4Y) {
+        if ((instr_word & 0x10 /*bit 4*/) == 0)
+            idx = 0;
+        else
+            idx = 1 + ((instr_word >> 5) & 0x7) /*bits 7:5*/;
+        info = &A32_ext_opc4y[info->code][idx];
+    } else if (info->type == EXT_OPC4) {
+        idx = decode_opc4(instr_word);
+        info = &A32_ext_opc4[info->code][idx];
+    } else if (info->type == EXT_IMM1916) {
+        idx = (((instr_word >> 16) & 0xf) /*bits 19:16*/ == 0) ? 0 : 1;
+        info = &A32_ext_imm1916[info->code][idx];
+    } else if (info->type == EXT_BIT4) {
+        idx = (instr_word >> 4) & 0x1 /*bit 4*/;
+        info = &A32_ext_bit4[info->code][idx];
+    }
+
+    /* Secondary extensions */
+    if (info->type == EXT_BIT9) {
+        idx = (instr_word >> 9) & 0x1 /*bit 9*/;
+        info = &A32_ext_bit9[info->code][idx];
+    } else if (info->type == EXT_BITS8) {
+        idx = (instr_word >> 8) & 0x3 /*bits 9:8*/;
+        info = &A32_ext_bits8[info->code][idx];
+    } else if (info->type == EXT_BITS0) {
+        idx = instr_word & 0x7 /*bits 2:0*/;
+        info = &A32_ext_bits0[info->code][idx];
+    } else if (info->type == EXT_IMM5) {
+        idx = (((instr_word >> 7) & 0x1f) /*bits 11:7*/ == 0) ? 0 : 1;
+        info = &A32_ext_imm5[info->code][idx];
+    } else if (info->type == EXT_RDPC) {
+        idx = ((instr_word & 0xf) /*bits 3:0*/ == 0xf) ? 1 : 0;
+        info = &A32_ext_RDPC[info->code][idx];
+    } else if (info->type == EXT_OPC4) {
+        idx = decode_opc4(instr_word);
+        info = &A32_ext_opc4[info->code][idx];
+    }
+
+    /* We should now have either a valid OP_ opcode or an invalid opcode */
+    if (info == NULL || info == &invalid_instr ||
+        info->type < OP_FIRST || info->type > OP_LAST) {
+        DODEBUG({
+            /* PR 605161: don't report on DR addresses */
+            if (report_invalid && !is_dynamo_address(di->start_pc)) {
+                SYSLOG_INTERNAL_WARNING_ONCE("Invalid opcode encountered");
+                LOG(THREAD_GET, LOG_ALL, 1, "Invalid opcode @"PFX": 0x%016x\n",
+                    di->start_pc, instr_word);
+            }
+        });
+        *ret_info = &invalid_instr;
+        return NULL;
+    }
+
+    /* Unlike x86, we have a fixed size, so we're done */
+    *ret_info = info;
+    return pc;
+}
+
+byte *
+decode_eflags_usage(dcontext_t *dcontext, byte *pc, uint *usage)
+{
+    const instr_info_t *info;
+    decode_info_t di;
+    /* XXX i#1551: set di.isa_mode here once we have it */
+    pc = read_instruction(pc, pc, &info, &di _IF_DEBUG(true));
+    *usage = info->eflags;
+    /* we're fine returning NULL on failure */
+    return pc;
+}
+
+byte *
+decode_opcode(dcontext_t *dcontext, byte *pc, instr_t *instr)
+{
+    const instr_info_t *info;
+    decode_info_t di;
+    /* XXX i#1551: set di.isa_mode here once we have it, and set mode of instr */
+    pc = read_instruction(pc, pc, &info, &di _IF_DEBUG(true));
+    instr_set_opcode(instr, info->type);
+    if (!instr_valid(instr)) {
+        CLIENT_ASSERT(!instr_valid(instr), "decode_opcode: invalid instr");
+        return NULL;
+    }
+    instr->eflags = info->eflags;
+    instr_set_eflags_valid(instr, true);
+    instr_set_operands_valid(instr, false);
+    instr_set_raw_bits(instr, pc, pc - di.orig_pc);
+    return pc;
+}
+
+/* XXX: some of this code could be shared with x86/decode.c */
+static byte *
+decode_common(dcontext_t *dcontext, byte *pc, byte *orig_pc, instr_t *instr)
+{
+    const instr_info_t *info;
+    decode_info_t di;
+    byte *next_pc;
+    int instr_num_dsts = 0, instr_num_srcs = 0;
+    opnd_t dsts[8];
+    opnd_t srcs[8];
+
+    CLIENT_ASSERT(instr->opcode == OP_INVALID || instr->opcode == OP_UNDECODED,
+                  "decode: instr is already decoded, may need to call instr_reset()");
+
+    /* XXX i#1551: set di.isa_mode here once we have it, and set mode of instr */
+    next_pc = read_instruction(pc, orig_pc, &info, &di
+                               _IF_DEBUG(!TEST(INSTR_IGNORE_INVALID, instr->flags)));
+    instr_set_opcode(instr, info->type);
+    /* failure up to this point handled fine -- we set opcode to OP_INVALID */
+    if (next_pc == NULL) {
+        LOG(THREAD, LOG_INTERP, 3, "decode: invalid instr at "PFX"\n", pc);
+        CLIENT_ASSERT(!instr_valid(instr), "decode: invalid instr");
+        return NULL;
+    }
+    instr->eflags = info->eflags;
+    instr_set_eflags_valid(instr, true);
+    /* since we don't use set_src/set_dst we must explicitly say they're valid */
+    instr_set_operands_valid(instr, true);
+
+    /* FIXME i#1551: set predicate flags in instr->prefixes, from di I guess */
+
+    /* operands */
+    do {
+        if (info->dst1_type != TYPE_NONE) {
+            if (!decode_operand(&di, info->dst1_type, info->dst1_size,
+                                &(dsts[instr_num_dsts++])))
+                goto decode_invalid;
+        }
+        if (info->dst2_type != TYPE_NONE) {
+            if (!decode_operand(&di, info->dst2_type, info->dst2_size,
+                                TEST(DECODE_4_SRCS, info->flags) ?
+                                &(srcs[instr_num_srcs++]) :
+                                &(dsts[instr_num_dsts++])))
+                goto decode_invalid;
+        }
+        if (info->src1_type != TYPE_NONE) {
+            if (!decode_operand(&di, info->src1_type, info->src1_size,
+                                TEST(DECODE_3_DSTS, info->flags) ?
+                                &(dsts[instr_num_dsts++]) :
+                                &(srcs[instr_num_srcs++])))
+                goto decode_invalid;
+        }
+        if (info->src2_type != TYPE_NONE) {
+            if (!decode_operand(&di, info->src2_type, info->src2_size,
+                                &(srcs[instr_num_srcs++])))
+                goto decode_invalid;
+        }
+        if (info->src3_type != TYPE_NONE) {
+            if (!decode_operand(&di, info->src3_type, info->src3_size,
+                                &(srcs[instr_num_srcs++])))
+                goto decode_invalid;
+        }
+        info = instr_info_extra_opnds(info);
+    } while (info != NULL);
+
+    /* now copy operands into their real slots */
+    instr_set_num_opnds(dcontext, instr, instr_num_dsts, instr_num_srcs);
+    if (instr_num_dsts > 0) {
+        memcpy(instr->dsts, dsts, instr_num_dsts*sizeof(opnd_t));
+    }
+    if (instr_num_srcs > 0) {
+        instr->src0 = srcs[0];
+        if (instr_num_srcs > 1) {
+            memcpy(instr->srcs, &(srcs[1]), (instr_num_srcs-1)*sizeof(opnd_t));
+        }
+    }
+
+    if (orig_pc != pc) {
+        /* We do not want to copy when encoding and condone an invalid
+         * relative target
+         */
+        instr_set_raw_bits_valid(instr, false);
+        instr_set_translation(instr, orig_pc);
+    } else {
+        /* we set raw bits AFTER setting all srcs and dsts b/c setting
+         * a src or dst marks instr as having invalid raw bits
+         */
+        IF_X64(ASSERT(CHECK_TRUNCATE_TYPE_uint(next_pc - pc)));
+        instr_set_raw_bits(instr, pc, (uint)(next_pc - pc));
+    }
+
+    return next_pc;
+
+ decode_invalid:
+    instr_set_operands_valid(instr, false);
+    instr_set_opcode(instr, OP_INVALID);
+    return NULL;
+}
+
+byte *
+decode(dcontext_t *dcontext, byte *pc, instr_t *instr)
+{
+    return decode_common(dcontext, pc, pc, instr);
+}
+
+byte *
+decode_from_copy(dcontext_t *dcontext, byte *copy_pc, byte *orig_pc, instr_t *instr)
+{
+    return decode_common(dcontext, copy_pc, orig_pc, instr);
+}
+
+byte *
+decode_cti(dcontext_t *dcontext, byte *pc, instr_t *instr)
+{
+    /* XXX i#1551: build a fast decoder for branches -- though it may not make
+     * sense for 32-bit where many instrs can write to the pc.
+     */
+    return decode(dcontext, pc, instr);
+}
+
+byte *
+decode_next_pc(dcontext_t *dcontext, byte *pc)
+{
+    /* FIXME i#1551: check for invalid opcodes */
+    /* FIXME i#1551: add Thumb support */
+    return pc + 4;
+}
+
+int
+decode_sizeof(dcontext_t *dcontext, byte *pc, int *num_prefixes
+              _IF_X64(uint *rip_rel_pos))
+{
+    /* FIXME i#1551: check for invalid opcodes */
+    /* FIXME i#1551: add Thumb support */
+    return 4;
+}
+
+/* XXX: share this with x86 */
+byte *
+decode_raw(dcontext_t *dcontext, byte *pc, instr_t *instr)
+{
+    /* XXX i#1551: set isa_mode of instr once we add that feature */
+    int sz = decode_sizeof(dcontext, pc, NULL _IF_X64(NULL));
+    if (sz == 0) {
+        /* invalid instruction! */
+        instr_set_opcode(instr, OP_INVALID);
+        return NULL;
+    }
+    instr_set_opcode(instr, OP_UNDECODED);
+    instr_set_raw_bits(instr, pc, sz);
+    /* assumption: operands are already marked invalid (instr was reset) */
+    return (pc + sz);
+}
+
+const instr_info_t *
+instr_info_extra_opnds(const instr_info_t *info)
+{
+    if (TEST(DECODE_EXTRA_SHIFT, info->flags))
+        return &extra_operands[0];
+    else if (TEST(DECODE_EXTRA_WRITEBACK, info->flags))
+        return &extra_operands[1];
+    else if (TEST(DECODE_EXTRA_OPERANDS, info->flags))
+        return (const instr_info_t *)(info->code);
+    else
+        return NULL;
+}
+
+/* num is 0-based */
+byte
+instr_info_opnd_type(const instr_info_t *info, bool src, int num)
+{
+    int i = 0;
+    while (info != NULL) {
+        if (!src && i++ == num)
+            return info->dst1_type;
+        if (TEST(DECODE_4_SRCS, info->flags)) {
+            if (src && i++ == num)
+                return info->dst2_type;
+        } else {
+            if (!src && i++ == num)
+                return info->dst2_type;
+        }
+        if (TEST(DECODE_3_DSTS, info->flags)) {
+            if (!src && i++ == num)
+                return info->src1_type;
+        } else {
+            if (src && i++ == num)
+                return info->src1_type;
+        }
+        if (src && i++ == num)
+            return info->src2_type;
+        if (src && i++ == num)
+            return info->src3_type;
+        info = instr_info_extra_opnds(info);
+    }
+    CLIENT_ASSERT(false, "internal decode error");
+    return TYPE_NONE;
+}
+
+const instr_info_t *
+get_next_instr_info(const instr_info_t * info)
+{
+    return (const instr_info_t *)(info->code);
+}
+
+byte
+decode_first_opcode_byte(int opcode)
+{
+    CLIENT_ASSERT(false, "should not be used on ARM");
+    return 0;
+}
+
+DR_API
+const char *
+decode_opcode_name(int opcode)
+{
+    const instr_info_t * info = op_instr[opcode];
+    return info->name;
+}
+
+opnd_size_t
+resolve_variable_size(decode_info_t *di, opnd_size_t sz, bool is_reg)
+{
+    return sz;
+}
+
+bool
+optype_is_indir_reg(int optype)
+{
+    return false;
+}
+
+
+#ifdef DECODE_UNIT_TEST
+/* FIXME i#1551: add unit tests here.  How divide vs suite/tests/api/ tests? */
+# include "instr_create.h"
+
+int main()
+{
+    bool res = true;
+    standalone_init();
+    return res;
+}
+
+#endif /* DECODE_UNIT_TEST */
---
+++ core/arch/arm/decode_private.h
@@ -0,0 +1,257 @@
+/* **********************************************************
+ * Copyright (c) 2014 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Goole, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/* file "decode_private.h" */
+
+#ifndef DECODE_PRIVATE_H
+#define DECODE_PRIVATE_H
+
+
+/* instr_info_t.type special codes: */
+enum {
+    /* not a valid opcode */
+    INVALID = OP_LAST + 1,
+    EXT_OPC4,    /* Indexed by bits 7:4 */
+    EXT_OPC4X,   /* Indexed by bits 7:4 in specific manner: see table */
+    EXT_OPC4Y,   /* Indexed by bits 7:4 w/ 1st entry covering all evens */
+    EXT_IMM1916, /* Indexed by whether imm4 in 19:16 is zero or not */
+    EXT_IMM5,    /* Indexed by whether imm5 11:7 is zero or not */
+    EXT_BITS0,   /* Indexed by bits 2:0 */
+    EXT_BITS8,   /* Indexed by bits 9:8 */
+    EXT_BIT4,    /* Indexed by bit 4 */
+    EXT_BIT9,    /* Indexed by bit 9 */
+    EXT_RDPC,    /* Indexed by whether RD != PC */
+    /* else, from OP_ enum */
+};
+
+/* instr_info_t.opcode: holds all the 1 bits for the opcode.
+ * We set it first, so we don't need to store 0's explicitly.
+ */
+
+/* instr_info_t.name: we store lowercase, and the disassembler upcases it
+ * for ARM-style disasm.
+ */
+
+/* instr_info.t operands: because the type tells us the encoding bit location,
+ * we are free to reorder them.  We pick the asm order.
+ */
+
+/* instr_info_t.flags values: */
+enum {
+    DECODE_EXTRA_OPERANDS     = 0x0001, /* additional opnds in entry at code field */
+    DECODE_EXTRA_SHIFT        = 0x0002, /* has 2 additional srcs @exop[0] */
+    DECODE_EXTRA_WRITEBACK    = 0x0004, /* has 1 additional src @exop[1] */
+    DECODE_4_SRCS             = 0x0008, /* dst2==src1, src1==src2, etc. */
+    DECODE_3_DSTS             = 0x0010, /* src1==dst3, src2==src1, etc. */
+    DECODE_PREDICATE          = 0x0020, /* takes a predicate */
+    DECODE_PREDICATE_AL_ONLY  = 0x0040, /* takes AL predicate only */
+    DECODE_UNPREDICTABLE      = 0x0080, /* unpredictable according to ISA spec */
+    /* ARM versions we care about */
+    DECODE_ARM_V8             = 0x0100, /* added in v8: not present in v7 */
+};
+
+/* instr_info_t.code:
+ * + for EXTENSION and *_EXT: index into extensions table
+ * + for OP_: pointer to next entry of that opcode
+ * + may also point to extra operand table
+ */
+
+struct _decode_info_t {
+    uint instr_word;
+
+    uint opcode;
+
+    /* For pc-relative references */
+    byte *start_pc;
+    byte *final_pc;
+    byte *orig_pc;
+
+    /* XXX i#1550: add dr_isa_mode_t isa_mode field to dcontext and
+     * instr_t and cache it here for use when decoding.
+     */
+
+    /* For instr_t* target encoding */
+    ptr_int_t cur_note;
+    bool has_instr_opnds;
+};
+
+
+/* N.B.: if you change the type enum, change the string names for
+ * them, kept in encode.c
+ */
+
+/* Operand types have 2 parts, type and size.  The type tells us in which bits
+ * the operand is encoded, and the type of operand.
+ */
+enum {
+    /* operand types */
+    TYPE_NONE,  /* must be 0 for invalid_instr */
+
+    /* We name the registers according to their encoded position: A, B, C, D.
+     * XXX: Rd is T32-11:8; T16-2:0; A64-4:0 so not always "C"
+     *
+     * XXX: record which registers are "unpredictable" if PC (or SP, or LR) is
+     * passed?  Many are, for many different opcodes.
+     */
+    TYPE_R_A,   /* A32-19:16 = Rn: source register, often memory base */
+    TYPE_R_B,   /* A32-15:12 = Rd or Rt: dest reg */
+    TYPE_R_C,   /* A32-11:8  = Rs: source register, often used as shift value */
+    TYPE_R_D,   /* A32-3:0   = Rm: source register, often used as offset */
+
+    TYPE_R_A_TOP, /* top half of register */
+    TYPE_R_B_TOP, /* top half of register */
+    TYPE_R_C_TOP, /* top half of register */
+    TYPE_R_D_TOP, /* top half of register */
+
+    TYPE_R_D_NEGATED, /* register's value is negated */
+
+    TYPE_R_B_EVEN, /* Must be an even-numbered reg */
+    TYPE_R_B_PLUS1, /* Subsequent reg after prior TYPE_R_B_EVEN opnd */
+    TYPE_R_D_EVEN, /* Must be an even-numbered reg */
+    TYPE_R_D_PLUS1, /* Subsequent reg after prior TYPE_R_D_EVEN opnd */
+
+    TYPE_CR_A, /* Control register in A slot */
+    TYPE_CR_B, /* Control register in B slot */
+    TYPE_CR_C, /* Control register in C slot */
+    TYPE_CR_D, /* Control register in D slot */
+
+    TYPE_APSR, /* Application Program Status Register */
+    TYPE_SPSR, /* Saved Program Status Register */
+    TYPE_CPSR, /* Current Program Status Register */
+    /* XXX: do we need these:
+     * DSPSR = Debug Saved Program Status Register
+     * FPCR  = Floating-Point Control Register
+     * FPSR  = Floating-Point Status Register
+     */
+
+    /* XXX: Register types we'll want to add:
+     *   Wn = 32-bit GPR
+     *   Xn = 64-bit GPR
+     *   Vn = general name for 128-bit SIMD register
+     *   Qn = 128-bit SIMD
+     *   Dn = bottom 64-bit SIMD
+     *   Sn = bottom 32-bit SIMD
+     *   Hn = bottom 16-bit SIMD
+     *   Bn = bottom 8-bit SIMD
+     */
+
+    /* Immediates are at several different bit positions and come in several
+     * different sizes.  We considered storing a bitmask to cover any type
+     * of immediate, but there are few enough that we are enumerating them:
+     */
+    TYPE_I_b0,
+    TYPE_NI_b0, /* negated immed */
+    TYPE_I_b3,
+    TYPE_I_b5,
+    TYPE_I_b7,
+    TYPE_I_b8,
+    TYPE_I_b10,
+    TYPE_I_b16,
+    TYPE_I_b20,
+    TYPE_I_b0_b8,
+    TYPE_NI_b0_b8, /* negated immed */
+    TYPE_I_b0_b16,
+    TYPE_I_b16_b9,
+    TYPE_I_b16_b8,
+
+    TYPE_SHIFT_b5,
+    TYPE_SHIFT_b6, /* value is :0 */
+    TYPE_SHIFT_LSL,   /* shift logical left */
+    TYPE_SHIFT_ASR,   /* shift arithmetic right */
+
+    TYPE_L_8,   /* 8-bit register list */
+    TYPE_L_13,  /* 13-bit register list */
+    TYPE_L_16,  /* 16-bit register list */
+
+    /* All memory addressing modes use fixed base and index registers:
+     * A32: base  = RD 19:16 ("Rn" in manual)
+     *      index = RA  3:0  ("Rm" in manual)
+     * T16: base  =
+     *      index =
+     * T32: base  =
+     *      index =
+     * A64: base  =
+     *      index =
+     *
+     * Shifted registers always use sh2, i5.
+     *
+     * To be compatible w/ x86, we don't want to list the index, offset, or shift
+     * operands separately for regular offset addressing: we want to hide them
+     * inside the memref.  So we have to record exactly how to decode and encode
+     * each piece.
+     *
+     * We don't encode in the memref whether it has writeback ("[Rn + Rm]!") or
+     * is post-indexed ("[Rn], Rm"): the disassembler has to look at the other
+     * opnds to figure out how to write down the memref, and single-memref-opnd
+     * disasm will NOT contain writeback or post-index info.
+     */
+    TYPE_M,           /* mem w/ just base */
+    TYPE_M_POS_REG,   /* mem offs + reg index */
+    TYPE_M_NEG_REG,   /* mem offs - reg index */
+    TYPE_M_POS_SHREG, /* mem offs + reg-shifted (or extended for A64) index */
+    TYPE_M_NEG_SHREG, /* mem offs - reg-shifted (or extended for A64) index */
+    TYPE_M_POS_I12,   /* mem offs + 12-bit immed @ 11:0 (A64: 21:10 + scaled) */
+    TYPE_M_NEG_I12,   /* mem offs - 12-bit immed @ 11:0 (A64: 21:10 + scaled) */
+    TYPE_M_SI9,       /* mem offs + signed 9-bit immed @ 20:12 */
+    TYPE_M_POS_I8,    /* mem offs + 8-bit immed @ 7:0 */
+    TYPE_M_NEG_I8,    /* mem offs - 8-bit immed @ 7:0 */
+    TYPE_M_POS_I4_4,  /* mem offs + 8-bit immed split @ 11:8|3:0 */
+    TYPE_M_NEG_I4_4,  /* mem offs - 8-bit immed split @ 11:8|3:0 */
+    TYPE_M_SI7,       /* mem offs + signed 7-bit immed @ 6:0 */
+    TYPE_M_POS_I5,    /* mem offs + 5-bit immed @ 5:0 */
+
+    TYPE_M_PCREL_S9,  /* mem offs pc-relative w/ signed 9-bit immed 23:5 scaled */
+    TYPE_M_PCREL_U9,  /* mem offs pc-relative w/ unsigned 9-bit immed 23:5 scaled */
+
+    TYPE_M_UP_OFFS,   /* mem w/ base plus ptr-sized disp */
+    TYPE_M_DOWN,      /* mem w/ base pointing at endpoint */
+    TYPE_M_DOWN_OFFS, /* mem w/ base minus ptr-sized disp pointing at endpoint */
+
+   /* when adding new types, update type_names[] in encode.c */
+};
+
+/* exported tables */
+extern const instr_info_t A32_pred_opc8[];
+extern const instr_info_t A32_nopred_opc8[];
+extern const instr_info_t A32_ext_opc4x[][6];
+extern const instr_info_t A32_ext_opc4y[][9];
+extern const instr_info_t A32_ext_opc4[][16];
+extern const instr_info_t A32_ext_imm1916[][2];
+extern const instr_info_t A32_ext_bits0[][8];
+extern const instr_info_t A32_ext_bits8[][4];
+extern const instr_info_t A32_ext_bit9[][2];
+extern const instr_info_t A32_ext_bit4[][2];
+extern const instr_info_t A32_ext_RDPC[][2];
+extern const instr_info_t A32_ext_imm5[][2];
+extern const instr_info_t extra_operands[];
+
+#endif /* DECODE_PRIVATE_H */
