7f5d2268dffef4839afea44b8ff2ccc259fde00f
Derek Bruening <bruening@google.com>
2014-11-19 18:24:29 +0000

    i#1551 port to ARM: split OP_msr_priv and OP_mrs_priv
    Splits off _priv versions of OP_msr and OP_mrs to avoid having instruction
    encodings that only differ in the size of the immediate.
    
    SVN-Revision: 2999

--- core/arch/arm/table_a32_pred.c
+++ core/arch/arm/table_a32_pred.c
@@ -955,26 +955,26 @@ const instr_info_t A32_ext_bits8[][4] = {
 /* Indexed by bit 9 */
 const instr_info_t A32_ext_bit9[][2] = {
   { /* 0 */
-    {OP_mrs,     0x010f0000, "mrs",    RBw, xx, CPSR, xx, xx, pred, x, END_LIST},
-    {OP_mrs,     0x01000200, "mrs",    RBw, xx, CPSR, i5x16_8, xx, pred, x, tb9[4][0x00]},
+    {OP_mrs,      0x010f0000, "mrs",    RBw, xx, CPSR, xx, xx, pred, x, END_LIST},
+    {OP_mrs_priv, 0x01000200, "mrs",    RBw, xx, CPSR, i5x16_8, xx, pred, x, END_LIST},
   }, { /* 1 */
     {OP_crc32b,  0xe1000040, "crc32b",  RBw, xx, RAw, RDb, xx, predAL|v8, x, END_LIST},
     {OP_crc32cb, 0xe1000240, "crc32cb", RBw, xx, RAw, RDb, xx, predAL|v8, x, END_LIST},
   }, { /* 2 */
-    {OP_msr,     0x0120f000, "msr",    CPSR, xx, i4_16, RAw, xx, pred, x, ti19[0][0x01]},
-    {OP_msr,     0x0120f000, "msr",    CPSR, xx, i5x16_8, RAw, xx, pred, x, tb9[2][0x00]},
+    {OP_msr,      0x0120f000, "msr",    CPSR, xx, i4_16, RAw, xx, pred, x, ti19[0][0x01]},
+    {OP_msr_priv, 0x0120f000, "msr",    CPSR, xx, i5x16_8, RAw, xx, pred, x, END_LIST},
   }, { /* 3 */
     {OP_crc32h,   0xe1200040, "crc32h",  RBw, xx, RAw, RDh, xx, predAL|v8, x, END_LIST},
     {OP_crc32ch, 0xe1200240, "crc32ch", RBw, xx, RAw, RDh, xx, predAL|v8, x, END_LIST},
   }, { /* 4 */
-    {OP_mrs,     0x014f0000, "mrs",    RBw, xx, SPSR, xx, xx, pred, x, tb9[0][0x00]},
-    {OP_mrs,     0x01400200, "mrs",    RBw, xx, SPSR, i5x16_8, xx, pred, x, tb9[0][0x01]},
+    {OP_mrs,      0x014f0000, "mrs",    RBw, xx, SPSR, xx, xx, pred, x, tb9[0][0x00]},
+    {OP_mrs_priv, 0x01400200, "mrs",    RBw, xx, SPSR, i5x16_8, xx, pred, x, tb9[0][0x01]},
   }, { /* 5 */
     {OP_crc32w,  0xe1400040, "crc32w",  RBw, xx, RAw, RDw, xx, predAL|v8, x, END_LIST},
     {OP_crc32cw, 0xe1400240, "crc32cw", RBw, xx, RAw, RDw, xx, predAL|v8, x, END_LIST},
   }, { /* 6 */
-    {OP_msr,     0x0160f000, "msr",    SPSR, xx, i4_16, RAw, xx, pred, x, tb9[2][0x01]},
-    {OP_msr,     0x0160f000, "msr",    SPSR, xx, i5x16_8, RAw, xx, pred, x, tb9[6][0x00]},
+    {OP_msr,      0x0160f000, "msr",    SPSR, xx, i4_16, RAw, xx, pred, x, tb9[2][0x00]},
+    {OP_msr_priv, 0x0160f000, "msr",    SPSR, xx, i5x16_8, RAw, xx, pred, x, tb9[2][0x01]},
   }, { /* 7 */
     /* XXX: or should we just mark INVALID? */
     {OP_crc32w,  0xe1600040, "crc32w",  RBw, xx, RAw, RDw, xx, predAL|v8|unp, x, tb9[5][0x00]},
