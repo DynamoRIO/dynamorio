25ff6bdb2c551ea1a4b0fe02c16c71767ed077f6
Qin Zhao <zhaoqin@google.com>
2015-05-19 00:01:57 -0400

    Update opcode sample: use separate count for different isa mode instructions
    - add comment about the size of msg buffer
    - use separate count for different isa mode instructions
    
    Review-URL: https://codereview.appspot.com/239060043

--- api/samples/opcodes.c
+++ api/samples/opcodes.c
@@ -57,10 +57,25 @@
  * XXX: we're using 32-bit counters.  64-bit counters are more challenging: they're
  * harder to make atomic on 32-bit x86, and drx does not yet support them on ARM.
  */
-static uint count[OP_LAST+1];
-#define NUM_COUNT sizeof(count)/sizeof(count[0])
+enum {
+#ifdef X86
+    ISA_X86_32,
+    ISA_X86_64,
+#elif defined(ARM)
+    ISA_ARM_A32,
+    ISA_ARM_THUMB,
+#endif
+    NUM_ISA_MODE,
+};
+static uint count[NUM_ISA_MODE][OP_LAST+1];
+#define NUM_COUNT sizeof(count[0])/sizeof(count[0][0])
 /* We only display the top 15 counts.  This sample could be extended to
  * write all the counts to a file.
+ *
+ * XXX: DynamoRIO uses a separate stack for better transparency. DynamoRIO stack
+ * has limited size, so we should keep NUM_COUNT_SHOW small to avoid the message
+ * buffer (char msg[NUM_COUNT_SHOW*80]) in event_exit() overflowing the stack.
+ * It won't work on Windows either if the output is too large.
  */
 #define NUM_COUNT_SHOW 15
 
@@ -92,16 +107,32 @@ dr_init(client_id_t id)
 }
 
 #ifdef SHOW_RESULTS
+/* We use cur_isa to iterate each ISA counters in event_exit, so there will be
+ * no race on accessing it in compare_counts.
+ */
+static uint cur_isa;
 static int
 compare_counts(const void *a_in, const void *b_in)
 {
     const uint a = *(const uint *)a_in;
     const uint b = *(const uint *)b_in;
-    if (count[a] > count[b])
+    if (count[cur_isa][a] > count[cur_isa][b])
         return 1;
-    if (count[a] < count[b])
+    if (count[cur_isa][a] < count[cur_isa][b])
         return -1;
     return 0;
+}
+
+static const char *
+get_isa_mode_name(uint isa_mode)
+{
+#ifdef X86
+    return (isa_mode == ISA_X86_32) ? "32-bit X86" : "64-bit AMD64";
+#elif defined(ARM)
+    return (isa_mode == ISA_ARM_A32) ? "32-bit ARM" : "32-bit Thumb";
+#else
+    return "unknown";
+#endif
 }
 #endif
 
@@ -114,26 +145,54 @@ event_exit(void)
     size_t sofar = 0;
     /* First, sort the counts */
     uint indices[NUM_COUNT];
-    for (i = 0; i <= OP_LAST; i++)
-        indices[i] = i;
-    qsort(indices, NUM_COUNT, sizeof(indices[0]), compare_counts);
+    for (cur_isa = 0; cur_isa < NUM_ISA_MODE; cur_isa++) {
+        sofar = 0;
+        for (i = 0; i <= OP_LAST; i++)
+            indices[i] = i;
+        qsort(indices, NUM_COUNT, sizeof(indices[0]), compare_counts);
 
-    len = dr_snprintf(msg, sizeof(msg)/sizeof(msg[0]),
-                      "Top %d opcode execution counts:\n", NUM_COUNT_SHOW);
-    DR_ASSERT(len > 0);
-    sofar += len;
-    for (i = OP_LAST - 1 - NUM_COUNT_SHOW; i <= OP_LAST; i++) {
-        if (count[indices[i]] != 0) {
-            len = dr_snprintf(msg + sofar, sizeof(msg)/sizeof(msg[0]) - sofar,
-                              "  %9lu : %-15s\n", count[indices[i]],
-                              decode_opcode_name(indices[i]));
-            DR_ASSERT(len > 0);
-            sofar += len;
+        if (count[cur_isa][indices[OP_LAST]] == 0)
+            continue;
+        len = dr_snprintf(msg, sizeof(msg)/sizeof(msg[0]),
+                          "Top %d opcode execution counts in %s mode:\n",
+                          NUM_COUNT_SHOW, get_isa_mode_name(cur_isa));
+        DR_ASSERT(len > 0);
+        sofar += len;
+        for (i = OP_LAST - 1 - NUM_COUNT_SHOW; i <= OP_LAST; i++) {
+            if (count[cur_isa][indices[i]] != 0) {
+                len = dr_snprintf(msg + sofar, sizeof(msg)/sizeof(msg[0]) - sofar,
+                                  "  %9lu : %-15s\n", count[cur_isa][indices[i]],
+                                  decode_opcode_name(indices[i]));
+                DR_ASSERT(len > 0);
+                sofar += len;
+            }
         }
+        NULL_TERMINATE(msg);
+        DISPLAY_STRING(msg);
     }
-    NULL_TERMINATE(msg);
-    DISPLAY_STRING(msg);
 #endif /* SHOW_RESULTS */
+}
+
+static uint
+get_count_isa_idx(void *drcontext)
+{
+    switch (dr_get_isa_mode(drcontext)) {
+#ifdef X86
+    case DR_ISA_X86:
+        return ISA_X86_32;
+    case DR_ISA_AMD64:
+        return ISA_X86_64;
+#elif defined(ARM)
+    case DR_ISA_ARM_A32:
+        return ISA_ARM_A32;
+        break;
+    case DR_ISA_ARM_THUMB:
+        return ISA_ARM_THUMB;
+#endif
+    default:
+        DR_ASSERT(false); /* NYI */
+    }
+    return 0;
 }
 
 static dr_emit_flags_t
@@ -142,6 +201,7 @@ event_basic_block(void *drcontext, void *tag, instrlist_t *bb,
 {
     instr_t *instr;
     instr_t *first = instrlist_first_app(bb);
+    uint isa_idx = get_count_isa_idx(drcontext);
 
 #ifdef VERBOSE
     dr_printf("in dynamorio_basic_block(tag="PFX")\n", tag);
@@ -158,7 +218,7 @@ event_basic_block(void *drcontext, void *tag, instrlist_t *bb,
          */
         drx_insert_counter_update(drcontext, bb, first,
                                   SPILL_SLOT_1, IF_ARM_(SPILL_SLOT_2)
-                                  &count[instr_get_opcode(instr)], 1,
+                                  &count[isa_idx][instr_get_opcode(instr)], 1,
                                   /* DRX_COUNTER_LOCK is not yet supported on ARM */
                                   IF_X86_ELSE(DRX_COUNTER_LOCK, 0));
     }
