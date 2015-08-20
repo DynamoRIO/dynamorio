40ea03f3608fb72dcd02b7d6082d2a917e0f227d
Peter Wei <peter.peng.wei@gmail.com>
2015-08-19 21:55:41 -0700

    i#1763 DRTLBSim: implement TLB's stats and entry classes
    - implement class tlb_entry_t by extending caching_device_block_t
    - implement class tlb_stats_t by extending caching_device_stats_t
    
    Review-URL: https://codereview.appspot.com/262910043

---
+++ clients/drcachesim/simulator/tlb_entry.h
@@ -0,0 +1,50 @@
+/* **********************************************************
+ * Copyright (c) 2015 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/* tlb_entry: represents a TLB entry.
+ */
+
+#ifndef _TLB_ENTRY_H_
+#define _TLB_ENTRY_H_ 1
+
+#include "caching_device_block.h"
+
+class tlb_entry_t : public caching_device_block_t
+{
+    // process ID to differentiate virtual pages
+    // that have the same VPN but belong to different processes.
+    memref_pid_t pid;
+
+    //XXX: support page privilege and MMU-related exceptions
+};
+
+#endif /* _TLB_ENTRY_H_ */
