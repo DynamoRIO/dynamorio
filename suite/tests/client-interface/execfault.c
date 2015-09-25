0ad3f7a7192f30521bbe3e98e37d9a766c624b94
Derek Bruening <bruening@google.com>
2015-09-25 13:06:33 -0400

    i#1775: pass execution fault on Windows to client exception event
    Adds passing of a raised exception (such as from a forged exception on an
    execution fault) on Windows to the client exception event.
    
    Adds a new test for this on Windows and Linux.
    
    Fixes #1775
    
    Review-URL: https://codereview.appspot.com/267960043

---
+++ suite/tests/client-interface/execfault.c
@@ -0,0 +1,124 @@
+/* **********************************************************
+ * Copyright (c) 2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2000-2010 VMware, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "tools.h" /* for print() */
+#include <stdint.h>
+
+#include <setjmp.h>
+static SIGJMP_BUF mark;
+
+static char app_handler_message[1024];
+
+typedef void (*generic_func_t) ();
+
+/* borrowed from core/ */
+static inline
+generic_func_t
+convert_data_to_function(void *data_ptr)
+{
+#ifdef WINDOWS
+#  pragma warning(push)
+#  pragma warning(disable : 4055)
+#endif
+    return (generic_func_t)data_ptr;
+#ifdef WINDOWS
+#  pragma warning(pop)
+#endif
+}
+
+#if defined(UNIX)
+# include <signal.h>
+# define SIG_XIP IF_X86_ELSE(IF_X64_ELSE(rip, eip), arm_pc)
+
+static void
+handle_sigsegv(int signal, siginfo_t *siginfo, ucontext_t *ucxt)
+{
+    /* Unfortunately the kernel does not fill in siginfo->si_addr for exec faults! */
+    struct sigcontext *sc = (struct sigcontext *) &(ucxt->uc_mcontext);
+    snprintf(app_handler_message,
+             BUFFER_SIZE_ELEMENTS(app_handler_message),
+             "app handler got signal %d with addr "PFX,
+             signal, (ptr_uint_t)sc->SIG_XIP);
+    NULL_TERMINATE_BUFFER(app_handler_message);
+    SIGLONGJMP(mark, 1);
+}
+
+#elif defined(WINDOWS)
+# include <windows.h>
+
+static LONG WINAPI
+handle_exception(struct _EXCEPTION_POINTERS *exception_pointers)
+{
+    PEXCEPTION_RECORD exception_record = exception_pointers->ExceptionRecord;
+    DWORD exception_code = exception_record->ExceptionCode;
+    void *fault_address = (void *)exception_record->ExceptionInformation[1];
+    _snprintf(app_handler_message,
+              BUFFER_SIZE_ELEMENTS(app_handler_message),
+              "app handler got exception %x with addr "PFX,
+              exception_code, (ptr_uint_t)fault_address);
+    NULL_TERMINATE_BUFFER(app_handler_message);
+    SIGLONGJMP(mark, 1);
+}
+#endif
+
+static void
+execute_from(void *address)
+{
+    generic_func_t f = convert_data_to_function(address);
+    strncpy(app_handler_message,
+            "app handler was not called",
+            BUFFER_SIZE_ELEMENTS(app_handler_message));
+    NULL_TERMINATE_BUFFER(app_handler_message);
+    if (SIGSETJMP(mark) == 0) {
+        f();
+    }
+    print("%s\n", app_handler_message);
+}
+
+int
+main(int argc, char *argv[])
+{
+#if defined(UNIX)
+    intercept_signal(SIGSEGV, (handler_3_t)&handle_sigsegv, false);
+#elif defined(WINDOWS)
+    SetUnhandledExceptionFilter(&handle_exception);
+#endif
+
+    print("executing from illegal addresses\n");
+    execute_from((void *)(uintptr_t)42);
+    execute_from((void *)(uintptr_t)77);
+
+    print("all done\n");
+    return 0;
+}
---
+++ suite/tests/client-interface/execfault.dll.c
@@ -0,0 +1,85 @@
+/* **********************************************************
+ * Copyright (c) 2011-2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2007-2010 VMware, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of VMware, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+#include "dr_api.h"
+#include "client_tools.h"
+
+#if defined(UNIX)
+# include <signal.h>
+
+static dr_signal_action_t
+signal_event(void *dcontext, dr_siginfo_t *info)
+{
+    if (info->sig == SIGABRT) {
+        /* do nothing */
+    } else {
+        dr_fprintf(STDERR,
+                   "dr handler got signal %d with addr "PFX"\n",
+                   info->sig,
+                   /* info->access_address is only for data accesses,
+                    * and info->raw_mcontext_valid is false for exec
+                    */
+                   (ptr_uint_t)info->mcontext->pc);
+    }
+    return DR_SIGNAL_DELIVER;
+}
+#elif defined(WINDOWS)
+
+static bool
+exception_event(void *dcontext, dr_exception_t *excpt)
+{
+    void *fault_address = (void *)excpt->record->ExceptionInformation[1];
+    dr_fprintf(STDERR,
+               "dr handler got exception %x with addr "PFX"\n",
+               excpt->record->ExceptionCode,
+               (ptr_uint_t)excpt->record->ExceptionInformation[1]);
+    return true;
+}
+#endif
+
+static void
+exit_event(void)
+{
+    dr_fprintf(STDERR, "dr exit handler\n");
+}
+
+DR_EXPORT void
+dr_init(client_id_t id)
+{
+    dr_register_exit_event(exit_event);
+#if defined(UNIX)
+    dr_register_signal_event(signal_event);
+#elif defined(WINDOWS)
+    dr_register_exception_event(exception_event);
+#endif
+}
