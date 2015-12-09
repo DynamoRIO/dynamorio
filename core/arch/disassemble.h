be7c1a52ca18cd51cdefa39bc260ce01d9badff7
Derek Bruening <bruening@google.com>
2015-12-09 12:30:26 -0500

    Document the instrlist disassembly format
    Adds missing documentation for the extra information included in the
    instrlist disassembly format.
    
    Review-URL: https://codereview.appspot.com/278520043

--- core/arch/disassemble.h
+++ core/arch/disassemble.h
@@ -246,7 +246,46 @@ opnd_disassemble_to_buffer(dcontext_t *dcontext, opnd_t opnd, char *buf, size_t 
 DR_API
 /**
  * Prints each instruction in \p ilist in sequence to \p outfile.
- * The default is to use DR's custom syntax (see disassemble_set_syntax()).
+ * The default is to use DR's custom syntax (see disassemble_set_syntax())
+ * with additional information.  The first column contains the offset
+ * in bytes from the start of the list.
+ * Next, each instruction is labeled according to its type, which
+ * will typically either be \p L3 for an unchanged application instruction
+ * or \p m4 for a tool instruction (the names come from "Level 3" and
+ * "meta Level 4", IR details which are no longer exposed to tools).
+ * Tool instructions have their IR heap addresses included (indicated with a
+ * leading @ character) to make instruction jump targets easier to
+ * identify.  The final two columns contain the raw bytes and the
+ * actual instruction disassembly.
+ *
+ * Below is an example where many tool instructions have been inserted around
+ * 3 application instructions, which can be identified by the \p L3 in the 2nd
+ * column.  The label instructions are referred to by branch and store
+ * instructions, as can be seen by searching for the addresses of the labels.
+ * \code
+ * TAG  0xf77576e6
+ *  +0    m4 @0xe7856eb4  64 89 0d 60 00 00 00 mov    %ecx -> %fs:0x00000060[4byte]
+ *  +7    m4 @0xe78574a8  64 8a 0d 52 00 00 00 mov    %fs:0x00000052[1byte] -> %cl
+ *  +14   m4 @0xe7855ad4  64 88 0d 54 00 00 00 mov    %cl -> %fs:0x00000054[1byte]
+ *  +21   L3              83 ee 06             sub    $0x00000006 %esi -> %esi
+ *  +24   m4 @0xe77c3acc  64 80 3d 52 00 00 00 cmp    %fs:0x00000052[1byte] $0x00
+ *                        00
+ *  +32   m4 @0xe7855c54  75 fe                jnz    @0xe7856054[4byte]
+ *  +34   m4 @0xe7856e28  64 c6 05 54 00 00 00 mov    $0x00 -> %fs:0x00000054[1byte]
+ *                        00
+ *  +42   m4 @0xe7856754  eb fe                jmp    @0xe7857350[4byte]
+ *  +44   m4 @0xe7856054                       <label>
+ *  +44   m4 @0xe7857428  b9 e9 76 75 f7       mov    $0xf77576e9 -> %ecx
+ *  +49   m4 @0xe7855b54  64 c7 05 64 00 00 00 mov    @0xe7856514[4byte] -> %fs:0x00000064[4byte]
+ *                        98 e6 7b e7
+ *  +60   m4 @0xe7857a30  e9 0a 35 07 00       jmp    $0xe7831ba7
+ *  +65   m4 @0xe7856514                       <label>
+ *  +65   m4 @0xe7857350                       <label>
+ *  +65   L3              83 fe 23             cmp    %esi $0x00000023
+ *  +68   m4 @0xe7856da8  64 8b 0d 60 00 00 00 mov    %fs:0x00000060[4byte] -> %ecx
+ *  +75   L3              0f 87 16 01 00 00    jnbe   $0xf7757808
+ * END 0xf77576e6
+ * \endcode
  */
 void
 instrlist_disassemble(dcontext_t *dcontext, app_pc tag,
