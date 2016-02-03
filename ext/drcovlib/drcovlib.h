DynamoRIO/dynamorio

commit a7b251109fa7be4b2aab64f6f47e41e579338fd2

i#1829: refactor drcov into an extension for use in other tools
Creates a new Extension called drcovlib that contains the guts of the drcov
code coverage tool.

Adds an options struct and a simple API to drcovlib: init, exit, get
logfile name, and request dump.

Moves clients/common/modules.c to become an internal file inside drcovlib.
We can export its routines from drcovlib in the future if another tool
wants to use it.

Follows a simple model of the caller requesting the coverage dump, either
via calling the exit routine, using its own soft_kills nudge, or an
explicit dump call for unusual cases.  This means that drx's soft_kills
remains inside drcov.  This is the easiest approach for coordinating
soft_kills among many libraries.

Removes the CBR_COVERAGE define.

Adds drmemorylib to the list of default exclusion filters in drcov2lcov.

Fixes #1829

Review-URL: https://codereview.appspot.com/276710043

ext/drcovlib/drcovlib.h
@@ -1,0 +1,209 @@
+/* **********************************************************
+ * Copyright (c) 2016 Google, Inc.   All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL GOOGLE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/* DynamoRIO Code Coverage Library */
+
+#ifndef _DRCOVLIB_H_
+#define _DRCOVLIB_H_ 1
+
+#include "drmgr.h"
+
+/**
+ * @file drcovlib.h
+ * @brief Header for DynamoRIO Code Coverage Library
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \addtogroup drcovlib Code Coverage Library
+ */
+/*@{*/ /* begin doxygen group */
+
+/** Success code for each drcovlib operation */
+typedef enum {
+    DRCOVLIB_SUCCESS,                  /**< Operation succeeded. */
+    DRCOVLIB_ERROR,                    /**< Operation failed. */
+    DRCOVLIB_ERROR_INVALID_PARAMETER,  /**< Operation failed: invalid parameter */
+    DRCOVLIB_ERROR_INVALID_SETUP,      /**< Operation failed: invalid DynamoRIO setup */
+    DRCOVLIB_ERROR_FEATURE_NOT_AVAILABLE, /**< Operation failed: not available */
+} drcovlib_status_t;
+
+/** Bitmask flags for use in #drcovlib_options_t.flags. */
+typedef enum {
+    /**
+     * Requests to dump the log file in text format.  By default the log file is
+     * in binary format.  When in text format, the log file is \em not readable
+     * by the post-processing tool \ref sec_drcov2lcov.
+     */
+    DRCOVLIB_DUMP_AS_TEXT,
+    /**
+     * By default, coverage information is dumped in a single process-wide log
+     * file.  If DynamoRIO is run with thread-private code caches (i.e.,
+     * dr_using_all_private_caches() returns true) and this flag is enabled,
+     * then per-thread coverage information will be stored and dumped in \p
+     * drcovlib's own thread exit events rather than in drcovlib_exit().
+     */
+    DRCOVLIB_THREAD_PRIVATE,
+} drcovlib_flags_t;
+
+/** Specifies the options when initializing drcovlib. */
+typedef struct _drcovlib_options_t {
+    /** Set this to the size of this structure. */
+    size_t struct_size;
+    /** Sets options specified by the bitmask values described in #drcovlib_flags_t. */
+    drcovlib_flags_t flags;
+    /**
+     * By default, log files are stored in the current directory.  This option
+     * overrides that default.
+     */
+    const char *logdir;
+    /**
+     * This is an experimental option for running natively (i.e., not under
+     * DynamoRIO control) until the nth thread, where n is the value of this
+     * option, is created.  This option only works under Windows.
+     */
+    int native_until_thread;
+} drcovlib_options_t;
+
+/***************************************************************************
+ * Coverage log file format for use in postprocessing.
+ *
+ * XXX: this is not a sufficient description: special strings for the version,
+ * flavor, module header, bb header, and module table entry are assumed in
+ * drcov2lcov.  It seems better to move the specific parsing into drcovlib and
+ * make drcovlib usable in standalone mode.
+ */
+
+/* file format version */
+#define DRCOV_VERSION 2
+
+/* i#1532: drsyms can't mix arch for ELF */
+#ifdef LINUX
+# ifdef X64
+#  define DRCOV_ARCH_FLAVOR "-64"
+# else
+#  define DRCOV_ARCH_FLAVOR "-32"
+# endif
+#else
+# define DRCOV_ARCH_FLAVOR ""
+#endif
+
+/* The bb_entry_t is used by both drcov client and post processing drcov2lcov.
+ * It has different sizes, and different members with other types of coverage
+ * (xref the now-removed CBR_COVERAGE).
+ * We use different flavor markers to make sure the drcov2lcov process has the
+ * right log file generated from corrsponding drcov client.
+ */
+#define DRCOV_FLAVOR "drcov" DRCOV_ARCH_FLAVOR
+
+/* Data structure for the coverage info itself */
+typedef struct _bb_entry_t {
+    uint   start;      /* offset of bb start from the image base */
+    ushort size;
+    ushort mod_id;
+} bb_entry_t;
+
+/***************************************************************************
+ * Exported functions
+ */
+
+DR_EXPORT
+/**
+ * Initializes the drcovlib extension.  Must be called prior to any of the
+ * other routines.  Can be called multiple times (by separate components,
+ * normally) but each call must be paired with a corresponding call to
+ * drcovlib_exit().
+ *
+ * Once this routine is called, drcovlib's operation goes into effect and it
+ * starts collecting coverage information.
+ *
+ * @param[in] ops  Specifies the optional parameters that control how
+ *   drcovlib operates.  Only the first caller's options are honored.
+ *
+ * @return whether successful or an error code on failure.
+ */
+drcovlib_status_t
+drcovlib_init(drcovlib_options_t *ops);
+
+DR_EXPORT
+/**
+ * Dumps the coverage information for this process into its log file and cleans
+ * up all resources allocated by the \p drcovlib extension.
+ * If #DRCOVLIB_THREAD_PRIVATE is in effect, coverage information is instead dumped
+ * in \p drcovlib's own thread exit events.
+ *
+ * @return whether successful or an error code on failure.
+ */
+drcovlib_status_t
+drcovlib_exit(void);
+
+DR_EXPORT
+/**
+ * Returns the name of the log file for this process (or for the thread
+ * specified by \p drcontext, if #DRCOVLIB_THREAD_PRIVATE is in effect).
+ *
+ * @param[in] drcontext  If #DRCOVLIB_THREAD_PRIVATE is in effect, specifies which
+ *   thread's log file name should be returned.  Otherwise this must be NULL.
+ * @param[out] path  The full path to the requested log file.
+ *
+ * @return whether successful or an error code on failure.
+ */
+drcovlib_status_t
+drcovlib_logfile(void *drcontext, OUT const char **path);
+
+DR_EXPORT
+/**
+ * Requests that coverage information be dumped to the log file for this process
+ * (or for the thread specified by \p drcontext, if #DRCOVLIB_THREAD_PRIVATE is
+ * in effect use).  Normally this happens during drcovlib_exit(), unless some
+ * unusual termination prevents it.  If this routine is called and
+ * drcovlib_exit() is also later called (or for #DRCOVLIB_THREAD_PRIVATE, \p
+ * drcovlib's own thread exit events are invoked by DynamoRIO), duplicate
+ * information will be dumped to the log files.  Thus, this routine should only
+ * be called when the regular dump will not occur.
+ *
+ * @return whether successful or an error code on failure.
+ */
+drcovlib_status_t
+drcovlib_dump(void *drcontext);
+
+
+/*@}*/ /* end doxygen group */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _DRCOVLIB_H_ */

api/docs/release.dox
@@ -1,5 +1,5 @@
 /* ******************************************************************************
- * Copyright (c) 2010-2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2010-2016 Google, Inc.  All rights reserved.
  * Copyright (c) 2011 Massachusetts Institute of Technology  All rights reserved.
  * Copyright (c) 2008-2010 VMware, Inc.  All rights reserved.
  * ******************************************************************************/
@@ -150,6 +150,9 @@ The changes between version \DR_VERSION and 6.0.0 include:
  - Added drreg_reservation_info().
  - Added opnd_create_rel_addr() and opnd_is_rel_addr() support for ARM.
  - Added drreg_is_register_dead() and drreg_reserve_dead_register().
+ - Split out the core functionality of the \ref page_drcov tool into
+   a new Extension \p drcovlib for easier inclusion of coverage output in
+   other tools.
 
 **************************************************
 <hr>

clients/drcov/CMakeLists.txt
@@ -1,5 +1,5 @@
 # **********************************************************
-# Copyright (c) 2010-2015 Google, Inc.    All rights reserved.
+# Copyright (c) 2010-2016 Google, Inc.    All rights reserved.
 # Copyright (c) 2009-2010 VMware, Inc.    All rights reserved.
 # **********************************************************
 
@@ -33,11 +33,6 @@ cmake_minimum_required(VERSION 2.6)
 
 include(../../make/policies.cmake NO_POLICY_SCOPE)
 
-option(CBR_COVERAGE "build with conditional branch coverage checks" OFF)
-if (CBR_COVERAGE)
-  add_definitions(-DCBR_COVERAGE)
-endif(CBR_COVERAGE)
-
 # We do not need libc, and we avoid a 10x size increase in both our
 # dll and pdb (plus we avoid stressing private library isolation) by
 # not using it (i#714).
@@ -50,15 +45,10 @@ else()
   set(libtype SHARED)
 endif ()
 
-add_library(drcov ${libtype}
-  drcov.c
-  ../common/modules.c
-  # add more here
-  )
+add_library(drcov ${libtype} drcov.c)
 configure_DynamoRIO_client(drcov)
-use_DynamoRIO_extension(drcov drmgr)
+use_DynamoRIO_extension(drcov drcovlib)
 use_DynamoRIO_extension(drcov drx)
-use_DynamoRIO_extension(drcov drcontainers)
 # We keep our shared libs in the lib dir, not the bin dir:
 place_shared_lib_in_lib_dir(drcov)
 

clients/drcov/postprocess/drcov2lcov.cpp
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2013-2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2013-2016 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
@@ -41,7 +41,7 @@
 
 #include "dr_api.h"
 #include "droption.h"
-#include "../drcov.h"
+#include "drcovlib.h"
 #include "drsyms.h"
 #include "hashtable.h"
 #include "dr_frontend.h"
@@ -97,10 +97,13 @@
 # define DR_LIB_NAME "dynamorio.dll"
 # define DR_PRELOAD_NAME "preinject.dll"
 # define DRCOV_LIB_NAME "drcov.dll"
+/* Often combined with Dr. Memory */
+# define DRMEM_LIB_NAME "drmemorylib.dll"
 #else
 # define DR_LIB_NAME "libdynamorio." /* cover .so and .dylib */
 # define DR_PRELOAD_NAME "libdrpreload."
 # define DRCOV_LIB_NAME "libdrcov."
+# define DRMEM_LIB_NAME "libdrmemorylib."
 #endif
 
 /***************************************************************************
@@ -742,7 +745,8 @@ module_is_from_tool(const char * path)
 {
     return (strstr(path, DR_LIB_NAME) != NULL ||
             strstr(path, DR_PRELOAD_NAME) != NULL ||
-            strstr(path, DRCOV_LIB_NAME) != NULL);
+            strstr(path, DRCOV_LIB_NAME) != NULL ||
+            strstr(path, DRMEM_LIB_NAME) != NULL);
 }
 
 static char *

core/win32/resources.rc
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2016 Google, Inc.  All rights reserved.
  * Copyright (c) 2003-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -193,6 +193,10 @@
 #elif defined(RC_IS_drreg) || defined(RC_IS_drreg_static)
 # define FILE_NAME "drreg.dll"
 # define FILE_DESCRIPTION PRODUCT_NAME " scratch register mediator"
+# define FILE_TYPE VFT_DLL
+#elif defined(RC_IS_drcovlib) || defined(RC_IS_drcovlib_static)
+# define FILE_NAME "drcovlib.dll"
+# define FILE_DESCRIPTION PRODUCT_NAME " code coverage library"
 # define FILE_TYPE VFT_DLL
 /* tests */
 #elif defined(RC_IS_TEST)

ext/CMakeLists.txt
@@ -1,5 +1,5 @@
 # **********************************************************
-# Copyright (c) 2010-2015 Google, Inc.    All rights reserved.
+# Copyright (c) 2010-2016 Google, Inc.    All rights reserved.
 # Copyright (c) 2010 VMware, Inc.    All rights reserved.
 # **********************************************************
 
@@ -119,6 +119,7 @@ add_subdirectory(drreg)
 add_subdirectory(drsyms)
 add_subdirectory(drutil)
 add_subdirectory(droption)
+add_subdirectory(drcovlib)
 
 # documentation is put into main DR docs/ dir
 

ext/drcovlib/CMakeLists.txt
@@ -1,0 +1,62 @@
+# **********************************************************
+# Copyright (c) 2016 Google, Inc.    All rights reserved.
+# **********************************************************
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of Google, Inc. nor the names of its contributors may be
+#   used to endorse or promote products derived from this software without
+#   specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL GOOGLE, INC. OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+
+cmake_minimum_required(VERSION 2.6)
+
+# DynamoRIO Code Coverage Library
+
+# Avoiding libc reduces lib size (i#714)
+set(DynamoRIO_USE_LIBC OFF)
+
+set(srcs drcovlib.c modules.c)
+
+# i#1491#2: VS generators fail if static lib has resources
+set(srcs_static ${srcs})
+
+if (WIN32)
+  set(srcs ${srcs} ${PROJECT_SOURCE_DIR}/core/win32/resources.rc)
+endif ()
+
+add_library(drcovlib ${libtype} ${srcs})
+set(PREFERRED_BASE 0x73800000)
+configure_extension(drcovlib OFF)
+use_DynamoRIO_extension(drcovlib drcontainers)
+use_DynamoRIO_extension(drcovlib drmgr)
+use_DynamoRIO_extension(drcovlib drx)
+
+if (NOT STATIC_LIBRARY)
+  add_library(drcovlib_static STATIC ${srcs_static})
+  configure_extension(drcovlib_static ON)
+  use_DynamoRIO_extension(drcovlib_static drcontainers)
+  use_DynamoRIO_extension(drcovlib_static drmgr_static)
+  use_DynamoRIO_extension(drcovlib_static drx_static)
+endif ()
+
+install_ext_header(drcovlib.h)

commit 061c433dc8540d1589d42c72a86bf908a987fc91

i#1867: fix bug involving drcov flags being ignored
Fixes a regression from a7b2511 where the drcov option flags ended up with
DRCOVLIB_DUMP_AS_TEXT set to 0 and thus ignored.

Fixes #1867

Review-URL: https://codereview.appspot.com/281670044

ext/drcovlib/drcovlib.h
@@ -67,7 +67,7 @@ typedef enum {
      * in binary format.  When in text format, the log file is \em not readable
      * by the post-processing tool \ref sec_drcov2lcov.
      */
-    DRCOVLIB_DUMP_AS_TEXT,
+    DRCOVLIB_DUMP_AS_TEXT    = 0x0001,
     /**
      * By default, coverage information is dumped in a single process-wide log
      * file.  If DynamoRIO is run with thread-private code caches (i.e.,
@@ -75,7 +75,7 @@ typedef enum {
      * then per-thread coverage information will be stored and dumped in \p
      * drcovlib's own thread exit events rather than in drcovlib_exit().
      */
-    DRCOVLIB_THREAD_PRIVATE,
+    DRCOVLIB_THREAD_PRIVATE  = 0x0002,
 } drcovlib_flags_t;
 
 /** Specifies the options when initializing drcovlib. */