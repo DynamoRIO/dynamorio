DynamoRIO/dynamorio

commit 3fe091a09a0f04945d88cc4863d321ecb8aa012a

i#460 private loader search order: search DT_RUNPATH
Adds support for searching DT_RUNPATH in our UNIX private loader.

Review-URL: https://codereview.appspot.com/290800043

core/unix/loader.c
@@ -657,16 +657,23 @@ privload_load_finalized(privmod_t *mod)
     /* nothing further to do */
 }
 
+/* dtag should be DT_RPATH or DT_RUNPATH */
 static bool
-privload_search_rpath(privmod_t *mod, const char *name,
+privload_search_rpath(privmod_t *mod, ELF_SWORD dtag, const char *name,
                       char *filename OUT /* buffer size is MAXIMUM_PATH */)
 {
 #ifdef LINUX
     os_privmod_data_t *opd;
     ELF_DYNAMIC_ENTRY_TYPE *dyn;
     ASSERT(mod != NULL && "can't look for rpath without a dependent module");
+    ASSERT(dtag == DT_RPATH || dtag == DT_RUNPATH);
     /* get the loading module's dir for RPATH_ORIGIN */
     opd = (os_privmod_data_t *) mod->os_privmod_data;
+    /* i#460: if DT_RUNPATH exists we must ignore ignore DT_RPATH and
+     * search DT_RUNPATH after LD_LIBRARY_PATH.
+     */
+    if (dtag == DT_RPATH && opd->os_data.has_runpath)
+        return false;
     const char *moddir_end = strrchr(mod->path, '/');
     size_t moddir_len = (moddir_end == NULL ? strlen(mod->path) : moddir_end - mod->path);
     const char *strtab;
@@ -675,11 +682,8 @@ privload_search_rpath(privmod_t *mod, const char *name,
     strtab = (char *) opd->os_data.dynstr;
     /* support $ORIGIN expansion to lib's current directory */
     while (dyn->d_tag != DT_NULL) {
-        /* FIXME i#460: we should also support DT_RUNPATH: if we see it,
-         * ignore DT_RPATH and search DT_RUNPATH after LD_LIBRARY_PATH.
-         */
-        if (dyn->d_tag == DT_RPATH) {
-            /* DT_RPATH is a colon-separated list of paths */
+        if (dyn->d_tag == dtag) {
+            /* DT_RPATH and DT_RUNPATH are each a colon-separated list of paths */
             const char *list = strtab + dyn->d_un.d_val;
             const char *sep, *origin;
             size_t len;
@@ -742,7 +746,7 @@ privload_locate(const char *name, privmod_t *dep,
      */
     /* the loader search order: */
     /* 0) DT_RPATH */
-    if (dep != NULL && privload_search_rpath(dep, name, filename))
+    if (dep != NULL && privload_search_rpath(dep, DT_RPATH, name, filename))
         return true;
 
     /* 1) client lib dir */
@@ -790,7 +794,11 @@ privload_locate(const char *name, privmod_t *dep,
         lib_paths = end;
     }
 
-    /* 4) FIXME: i#460, we use our system paths instead of /etc/ld.so.cache. */
+    /* 4) DT_RUNPATH */
+    if (dep != NULL && privload_search_rpath(dep, DT_RUNPATH, name, filename))
+        return true;
+
+    /* 5) FIXME: i#460, we use our system paths instead of /etc/ld.so.cache. */
     for (i = 0; i < NUM_SYSTEM_LIB_PATHS; i++) {
         snprintf(filename, MAXIMUM_PATH, "%s/%s", system_lib_paths[i], name);
         /* NULL_TERMINATE_BUFFER(filename) */

core/unix/module.h
@@ -80,6 +80,7 @@ typedef struct _os_module_data_t {
     app_pc dynstr;        /* absolute addr of .dynstr */
     size_t dynstr_size;   /* size of .dynstr */
     size_t symentry_size; /* size of a .dynsym entry */
+    bool has_runpath;     /* is DT_RUNPATH present? */
     /* for .gnu.hash */
     app_pc gnu_bitmask;
     ptr_uint_t gnu_shift;

core/unix/module_elf.c
@@ -428,6 +428,8 @@ module_fill_os_data(ELF_PROGRAM_HEADER_TYPE *prog_hdr, /* PT_DYNAMIC entry */
                     out_data->dynstr_size = (size_t) dyn->d_un.d_val;
                 } else if (dyn->d_tag == DT_SYMENT) {
                     out_data->symentry_size = (size_t) dyn->d_un.d_val;
+                } else if (dyn->d_tag == DT_RUNPATH) {
+                    out_data->has_runpath = true;
 #ifndef ANDROID
                 } else if (dyn->d_tag == DT_CHECKSUM) {
                     out_data->checksum = (size_t) dyn->d_un.d_val;

core/unix/module_elf.h
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2016 Google, Inc.  All rights reserved.
  * Copyright (c) 2008-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -48,6 +48,7 @@
 # define ELF_DYNAMIC_ENTRY_TYPE Elf64_Dyn
 # define ELF_ADDR Elf64_Addr
 # define ELF_WORD Elf64_Xword
+# define ELF_SWORD Elf64_Sxword
 # define ELF_HALF Elf64_Half
 # define ELF_SYM_TYPE Elf64_Sym
 # define ELF_WORD_SIZE 64 /* __ELF_NATIVE_CLASS */
@@ -68,6 +69,7 @@
 # define ELF_DYNAMIC_ENTRY_TYPE Elf32_Dyn
 # define ELF_ADDR Elf32_Addr
 # define ELF_WORD Elf32_Word
+# define ELF_SWORD Elf32_Sword
 # define ELF_HALF Elf32_Half
 # define ELF_SYM_TYPE Elf32_Sym
 # define ELF_WORD_SIZE 32 /* __ELF_NATIVE_CLASS */

core/unix/module_private.h
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2016 Google, Inc.  All rights reserved.
  * Copyright (c) 2008-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 

commit 098dc95da9ead079aebd2c83b7f0a3bd10389de0

Fix Mac build breakage
Fix build breakage on Mac from 3fe091a

Review-URL: https://codereview.appspot.com/288190043

core/unix/loader.c
@@ -657,22 +657,21 @@ privload_load_finalized(privmod_t *mod)
     /* nothing further to do */
 }
 
-/* dtag should be DT_RPATH or DT_RUNPATH */
+/* If runpath, then DT_RUNPATH is searched; else, DT_RPATH. */
 static bool
-privload_search_rpath(privmod_t *mod, ELF_SWORD dtag, const char *name,
+privload_search_rpath(privmod_t *mod, bool runpath, const char *name,
                       char *filename OUT /* buffer size is MAXIMUM_PATH */)
 {
 #ifdef LINUX
     os_privmod_data_t *opd;
     ELF_DYNAMIC_ENTRY_TYPE *dyn;
     ASSERT(mod != NULL && "can't look for rpath without a dependent module");
-    ASSERT(dtag == DT_RPATH || dtag == DT_RUNPATH);
     /* get the loading module's dir for RPATH_ORIGIN */
     opd = (os_privmod_data_t *) mod->os_privmod_data;
     /* i#460: if DT_RUNPATH exists we must ignore ignore DT_RPATH and
      * search DT_RUNPATH after LD_LIBRARY_PATH.
      */
-    if (dtag == DT_RPATH && opd->os_data.has_runpath)
+    if (!runpath && opd->os_data.has_runpath)
         return false;
     const char *moddir_end = strrchr(mod->path, '/');
     size_t moddir_len = (moddir_end == NULL ? strlen(mod->path) : moddir_end - mod->path);
@@ -682,7 +681,7 @@ privload_search_rpath(privmod_t *mod, ELF_SWORD dtag, const char *name,
     strtab = (char *) opd->os_data.dynstr;
     /* support $ORIGIN expansion to lib's current directory */
     while (dyn->d_tag != DT_NULL) {
-        if (dyn->d_tag == dtag) {
+        if (dyn->d_tag == (runpath ? DT_RUNPATH : DT_RPATH)) {
             /* DT_RPATH and DT_RUNPATH are each a colon-separated list of paths */
             const char *list = strtab + dyn->d_un.d_val;
             const char *sep, *origin;
@@ -746,7 +745,7 @@ privload_locate(const char *name, privmod_t *dep,
      */
     /* the loader search order: */
     /* 0) DT_RPATH */
-    if (dep != NULL && privload_search_rpath(dep, DT_RPATH, name, filename))
+    if (dep != NULL && privload_search_rpath(dep, false/*rpath*/, name, filename))
         return true;
 
     /* 1) client lib dir */
@@ -795,7 +794,7 @@ privload_locate(const char *name, privmod_t *dep,
     }
 
     /* 4) DT_RUNPATH */
-    if (dep != NULL && privload_search_rpath(dep, DT_RUNPATH, name, filename))
+    if (dep != NULL && privload_search_rpath(dep, true/*runpath*/, name, filename))
         return true;
 
     /* 5) FIXME: i#460, we use our system paths instead of /etc/ld.so.cache. */