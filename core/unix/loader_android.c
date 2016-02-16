958f73acd18e37f10f1a384888e41e794ef6b283
Derek Bruening <bruening@google.com>
2016-02-15 22:16:42 -0500

    i#1875: fix multi-thread Android bug
    Fixes a regression from 8f651e1 where the Bionic support only included
    primary thread support.  Adds secondary thread support by mmapping a new
    pthread data structure and filling in key fields.
    
    Fixes #1875
    
    Review-URL: https://codereview.appspot.com/289220043

--- core/unix/loader_android.c
+++ core/unix/loader_android.c
@@ -62,12 +62,10 @@ privload_mod_tls_init(privmod_t *mod)
     /* Android does not yet support per-module TLS */
 }
 
-/* We expect app_tls to be either NULL or garbage, as we have early injection */
 void *
 privload_tls_init(void *app_tls)
 {
-    char **e;
-
+    void *res;
     /* We have to exactly duplicate the offset of key fields in Android's
      * pthread_internal_t struct.
      */
@@ -75,44 +73,71 @@ privload_tls_init(void *app_tls)
     ASSERT(DR_TLS_BASE_OFFSET == offsetof(android_pthread_internal_t, dr_tls_base) -
            PTHREAD_TLS_OFFS /* the self slot */);
 
-    init_thread.tid = dynamorio_syscall(SYS_set_tid_address, 1, &init_thread.tid);
-    init_thread.cached_pid_ = init_thread.tid;
+    if (!dynamo_initialized) {
+        char **e;
+        /* We have to duplicate the pthread setup that the Android loader does.
+         * We expect app_tls to be either NULL or garbage, as we have early injection.
+         */
+        init_thread.tid = dynamorio_syscall(SYS_set_tid_address, 1, &init_thread.tid);
+        init_thread.cached_pid_ = init_thread.tid;
 
-    /* init_thread.attr is set to all 0 (sched is SCHED_NORMAL==0, and sizes are
-     * zeroed out)
-     */
+        /* init_thread.attr is set to all 0 (sched is SCHED_NORMAL==0, and sizes are
+         * zeroed out)
+         */
 
-    /* init_thread.join_state is set to 0 (THREAD_NOT_JOINED) */
+        /* init_thread.join_state is set to 0 (THREAD_NOT_JOINED) */
 
-    init_thread.tls[ANDROID_TLS_SLOT_SELF] = init_thread.tls;
-    init_thread.tls[ANDROID_TLS_SLOT_THREAD_ID] = &init_thread;
-    /* tls[TLS_SLOT_STACK_GUARD] is set to 0 */
+        init_thread.tls[ANDROID_TLS_SLOT_SELF] = init_thread.tls;
+        init_thread.tls[ANDROID_TLS_SLOT_THREAD_ID] = &init_thread;
+        /* tls[TLS_SLOT_STACK_GUARD] is set to 0 */
 
-    /* Set up the data struct pointing at kernel args that Bionic expects */
-    kernel_args.argc = *(int *)kernel_init_sp;
-    kernel_args.argv = (char **)kernel_init_sp + 1;
-    kernel_args.envp = kernel_args.argv + kernel_args.argc + 1;
-    /* The aux vector is after the last environment pointer. */
-    for (e = kernel_args.envp; *e != NULL; e++)
-        ; /* nothing */
-    kernel_args.auxv = (ELF_AUXV_TYPE *)(e + 1);
-    init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT] = &kernel_args;
+        /* Set up the data struct pointing at kernel args that Bionic expects */
+        kernel_args.argc = *(int *)kernel_init_sp;
+        kernel_args.argv = (char **)kernel_init_sp + 1;
+        kernel_args.envp = kernel_args.argv + kernel_args.argc + 1;
+        /* The aux vector is after the last environment pointer. */
+        for (e = kernel_args.envp; *e != NULL; e++)
+            ; /* nothing */
+        kernel_args.auxv = (ELF_AUXV_TYPE *)(e + 1);
+        init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT] = &kernel_args;
 
-    /* We use our own alternate signal stack */
+        /* We use our own alternate signal stack */
 
-    LOG(GLOBAL, LOG_LOADER, 2, "%s: kernel sp is "PFX"; TLS set to "PFX"\n",
-        __FUNCTION__, init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT],
-        init_thread.tls[ANDROID_TLS_SLOT_SELF]);
+        LOG(GLOBAL, LOG_LOADER, 2, "%s: kernel sp is "PFX"; TLS set to "PFX"\n",
+            __FUNCTION__, init_thread.tls[ANDROID_TLS_SLOT_BIONIC_PREINIT],
+            init_thread.tls[ANDROID_TLS_SLOT_SELF]);
+
+        res = init_thread.tls[ANDROID_TLS_SLOT_SELF];
+    } else {
+        android_pthread_internal_t *thrd;
+        res = heap_mmap(ALIGN_FORWARD(sizeof(android_pthread_internal_t), PAGE_SIZE));
+        LOG(GLOBAL, LOG_LOADER, 2, "%s: allocated new TLS at "PFX"; copying from "PFX"\n",
+            __FUNCTION__, res, app_tls);
+        if (app_tls != NULL)
+            memcpy(res, app_tls, sizeof(android_pthread_internal_t));
+        thrd = (android_pthread_internal_t *) res;
+        thrd->tls[ANDROID_TLS_SLOT_SELF] = thrd->tls;
+        thrd->tls[ANDROID_TLS_SLOT_THREAD_ID] = thrd;
+        thrd->tid = get_thread_id();
+        thrd->dr_tls_base = NULL;
+        res = thrd->tls[ANDROID_TLS_SLOT_SELF];
+        LOG(GLOBAL, LOG_LOADER, 2, "%s: TLS set to "PFX"\n",
+            __FUNCTION__, thrd->tls[ANDROID_TLS_SLOT_SELF]);
+    }
 
     /* Android does not yet support per-module TLS */
 
-    return init_thread.tls[ANDROID_TLS_SLOT_SELF];
+    return res;
 }
 
 void
 privload_tls_exit(void *dr_tp)
 {
-    /* nothing to do */
+    byte *alloc;
+    if (dr_tp == NULL || dr_tp == init_thread.tls)
+        return;
+    alloc = (byte *)dr_tp - offsetof(android_pthread_internal_t, tls);
+    heap_munmap(alloc, ALIGN_FORWARD(sizeof(android_pthread_internal_t), PAGE_SIZE));
 }
 
 /* For standalone lib usage (i#1862: the Android loader passes
