b93595da5508f60c9a39acbf2f8b08a219661394
Derek Bruening <bruening@google.com>
2016-07-04 15:50:28 -0400

    i#1962 Travis CI: add iniital Travis setup
    Adds the .travis.yml configuration file for Travis CI.
    
    Adds a wrapper script to run runsuite.cmake, parse the results, and set the
    exit code, which is what Travis checks.
    
    Review-URL: https://codereview.appspot.com/298680043

---
+++ suite/runsuite_wrapper.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+
+# **********************************************************
+# Copyright (c) 2016 Google, Inc.  All rights reserved.
+# **********************************************************
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of Google, Inc. nor the names of its contributors may be
+#   used to endorse or promote products derived from this software without
+#   specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+
+# Build-and-test driver for Travis CI.
+# Travis uses the exit code to check success, so we need a layer outside of
+# ctest on runsuite.
+# We stick with runsuite rather than creating a parallel scheme using
+# a Travis matrix of builds.
+# Travis only supports Linux and Mac, so we're ok relying on perl.
+
+use strict;
+use Cwd 'abs_path';
+use File::Basename;
+my $mydir = dirname(abs_path($0));
+
+my $res = `ctest -S ${mydir}/runsuite.cmake 2>&1`;
+
+my @lines = split('\n', $res);
+my $should_print = 0;
+my $exit_code = 0;
+foreach my $line (@lines) {
+    $should_print = 1 if ($line =~ /^RESULTS/);
+    if ($line =~ /^([-\w]+):.*\*\*/) {
+        my $fail = 0;
+        my $name = $1;
+        if ($line =~ /build errors/ ||
+            $line =~ /configure errors/ ||
+            $line =~ /tests failed:/) {
+            $fail = 1;
+        } elsif ($line =~ /(\d+) tests failed, of which (\d+)/) {
+            $fail = 1 if ($2 < $1);
+        }
+        if ($fail) {
+            $exit_code++;
+            print "\n====> FAILURE in $name <====\n";
+        }
+    }
+    print "$line\n" if ($should_print);
+}
+exit $exit_code;
