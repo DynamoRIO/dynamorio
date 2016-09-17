a71bf92fb995dada7bfb93046655b6f7c94a22e0
Derek Bruening <bruening@google.com>
2016-09-16 22:33:59 -0400

    i#1729 offline traces: add a generate-and-read multi-step test
    Adds support for expanding * in commands passed to runmulti.cmake, to
    locate the output trace file.  If the expansion is empty in a pre-command,
    the command is skipped (to support removing trace files, if any, prior to
    testing).
    
    Adds a multi-step test of generating, reading, and simulating an offline
    trace file.
    
    Review-URL: https://codereview.appspot.com/305540043

--- suite/tests/runmulti.cmake
+++ suite/tests/runmulti.cmake
@@ -1,5 +1,5 @@
 # **********************************************************
-# Copyright (c) 2015 Google, Inc.    All rights reserved.
+# Copyright (c) 2015-2016 Google, Inc.    All rights reserved.
 # **********************************************************
 
 # Redistribution and use in source and binary forms, with or without
@@ -34,29 +34,48 @@
 #     should have intra-arg space=@@ and inter-arg space=@ and ;=!
 # * postcmd = post processing command to run
 # * cmp = the file containing the expected output
+#
+# A "*" in any command line will be glob-expanded right before running.
+# If the expansion is empty for precmd, the precmd execution is skipped.
 
 # Intra-arg space=@@ and inter-arg space=@.
 # XXX i#1327: now that we have -c and other option passing improvements we
 # should be able to get rid of this @@ stuff.
-string(REGEX REPLACE "@@" " " precmd "${precmd}")
-string(REGEX REPLACE "@" ";" precmd "${precmd}")
-string(REGEX REPLACE "!" "\\\;" precmd "${precmd}")
+macro(process_cmdline var)
+  string(REGEX REPLACE "@@" " " ${var} "${${var}}")
+  string(REGEX REPLACE "@" ";" ${var} "${${var}}")
+  string(REGEX REPLACE "!" "\\\;" ${var} "${${var}}")
 
-string(REGEX REPLACE "@@" " " cmd "${cmd}")
-string(REGEX REPLACE "@" ";" cmd "${cmd}")
-string(REGEX REPLACE "!" "\\\;" cmd "${cmd}")
+  if (${var} MATCHES "\\*")
+    set(newcmd "")
+    foreach (token ${${var}})
+      if (token MATCHES "\\*")
+        file(GLOB expand ${token})
+        if (expand STREQUAL "")
+          set(globempty_${var} ON)
+        endif ()
+        set(newcmd ${newcmd} ${expand})
+      else ()
+        set(newcmd ${newcmd} ${token})
+      endif ()
+    endforeach ()
+    set(${var} ${newcmd})
+  endif ()
+endmacro()
 
-string(REGEX REPLACE "@@" " " postcmd "${postcmd}")
-string(REGEX REPLACE "@" ";" postcmd "${postcmd}")
-string(REGEX REPLACE "!" "\\\;" postcmd "${postcmd}")
+process_cmdline(precmd)
 
-execute_process(COMMAND ${precmd}
-  RESULT_VARIABLE cmd_result
-  ERROR_VARIABLE cmd_err
-  OUTPUT_VARIABLE cmd_out)
-if (cmd_result)
-  message(FATAL_ERROR "*** ${precmd} failed (${cmd_result}): ${cmd_err}***\n")
-endif (cmd_result)
+if (NOT precmd STREQUAL "" AND NOT globempty_precmd)
+  execute_process(COMMAND ${precmd}
+    RESULT_VARIABLE cmd_result
+    ERROR_VARIABLE cmd_err
+    OUTPUT_VARIABLE cmd_out)
+  if (cmd_result)
+    message(FATAL_ERROR "*** ${precmd} failed (${cmd_result}): ${cmd_err}***\n")
+  endif (cmd_result)
+endif ()
+
+process_cmdline(cmd)
 
 execute_process(COMMAND ${cmd}
   RESULT_VARIABLE cmd_result
@@ -68,6 +87,8 @@ if (cmd_result)
 endif (cmd_result)
 set(tomatch "${cmd_err}${cmd_out}")
 
+process_cmdline(postcmd)
+
 execute_process(COMMAND ${postcmd}
   RESULT_VARIABLE cmd_result
   ERROR_VARIABLE cmd_err
@@ -75,10 +96,11 @@ execute_process(COMMAND ${postcmd}
 if (cmd_result)
   message(FATAL_ERROR "*** ${postcmd} failed (${cmd_result}): ${cmd_err}***\n")
 endif (cmd_result)
+set(tomatch "${tomatch}${cmd_err}${cmd_out}")
 
 # get expected output (must already be processed w/ regex => literal, etc.)
 file(READ "${cmp}" str)
 
-if (NOT "${tomatch}" STREQUAL "${str}")
+if (NOT "${tomatch}" MATCHES "${str}")
   message(FATAL_ERROR "output |${tomatch}| failed to match expected output |${str}|")
 endif ()
