DynamoRIO/dynamorio

commit bbd70d3b3dc164e24bfcd873c47958c8f2913ee3

i#1680 large pages: Adjust option value defaults for page size.
The function adjust_defaults_for_page_size is invoked in two places.
If the page size is not 4 KiB, it adjusts certain option values to
make them valid.

Review-URL: https://codereview.appspot.com/311950043

core/options.c
@@ -194,12 +194,45 @@ DECLARE_CXTSWPROT_VAR(read_write_lock_t options_lock, INIT_READWRITE_LOCK(option
 
 /* INITIALIZATION */
 
+static void
+adjust_defaults_for_page_size(options_t *options)
+{
+    size_t page_size = PAGE_SIZE;
+
+    /* The defaults are known to be appropriate for 4 KiB pages. */
+    if (page_size == 4096)
+        return;
+
+    /* XXX: This approach is not scalable or maintainable as there may in
+     * future be many more options that depend on the page size.
+     */
+
+    /* Some of these are a small multiple of the page size because they include
+     * one or two guard pages.
+     */
+    options->vmm_block_size =
+        ALIGN_FORWARD(options->vmm_block_size, page_size);
+    options->stack_size =
+        MAX(ALIGN_FORWARD(options->stack_size, page_size), 2 * page_size);
+    options->initial_heap_unit_size =
+        MAX(ALIGN_FORWARD(options->initial_heap_unit_size, page_size),
+            3 * page_size);
+    options->initial_global_heap_unit_size =
+        MAX(ALIGN_FORWARD(options->initial_global_heap_unit_size, page_size),
+            3 * page_size);
+    options->heap_commit_increment =
+        ALIGN_FORWARD(options->heap_commit_increment, page_size);
+    options->cache_commit_increment =
+        ALIGN_FORWARD(options->cache_commit_increment, page_size);
+}
+
 /* sets defaults just like the above initialization */
 CORE_STATIC void
 set_dynamo_options_defaults(options_t *options)
 {
     ASSERT_OWN_OPTIONS_LOCK(options==&dynamo_options || options==&temp_options, &options_lock);
     *options = default_options;
+    adjust_defaults_for_page_size(options);
 }
 #undef OPTION_COMMAND_INTERNAL
 
@@ -2072,6 +2105,7 @@ options_init()
     write_lock(&options_lock);
     ASSERT(sizeof(dynamo_options) == sizeof(options_t));
     /* get dynamo options */
+    adjust_defaults_for_page_size(&dynamo_options);
     retval = get_parameter(PARAM_STR(DYNAMORIO_VAR_OPTIONS), option_string,
                            sizeof(option_string));
     if (IS_GET_PARAMETER_SUCCESS(retval))

core/optionsx.h
@@ -675,6 +675,7 @@
         IF_DEBUG_ELSE_0(60)*3*1000, /* disabled in release */
         "timeout (in ms) before assuming a deadlock had occurred (0 to disable)")
 
+    /* stack_size may be adjusted by adjust_defaults_for_page_size(). */
     OPTION_DEFAULT(uint_size, stack_size,
                    /* the CI build has a larger MAX_OPTIONS_STRING so we need
                     * a larger stack even w/ no client present.
@@ -1113,16 +1114,22 @@
      * FIXME: for Windows, if we reserve the whole region up front and
      * just commit pieces, why do we need to match the Windows kernel
      * alloc granularity?
+     *
+     * vmm_block_size may be adjusted by adjust_defaults_for_page_size().
      */
     OPTION_DEFAULT_INTERNAL(uint_size, vmm_block_size, (IF_WINDOWS_ELSE(64,16)*1024),
                             "allocation unit for virtual memory manager")
+    /* initial_heap_unit_size may be adjusted by adjust_defaults_for_page_size(). */
     OPTION_DEFAULT_INTERNAL(uint_size, initial_heap_unit_size, 32*1024, "initial private heap unit size")
+    /* initial_global_heap_unit_size may be adjusted by adjust_defaults_for_page_size(). */
     OPTION_DEFAULT_INTERNAL(uint_size, initial_global_heap_unit_size, 32*1024, "initial global heap unit size")
     /* if this is too small then once past the vm reservation we have too many
      * DR areas and subsequent problems with DR areas and allmem synch (i#369)
      */
     OPTION_DEFAULT_INTERNAL(uint_size, max_heap_unit_size, 256*1024, "maximum heap unit size")
+    /* heap_commit_increment may be adjusted by adjust_defaults_for_page_size(). */
     OPTION_DEFAULT(uint_size, heap_commit_increment, 4*1024, "heap commit increment")
+    /* cache_commit_increment may be adjusted by adjust_defaults_for_page_size(). */
     OPTION_DEFAULT(uint, cache_commit_increment, 4*1024, "cache commit increment")
 
     /* cache capacity control

commit 150001f3daa5dfaefa9feca0da168dc13b95fac7

Final attempt to fix Windows build broken by bbd70d3.
Review-URL: https://codereview.appspot.com/315000043

core/options.c
@@ -198,7 +198,7 @@ static void
 adjust_defaults_for_page_size(options_t *options)
 {
 #ifndef NOT_DYNAMORIO_CORE /* XXX: clumsy fix for Windows */
-    uint page_size = PAGE_SIZE;
+    uint page_size = (uint)PAGE_SIZE;
 
     /* The defaults are known to be appropriate for 4 KiB pages. */
     if (page_size == 4096)