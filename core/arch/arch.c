DynamoRIO/dynamorio

commit 2dd9659475aef0db32440e1f6adb74562a5dfdbf

i#2157 re-attach: add best-effort re-attach support
While there are many globals that are not reset, we take a best-effort
approach here and reset the ones that matter, allowing us to re-attach with
a memtrace client and static DR.

Portions of this were initially based on
https://codereview.appspot.com/13314047/ from Peter Goodman.

For DO_ONCE, an incremented counter is used.

For numerous other globals, at exit time if doing_detach is set we reset
them to NULL.

For locks, we simply reset count_times_acquired but leave the deleted field
as it does not matter much (part of the whole theme here: this may never be
rock-solid for unusual option combinations but our goal is supporting the
common case).

For drreg, with the new multi-init feature we have to zero the options at
exit time.

Adds several tests by adding loops around existing start/stop tests, but
stops short on burst_threads until i#2175 is fixed.

Review-URL: https://codereview.appspot.com/318520043

core/arch/arch.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2010-2016 Google, Inc.  All rights reserved.
+ * Copyright (c) 2010-2017 Google, Inc.  All rights reserved.
  * Copyright (c) 2000-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -880,6 +880,20 @@ arch_exit(IF_WINDOWS_ELSE_NP(bool detach_stacked_callbacks, void))
 #endif
     interp_exit();
     mangle_exit();
+
+    if (doing_detach) {
+        /* Clear for possible re-attach. */
+        shared_code = NULL;
+#if defined(X86) && defined(X64)
+        shared_code_x86 = NULL;
+        shared_code_x86_to_x64 = NULL;
+#endif
+        syscall_method = SYSCALL_METHOD_UNINITIALIZED;
+        app_sysenter_instr_addr = NULL;
+#ifdef UNIX
+        sysenter_hook_failed = false;
+#endif
+    }
 }
 
 static byte *

clients/drcachesim/tests/burst_static.cpp
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2016 Google, Inc.  All rights reserved.
+ * Copyright (c) 2016-2017 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
@@ -76,27 +76,30 @@ main(int argc, const char *argv[])
     if (!my_setenv("DYNAMORIO_OPTIONS", "-stderr_mask 0xc -client_lib ';;-offline'"))
         std::cerr << "failed to set env var!\n";
 
-    std::cerr << "pre-DR init\n";
-    dr_app_setup();
-    assert(!dr_app_running_under_dynamorio());
+    /* We use an outer loop to test re-attaching (i#2157). */
+    for (int j = 0; j < 3; ++j) {
+        std::cerr << "pre-DR init\n";
+        dr_app_setup();
+        assert(!dr_app_running_under_dynamorio());
 
-    for (int i = 0; i < outer_iters; ++i) {
-        if (i == iter_start) {
-            std::cerr << "pre-DR start\n";
-            dr_app_start();
+        for (int i = 0; i < outer_iters; ++i) {
+            if (i == iter_start) {
+                std::cerr << "pre-DR start\n";
+                dr_app_start();
+            }
+            if (i >= iter_start && i <= iter_stop)
+                assert(dr_app_running_under_dynamorio());
+            else
+                assert(!dr_app_running_under_dynamorio());
+            if (do_some_work(i) < 0)
+                std::cerr << "error in computation\n";
+            if (i == iter_stop) {
+                std::cerr << "pre-DR detach\n";
+                dr_app_stop_and_cleanup();
+            }
         }
-        if (i >= iter_start && i <= iter_stop)
-            assert(dr_app_running_under_dynamorio());
-        else
-            assert(!dr_app_running_under_dynamorio());
-        if (do_some_work(i) < 0)
-            std::cerr << "error in computation\n";
-        if (i == iter_stop) {
-            std::cerr << "pre-DR detach\n";
-            dr_app_stop_and_cleanup();
-        }
+        std::cerr << "all done\n";
     }
-    std::cerr << "all done\n";
     return 0;
 }
 

clients/drcachesim/tests/burst_threads.cpp
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2016 Google, Inc.  All rights reserved.
+ * Copyright (c) 2016-2017 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
@@ -86,22 +86,30 @@ thread_func(void *arg)
     static const int iter_start = outer_iters/3;
     static const int iter_stop = iter_start + 4;
 
-    for (int i = 0; i < outer_iters; ++i) {
-        if (idx == burst_owner && i == iter_start) {
-            std::cerr << "pre-DR start\n";
-            dr_app_start();
-        }
-        if (idx == burst_owner) {
-            if (i >= iter_start && i <= iter_stop)
-                assert(dr_app_running_under_dynamorio());
-            else
-                assert(!dr_app_running_under_dynamorio());
-        }
-        if (do_some_work(i) < 0)
-            std::cerr << "error in computation\n";
-        if (idx == burst_owner && i == iter_stop) {
-            std::cerr << "pre-DR detach\n";
-            dr_app_stop_and_cleanup();
+    /* We use an outer loop to test re-attaching (i#2157), except
+     * there is an unfixed bug i#2175.
+     * XXX i#2175: up the iter count once we fix the bug.
+     */
+    for (int j = 0; j < 1; ++j) {
+        if (j > 0 && idx == burst_owner)
+            dr_app_setup();
+        for (int i = 0; i < outer_iters; ++i) {
+            if (idx == burst_owner && i == iter_start) {
+                std::cerr << "pre-DR start\n";
+                dr_app_start();
+            }
+            if (idx == burst_owner) {
+                if (i >= iter_start && i <= iter_stop)
+                    assert(dr_app_running_under_dynamorio());
+                else
+                    assert(!dr_app_running_under_dynamorio());
+            }
+            if (do_some_work(i) < 0)
+                std::cerr << "error in computation\n";
+            if (idx == burst_owner && i == iter_stop) {
+                std::cerr << "pre-DR detach\n";
+                dr_app_stop_and_cleanup();
+            }
         }
     }
     finished[idx] = true;

clients/drcachesim/tests/offline-burst_static.templatex
@@ -2,6 +2,14 @@ pre-DR init
 pre-DR start
 pre-DR detach
 all done
+pre-DR init
+pre-DR start
+pre-DR detach
+all done
+pre-DR init
+pre-DR start
+pre-DR detach
+all done
 Core #0 \(1 thread\(s\)\)
   L1I stats:
     Hits:                         *[0-9,\.]*.....

clients/drcachesim/tracer/tracer.cpp
@@ -786,6 +786,7 @@ event_exit(void)
                                                       event_bb_instru2instru) ||
         drreg_exit() != DRREG_SUCCESS)
         DR_ASSERT(false);
+    dr_unregister_exit_event(event_exit);
 
     dr_mutex_destroy(mutex);
     drutil_exit();

core/config.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2015 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2017 Google, Inc.  All rights reserved.
  * Copyright (c) 2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -692,7 +692,11 @@ config_heap_exit(void)
 void
 config_exit(void)
 {
-    /* nothing -- so not called on fast exit */
+#if !defined(NOT_DYNAMORIO_CORE) && !defined(NOT_DYNAMORIO_CORE_PROPER)
+    if (doing_detach)
+        memset(&config, 0, sizeof config); /* for possible re-attach */
+#endif
+    /* nothing -- so not called on fast exit (is called on detach) */
 }
 
 /* Our parameters (option string, logdir, etc.) can be configured

core/dynamo.c
@@ -1474,6 +1474,33 @@ dynamo_process_exit(void)
 #endif /* !DEBUG */
 }
 
+void
+dynamo_exit_post_detach(void)
+{
+    /* i#2157: best-effort re-init in case of re-attach */
+
+    do_once_generation++; /* Increment the generation in case we re-attach */
+
+    dynamo_initialized = false;
+    dynamo_heap_initialized = false;
+    automatic_startup = false;
+    control_all_threads = false;
+    dr_api_entry = false;
+    dr_api_exit  = false;
+#ifdef UNIX
+    dynamo_exiting = false;
+#endif
+    dynamo_exited = false;
+    dynamo_exited_and_cleaned = false;
+#ifdef DEBUG
+    dynamo_exited_log_and_stats = false;
+#endif
+    dynamo_resetting = false;
+#ifdef UNIX
+    post_execve = false;
+#endif
+}
+
 dcontext_t *
 create_new_dynamo_context(bool initial, byte *dstack_in, priv_mcontext_t *mc)
 {

core/globals.h
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2016 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2017 Google, Inc.  All rights reserved.
  * Copyright (c) 2000-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -569,6 +569,7 @@ int dynamo_shared_exit(thread_record_t *toexit
 void dynamo_process_exit_with_thread_info(void);
 /* thread cleanup prior to clean exit event */
 void dynamo_thread_exit_pre_client(dcontext_t *dcontext, thread_id_t id);
+void dynamo_exit_post_detach(void);
 
 /* enter/exit DR hooks */
 void entering_dynamorio(void);

commit a213558497e76abc6abf92558f3cc8bb4b2fcf14

i#2157 re-attach: fix Mac breakage
Fixes Mac build breakage from 2dd9659.

core/arch/arch.c
@@ -890,7 +890,7 @@ arch_exit(IF_WINDOWS_ELSE_NP(bool detach_stacked_callbacks, void))
 #endif
         syscall_method = SYSCALL_METHOD_UNINITIALIZED;
         app_sysenter_instr_addr = NULL;
-#ifdef UNIX
+#ifdef LINUX
         sysenter_hook_failed = false;
 #endif
     }