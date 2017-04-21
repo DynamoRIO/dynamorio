DynamoRIO/dynamorio

commit d7c0a6fb7021c6e7e63dbb5e8e9120aee6179b0a

fixed issue 1265 - replace mini-stack with code stub to avoid overflow
SVN-Revision: 2277

core/native_exec.c
@@ -158,8 +158,14 @@ void
 native_exec_module_unload(module_area_t *ma)
 {
     bool is_native = check_and_mark_native_exec(ma, false/*!add*/);
-    if (is_native && DYNAMO_OPTION(native_exec_retakeover))
-        native_module_unhook(ma);
+    if (DYNAMO_OPTION(native_exec_retakeover)) {
+        if (is_native)
+            native_module_unhook(ma);
+#ifdef UNIX
+        else
+            native_module_nonnative_mod_unload(ma);
+#endif
+    }
 }
 
 /* Clean call called on every fcache to native transition.  Turns on and off
@@ -198,14 +204,35 @@ entering_native(dcontext_t *dcontext)
     STATS_INC(num_native_module_enter);
 }
 
-void
-call_to_native(app_pc *sp)
+/* We replace the actual return target on the app stack with a stub pc
+ * so that the control transfer back to code cache or DR after native module
+ * returns.
+ */
+static bool
+prepare_return_from_native_via_stub(dcontext_t *dcontext, app_pc *app_sp)
 {
-    dcontext_t *dcontext;
+#ifdef UNIX
+    app_pc stub_pc;
+    ASSERT(DYNAMO_OPTION(native_exec_retakeover) && !is_native_pc(*app_sp));
+    /* i#1238-c#4: the inline asm stub does not support kstats, so we
+     * only support it when native_exec_opt is on, which turns kstats off.
+     */
+    if (!DYNAMO_OPTION(native_exec_opt))
+        return false;
+    stub_pc = native_module_get_ret_stub(dcontext, *app_sp);
+    if (stub_pc == NULL)
+        return false;
+    *app_sp = stub_pc;
+    return true;
+#endif
+    return false;
+}
+
+static void
+prepare_return_from_native_via_stack(dcontext_t *dcontext, app_pc *app_sp)
+{
     uint i;
-    ENTERING_DR();
-    dcontext = get_thread_private_dcontext();
-    ASSERT(dcontext != NULL);
+    ASSERT(DYNAMO_OPTION(native_exec_retakeover) && !is_native_pc(*app_sp));
     /* Push the retaddr and stack location onto our stack.  The current entry
      * should be free and we should have enough space.
      * XXX: it would be nice to abort in a release build, but this can be perf
@@ -213,6 +240,25 @@ call_to_native(app_pc *sp)
      */
     i = dcontext->native_retstack_cur;
     ASSERT(i < MAX_NATIVE_RETSTACK);
+    dcontext->native_retstack[i].retaddr = *app_sp;
+    dcontext->native_retstack[i].retloc = (app_pc) app_sp;
+    dcontext->native_retstack_cur = i + 1;
+    /* i#978: We use a different return stub for every nested call to native
+     * code.  Each stub pushes a different index into the retstack.  We could
+     * use the SP at return time to try to find the app's return address, but
+     * because of ret imm8 instructions, that's not robust.
+     */
+    *app_sp = retstub_start + i * BACK_FROM_NATIVE_RETSTUB_SIZE;
+}
+
+void
+call_to_native(app_pc *app_sp)
+{
+    dcontext_t *dcontext;
+
+    ENTERING_DR();
+    dcontext = get_thread_private_dcontext();
+    ASSERT(dcontext != NULL);
     /* i#1090: If the return address is also in a native module, then leave it
      * alone.  This happens on:
      * - native call
@@ -221,19 +267,15 @@ call_to_native(app_pc *sp)
      * - native ret                     # should stay native
      * XXX: Doing a vmvector binary search on every call to native is expensive.
      */
-    if (!is_native_pc(*sp)) {
-        dcontext->native_retstack[i].retaddr = *sp;
-        dcontext->native_retstack[i].retloc = (app_pc) sp;
-        dcontext->native_retstack_cur = i + 1;
-        /* i#978: We use a different return stub for every nested call to native
-         * code.  Each stub pushes a different index into the retstack.  We could
-         * use the SP at return time to try to find the app's return address, but
-         * because of ret imm8 instructions, that's not robust.
+    if (DYNAMO_OPTION(native_exec_retakeover) && !is_native_pc(*app_sp)) {
+        /* We try to use stub for fast return-from-native handling, if fails 
+         * (e.g., on Windows or optimization disabled), fall back to use the stack.
          */
-        *sp = retstub_start + i * BACK_FROM_NATIVE_RETSTUB_SIZE;
+        if (!prepare_return_from_native_via_stub(dcontext, app_sp))
+            prepare_return_from_native_via_stack(dcontext, app_sp);
     }
     LOG(THREAD, LOG_ASYNCH, 1,
-        "!!!! Entering module NATIVELY, retaddr="PFX"\n\n", *sp);
+        "!!!! Entering module NATIVELY, retaddr="PFX"\n\n", *app_sp);
     entering_native(dcontext);
     EXITING_DR();
 }

core/link.c
@@ -234,7 +234,7 @@ DECLARE_NEVERPROT_VAR(static const linkstub_t linkstub_ibl_bb_jmp,
     { LINK_FAKE | LINK_INDIRECT | LINK_JMP, 0 });
 DECLARE_NEVERPROT_VAR(static const linkstub_t linkstub_ibl_bb_call,
     { LINK_FAKE | LINK_INDIRECT | LINK_CALL, 0 });
-/* we only need special_*_ret and *_call for client_ibl and native_plt_ibl */
+/* we only need special_*_ret and *_call for client_ibl and native_plt/ret_ibl */
 DECLARE_NEVERPROT_VAR(static const linkstub_t linkstub_special_ibl_bb_ret,
     { LINK_FAKE | LINK_INDIRECT | LINK_RETURN, 0 } /* client_ibl */);
 DECLARE_NEVERPROT_VAR(static const linkstub_t linkstub_special_ibl_bb_call,

core/native_exec.h
@@ -84,6 +84,15 @@ native_module_hook(module_area_t *ma, bool at_map);
 void
 native_module_unhook(module_area_t *ma);
 
+#ifdef UNIX
+void
+native_module_nonnative_mod_unload(module_area_t *ma);
+
+/* get (create if not exist) a ret_stub for tgt */
+app_pc
+native_module_get_ret_stub(dcontext_t *dcontext, app_pc ret_tgt);
+#endif
+
 /* Update next_tag with the real app return address. */
 void
 interpret_back_from_native(dcontext_t *dcontext);

core/unix/native_elf.c
@@ -38,10 +38,19 @@
 #include "module.h"
 #include "instr.h"
 #include "instr_create.h"
+#include "instrument.h" /* instrlist_meta_append */
 #include "decode.h"
 #include "disassemble.h"
+#include "../hashtable.h"
 
 #include <link.h>  /* for struct link_map */
+#include <stddef.h> /* offsetof */
+
+#ifndef X86
+# error X86-only!
+#endif
+
+#define APP  instrlist_meta_append
 
 /* According to the SysV amd64 psABI docs[1], there are three reserved entries
  * in the PLTGOT:
@@ -85,10 +94,27 @@ static byte plt_stub_template[MAX_STUB_SIZE];
 static uint plt_stub_immed_offset;
 static uint plt_stub_jmp_tgt_offset;
 static size_t plt_stub_size = MAX_STUB_SIZE;
-static app_pc stub_heap;
+static app_pc plt_stub_heap;
 #ifdef X64
-static app_pc reachability_stub;
+static app_pc plt_reachability_stub;
 #endif
+
+/* stub code for transfer ret from native module to DR
+ * 0x558ed060:  movabs %rax,%gs:0x0         // save xax
+ * 0x558ed06b:  movabs $0x7f22caf2d5e3,%rax // put target into xax
+ * 0x558ed075:  jmpq   0x558bfd80           // jmp to ibl_xfer
+ * 0x558ed07a:  ...
+ */
+static const size_t ret_stub_size = 0x20;
+static app_pc ret_stub_heap;
+/* hashtable for native-exec return target
+ * - key: the return target in the non-native module
+ * - payload: code stub for the return target.
+ *   The payload will not be freed until the corrsesponding module is unloaded,
+ *   so we can use it (storing it on the app stack) without holding the table lock.
+ */
+static generic_table_t *native_ret_table;
+#define INIT_HTABLE_SIZE_NERET 6 /* should remain small */
 
 static bool
 module_contains_pc(module_area_t *ma, app_pc pc)
@@ -275,7 +301,7 @@ create_opt_plt_stub(app_pc plt_tgt, app_pc stub_pc)
 static app_pc
 create_plt_stub(app_pc plt_target)
 {
-    app_pc stub_pc = special_heap_alloc(stub_heap);
+    app_pc stub_pc = special_heap_alloc(plt_stub_heap);
     app_pc *tgt_immed;
     app_pc jmp_tgt;
 
@@ -288,7 +314,7 @@ create_plt_stub(app_pc plt_target)
     *tgt_immed = plt_target;
 #ifdef X64
     /* This is a reladdr operand, which we patch in just the same way. */
-    insert_relative_target(jmp_tgt, reachability_stub, false/*!hotpatch*/);
+    insert_relative_target(jmp_tgt, plt_reachability_stub, false/*!hotpatch*/);
 #else
     insert_relative_target(jmp_tgt, (app_pc) native_plt_call,
                            false/*!hotpatch*/);
@@ -304,7 +330,7 @@ destroy_plt_stub(app_pc stub_pc)
     app_pc orig_tgt;
     app_pc *tgt_immed = (app_pc *) (stub_pc + plt_stub_immed_offset);
     orig_tgt = *tgt_immed;
-    special_heap_free(stub_heap, stub_pc);
+    special_heap_free(plt_stub_heap, stub_pc);
     return orig_tgt;
 }
 
@@ -323,12 +349,12 @@ plt_reloc_entry_size(ELF_WORD pltrel)
 }
 
 static bool
-is_special_stub(app_pc stub_pc)
+is_special_plt_stub(app_pc stub_pc)
 {
     special_heap_iterator_t shi;
     bool found = false;
     /* XXX: this acquires a lock in a nested loop. */
-    special_heap_iterator_start(stub_heap, &shi);
+    special_heap_iterator_start(plt_stub_heap, &shi);
     while (special_heap_iterator_hasnext(&shi)) {
         app_pc start, end;
         special_heap_iterator_next(&shi, &start, &end);
@@ -372,7 +398,7 @@ update_plt_relocations(module_area_t *ma, os_privmod_data_t *opd, bool add_hooks
             /* XXX: pull the ranges out of the heap up front to avoid lock
              * acquisitions.
              */
-            if (is_special_stub(gotval)) {
+            if (is_special_plt_stub(gotval)) {
                 *r_addr = destroy_plt_stub(gotval);
             }
         }
@@ -510,17 +536,27 @@ native_module_init(void)
 {
     if (!DYNAMO_OPTION(native_exec_retakeover))
         return;
-    ASSERT(stub_heap == NULL && "init should only happen once");
+    ASSERT(plt_stub_heap == NULL && "init should only happen once");
     initialize_plt_stub_template();
-    stub_heap = special_heap_init(plt_stub_size, true/*locked*/,
-                                  true/*executable*/, true/*persistent*/);
+    plt_stub_heap = special_heap_init(plt_stub_size, true/*locked*/,
+                                      true/*executable*/, true/*persistent*/);
 #ifdef X64
     /* i#719: native_plt_call may not be reachable from the stub heap, so we
      * indirect through this "stub".
      */
-    reachability_stub = special_heap_alloc(stub_heap);
-    *((app_pc*)reachability_stub) = (app_pc)native_plt_call;
+    plt_reachability_stub = special_heap_alloc(plt_stub_heap);
+    *((app_pc*)plt_reachability_stub) = (app_pc)native_plt_call;
 #endif
+
+    ASSERT(ret_stub_heap == NULL && "init should only happen once");
+    ret_stub_heap = special_heap_init(ret_stub_size,
+                                      true /* locked */,
+                                      true /* exectable */,
+                                      true /* persistent */);
+    native_ret_table = generic_hash_create(GLOBAL_DCONTEXT, INIT_HTABLE_SIZE_NERET,
+                                           50 /* load factor: perf-critical */,
+                                           HASHTABLE_SHARED,
+                                           NULL _IF_DEBUG("neret table"));
 }
 
 void
@@ -531,6 +567,10 @@ native_module_exit(void)
      */
     module_iterator_t *mi;
     module_area_t *ma;
+    int iter;
+    void *stub_pc;
+    ptr_uint_t key;
+
     mi = module_iterator_start();
     while (module_iterator_hasnext(mi)) {
          ma = module_iterator_next(mi);
@@ -541,15 +581,141 @@ native_module_exit(void)
     module_iterator_stop(mi);
 
 #ifdef X64
-    if (reachability_stub != NULL) {
-        special_heap_free(stub_heap, reachability_stub);
-        reachability_stub = NULL;
+    if (plt_reachability_stub != NULL) {
+        special_heap_free(plt_stub_heap, plt_reachability_stub);
+        plt_reachability_stub = NULL;
     }
 #endif
 
-    if (stub_heap != NULL) {
-        special_heap_exit(stub_heap);
-        stub_heap = NULL;
+    if (plt_stub_heap != NULL) {
+        special_heap_exit(plt_stub_heap);
+        plt_stub_heap = NULL;
+    }
+
+    /* free ret_stub_heap */
+    if (native_ret_table != NULL) {
+        TABLE_RWLOCK(native_ret_table, write, lock);
+        iter = 0;
+        do {
+            iter = generic_hash_iterate_next(GLOBAL_DCONTEXT, native_ret_table,
+                                             iter, &key, &stub_pc);
+            if (iter < 0)
+                break;
+            /* remove from hashtable */
+            iter = generic_hash_iterate_remove(GLOBAL_DCONTEXT, native_ret_table,
+                                               iter, key);
+            /* free stub from special heap */
+            special_heap_free(ret_stub_heap, stub_pc);
+        } while (true);
+        TABLE_RWLOCK(native_ret_table, write, unlock);
+        generic_hash_destroy(GLOBAL_DCONTEXT, native_ret_table);
+        native_ret_table = NULL;
+    }
+    if (ret_stub_heap != NULL) {
+        special_heap_exit(ret_stub_heap);
+        ret_stub_heap = NULL;
     }
 }
 
+/* called on unloading a non-native module */
+void
+native_module_nonnative_mod_unload(module_area_t *ma)
+{
+    int iter;
+    app_pc stub_pc, pc;
+    ASSERT(DYNAMO_OPTION(native_exec_retakeover) &&
+           DYNAMO_OPTION(native_exec_opt));
+    TABLE_RWLOCK(native_ret_table, write, lock);
+    iter = 0;
+    do {
+        bool remove = false;
+        os_module_data_t *os_data;
+        iter = generic_hash_iterate_next(GLOBAL_DCONTEXT, native_ret_table, iter,
+                                         (ptr_uint_t *)&pc,
+                                         (void **)&stub_pc);
+        if (iter < 0)
+            break;
+        if (pc < ma->start || pc >= ma->end)
+            continue;
+        os_data = &ma->os_data;
+        if (os_data->contiguous)
+            remove = true;
+        else {
+            int i;
+            for (i = 0; i < os_data->num_segments; i++) {
+                if (pc >= os_data->segments[i].start &&
+                    pc <  os_data->segments[i].end) {
+                    remove = true;
+                    break;
+                }
+            }
+        }
+        /* remove from hashtable */
+        if (remove) {
+            iter = generic_hash_iterate_remove(GLOBAL_DCONTEXT, native_ret_table,
+                                               iter, (ptr_uint_t)pc);
+            special_heap_free(ret_stub_heap, pc);
+        }
+    } while (true);
+    TABLE_RWLOCK(native_ret_table, write, unlock);
+}
+
+/* We create a ret_stub for each return target of the call site from non-native
+ * module to native modue. The stub_pc will replace the real return target so that
+ * DR can regain the control after the native module returns.
+ */
+static app_pc
+special_ret_stub_create(dcontext_t *dcontext, app_pc tgt)
+{
+    instrlist_t ilist;
+    app_pc stub_pc, pc;
+
+    /* alloc and encode special ret stub */
+    stub_pc = special_heap_alloc(ret_stub_heap);
+
+    instrlist_init(&ilist);
+    /* we need to steal xax register, xax restore is in the ibl_xfer code from
+     * emit_native_ret_ibl_xfer.
+     */
+    APP(&ilist, instr_create_save_to_tls(dcontext, REG_XAX, TLS_XAX_SLOT));
+    /* the rest is similar to opt_plt_stub */
+    /* mov tgt => XAX */
+    APP(&ilist, INSTR_CREATE_mov_imm(dcontext,
+                                     opnd_create_reg(REG_XAX),
+                                     OPND_CREATE_INTPTR(tgt)));
+    /* jmp native_ret_ibl */
+    APP(&ilist, INSTR_CREATE_jmp(dcontext,
+                                 opnd_create_pc
+                                 (get_native_ret_ibl_xfer_entry(dcontext))));
+    pc = instrlist_encode(dcontext, &ilist, stub_pc, false);
+    instrlist_clear(dcontext, &ilist);
+
+    TABLE_RWLOCK(native_ret_table, write, lock);
+    /* lookup again */
+    pc = (app_pc) generic_hash_lookup(GLOBAL_DCONTEXT, native_ret_table, (ptr_uint_t)tgt);
+    if (pc != NULL) {
+        TABLE_RWLOCK(native_ret_table, write, unlock);
+        /* we found one, delete current one, and return the one we found */
+        special_heap_free(ret_stub_heap, stub_pc);
+        return pc;
+    }
+    generic_hash_add(GLOBAL_DCONTEXT, native_ret_table, (ptr_uint_t)tgt, (void *)stub_pc);
+    TABLE_RWLOCK(native_ret_table, write, unlock);
+    return stub_pc;
+}
+
+/* get (create if not exist) a ret_stub for the return target: tgt */
+app_pc
+native_module_get_ret_stub(dcontext_t *dcontext, app_pc tgt)
+{
+    app_pc stub_pc;
+
+    TABLE_RWLOCK(native_ret_table, read, lock);
+    stub_pc = (app_pc) generic_hash_lookup(GLOBAL_DCONTEXT, native_ret_table,
+                                           (ptr_uint_t)tgt);
+    TABLE_RWLOCK(native_ret_table, read, unlock);
+    if (stub_pc == NULL)
+        stub_pc = special_ret_stub_create(dcontext, tgt);
+    ASSERT(stub_pc != NULL);
+    return stub_pc;
+}

core/x86/arch.c
@@ -183,12 +183,14 @@ dump_emitted_routines(dcontext_t *dcontext, file_t file,
             else if (last_pc == code->trace_head_return_coarse)
                 print_file(file, "trace_head_return_coarse:\n");
 # ifdef CLIENT_INTERFACE
-            else if (last_pc == code->special_ibl_xfer[client_ibl_idx])
+            else if (last_pc == code->special_ibl_xfer[CLIENT_IBL_IDX])
                 print_file(file, "client_ibl_xfer:\n");
 # endif
 # ifdef UNIX
-            else if (last_pc == code->special_ibl_xfer[native_plt_ibl_idx])
+            else if (last_pc == code->special_ibl_xfer[NATIVE_PLT_IBL_IDX])
                 print_file(file, "native_plt_ibl_xfer:\n");
+            else if (last_pc == code->special_ibl_xfer[NATIVE_RET_IBL_IDX])
+                print_file(file, "native_ret_ibl_xfer:\n");
 # endif
             else if (last_pc == code->clean_call_save)
                 print_file(file, "clean_call_save:\n");
@@ -465,15 +467,19 @@ shared_gencode_init(IF_X64_ELSE(gencode_mode_t gencode_mode, void))
 
     if (!special_ibl_xfer_is_thread_private()) {
 #ifdef CLIENT_INTERFACE
-        gencode->special_ibl_xfer[client_ibl_idx] = pc;
+        gencode->special_ibl_xfer[CLIENT_IBL_IDX] = pc;
         pc = emit_client_ibl_xfer(GLOBAL_DCONTEXT, pc, gencode);
 #endif
 #ifdef UNIX
         /* i#1238: native exec optimization */
         if (DYNAMO_OPTION(native_exec_opt)) {
             pc = check_size_and_cache_line(gencode, pc);
-            gencode->special_ibl_xfer[native_plt_ibl_idx] = pc;
+            gencode->special_ibl_xfer[NATIVE_PLT_IBL_IDX] = pc;
             pc = emit_native_plt_ibl_xfer(GLOBAL_DCONTEXT, pc, gencode);
+            /* native ret */
+            pc = check_size_and_cache_line(gencode, pc);
+            gencode->special_ibl_xfer[NATIVE_RET_IBL_IDX] = pc;
+            pc = emit_native_ret_ibl_xfer(GLOBAL_DCONTEXT, pc, gencode);
         }
 #endif
     }
@@ -1127,15 +1133,19 @@ arch_thread_init(dcontext_t *dcontext)
 
     if (special_ibl_xfer_is_thread_private()) {
 #ifdef CLIENT_INTERFACE
-        code->special_ibl_xfer[client_ibl_idx] = pc;
+        code->special_ibl_xfer[CLIENT_IBL_IDX] = pc;
         pc = emit_client_ibl_xfer(dcontext, pc, code);
 #endif
 #ifdef UNIX
         /* i#1238: native exec optimization */
         if (DYNAMO_OPTION(native_exec_opt)) {
             pc = check_size_and_cache_line(code, pc);
-            code->special_ibl_xfer[native_plt_ibl_idx] = pc;
+            code->special_ibl_xfer[NATIVE_PLT_IBL_IDX] = pc;
             pc = emit_native_plt_ibl_xfer(dcontext, pc, code);
+            /* native ret */
+            pc = check_size_and_cache_line(code, pc);
+            code->special_ibl_xfer[NATIVE_RET_IBL_IDX] = pc;
+            pc = emit_native_ret_ibl_xfer(dcontext, pc, code);
         }
 #endif
     }
@@ -1695,7 +1705,7 @@ get_special_ibl_xfer_entry(dcontext_t *dcontext, int index)
 cache_pc
 get_client_ibl_xfer_entry(dcontext_t *dcontext)
 {
-    return get_special_ibl_xfer_entry(dcontext, client_ibl_idx);
+    return get_special_ibl_xfer_entry(dcontext, CLIENT_IBL_IDX);
 }
 #endif
 
@@ -1703,7 +1713,13 @@ get_client_ibl_xfer_entry(dcontext_t *dcontext)
 cache_pc
 get_native_plt_ibl_xfer_entry(dcontext_t *dcontext)
 {
-    return get_special_ibl_xfer_entry(dcontext, native_plt_ibl_idx);
+    return get_special_ibl_xfer_entry(dcontext, NATIVE_PLT_IBL_IDX);
+}
+
+cache_pc
+get_native_ret_ibl_xfer_entry(dcontext_t *dcontext)
+{
+    return get_special_ibl_xfer_entry(dcontext, NATIVE_RET_IBL_IDX);
 }
 #endif
 

core/x86/arch.h
@@ -586,9 +586,10 @@ typedef struct ibl_code_t {
 } ibl_code_t;
 
 /* special ibls */
-#define NUM_SPECIAL_IBL_XFERS 2 /* client_ibl and native_plt_ibl */
-extern const int client_ibl_idx;
-extern const int native_plt_ibl_idx;
+#define NUM_SPECIAL_IBL_XFERS 3 /* client_ibl and native_plt/ret__ibl */
+#define CLIENT_IBL_IDX     0
+#define NATIVE_PLT_IBL_IDX 1
+#define NATIVE_RET_IBL_IDX 2
 
 /* Each thread needs its own copy of these routines, but not all
  * routines here are created in a thread-private: we could save space
@@ -914,6 +915,9 @@ emit_client_ibl_xfer(dcontext_t *dcontext, byte *pc, generated_code_t *code);
 #ifdef UNIX
 byte *
 emit_native_plt_ibl_xfer(dcontext_t *dcontext, byte *pc, generated_code_t *code);
+
+byte *
+emit_native_ret_ibl_xfer(dcontext_t *dcontext, byte *pc, generated_code_t *code);
 #endif
 
 /* clean calls are used by core DR: native_exec, so not in CLIENT_INTERFACE */

core/x86/arch_exports.h
@@ -1213,6 +1213,9 @@ get_client_ibl_xfer_entry(dcontext_t *dcontext);
 #ifdef UNIX
 cache_pc
 get_native_plt_ibl_xfer_entry(dcontext_t *dcontext);
+
+cache_pc
+get_native_ret_ibl_xfer_entry(dcontext_t *dcontext);
 #endif
 
 

commit 237e71f1d4fd2014342c3cf44ab79910401a9416

i#2372: do not lose control in native_exec with default options
Fixes a regression from d7c0a6fb7 which erroneously removed the retaddr
changing unless -native_exec_retakeover was set.

Augments the native_exec test with annotations so we can actually tell
whether we lost control.

Fixes #2372

core/native_exec.c
@@ -236,7 +236,7 @@ prepare_return_from_native_via_stub(dcontext_t *dcontext, app_pc *app_sp)
 {
 #ifdef UNIX
     app_pc stub_pc;
-    ASSERT(DYNAMO_OPTION(native_exec_retakeover) && !is_native_pc(*app_sp));
+    ASSERT(!is_native_pc(*app_sp));
     /* i#1238-c#4: the inline asm stub does not support kstats, so we
      * only support it when native_exec_opt is on, which turns kstats off.
      */
@@ -255,7 +255,7 @@ static void
 prepare_return_from_native_via_stack(dcontext_t *dcontext, app_pc *app_sp)
 {
     uint i;
-    ASSERT(DYNAMO_OPTION(native_exec_retakeover) && !is_native_pc(*app_sp));
+    ASSERT(!is_native_pc(*app_sp));
     /* Push the retaddr and stack location onto our stack.  The current entry
      * should be free and we should have enough space.
      * XXX: it would be nice to abort in a release build, but this can be perf
@@ -290,7 +290,7 @@ call_to_native(app_pc *app_sp)
      * - native ret                     # should stay native
      * XXX: Doing a vmvector binary search on every call to native is expensive.
      */
-    if (DYNAMO_OPTION(native_exec_retakeover) && !is_native_pc(*app_sp)) {
+    if (!is_native_pc(*app_sp)) {
         /* We try to use stub for fast return-from-native handling, if fails
          * (e.g., on Windows or optimization disabled), fall back to use the stack.
          */

suite/tests/CMakeLists.txt
@@ -566,7 +566,12 @@ endfunction(tobuild)
 
 # normal app w/ options
 function(tobuild_ops test source dr_ops exe_ops)
-  add_exe(${test} ${source})
+  set(srcs ${source})
+  # XXX: add this _use_annotations to other to* routine if necessary.
+  if (${test}_use_annotations)
+    use_DynamoRIO_annotations(${test} srcs)
+  endif ()
+  add_exe(${test} ${srcs})
   set(testlist_normal ${testlist_normal} ${test} PARENT_SCOPE)
   set(${test}_source ${source} PARENT_SCOPE)
   set(${test}_dr_ops ${dr_ops} PARENT_SCOPE)
@@ -1553,6 +1558,7 @@ if (X86) # FIXME i#1551, i#1569: port asm to ARM and AArch64
     get_target_property(native_dll_name common.nativeexec.appdll
       LOCATION${location_suffix})
     get_filename_component(native_dll_name "${native_dll_name}" NAME)
+    set(common.nativeexec_use_annotations ON)
     tobuild_ops(common.nativeexec common/nativeexec.c
       "-no_early_inject -native_exec_list ${native_dll_name}" "")
     target_link_libraries(common.nativeexec common.nativeexec.appdll)
@@ -1563,9 +1569,10 @@ if (X86) # FIXME i#1551, i#1569: port asm to ARM and AArch64
       torunonly(common.nativeexec_retakeover common.nativeexec common/nativeexec.c
         "-no_early_inject -native_exec_list ${native_dll_name} -native_exec_retakeover"
         "")
-      torunonly(common.nativeexec_exe common.nativeexec common/nativeexec.c
+      torunonly(common.nativeexec_exe common.nativeexec
+        common/nativeexec_exenative.c
         "-no_early_inject -native_exec_list common.nativeexec -native_exec_retakeover" "")
-      torunonly(common.nativeexec_bindnow common.nativeexec common/nativeexec.c
+      torunonly(common.nativeexec_bindnow common.nativeexec common/nativeexec_exenative.c
         "-no_early_inject -native_exec_list common.nativeexec -native_exec_retakeover"
         "-bind_now")
       # XXX: the next 3 tests, and perhaps all of these native_exec tests,
@@ -1577,9 +1584,10 @@ if (X86) # FIXME i#1551, i#1569: port asm to ARM and AArch64
       torunonly(common.nativeexec_retakeover_opt common.nativeexec common/nativeexec.c
         "-no_early_inject -native_exec_list ${native_dll_name} -native_exec_retakeover  -native_exec_opt -no_kstats" "")
       # No swap TLS and no kstats for optimized native_exec
-      torunonly(common.nativeexec_exe_opt common.nativeexec common/nativeexec.c
+      torunonly(common.nativeexec_exe_opt common.nativeexec common/nativeexec_exenative.c
         "-no_early_inject -native_exec_list common.nativeexec -native_exec_retakeover -native_exec_opt -no_kstats" "")
-      torunonly(common.nativeexec_bindnow_opt common.nativeexec common/nativeexec.c
+      torunonly(common.nativeexec_bindnow_opt common.nativeexec
+        common/nativeexec_exenative.c
         "-no_early_inject -native_exec_list common.nativeexec -native_exec_retakeover -native_exec_opt -no_kstats" "-bind_now")
     endif ()
   endif ()

suite/tests/common/nativeexec.c
@@ -1,4 +1,5 @@
 /* **********************************************************
+ * Copyright (c) 2017 Google, Inc.  All rights reserved.
  * Copyright (c) 2005 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -36,6 +37,7 @@
  * different call* constructions
  */
 #include "tools.h"
+#include "dr_annotations.h"
 
 #include <setjmp.h>
 
@@ -120,6 +122,9 @@ main(int argc, char **argv)
 
     INIT();
 
+    if (!DYNAMORIO_ANNOTATE_RUNNING_ON_DYNAMORIO())
+        print("Not under DR!\n");
+
     if (argc > 2 && strcmp("-bind_now", argv[1])) {
 #ifdef WINDOWS
         print("-bind_now is Linux-only\n");
@@ -183,6 +188,9 @@ main(int argc, char **argv)
     print("calling loop_test\n");
     loop_test();
 
+    /* i#2372: make sure to verify we did not lose control! */
+    if (!DYNAMORIO_ANNOTATE_RUNNING_ON_DYNAMORIO())
+        print("Not under DR!\n");
     print("all done\n");
 
     return 0;

suite/tests/common/nativeexec_exenative.expect
@@ -1,0 +1,17 @@
+Not under DR!
+calling via IAT-style call
+nativeexec.dll:import_me1(57)
+calling via PLT-style call
+nativeexec.dll:import_me2(37)
+calling via funky ind call
+nativeexec.dll:import_me3(17)
+calling nested native
+nativeexec.exe:print_int(42)
+calling cross-module unwinder
+before longjmp, 1
+after longjmp
+calling indirect ret_imm
+ -> 40
+calling loop_test
+Not under DR!
+all done