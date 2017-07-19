6a321dc1218384936b70fe56c6ac45b9ca09b80d
Derek Bruening <bruening@google.com>
2017-07-19 11:20:21 -0400

    Enable test building for debug cross-compiles
    Adds building, but not running, of tests to the ARM and AArch64
    cross-compile suite builds.
    
    Adds temporary -VV to figure out why these builds are not configuring on
    Travis.

--- suite/runsuite.cmake
+++ suite/runsuite.cmake
@@ -308,9 +308,12 @@ if (UNIX AND ARCH_IS_X86)
   set(ARCH_IS_X86 OFF)
   set(ENV{CFLAGS} "") # environment vars do not obey the normal scope rules--must reset
   set(ENV{CXXFLAGS} "")
+  set(prev_run_tests ${run_tests})
+  set(run_tests OFF) # build tests but don't run them
   testbuild_ex("arm-debug-internal-32" OFF "
     DEBUG:BOOL=ON
     INTERNAL:BOOL=ON
+    BUILD_TESTS:BOOL=ON
     CMAKE_TOOLCHAIN_FILE:PATH=${CTEST_SOURCE_DIRECTORY}/make/toolchain-arm32.cmake
     " OFF OFF "")
   testbuild_ex("arm-release-external-32" OFF "
@@ -321,6 +324,7 @@ if (UNIX AND ARCH_IS_X86)
   testbuild_ex("arm-debug-internal-64" ON "
     DEBUG:BOOL=ON
     INTERNAL:BOOL=ON
+    BUILD_TESTS:BOOL=ON
     CMAKE_TOOLCHAIN_FILE:PATH=${CTEST_SOURCE_DIRECTORY}/make/toolchain-arm64.cmake
     " OFF OFF "")
   testbuild_ex("arm-release-external-64" ON "
@@ -328,6 +332,7 @@ if (UNIX AND ARCH_IS_X86)
     INTERNAL:BOOL=OFF
     CMAKE_TOOLCHAIN_FILE:PATH=${CTEST_SOURCE_DIRECTORY}/make/toolchain-arm64.cmake
     " OFF OFF "")
+  set(run_tests ${prev_run_tests})
 
   # Android cross-compilation and running of tests using "adb shell"
   find_program(ADB adb DOC "adb Android utility")
