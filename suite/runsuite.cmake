DynamoRIO/dynamorio

commit 66fa2c78eb8d5c52bed7b7e4105f0135b8ca593b

i#2511: isolate raw2trace (#2512)
Splits out a self-contained library, which:
- only takes already opened std::istream and std::ostream-compatible files
and doesn't own them, so it can be used with more exotic filesystems;
- takes in the contents of the module file to avoid doing IO from
drmodtrack_offline_read();
- returns errors on failure;
- doesn't depend on dr_frontend.

Preserves the current behavior with raw2trace_helper_t, which walks
directories and opens files.

Adds a ptrace-based test checking that raw2trace doesn't open files.

TESTED: ctest

Fixes #2511

clients/drcachesim/CMakeLists.txt
@@ -77,6 +77,12 @@ add_library(simulator STATIC
   simulator/tlb_simulator.cpp
   )
 
+add_library(raw2trace STATIC
+  tracer/raw2trace.cpp
+  tracer/raw2trace_directory.cpp
+  )
+configure_DynamoRIO_standalone(raw2trace)
+
 add_executable(drcachesim
   launcher.cpp
   analyzer.cpp
@@ -87,15 +93,13 @@ add_executable(drcachesim
   ${zlib_reader}
   reader/ipc_reader.cpp
   simulator/analyzer_interface.cpp
-  # We embed the raw2trace conversion for convenience:
-  tracer/raw2trace.cpp
   tracer/instru.cpp
   tracer/instru_online.cpp
   )
 # In order to embed raw2trace we need to be standalone:
 configure_DynamoRIO_standalone(drcachesim)
 # Link in our tools:
-target_link_libraries(drcachesim simulator reuse_distance histogram reuse_time)
+target_link_libraries(drcachesim simulator reuse_distance histogram reuse_time raw2trace)
 # To avoid dup symbol errors between drinjectlib and the drdecode brought in
 # by drfrontendlib we have to explicitly list drdecode up front:
 target_link_libraries(drcachesim drdecode drinjectlib drconfiglib drfrontendlib)
@@ -164,10 +168,10 @@ install_client_header(tracer/drmemtrace.h)
 
 add_executable(drraw2trace
   tracer/raw2trace_launcher.cpp
-  tracer/raw2trace.cpp
   tracer/instru.cpp
   tracer/instru_online.cpp
   )
+target_link_libraries(drraw2trace raw2trace)
 # To avoid dup symbol errors on some VS builds we list drdecode before DR:
 target_link_libraries(drraw2trace drdecode)
 configure_DynamoRIO_standalone(drraw2trace)
@@ -332,8 +336,23 @@ if (BUILD_TESTS)
     add_win32_flags(tool.drcacheoff.burst_threads)
     target_link_libraries(tool.drcacheoff.burst_threads ${libpthread})
 
-    # FIXME i#2099: the weak symbol is not supported on Windows.
     if (UNIX)
+      if (NOT APPLE)
+        # uses ptrace and looks for linux-specific syscalls
+        add_executable(tool.drcacheoff.raw2trace_io tests/raw2trace_io.cpp
+          tracer/instru.cpp
+          tracer/instru_online.cpp)
+        configure_DynamoRIO_standalone(tool.drcacheoff.raw2trace_io)
+        add_win32_flags(tool.drcacheoff.raw2trace_io)
+        target_link_libraries(tool.drcacheoff.raw2trace_io raw2trace)
+        use_DynamoRIO_extension(tool.drcacheoff.raw2trace_io droption)
+        target_link_libraries(tool.drcacheoff.raw2trace_io drdecode)
+        use_DynamoRIO_extension(tool.drcacheoff.raw2trace_io drcovlib_static)
+        # Because we're leveraging instru_online code we have to link with drutil:
+        use_DynamoRIO_extension(tool.drcacheoff.raw2trace_io drutil_static)
+      endif ()
+
+      # FIXME i#2099: the weak symbol is not supported on Windows.
       add_executable(tool.drcacheoff.burst_client tests/burst_static.cpp)
       append_property_list(TARGET tool.drcacheoff.burst_client
         COMPILE_DEFINITIONS "TEST_APP_DR_CLIENT_MAIN")

suite/tests/CMakeLists.txt
@@ -2563,6 +2563,29 @@ if (CLIENT_INTERFACE)
           torunonly_drcacheoff(burst_threads tool.drcacheoff.burst_threads "" "")
           set(tool.drcacheoff.burst_threads_nodr ON)
 
+          if (NOT APPPLE)
+            # Test that raw2trace doesn't do more IO than it should.
+            get_target_path_for_execution(raw2trace_io_path tool.drcacheoff.raw2trace_io)
+            prefix_cmd_if_necessary(raw2trace_io_path ON ${raw2trace_io_path})
+            macro (torunonly_raw2trace testname exetgt extra_ops app_args)
+              torunonly_ci(tool.raw2trace.${testname} ${exetgt} drcachesim
+                "raw2trace-${testname}.c" # for templatex basename
+                "-offline ${extra_ops}" "" "${app_args}")
+              set(tool.raw2trace.${testname}_toolname "drcachesim")
+              set(tool.raw2trace.${testname}_basedir
+                "${PROJECT_SOURCE_DIR}/clients/drcachesim/tests")
+              set(tool.raw2trace.${testname}_rawtemp ON) # no preprocessor
+              set(tool.raw2trace.${testname}_runcmp
+                "${CMAKE_CURRENT_SOURCE_DIR}/runmulti.cmake")
+              set(tool.raw2trace.${testname}_precmd
+                "foreach@${CMAKE_COMMAND}@-E@remove_directory@drmemtrace.${exetgt}.*.dir")
+              set(tool.raw2trace.${testname}_postcmd
+              "${raw2trace_io_path}@-indir@drmemtrace.${exetgt}.*.dir@-out@drraw2trace.${exetgt}.out")
+            endmacro()
+            # actual trace processing not important -- set a very small limit for speed
+            torunonly_raw2trace(simple ${ci_shared_app} "-max_trace_size 8K" "")
+          endif()
+
           # FIXME i#2099: the weak symbol is not supported not work on Windows
           torunonly_drcacheoff(burst_client tool.drcacheoff.burst_client "" "")
           set(tool.drcacheoff.burst_client_nodr ON)

clients/drcachesim/analyzer_multi.cpp
@@ -40,6 +40,7 @@
 # include "reader/compressed_file_reader.h"
 #endif
 #include "reader/ipc_reader.h"
+#include "tracer/raw2trace_directory.h"
 #include "tracer/raw2trace.h"
 
 analyzer_multi_t::analyzer_multi_t()
@@ -65,8 +66,11 @@ analyzer_multi_t::analyzer_multi_t()
             trace_iter = existing;
         else {
             delete existing;
-            raw2trace_t raw2trace(op_indir.get_value(), tracefile);
-            raw2trace.do_conversion();
+            raw2trace_directory_t dir(op_indir.get_value(), tracefile);
+            raw2trace_t raw2trace(dir.modfile_bytes, dir.thread_files, &dir.out_file);
+            std::string error = raw2trace.do_conversion();
+            if (!error.empty())
+                ERRMSG("raw2trace failed: %s", error.c_str());
             trace_iter = new file_reader_t(tracefile.c_str());
         }
         // We don't support a compressed file here (is_complete() is too hard

clients/drcachesim/common/utils.h
@@ -48,6 +48,7 @@
 #define BUFFER_SIZE_ELEMENTS(buf)   (BUFFER_SIZE_BYTES(buf) / sizeof(buf[0]))
 #define BUFFER_LAST_ELEMENT(buf)    buf[BUFFER_SIZE_ELEMENTS(buf) - 1]
 #define NULL_TERMINATE_BUFFER(buf)  BUFFER_LAST_ELEMENT(buf) = 0
+#define TESTANY(mask, var) (((mask) & (var)) != 0)
 
 #define BOOLS_MATCH(b1, b2) (!!(b1) == !!(b2))
 

clients/drcachesim/tests/raw2trace-simple.templatex
@@ -1,0 +1,5 @@
+\[drmemtrace\]: Reading module file from memory
+open|close
+\[drmemtrace\]: Successfully read [0-9]+ modules
+\[drmemtrace\]: Successfully converted [0-9]+ thread files
+Processed

clients/drcachesim/tests/raw2trace_io.cpp
@@ -1,0 +1,166 @@
+/* **********************************************************
+ * Copyright (c) 2017 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Google, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL GOOGLE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/* This application processes a set of existing raw files with raw2trace_t.
+ * It uses ptrace to make sure raw2trace only interacts with the filesystem when expected
+ * -- it doesn't open/close any files outside of mapping modules (which should be local).
+ *
+ * XXX: We use ptrace as opposed to running under memtrace with replaced file operations
+ * because raw2trace uses drmodtrack, which doesn't isolate under static memtrace.
+ */
+
+#include "droption.h"
+#include "tracer/raw2trace.h"
+#include "tracer/raw2trace_directory.h"
+#include <cassert>
+#include <errno.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <syscall.h>
+#include <unistd.h>
+
+static droption_t<std::string> op_indir(DROPTION_SCOPE_FRONTEND, "indir", "",
+                                        "[Required] Directory with trace input files",
+                                        "Specifies a directory with raw files.");
+
+static droption_t<std::string> op_out(DROPTION_SCOPE_FRONTEND, "out", "",
+                                      "[Required] Path to output file",
+                                      "Specifies the path to the output file.");
+#if defined(X64)
+# define SYS_NUM(r) (r).orig_rax
+#elif defined(X86)
+# define SYS_NUM(r) (r).orig_eax
+#else
+# error "Test only supports x86"
+#endif
+
+#ifndef LINUX
+# error "Test only supports Linux"
+#endif
+
+int
+main(int argc, const char *argv[])
+{
+    std::string parse_err;
+    if (!droption_parser_t::parse_argv(DROPTION_SCOPE_FRONTEND, argc, (const char **)argv,
+                                       &parse_err, NULL) ||
+        op_indir.get_value().empty() || op_out.get_value().empty()) {
+        std::cerr << "Usage error: " << parse_err << "\nUsage:\n"
+                  << droption_parser_t::usage_short(DROPTION_SCOPE_ALL);
+        return 1;
+    }
+
+    /* Open input/output files outside of traced region. And explicitly don't destroy dir,
+     * so they never get closed.
+     */
+    raw2trace_directory_t *dir =
+        new raw2trace_directory_t(op_indir.get_value(), op_out.get_value());
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        std::cerr << "Fork failed\n";
+        return 1;
+    }
+    if (pid != 0) {
+        /* parent */
+        long res;
+        int status;
+        while (true) {
+            int wait_res = waitpid(pid, &status, __WALL);
+            if (wait_res < 0) {
+                if (errno == EINTR)
+                    continue;
+                perror("Failed waiting");
+                return 1;
+            }
+            if (WIFEXITED(status))
+                break;
+            user_regs_struct regs;
+            res = ptrace(PTRACE_GETREGS, pid, NULL, &regs);
+            if (res < 0) {
+                perror("ptrace failed");
+                return 1;
+            }
+            /* We don't distinguish syscall entry/exit because orig_rax is set
+             * on both.
+             */
+            if (SYS_NUM(regs) == __NR_open || SYS_NUM(regs) == __NR_openat ||
+                SYS_NUM(regs) == __NR_creat) {
+                std::cerr << "open\n";
+            } else if (SYS_NUM(regs) == __NR_close) {
+                std::cerr << "close\n";
+            }
+            res = ptrace(PTRACE_SYSCALL, pid, NULL, 0);
+            if (res < 0) {
+                perror("ptrace failed");
+                return 1;
+            }
+        }
+        if (WIFEXITED(status)) {
+            return 0;
+        }
+        res = ptrace(PTRACE_DETACH, pid, NULL, NULL);
+        if (res < 0) {
+            perror("ptrace failed");
+            return 1;
+        }
+        std::cerr << "Detached\n";
+        return 0;
+    } else {
+        /* child */
+        long res = ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        if (res < 0) {
+            perror("ptrace me failed");
+            return 1;
+        }
+        /* Force a wait until parent attaches, so we don't race on the fork. */
+        raise(SIGSTOP);
+
+        /* Sycalls below will be ptraced. We don't expect any open/close calls outside of
+         * raw2trace::read_and_map_modules().
+         */
+        raw2trace_t raw2trace(dir->modfile_bytes, dir->thread_files, &dir->out_file,
+                              GLOBAL_DCONTEXT, 1);
+        std::string error = raw2trace.do_conversion();
+        if (!error.empty()) {
+            std::cerr << "raw2trace failed " << error << "\n";
+            return 1;
+        }
+        std::cerr << "Processed\n";
+        return 0;
+    }
+    return 0;
+}

clients/drcachesim/tracer/raw2trace.h
@@ -38,7 +38,7 @@
 
 #include "dr_api.h"
 #include "drmemtrace.h"
-#include "../common/trace_entry.h"
+#include "trace_entry.h"
 #include <fstream>
 #include <vector>
 
@@ -58,32 +58,37 @@ struct module_t {
 
 class raw2trace_t {
 public:
-    raw2trace_t(std::string indir, std::string outname);
+    // module_map, thread_files and out_file are all owned and opened/closed by the
+    // caller.
+    raw2trace_t(const char *module_map, const std::vector<std::istream*> &thread_files,
+                std::ostream *out_file, void *dcontext = NULL,
+                unsigned int verbosity = 0);
     ~raw2trace_t();
-    void do_conversion();
+    // Returns non-empty error message on failure.
+    std::string do_conversion();
+    static std::string check_thread_file(std::istream *f);
 
 private:
-    void read_and_map_modules(void);
-    void unmap_modules(void);
-    void open_thread_log_file(const char *basename);
-    void open_thread_files();
-    void merge_and_process_thread_files();
-    bool append_bb_entries(uint tidx, offline_entry_t *in_entry);
-    trace_entry_t *append_memref(trace_entry_t *buf_in, uint tidx, instr_t *instr,
-                                 opnd_t ref, bool write);
+    std::string read_and_map_modules(const char *module_map);
+    std::string unmap_modules(void);
+    std::string merge_and_process_thread_files();
+    std::string append_bb_entries(uint tidx, offline_entry_t *in_entry,
+                                  OUT bool *handled);
+    std::string append_memref(INOUT trace_entry_t **buf_in, uint tidx, instr_t *instr,
+                              opnd_t ref, bool write);
 
-    std::string indir;
-    std::string outname;
-    std::ofstream out_file;
     static const uint MAX_COMBINED_ENTRIES = 64;
+    const char *modmap;
     void *modhandle;
     std::vector<module_t> modvec;
-    std::vector<std::ifstream*> thread_files;
+    std::vector<std::istream*> thread_files;
+    std::ostream *out_file;
     void *dcontext;
     bool prev_instr_was_rep_string;
     // This indicates that each memref has its own PC entry and that each
     // icache entry does not need to be considered a memref PC entry as well.
     bool instrs_are_separate;
+    unsigned int verbosity;
 };
 
 #endif /* _RAW2TRACE_H_ */

commit f3d4486d5338d6c661ad966688d96546538db041

i#2511 isolate raw2trace: build tests on x86 only
Fixes ARM build breakage from 66fa2c78 whose test is x86-specific.

Adds test building to the ARM and AArch64 cross-compile debug builds to
catch such errors in the future.

clients/drcachesim/CMakeLists.txt
@@ -337,7 +337,7 @@ if (BUILD_TESTS)
     target_link_libraries(tool.drcacheoff.burst_threads ${libpthread})
 
     if (UNIX)
-      if (NOT APPLE)
+      if (X86 AND NOT APPLE) # This test is x86-specific.
         # uses ptrace and looks for linux-specific syscalls
         add_executable(tool.drcacheoff.raw2trace_io tests/raw2trace_io.cpp
           tracer/instru.cpp

suite/runsuite.cmake
@@ -311,6 +311,7 @@ if (UNIX AND ARCH_IS_X86)
   testbuild_ex("arm-debug-internal-32" OFF "
     DEBUG:BOOL=ON
     INTERNAL:BOOL=ON
+    BUILD_TESTS:BOOL=ON
     CMAKE_TOOLCHAIN_FILE:PATH=${CTEST_SOURCE_DIRECTORY}/make/toolchain-arm32.cmake
     " OFF OFF "")
   testbuild_ex("arm-release-external-32" OFF "
@@ -321,6 +322,7 @@ if (UNIX AND ARCH_IS_X86)
   testbuild_ex("arm-debug-internal-64" ON "
     DEBUG:BOOL=ON
     INTERNAL:BOOL=ON
+    BUILD_TESTS:BOOL=ON
     CMAKE_TOOLCHAIN_FILE:PATH=${CTEST_SOURCE_DIRECTORY}/make/toolchain-arm64.cmake
     " OFF OFF "")
   testbuild_ex("arm-release-external-64" ON "

suite/tests/CMakeLists.txt
@@ -2564,7 +2564,7 @@ if (CLIENT_INTERFACE)
           torunonly_drcacheoff(burst_threads tool.drcacheoff.burst_threads "" "")
           set(tool.drcacheoff.burst_threads_nodr ON)
 
-          if (NOT APPPLE)
+          if (X86 AND NOT APPPLE) # This test is x86-specific.
             # Test that raw2trace doesn't do more IO than it should.
             get_target_path_for_execution(raw2trace_io_path tool.drcacheoff.raw2trace_io)
             prefix_cmd_if_necessary(raw2trace_io_path ON ${raw2trace_io_path})