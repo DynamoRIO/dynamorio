DynamoRIO/dynamorio

commit 240b75d614feb7ef244b6190a25a33010f367126

i#2592: add UNIX stack overflow detection (#2596)
Removes the STACK_GUARD_PAGE define and makes all of its code always
compiled.

Implements stack overflow detection on UNIX by using a guard page at the
bottom of the stack.  Adds a new label on a crash that distinguishes a
stack overflow from a general crash.  Adds a new option -stack_guard_pages
covering this feature along with a new test.

Does not support the feature on Windows where the kernel and -vm_reserve
end up not working well together: i#2595 covers addressing that.

Fixes #2592

core/win32/callback.c
@@ -179,10 +179,8 @@ DECLARE_CXTSWPROT_VAR(static mutex_t map_intercept_pc_lock,
 DECLARE_CXTSWPROT_VAR(static mutex_t emulate_write_lock,
                       INIT_LOCK_FREE(emulate_write_lock));
 
-#ifdef STACK_GUARD_PAGE
 DECLARE_CXTSWPROT_VAR(static mutex_t exception_stack_lock,
                       INIT_LOCK_FREE(exception_stack_lock));
-#endif
 
 DECLARE_CXTSWPROT_VAR(static mutex_t intercept_hook_lock,
                       INIT_LOCK_FREE(intercept_hook_lock));
@@ -4302,15 +4300,13 @@ found_modified_code(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
     ASSERT_NOT_REACHED(); /* should never get here */
 }
 
-#ifdef STACK_GUARD_PAGE
 static bool
 is_dstack_overflow(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
                    CONTEXT *cxt)
 {
-    if (pExcptRec->ExceptionCode == EXCEPTION_GUARD_PAGE) {
-        /* Richter book says that only access violation fills in info array,
-         * but on win2k guard page seems to fill it in!
-         */
+    if (pExcptRec->ExceptionCode == EXCEPTION_GUARD_PAGE ||
+        pExcptRec->ExceptionCode == EXCEPTION_STACK_OVERFLOW) {
+        /* Both of these seem to put the target in info slot 1. */
         if (pExcptRec->NumberParameters >= 2) {
             app_pc target = (app_pc) pExcptRec->ExceptionInformation[1];
             LOG(THREAD, LOG_ASYNCH, 2,
@@ -4320,7 +4316,6 @@ is_dstack_overflow(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
     }
     return false;
 }
-#endif /* STACK_GUARD_PAGE */
 
 /* To allow execution from a writable memory region, we mark it read-only.
  * When we get a seg fault, we call this routine, which determines if it's
@@ -4779,7 +4774,8 @@ report_app_exception(dcontext_t *dcontext, uint appfault_flags,
 
 void
 report_internal_exception(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
-                          CONTEXT *cxt, uint dumpcore_flag, const char *prefix)
+                          CONTEXT *cxt, uint dumpcore_flag, const char *prefix,
+                          const char *crash_label)
 {
     /* WARNING: a fault in DR means that potentially anything could be
      * inconsistent or corrupted!  Do not grab locks or traverse
@@ -4840,7 +4836,7 @@ report_internal_exception(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
     report_dynamorio_problem(dcontext, dumpcore_flag,
                              (app_pc) pExcptRec->ExceptionAddress,
                              (app_pc) cxt->CXT_XBP,
-                             fmt, prefix, CRASH_NAME,
+                             fmt, prefix, crash_label,
                              (app_pc) pExcptRec->ExceptionAddress,
                              pExcptRec->ExceptionCode, pExcptRec->ExceptionFlags,
                              cxt->CXT_XIP, pExcptRec->ExceptionAddress,
@@ -4860,23 +4856,25 @@ report_internal_exception(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
 
 void
 internal_exception_info(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
-                        CONTEXT *cxt, bool dstack_overflow)
+                        CONTEXT *cxt, bool dstack_overflow, bool is_client)
 {
-    report_internal_exception(dcontext, pExcptRec, cxt, DUMPCORE_INTERNAL_EXCEPTION,
+    report_internal_exception(dcontext, pExcptRec, cxt,
+                              (is_client ? DUMPCORE_CLIENT_EXCEPTION :
+                               DUMPCORE_INTERNAL_EXCEPTION) |
+                              (dstack_overflow ? DUMPCORE_STACK_OVERFLOW : 0),
                               /* for clients we need to let them override the label */
-                              IF_NOT_CLIENT_INTERFACE(dstack_overflow ?
-                                                      "Stack overflow" :)
-                              exception_label_core);
+                              is_client ? exception_label_client : exception_label_core,
+                              dstack_overflow ? STACK_OVERFLOW_NAME : CRASH_NAME);
 }
 
 static void
-internal_dynamo_exception(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec, CONTEXT *cxt)
+internal_dynamo_exception(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec, CONTEXT *cxt,
+                          bool is_client)
 {
     /* recursive bailout: avoid infinite loop due to fault in fault handling
      * by using DO_ONCE
      * FIXME: any worries about lack of mutex w/ DO_ONCE?
      */
-#ifdef STACK_GUARD_PAGE
     /* PR 203701: If we've exhausted the dstack, then we'll switch
      * to a separate exception handling stack to make sure we have
      * enough space to report the problem.  One guard page is not
@@ -4889,12 +4887,9 @@ internal_dynamo_exception(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec, CON
             mutex_unlock(&exception_stack_lock);
         }
         else {
-            internal_exception_info(dcontext, pExcptRec, cxt, false);
+            internal_exception_info(dcontext, pExcptRec, cxt, false, is_client);
         }
     });
-#else
-    DO_ONCE({ internal_exception_info(dcontext, pExcptRec, cxt, false); });
-#endif
     os_terminate(dcontext, TERMINATE_PROCESS);
     ASSERT_NOT_REACHED();
 }
@@ -5164,7 +5159,7 @@ check_internal_exception(dcontext_t *dcontext, CONTEXT *cxt,
             }
         }
 
-        internal_dynamo_exception(dcontext, pExcptRec, cxt);
+        internal_dynamo_exception(dcontext, pExcptRec, cxt, false);
         ASSERT_NOT_REACHED();
     }
 }
@@ -5284,7 +5279,7 @@ intercept_exception(app_state_at_intercept_t *state)
             /* there is no good reason for this, other than DR error */
             ASSERT(is_dynamo_address((app_pc)pExcptRec->ExceptionAddress));
             pExcptRec->ExceptionFlags = 0xbadDC;
-            internal_dynamo_exception(dcontext, pExcptRec, cxt);
+            internal_dynamo_exception(dcontext, pExcptRec, cxt, false);
             ASSERT_NOT_REACHED();
         }
 
@@ -5436,8 +5431,7 @@ intercept_exception(app_state_at_intercept_t *state)
                 /* won't return if it was a made-read-only code page */
                 check_for_modified_code(dcontext, pExcptRec, cxt, MOD_CODE_APP_CXT, NULL);
             }
-            report_internal_exception(dcontext, pExcptRec, cxt, DUMPCORE_CLIENT_EXCEPTION,
-                                      exception_label_client);
+            internal_dynamo_exception(dcontext, pExcptRec, cxt, true);
             os_terminate(dcontext, TERMINATE_PROCESS);
             ASSERT_NOT_REACHED();
         }
@@ -5852,7 +5846,7 @@ intercept_exception(app_state_at_intercept_t *state)
                  * native or in the cache we hack the exception flags
                  */
                 pExcptRec->ExceptionFlags = 0xbadcad;
-                internal_dynamo_exception(dcontext, pExcptRec, cxt);
+                internal_dynamo_exception(dcontext, pExcptRec, cxt, false);
                 ASSERT_NOT_REACHED();
             }
 
@@ -8530,9 +8524,7 @@ callback_exit()
 {
     DELETE_LOCK(emulate_write_lock);
     DELETE_LOCK(map_intercept_pc_lock);
-#ifdef STACK_GUARD_PAGE
     DELETE_LOCK(exception_stack_lock);
-#endif
     DELETE_LOCK(intercept_hook_lock);
 }
 

core/arch/arch_exports.h
@@ -1153,13 +1153,11 @@ int dynamorio_syscall_wow64_noedx(int sysnum, ...);
 void get_segments_cs_ss(cxt_seg_t *cs, cxt_seg_t *ss);
 void get_segments_defg(cxt_seg_t *ds, cxt_seg_t *es, cxt_seg_t *fs, cxt_seg_t *gs);
 void get_own_context_helper(CONTEXT *cxt);
-# ifdef STACK_GUARD_PAGE
 /* PR203701: If the dstack is exhausted we'll use this function to
  * call internal_exception_info() with a separate exception stack.
  */
 void call_intr_excpt_alt_stack(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
                                CONTEXT *cxt, byte *stack);
-# endif
 void dynamorio_earliest_init_takeover(void);
 #else /* UNIX */
 void client_int_syscall(void);

core/arch/x86/x86.asm
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2016 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2017 Google, Inc.  All rights reserved.
  * Copyright (c) 2001-2010 VMware, Inc.  All rights reserved.
  * ********************************************************** */
 
@@ -2060,10 +2060,8 @@ call_modcode_alt_stack_no_free:
 #undef flags
 #undef using_initstack
 
-#ifdef STACK_GUARD_PAGE
-/*
- * void call_intr_excpt_alt_stack(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
- *                                CONTEXT *cxt, byte *stack)
+/* void call_intr_excpt_alt_stack(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
+ *                                CONTEXT *cxt, byte *stack, bool is_client)
  *
  * Routine to switch to a separate exception stack before calling
  * internal_exception_info().  This switch is useful if the dstack
@@ -2074,11 +2072,13 @@ call_modcode_alt_stack_no_free:
 #define pExcptRec       ARG2
 #define cxt             ARG3
 #define stack           ARG4
+#define is_client       ARG5
         DECLARE_FUNC(call_intr_excpt_alt_stack)
 GLOBAL_LABEL(call_intr_excpt_alt_stack:)
         mov      REG_XAX, dcontext
         mov      REG_XBX, pExcptRec
         mov      REG_XDI, cxt
+        mov      REG_XBP, is_client
         mov      REG_XSI, REG_XSP
         mov      REG_XSP, stack
 # ifdef X64
@@ -2089,7 +2089,8 @@ GLOBAL_LABEL(call_intr_excpt_alt_stack:)
                REG_XAX /* dcontext */,  \
                REG_XBX /* pExcptRec */, \
                REG_XDI /* cxt */,       \
-               1       /* dstack overflow == true */)
+               1       /* dstack overflow == true */, \
+               REG_XBP /* is_client */)
         pop      REG_XSP
         ret
         END_FUNC(call_intr_excpt_alt_stack)
@@ -2097,7 +2098,6 @@ GLOBAL_LABEL(call_intr_excpt_alt_stack:)
 #undef pExcptRec
 #undef cxt
 #undef stack
-#endif /* STACK_GUARD_PAGE */
 
 /* CONTEXT.Seg* is WORD for x64 but DWORD for x86 */
 #ifdef X64

core/dynamo.c
@@ -140,7 +140,7 @@ bool    post_execve = false;
 /* initial stack so we don't have to use app's */
 byte *  initstack;
 
-#if defined(WINDOWS) && defined(STACK_GUARD_PAGE)
+#ifdef WINDOWS
 /* PR203701: separate stack for error reporting when the dstack is exhausted */
 #define EXCEPTION_STACK_SIZE (2 * PAGE_SIZE)
 DECLARE_NEVERPROT_VAR(byte *exception_stack, NULL);
@@ -523,7 +523,7 @@ dynamorio_app_init(void)
         LOG(GLOBAL, LOG_SYNCH, 2, "initstack is "PFX"-"PFX"\n",
             initstack - DYNAMORIO_STACK_SIZE, initstack);
 
-#if defined(WINDOWS) && defined(STACK_GUARD_PAGE)
+#ifdef WINDOWS
         /* PR203701: separate stack for error reporting when the
          * dstack is exhausted
          */
@@ -1084,7 +1084,7 @@ dynamo_shared_exit(thread_record_t *toexit /* must ==cur thread for Linux */
     if (DYNAMO_OPTION(hot_patching))
         hotp_exit();
 #endif
-#if defined(WINDOWS) && defined(STACK_GUARD_PAGE) && defined(DEBUG)
+#ifdef WINDOWS
     /* Free exception stack before calling heap_exit */
     stack_free(exception_stack, EXCEPTION_STACK_SIZE);
     exception_stack = NULL;

core/globals.h
@@ -483,7 +483,7 @@ extern byte *  initstack;
 extern mutex_t   initstack_mutex;
 extern byte *  initstack_app_xsp;
 
-#if defined(WINDOWS) && defined(STACK_GUARD_PAGE)
+#ifdef WINDOWS
 /* PR203701: separate stack for error reporting when the dstack is exhausted */
 extern byte *  exception_stack;
 #endif

core/heap.c
@@ -2453,44 +2453,54 @@ heap_munmap(void *p, size_t size, which_vmm_t which)
     heap_munmap_ex(p, size, true/*guarded*/, which);
 }
 
-#ifdef STACK_GUARD_PAGE
-# define STACK_GUARD_PAGES 1
-#endif
-
 /* use stack_alloc to build a stack -- it returns TOS
- * For STACK_GUARD_PAGE, it also marks the bottom STACK_GUARD_PAGES==1
- * to detect overflows when used.
+ * For -stack_guard_pages, also allocates an extra page
+ * on the bottom and uses it to detect overflows when accessed.
  */
 void *
 stack_alloc(size_t size, byte *min_addr)
 {
     void *p;
-
     /* we reserve and commit at once for now
      * FIXME case 2330: commit-on-demand could allow larger max sizes w/o
      * hurting us in the common case
      */
-    p = get_guarded_real_memory(size, size, MEMPROT_READ|MEMPROT_WRITE, true, true,
-                                min_addr, VMM_STACK _IF_DEBUG("stack_alloc"));
+    size_t alloc_size = size;
+    if (!DYNAMO_OPTION(guard_pages) && DYNAMO_OPTION(stack_guard_pages))
+        alloc_size += PAGE_SIZE;
+    p = get_guarded_real_memory(alloc_size, alloc_size, MEMPROT_READ|MEMPROT_WRITE, true,
+                                true, min_addr, VMM_STACK _IF_DEBUG("stack_alloc"));
+    if (!DYNAMO_OPTION(guard_pages) && DYNAMO_OPTION(stack_guard_pages))
+        p = (byte *)p + PAGE_SIZE;
 #ifdef DEBUG_MEMORY
     memset(p, HEAP_ALLOCATED_BYTE, size);
 #endif
 
-#ifdef STACK_GUARD_PAGE
-    /* mark the bottom page non-accessible to trap stack overflow */
-    /* NOTE: the guard page should be included in the total memory requested */
-# ifdef WINDOWS
-    mark_page_as_guard((byte *)p + ((STACK_GUARD_PAGES - 1) * PAGE_SIZE));
-# else
-    /* FIXME: make no access, not just no write -- and update signal.c to
-     * look at reads and not just writes -- though unwritable is nearly as good
-     */
-#  if defined(CLIENT_INTERFACE) || defined(STANDALONE_UNIT_TEST)
-    if (!standalone_library)
-#  endif
-        make_unwritable(p, STACK_GUARD_PAGES * PAGE_SIZE);
-# endif
+    if (DYNAMO_OPTION(stack_guard_pages)) {
+        /* XXX: maybe we should this option a count of how many pages, to catch
+         * overflow that uses a large stride and skips over one page (UNIX-only
+         * since Windows code always uses chkstk to trigger guard pages).
+         */
+        /* We place a guard on UNIX signal stacks too: although we can't report
+         * such overflows, we'd rather have a clear crash than memory corruption
+         * from clobbering whatever memory is below the stack.
+         */
+        /* mark the bottom page non-accessible to trap stack overflow */
+        byte *guard = (byte *)p - PAGE_SIZE;
+#ifdef WINDOWS
+        /* Only a committed page can be a guard page. */
+        /* XXX: this doesn't work well with -vm_reserve where the kernel will
+         * auto-expand the stack into adjacent allocations below the stack.
+         */
+        heap_error_code_t error_code;
+        if (vmm_heap_commit(guard, PAGE_SIZE, MEMPROT_READ|MEMPROT_WRITE, &error_code))
+            mark_page_as_guard(guard);
+#else
+        /* For UNIX we just mark it as inaccessible. */
+        if (!DYNAMO_OPTION(guard_pages))
+            make_unwritable(guard, PAGE_SIZE);
 #endif
+    }
 
     RSTATS_ADD_PEAK(stack_capacity, size);
     /* stack grows from high to low */
@@ -2501,36 +2511,42 @@ stack_alloc(size_t size, byte *min_addr)
 void
 stack_free(void *p, size_t size)
 {
+    size_t alloc_size;
     if (size == 0)
         size = DYNAMORIO_STACK_SIZE;
+    alloc_size = size;
     p = (void *) ((vm_addr_t)p - size);
-    release_guarded_real_memory((vm_addr_t)p, size, true/*update DR areas immediately*/,
-                                true, VMM_STACK);
+    if (!DYNAMO_OPTION(guard_pages) && DYNAMO_OPTION(stack_guard_pages)) {
+        alloc_size += PAGE_SIZE;
+        p = (byte *)p - PAGE_SIZE;
+    }
+    release_guarded_real_memory((vm_addr_t)p, alloc_size,
+                                true/*update DR areas immediately*/, true, VMM_STACK);
     if (IF_DEBUG_ELSE(!dynamo_exited_log_and_stats, true))
         RSTATS_SUB(stack_capacity, size);
 }
 
-#ifdef STACK_GUARD_PAGE
 /* only checks initstack and current dcontext
  * does not check any dstacks on the callback stack (win32) */
 bool
 is_stack_overflow(dcontext_t *dcontext, byte *sp)
 {
     /* ASSUMPTION: size of stack is DYNAMORIO_STACK_SIZE = dynamo_options.stack_size
-     * currently sideline violates that for a thread stack
-     * but all dstacks and initstack should be this size
+     * Currently sideline violates that for a thread stack, and we have separated
+     * -signal_stack_size, but all dstacks and initstack should be this size.
      */
     byte *bottom = dcontext->dstack - DYNAMORIO_STACK_SIZE;
+    if (!DYNAMO_OPTION(stack_guard_pages))
+        return false;
     /* see if in bottom guard page of dstack */
-    if (sp >= bottom && sp < bottom + (STACK_GUARD_PAGES * PAGE_SIZE))
+    if (sp >= bottom - PAGE_SIZE && sp < bottom)
         return true;
     /* now check the initstack */
     bottom = initstack - DYNAMORIO_STACK_SIZE;
-    if (sp >= bottom && sp < bottom + (STACK_GUARD_PAGES * PAGE_SIZE))
+    if (sp >= bottom - PAGE_SIZE && sp < bottom)
         return true;
     return false;
 }
-#endif
 
 byte *
 map_file(file_t f, size_t *size INOUT, uint64 offs, app_pc addr, uint prot,

core/heap.h
@@ -192,10 +192,8 @@ heap_mmap_retract_commitment(void *retract_start, size_t decommit_size,
 void *stack_alloc(size_t size, byte *min_addr);
 void stack_free(void *p, size_t size);
 
-#ifdef STACK_GUARD_PAGE
 /* checks if pc is in guard page on stack */
 bool is_stack_overflow(dcontext_t *dcontext, byte *sp);
-#endif
 
 /* these are for thread-local allocs
  * passing dcontext == GLOBAL_DCONTEXT will end up calling global_heap_{alloc,free}

core/options.c
@@ -1560,6 +1560,12 @@ check_option_compatibility_helper(int recurse_count)
     }
 
 #ifdef WINDOWS
+    if (DYNAMO_OPTION(stack_guard_pages)) {
+        /* XXX i#2595: this does not interact well with -vm_reserve. */
+        USAGE_ERROR("-stack_guard_pages is not supported on Windows");
+        dynamo_options.stack_guard_pages = false;
+        changed_options = true;
+    }
 # ifdef PROGRAM_SHEPHERDING
     if (DYNAMO_OPTION(IAT_convert) && !DYNAMO_OPTION(emulate_IAT_writes)) {
         /* FIXME: case 1948 we should in fact depend on emulate_IAT_read */

commit 5c136ac7958404928e1489faef4035d71dc9dbf8

i#2592 stack overflow: fix VPS build
Fixes the VPS Windows build broken by 240b75d6.

core/win32/callback.c
@@ -4858,13 +4858,14 @@ void
 internal_exception_info(dcontext_t *dcontext, EXCEPTION_RECORD *pExcptRec,
                         CONTEXT *cxt, bool dstack_overflow, bool is_client)
 {
-    report_internal_exception(dcontext, pExcptRec, cxt,
-                              (is_client ? DUMPCORE_CLIENT_EXCEPTION :
-                               DUMPCORE_INTERNAL_EXCEPTION) |
-                              (dstack_overflow ? DUMPCORE_STACK_OVERFLOW : 0),
-                              /* for clients we need to let them override the label */
-                              is_client ? exception_label_client : exception_label_core,
-                              dstack_overflow ? STACK_OVERFLOW_NAME : CRASH_NAME);
+    report_internal_exception
+        (dcontext, pExcptRec, cxt,
+         (IF_CLIENT_INTERFACE(is_client ? DUMPCORE_CLIENT_EXCEPTION :)
+          DUMPCORE_INTERNAL_EXCEPTION) |
+         (dstack_overflow ? DUMPCORE_STACK_OVERFLOW : 0),
+         /* for clients we need to let them override the label */
+         IF_CLIENT_INTERFACE(is_client ? exception_label_client :) exception_label_core,
+         dstack_overflow ? STACK_OVERFLOW_NAME : CRASH_NAME);
 }
 
 static void