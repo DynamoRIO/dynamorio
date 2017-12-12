a4df50fd47d06d662041dabd3ca7a9a84406ea35
Snehasish Kumar <snehasishk@google.com>
2017-12-12 10:27:35 -0800

    Fixed brace style.

--- ext/droption/droption.h
+++ ext/droption/droption.h
@@ -453,7 +453,7 @@ droption_t<unsigned int>::convert_from_string(const std::string s)
     return true;
 }
 template<> inline bool
-droption_t<double>::convert_from_string(const std::string s) 
+droption_t<double>::convert_from_string(const std::string s)
 {
     // strtod will return 0.0 for invalid conversions
     char * pEnd = NULL;
