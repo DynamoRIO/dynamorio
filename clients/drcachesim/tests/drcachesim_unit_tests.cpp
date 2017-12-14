c30d1648f006dddfdde1c7c2abbd6b63ef22a464
Snehasish Kumar <snehasishk@google.com>
2017-12-13 17:39:04 -0800

    Removed trailing whitespace.

--- clients/drcachesim/tests/drcachesim_unit_tests.cpp
+++ clients/drcachesim/tests/drcachesim_unit_tests.cpp
@@ -30,59 +30,59 @@
  * DAMAGE.
  */
 
-// Unit tests for drcachesim 
+// Unit tests for drcachesim
 #include <iostream>
 #include <cstdlib>
 #include "simulator/cache_simulator.h"
 #include "../common/memref.h"
 
-void  
+void
 unit_test_warmup_fraction() {
     cache_simulator_t * cache_sim = new cache_simulator_t(1, 64, 32*64, 32*64,
                                                           32, 32, 32*64,
-                                                          32, "", "LRU", "none", 
+                                                          32, "", "LRU", "none",
                                                           0, 0, 0.5, 1ULL << 63, 0);
 
     // Feed it some memrefs, warmup fraction is set to 0.5 where the capacity at
     // each level is 32 lines each. The first 16 memrefs warm up the cache and
     // the 17th allows us to check for the warmup_fraction.
     for(int i = 0; i < 16 + 1; i++) {
-        memref_t ref = { .data = { .type = TRACE_TYPE_READ, .pid = 0, 
+        memref_t ref = { .data = { .type = TRACE_TYPE_READ, .pid = 0,
                                    .tid = 0, .addr = 0, .size = 8, .pc = 0} };
         ref.data.addr = i * 128;
         cache_sim->process_memref(ref);
     }
-   
+
     if (!cache_sim->check_warmed_up()) {
         std::cerr << "drcachesim unit_test_warmup_fraction failed\n";
         exit(1);
     }
-      
+
     delete cache_sim;
 }
 
-void 
+void
 unit_test_warmup_refs() {
     cache_simulator_t * cache_sim = new cache_simulator_t(1, 64, 32*64, 32*64,
                                                           32, 32, 32*64,
-                                                          32, "", "LRU", "none", 
+                                                          32, "", "LRU", "none",
                                                           0, 16, 0.0, 1ULL << 63, 0);
 
     // Feed it some memrefs, warmup refs = 16 where the capacity at
     // each level is 32 lines each. The first 16 memrefs warm up the cache and
     // the 17th allows us to check.
     for(int i = 0; i < 16 + 1; i++) {
-        memref_t ref = { .data = { .type = TRACE_TYPE_READ, .pid = 0, 
+        memref_t ref = { .data = { .type = TRACE_TYPE_READ, .pid = 0,
                                    .tid = 0, .addr = 0, .size = 8, .pc = 0} };
         ref.data.addr = i * 128;
         cache_sim->process_memref(ref);
     }
-   
+
     if (!cache_sim->check_warmed_up()) {
         std::cerr << "drcachesim unit_test_warmup_refs failed\n";
         exit(1);
     }
-      
+
     delete cache_sim;
 }
 
