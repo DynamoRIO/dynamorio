52a3442bf3da4e41002a90467906c855d30316ad
johnfxgalea <john.galea@stx.ox.ac.uk>
2017-12-19 16:33:44 +0000

    Removed another trailing white-space.

--- ext/drcontainers/hashtable.h
+++ ext/drcontainers/hashtable.h
@@ -195,7 +195,7 @@ hashtable_remove_range(hashtable_t *table, void *start, void *end);
 
 /**
  * Calls the apply_func for each payload
- * 
+ *
  * @param apply_func A pointer to a function that is called for all payloads in
  * stored in the map.
  */
