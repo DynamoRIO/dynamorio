d59f044bb00153d2a54d689020142738fcb879f3
Nilay Vaish <nilayvaish@google.com>
2017-12-28 11:15:23 -0800

    Moves strncat so that it is visible before use.
    On MacOS, we observed build failure since strncat was defined after it
    had been used.  For other operating systems, a different path is being
    taken.  So they were working fine.

--- core/string.c
+++ core/string.c
@@ -129,6 +129,18 @@ strncpy(char *dst, const char *src, size_t n)
     return dst;
 }
 
+/* Private strncat. */
+char *
+strncat(char *dest, const char *src, size_t n)
+{
+    size_t dest_len = strlen(dest);
+    size_t i;
+    for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[dest_len + i] = src[i];
+    dest[dest_len + i] = '\0';
+    return dest;
+}
+
 /* Private memcpy is in arch/<arch>/<arch>.asm */
 
 /* Private memset is in arch/<arch>/<arch>.asm */
@@ -195,18 +207,6 @@ __strncat_chk(char *dst, const char *src, size_t n, size_t dst_len)
     __attribute__((alias("strncat")));
 # endif
 #endif
-
-/* Private strncat. */
-char *
-strncat(char *dest, const char *src, size_t n)
-{
-    size_t dest_len = strlen(dest);
-    size_t i;
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[dest_len + i] = src[i];
-    dest[dest_len + i] = '\0';
-    return dest;
-}
 
 /* Private strcmp. */
 int
