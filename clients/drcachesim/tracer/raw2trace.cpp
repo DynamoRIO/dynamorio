DynamoRIO/dynamorio

commit aedf9be221d6d3a1fc4b980a2e9e1a1398b50ca5

i#2843 trace cpus: add cpu and timestamp trace markers (#2849)
Adds a timestamp marker and a cpu marker to the trace buffer header for
each thread's buffer unit output.  The timestamp was already in the raw
offline trace format, but now it is in the final trace for both offline and
online as a new marker type.  The cpu is completely new and specifies which
core that thread executed on at the granularity of the buffer unit.

Refactors the initial and per-output buffer headers to fix warts in the
tracer: now the initial buffer's timestamp is from output time rather than
thread init time; the initial header is more cleanly skipped for virt2phys;
header uses are more normalized and easier to understand.

Updates the basic_counts tool to separate these new scheduling marks from
kernel transfer and other markers.  Updates the corresponding
documentation.

A forthcoming change will update the cache simulator to schedule threads
based on executed cores rather than a thread round-robin scheme.

Issue: #2843

clients/drcachesim/tracer/raw2trace.cpp
@@ -597,9 +597,28 @@ raw2trace_t::merge_and_process_thread_files()
     offline_entry_t in_entry;
     online_instru_t instru(NULL, false, NULL);
     bool last_bb_handled = true;
+    size_t size;
     std::vector<thread_id_t> tids(thread_files.size(), INVALID_THREAD_ID);
+    std::vector<process_id_t> pids(thread_files.size(), (process_id_t)INVALID_PROCESS_ID);
+    std::vector<bool> wrote_pid(thread_files.size(), false);
     std::vector<uint64> times(thread_files.size(), 0);
     byte buf_base[MAX_COMBINED_ENTRIES * sizeof(trace_entry_t)];
+
+    // First read the tid and pid entries which precede any timestamps.
+    // We append the tid to the output on every thread switch, and the pid
+    // the very first time (using wrote_pid[]).
+    for (uint i=0; i<thread_files.size(); ++i) {
+        if (!read_from_thread_file(i, &in_entry, 1))
+            return "Failed to read header from input file";
+        DR_ASSERT(in_entry.tid.type == OFFLINE_TYPE_THREAD);
+        VPRINT(2, "File %u is thread %u\n", i, (uint)in_entry.tid.tid);
+        tids[i] = in_entry.tid.tid;
+        if (!read_from_thread_file(i, &in_entry, 1))
+            return "Failed to read header from input file";
+        DR_ASSERT(in_entry.pid.type == OFFLINE_TYPE_PID);
+        VPRINT(2, "File %u is process %u\n", i, (uint)in_entry.pid.pid);
+        pids[i] = in_entry.pid.pid;
+    }
 
     // We read the thread files simultaneously in lockstep and merge them into
     // a single output file in timestamp order.
@@ -607,7 +626,6 @@ raw2trace_t::merge_and_process_thread_files()
     // We convert each offline entry into a trace_entry_t.
     // We fill in instr entries and memref type and size.
     do {
-        int size = 0;
         byte *buf = buf_base;
         if (tidx >= thread_files.size()) {
             // Pick the next thread by looking for the smallest timestamp.
@@ -632,20 +650,24 @@ raw2trace_t::merge_and_process_thread_files()
             VPRINT(2, "Next thread in timestamp order is %u @0x" ZHEX64_FORMAT_STRING
                    "\n", (uint)tids[next_tidx], times[next_tidx]);
             tidx = next_tidx;
+            // Write out the tid (and pid for the first entry).
+            DR_ASSERT(tids[tidx] != INVALID_THREAD_ID);
+            buf += instru.append_tid(buf, tids[tidx]);
+            if (!wrote_pid[tidx]) {
+                DR_ASSERT(pids[tidx] != (process_id_t)INVALID_PROCESS_ID);
+                buf += instru.append_pid(buf, pids[tidx]);
+                wrote_pid[tidx] = true;
+            }
+            buf += instru.append_marker(buf, TRACE_MARKER_TYPE_TIMESTAMP,
+                                        // Truncated for 32-bit, as documented.
+                                        (uintptr_t)times[tidx]);
+            // We have to write this now before we append any bb entries.
+            size = buf - buf_base;
+            CHECK((uint)size < MAX_COMBINED_ENTRIES, "Too many entries");
+            if (!out_file->write((char*)buf_base, size))
+                return "Failed to write to output file";
+            buf = buf_base;
             times[tidx] = 0; // Read from file for this thread's next timestamp.
-            if (tids[tidx] != INVALID_THREAD_ID) {
-                // The initial read from a file may not have seen its tid entry
-                // yet.  We expect to hit that entry next.
-                size += instru.append_tid(buf, tids[tidx]);
-            }
-            if (size > 0) {
-                // We have to write this now before we append any bb entries.
-                CHECK((uint)size < MAX_COMBINED_ENTRIES, "Too many entries");
-                if (!out_file->write((char*)buf_base, size))
-                    return "Failed to write to output file";
-                buf = buf_base;
-            }
-            size = 0;
         }
         VPRINT(4, "About to read thread %d at pos %d\n",
                (uint)tids[tidx], (int)thread_files[tidx]->tellg());
@@ -681,19 +703,16 @@ raw2trace_t::merge_and_process_thread_files()
                     return "Footer is not the final entry";
                 CHECK(tids[tidx] != INVALID_THREAD_ID, "Missing thread id");
                 VPRINT(2, "Thread %d exit\n", (uint)tids[tidx]);
-                size += instru.append_thread_exit(buf, tids[tidx]);
-                buf += size;
+                buf += instru.append_thread_exit(buf, tids[tidx]);
                 --thread_count;
                 tidx = (uint)thread_files.size(); // Request thread scan.
             } else if (in_entry.extended.ext == OFFLINE_EXT_TYPE_MARKER) {
-                trace_entry_t *entry = (trace_entry_t *) buf;
-                entry->type = TRACE_TYPE_MARKER;
-                entry->size = in_entry.extended.valueB;
-                entry->addr = (addr_t) in_entry.extended.valueA;
-                VPRINT(3, "Appended marker type %u value %zu\n", entry->size,
-                       entry->addr);
-                size += sizeof(*entry);
-                buf += size;
+                buf += instru.append_marker(buf,
+                                            (trace_marker_type_t)in_entry.extended.valueB,
+                                            (uintptr_t)in_entry.extended.valueA);
+                VPRINT(3, "Appended marker type %u value %zu\n",
+                       (trace_marker_type_t)in_entry.extended.valueB,
+                       (uintptr_t)in_entry.extended.valueA);
             } else {
                 std::stringstream ss;
                 ss << "Invalid extension type " << (int)in_entry.extended.ext;
@@ -710,8 +729,7 @@ raw2trace_t::merge_and_process_thread_files()
                 entry->addr = (addr_t) in_entry.combined_value;
                 VPRINT(4, "Appended non-module memref to " PFX "\n",
                        (ptr_uint_t)entry->addr);
-                size += sizeof(*entry);
-                buf += size;
+                buf += sizeof(*entry);
             } else {
                 // We should see an instr entry first
                 return "memref entry found outside of bb";
@@ -720,16 +738,6 @@ raw2trace_t::merge_and_process_thread_files()
             result = append_bb_entries(tidx, &in_entry, &last_bb_handled);
             if (!result.empty())
                 return result;
-        } else if (in_entry.tid.type == OFFLINE_TYPE_THREAD) {
-            VPRINT(2, "Thread %u entry\n", (uint)in_entry.tid.tid);
-            if (tids[tidx] == INVALID_THREAD_ID)
-                tids[tidx] = in_entry.tid.tid;
-            size += instru.append_tid(buf, in_entry.tid.tid);
-            buf += size;
-        } else if (in_entry.pid.type == OFFLINE_TYPE_PID) {
-            VPRINT(2, "Process %u entry\n", (uint)in_entry.pid.pid);
-            size += instru.append_pid(buf, in_entry.pid.pid);
-            buf += size;
         } else if (in_entry.addr.type == OFFLINE_TYPE_IFLUSH) {
             offline_entry_t entry;
             if (!read_from_thread_file(tidx, &entry, 1) ||
@@ -737,15 +745,15 @@ raw2trace_t::merge_and_process_thread_files()
                 return "Flush missing 2nd entry";
             VPRINT(2, "Flush " PFX"-" PFX"\n", (ptr_uint_t)in_entry.addr.addr,
                    (ptr_uint_t)entry.addr.addr);
-            size += instru.append_iflush(buf, in_entry.addr.addr,
-                                         (size_t)(entry.addr.addr - in_entry.addr.addr));
-            buf += size;
+            buf += instru.append_iflush(buf, in_entry.addr.addr,
+                                        (size_t)(entry.addr.addr - in_entry.addr.addr));
         } else {
             std::stringstream ss;
             ss << "Unknown trace type " << (int)in_entry.timestamp.type;
             return ss.str();
         }
-        if (size > 0) {
+        if (buf > buf_base) {
+            size_t size = buf - buf_base;
             CHECK((uint)size < MAX_COMBINED_ENTRIES, "Too many entries");
             if (!out_file->write((char*)buf_base, size))
                 return "Failed to write to output file";

clients/drcachesim/common/memref.h
@@ -87,7 +87,8 @@ struct _memref_thread_exit_t {
 
 /**
  * A trace entry containing metadata identifying some event that occurred at this
- * point in the trace.  The most common marker type represents a kernel-mediated
+ * point in the trace.  Common markers include timestamp and cpu information for
+ * certain points in the trace.  Another marker type represents a kernel-mediated
  * control flow change such as a signal delivery, entry into an APC, callback, or
  * exception dispatcher on Windows, or a system call that changes the context such as
  * a signal return.

clients/drcachesim/common/trace_entry.h
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2015-2017 Google, Inc.  All rights reserved.
+ * Copyright (c) 2015-2018 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
@@ -178,6 +178,17 @@ typedef enum {
      * NtSetContextThread on Windows.
      */
     TRACE_MARKER_TYPE_KERNEL_XFER,
+    /**
+     * The marker value contains a timestamp for this point in the trace, in units
+     * of microseconds since Jan 1, 1601 (the UTC time).  For 32-bit, the value
+     * is truncated to 32 bits.
+     */
+    TRACE_MARKER_TYPE_TIMESTAMP,
+    /**
+     * The marker value contains the cpu identifier of the cpu this thread was running
+     * on at this point in the trace.
+     */
+    TRACE_MARKER_TYPE_CPU_ID,
 
     // ...
     // These values are reserved for future built-in marker types.
@@ -214,7 +225,8 @@ type_is_prefetch(const trace_type_t type)
         type == TRACE_TYPE_HARDWARE_PREFETCH;
 }
 
-// This is the data format generated by the online tracer.
+// This is the data format generated by the online tracer and produced after
+// post-processing of raw offline traces.
 // The reader_t class transforms this into memref_t before handing to analysis tools.
 // Each trace entry is a <type, size, addr> tuple representing:
 // - a memory reference

clients/drcachesim/drcachesim.dox.in
@@ -303,34 +303,42 @@ Estimation of pi is 3.142425985001098
 ---- <application exited with code 0> ----
 Basic counts tool results:
 Total counts:
-      260951 total (fetched) instructions
-        6543 total non-fetched instructions
+      267193 total (fetched) instructions
+         345 total non-fetched instructions
            0 total prefetches
-       71754 total data loads
-       28828 total data stores
-           0 total markers
+       67686 total data loads
+       22503 total data stores
            3 total threads
-Thread 32370 counts:
-      250579 (fetched) instructions
-        6543 non-fetched instructions
+         280 total scheduling markers
+           0 total transfer markers
+           0 total other markers
+Thread 247451 counts:
+      255009 (fetched) instructions
+         345 non-fetched instructions
            0 prefetches
-       68565 data loads
-       27694 data stores
-           0 markers
-Thread 32371 counts:
-        8729 (fetched) instructions
+       64453 data loads
+       21243 data stores
+         258 scheduling markers
+           0 transfer markers
+           0 other markers
+Thread 247453 counts:
+        9195 (fetched) instructions
            0 non-fetched instructions
            0 prefetches
-        2673 data loads
-         926 data stores
-           0 markers
-Thread 32372 counts:
-        1643 (fetched) instructions
+        2444 data loads
+         937 data stores
+          12 scheduling markers
+           0 transfer markers
+           0 other markers
+Thread 247454 counts:
+        2989 (fetched) instructions
            0 non-fetched instructions
            0 prefetches
-         516 data loads
-         208 data stores
-           0 markers
+         789 data loads
+         323 data stores
+          10 scheduling markers
+           0 transfer markers
+           0 other markers
 \endcode
 
 The non-fetched instructions are x86 string loop instructions, where
@@ -602,9 +610,10 @@ trace and calls the analysis_tool_t::process_memref() function of each
 tool.  An alternative mode is supported which exposes the iterator and
 allows a separate control infrastructure to be built.
 
-Each trace entry is of type #memref_t
-and represents one instruction or data reference or a metadata operation
-such as a thread exit or marker.  There are built-in markers indicating
+Each trace entry is of type #memref_t and represents one instruction or
+data reference or a metadata operation such as a thread exit or marker.
+There are built-in scheduling markers providing the timestamp and cpu
+identifier on each thread transition.  Other built-in markers indicate
 disruptions in user mode control flow such as signal handler entry and
 exit.
 

clients/drcachesim/tests/basic_counts.templatex
@@ -25,20 +25,24 @@ Total counts:
      .* total prefetches
      .* total data loads
      .* total data stores
-#if defined(WINDOWS) && !defined(X64)
-          26 total markers
-#else
-          13 total markers
-#endif
            1 total threads
+     .* total scheduling markers
+#if defined(WINDOWS) && !defined(X64)
+          26 total transfer markers
+#else
+          13 total transfer markers
+#endif
+           0 total other markers
 Thread .* counts:
      .* \(fetched\) instructions
      .* non-fetched instructions
      .* prefetches
      .* data loads
      .* data stores
+     .* scheduling markers
 #if defined(WINDOWS) && !defined(X64)
-          26 markers
+          26 transfer markers
 #else
-          13 markers
+          13 transfer markers
 #endif
+           0 other markers

clients/drcachesim/tests/offline-basic_counts.templatex
@@ -24,20 +24,24 @@ Total counts:
      .* total prefetches
      .* total data loads
      .* total data stores
-#if defined(WINDOWS) && !defined(X64)
-          26 total markers
-#else
-          13 total markers
-#endif
            1 total threads
+     .* total scheduling markers
+#if defined(WINDOWS) && !defined(X64)
+          26 total transfer markers
+#else
+          13 total transfer markers
+#endif
+           0 total other markers
 Thread .* counts:
      .* \(fetched\) instructions
      .* non-fetched instructions
      .* prefetches
      .* data loads
      .* data stores
+     .* scheduling markers
 #if defined(WINDOWS) && !defined(X64)
-          26 markers
+          26 transfer markers
 #else
-          13 markers
+          13 transfer markers
 #endif
+           0 other markers

clients/drcachesim/tests/offline-burst_threadL0filter.templatex
@@ -18,5 +18,7 @@ Total counts:
     *[0-9]* total prefetches
     *[0-9]* total data loads
     *[0-9]* total data stores
-           0 total markers
            4 total threads
+    *[0-9]* total scheduling markers
+    *[0-9]* total transfer markers
+           0 total other markers

clients/drcachesim/tests/offline-burst_threadfilter.templatex
@@ -18,6 +18,8 @@ Total counts:
     *[0-9]* total prefetches
     *[0-9]* total data loads
     *[0-9]* total data stores
-           0 total markers
            4 total threads
+    *[0-9]* total scheduling markers
+    *[0-9]* total transfer markers
+           0 total other markers
 .*

commit 3167e1e48028915b84217cc12b5f294aec283e0a

i#2843 trace cpus: support legacy headers
The trace header refactoring in aedf9be broke post-processing of legacy
trace headers where the first timestamp precedes the first thread id.  We
fix that here.

Issue: #2843

clients/drcachesim/tracer/raw2trace.cpp
@@ -610,6 +610,12 @@ raw2trace_t::merge_and_process_thread_files()
     for (uint i=0; i<thread_files.size(); ++i) {
         if (!read_from_thread_file(i, &in_entry, 1))
             return "Failed to read header from input file";
+        // Handle legacy traces which have the timestamp first.
+        if (in_entry.tid.type == OFFLINE_TYPE_TIMESTAMP) {
+            times[i] = in_entry.timestamp.usec;
+            if (!read_from_thread_file(i, &in_entry, 1))
+                return "Failed to read header from input file";
+        }
         DR_ASSERT(in_entry.tid.type == OFFLINE_TYPE_THREAD);
         VPRINT(2, "File %u is thread %u\n", i, (uint)in_entry.tid.tid);
         tids[i] = in_entry.tid.tid;