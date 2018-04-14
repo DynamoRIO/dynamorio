dbf63aace5a7c0b55a88efa8ae224943d583512e
Mircea Trofin <mtrofin@google.com>
2018-04-13 18:54:55 -0700

    i#2928 expose stats: add API for fetching num bbs (#2926)
    Adds an API for fetching statistics from DR, initially populated with the number of basic blocks.
    
    Adds a test piggy-backing on api/static_prepop.
    
    Issue: #2928

--- suite/tests/api/static_prepop.c
+++ suite/tests/api/static_prepop.c
@@ -112,9 +112,13 @@ main(int argc, const char *argv[])
     print("pre-DR init\n");
     dr_app_setup();
     assert(!dr_app_running_under_dynamorio());
-
+    dr_stats_t stats = { sizeof(dr_stats_t) };
+    assert(dr_get_stats(&stats));
+    assert(stats.basic_block_count == 0);
     success = dr_prepopulate_cache(tags, sizeof(tags)/sizeof(tags[0]));
     assert(success);
+    assert(dr_get_stats(&stats));
+    assert(stats.basic_block_count > 0);
 
     print("pre-DR start\n");
     dr_app_start();
