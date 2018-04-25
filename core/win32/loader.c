ba724085138408b6aeb060d15f7a6661767264ab
Alberto Garcia Illera <agarciaillera@gmail.com>
2018-04-25 04:23:49 +0200

    i#2953: Add missing API-MS-Win-Core-Atoms pseudo-dll (#2959)
    Adds a missing pseudo-dll API-MS-Win-Core-Atoms which maps to kernel32.dll.
    
    Fixes #2953

--- core/win32/loader.c
+++ core/win32/loader.c
@@ -1531,7 +1531,8 @@ map_api_set_dll(const char *name, privmod_t *dependent)
              str_case_prefix(name, "API-MS-Win-Core-VersionAnsi-L1-1") ||
              str_case_prefix(name, "API-MS-Win-Eventing-Provider-L1-1"))
         return "kernelbase.dll";
-    else if (str_case_prefix(name, "API-MS-Win-Core-PrivateProfile-L1-1"))
+    else if (str_case_prefix(name, "API-MS-Win-Core-PrivateProfile-L1-1") ||
+             str_case_prefix(name, "API-MS-Win-Core-Atoms-L1-1"))
         return "kernel32.dll";
     else if (str_case_prefix(name, "API-MS-Win-Core-WinRT-Error-L1-1"))
         return "combase.dll";
