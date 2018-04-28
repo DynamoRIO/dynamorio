32703cd32421f377308f5b9b8821afc5ca0cdd56
Mircea Trofin <mtrofin@google.com>
2018-04-27 19:40:25 -0700

    API to collect stats at detach time. (#2958)
    We clear the stats when calling dr_app_stop_and_cleanup. This means
    that a client wishing to consume stats programmatically could at most
    try and get them right before detach, which would risk missing some
    last-moment data.
    
    We introduce dr_app_stop_and_cleanup_with_stats for this purpose.
    We add a test to api.static_prepop.

--- core/lib/instrument.c
+++ core/lib/instrument.c
@@ -7621,15 +7621,7 @@ DR_API
 bool
 dr_get_stats(dr_stats_t *drstats)
 {
-    if (!GLOBAL_STATS_ON())
-        return false;
-    CLIENT_ASSERT(drstats != NULL, "Expected non-null value for parameter drstats.");
-    /* We are at V1 of the structure, and we can't return less than the one
-     * field. We need to remove this assert when we add more fields.
-     */
-    CLIENT_ASSERT(drstats->size >= sizeof(dr_stats_t), "Invalid drstats->size value.");
-    drstats->basic_block_count = GLOBAL_STAT(num_bbs);
-    return true;
+    return stats_get_snapshot(drstats);
 }
 
 
