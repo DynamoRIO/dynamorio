bc963bf185fb8de5e75d2bc69ab474b0ab9add00
Florian Hahn <florian.hahn@arm.com>
2018-05-23 10:10:27 +0100

    Undo formatting changes, to avoid another re-formatting round
    This way, we won't have 2 commits changing the formatting here, if we
    change some settings before the big re-formatting.
    
    Change-Id: I59a7245f0c2c005c21f3f366034f0f87fc647912

--- core/arch/aarch64/instr.c
+++ core/arch/aarch64/instr.c
@@ -79,15 +79,15 @@ instr_branch_type(instr_t *cti_instr)
     case OP_cbz:
     case OP_tbnz:
     case OP_tbz:
-        return LINK_DIRECT | LINK_JMP;
+        return LINK_DIRECT|LINK_JMP;
     case OP_bl:
-        return LINK_DIRECT | LINK_CALL;
+        return LINK_DIRECT|LINK_CALL;
     case OP_blr:
-        return LINK_INDIRECT | LINK_CALL;
+        return LINK_INDIRECT|LINK_CALL;
     case OP_br:
-        return LINK_INDIRECT | LINK_JMP;
+        return LINK_INDIRECT|LINK_JMP;
     case OP_ret:
-        return LINK_INDIRECT | LINK_RETURN;
+        return LINK_INDIRECT|LINK_RETURN;
     }
     CLIENT_ASSERT(false, "instr_branch_type: unknown opcode");
     return LINK_INDIRECT;
@@ -146,8 +146,8 @@ instr_is_cbr_arch(instr_t *instr)
 {
     int opc = instr->opcode; /* caller ensures opcode is valid */
     return (opc == OP_bcond || /* clang-format: keep */
-            opc == OP_cbnz || opc == OP_cbz || /* clang-format: keep */
-            opc == OP_tbnz || opc == OP_tbz);
+            opc == OP_cbnz || opc ==  OP_cbz ||  /* clang-format: keep */
+            opc == OP_tbnz || opc ==  OP_tbz);
 }
 
 bool
@@ -237,7 +237,8 @@ instr_is_mov_constant(instr_t *instr, ptr_int_t *value)
         opnd_t reg = instr_get_src(instr, 0);
         opnd_t imm = instr_get_src(instr, 1);
         if (opnd_is_reg(reg) &&
-            (opnd_get_reg(reg) == DR_REG_WZR || opnd_get_reg(reg) == DR_REG_XZR) &&
+            (opnd_get_reg(reg) == DR_REG_WZR ||
+             opnd_get_reg(reg) == DR_REG_XZR) &&
             opnd_is_immed_int(imm)) {
             *value = opnd_get_immed_int(imm);
             return true;
@@ -295,7 +296,8 @@ instr_is_undefined(instr_t *instr)
      * unallocated encodings, but for testing purposes we can recognise
      * some of them: blocks at the top and bottom of the encoding space.
      */
-    if (instr_opcode_valid(instr) && instr_get_opcode(instr) == OP_xx) {
+    if (instr_opcode_valid(instr) &&
+        instr_get_opcode(instr) == OP_xx) {
         uint enc = opnd_get_immed_int(instr_get_src(instr, 0));
         return ((enc & 0x18000000) == 0 || (~enc & 0xde000000) == 0);
     }
@@ -389,14 +391,16 @@ instr_create_nbyte_nop(dcontext_t *dcontext, uint num_bytes, bool raw)
 bool
 instr_reads_thread_register(instr_t *instr)
 {
-    return (instr_get_opcode(instr) == OP_mrs && opnd_is_reg(instr_get_src(instr, 0)) &&
+    return (instr_get_opcode(instr) == OP_mrs &&
+            opnd_is_reg(instr_get_src(instr, 0)) &&
             opnd_get_reg(instr_get_src(instr, 0)) == DR_REG_TPIDR_EL0);
 }
 
 bool
 instr_writes_thread_register(instr_t *instr)
 {
-    return (instr_get_opcode(instr) == OP_msr && instr_num_dsts(instr) == 1 &&
+    return (instr_get_opcode(instr) == OP_msr &&
+            instr_num_dsts(instr) == 1 &&
             opnd_is_reg(instr_get_dst(instr, 0)) &&
             opnd_get_reg(instr_get_dst(instr, 0)) == DR_REG_TPIDR_EL0);
 }
