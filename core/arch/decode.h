b2d0ebdaa5e05182ef5b18c22819ab93f28feaa5
Florian Hahn <florian.hahn@arm.com>
2018-07-03 18:34:18 +0100

    i#3044 SVE encoder: Add sve_int_bin_pred_log group. (#3078)
    This patch adds a new register type for SVE predicate registers
    (P0-P15) and support for the sve_int_bin_pred_log encoding group.
    
    The macros added follow the SVE assembly syntax, where the shared input
    and output register Zdn has to be supplied twice.
    
    Issue #3044

--- core/arch/decode.h
+++ core/arch/decode.h
@@ -322,6 +322,7 @@ enum {
     OPSZ_124, /**< 124 bytes. Needed for load/store of register lists. */
     OPSZ_128, /**< 128 bytes. Needed for load/store of register lists. */
     OPSZ_SCALABLE, /** Scalable size for SVE vector registers. */
+    OPSZ_SCALABLE_PRED, /** Scalable size for SVE predicate registers. */
 #ifdef AVOID_API_EXPORT
     /* Add new size here.  Also update size_names[] in decode_shared.c along with
      * the size routines in opnd_shared.c.
