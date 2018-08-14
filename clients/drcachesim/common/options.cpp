80ed5535cfef43e0548f75b9973f13e8fbbd978f
Louis Ye <yelouis@google.com>
2018-08-14 15:40:21 -0700

    i#3048 func-trace: fix format issue
    Fix format issue.
    
    Fixes #3048

--- clients/drcachesim/common/options.cpp
+++ clients/drcachesim/common/options.cpp
@@ -396,8 +396,8 @@ droption_t<std::string> op_record_heap_value(
     "malloc|0|1" OP_RECORD_FUNC_ITEM_SEP "free|1|1" OP_RECORD_FUNC_ITEM_SEP
     "tc_malloc|2|1" OP_RECORD_FUNC_ITEM_SEP "tc_free|3|1" OP_RECORD_FUNC_ITEM_SEP
     "__libc_malloc|4|1" OP_RECORD_FUNC_ITEM_SEP "__libc_free|5|1" OP_RECORD_FUNC_ITEM_SEP
-    "operator new|10|1" OP_RECORD_FUNC_ITEM_SEP "operator delete|11|1"
-    OP_RECORD_FUNC_ITEM_SEP "calloc|6|2",
+    "operator new|10|1" OP_RECORD_FUNC_ITEM_SEP
+    "operator delete|11|1" OP_RECORD_FUNC_ITEM_SEP "calloc|6|2",
     "Functions recorded by -record_heap",
     "Functions recorded by -record_heap. The option value should fit the same"
     " format required by -record_function. These functions will not"
