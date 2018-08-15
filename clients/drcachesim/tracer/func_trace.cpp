fd63499c847c6a99d15a284db5f3c13eb002a94a
Louis Ye <yelouis@google.com>
2018-08-14 18:12:12 -0700

    i#3048 func-trace: Replace dr_fprint with NOTIFY, check null string
    Replace dr_fprint with NOTIFY level 1 to control output;
    Check if the string is NULL before printing.
    
    Fixes #3048

--- clients/drcachesim/tracer/func_trace.cpp
+++ clients/drcachesim/tracer/func_trace.cpp
@@ -49,6 +49,8 @@
 // where function_name can contain spaces (for instance, C++ namespace prefix)
 #define PATTERN_SEPARATOR "|"
 
+#define EMPTY_IF_NULL(str) ((str) == NULL ? "" : (str))
+
 #define NOTIFY(level, ...)                     \
     do {                                       \
         if (op_verbose.get_value() >= (level)) \
@@ -131,7 +133,8 @@ get_pc_by_symbol(const module_data_t *mod, const char *symbol)
     // Try to find the symbol in the dynamic symbol table.
     app_pc pc = (app_pc)dr_get_proc_address(mod->handle, symbol);
     if (pc != NULL) {
-        dr_fprintf(STDERR, "dr_get_proc_address found symbol %s at pc=%p\n", symbol, pc);
+        NOTIFY(1, "dr_get_proc_address found symbol %s at pc=%p\n", EMPTY_IF_NULL(symbol),
+               pc);
         return pc;
     } else {
         // If failed to find the symbol in the dynamic symbol table, then we try to find
@@ -145,12 +148,12 @@ get_pc_by_symbol(const module_data_t *mod, const char *symbol)
             drsym_lookup_symbol(mod->full_path, symbol, &offset, DRSYM_DEMANGLE);
         if (err == DRSYM_SUCCESS) {
             pc = mod->start + offset;
-            dr_fprintf(STDERR, "drsym_lookup_symbol found symbol %s at pc=%p\n", symbol,
-                       pc);
+            NOTIFY(1, "drsym_lookup_symbol found symbol %s at pc=%p\n",
+                   EMPTY_IF_NULL(symbol), pc);
             return pc;
         } else {
-            dr_fprintf(STDERR, "Failed to find symbol %s, drsym_error_t=%d\n", symbol,
-                       err);
+            NOTIFY(1, "Failed to find symbol %s, drsym_error_t=%d\n",
+                   EMPTY_IF_NULL(symbol), err);
             return NULL;
         }
     }
@@ -162,15 +165,17 @@ instru_funcs_module_load(void *drcontext, const module_data_t *mod, bool loaded)
     if (drcontext == NULL || mod == NULL)
         return;
 
-    dr_fprintf(STDERR, "instru_funcs_module_load, mod->full_path=%s\n", mod->full_path);
+    NOTIFY(1, "instru_funcs_module_load, mod->full_path=%s\n",
+           EMPTY_IF_NULL(mod->full_path));
     for (size_t i = 0; i < funcs.entries; i++) {
         func_metadata_t *f = (func_metadata_t *)drvector_get_entry(&funcs, (uint)i);
         app_pc f_pc = get_pc_by_symbol(mod, f->name);
         if (f_pc != NULL) {
             if (drwrap_wrap_ex(f_pc, func_pre_hook, func_post_hook, (void *)i, 0)) {
-                dr_fprintf(STDERR, "Inserted hooks for function %s\n", f->name);
+                NOTIFY(1, "Inserted hooks for function %s\n", EMPTY_IF_NULL(f->name));
             } else {
-                dr_fprintf(STDERR, "Failed to insert hooks for function %s\n", f->name);
+                NOTIFY(1, "Failed to insert hooks for function %s\n",
+                       EMPTY_IF_NULL(f->name));
             }
         }
     }
