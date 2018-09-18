c23819e87891e4bd84e6e84563c2303772870e2c
Assad Hashmi <assad.hashmi@arm.com>
2018-09-18 19:44:17 +0200

    Add TEST assert to instr_get_label_callback()

--- core/arch/instr_shared.c
+++ core/arch/instr_shared.c
@@ -168,9 +168,6 @@ instr_init(dcontext_t *dcontext, instr_t *instr)
 void
 instr_free(dcontext_t *dcontext, instr_t *instr)
 {
-    CLIENT_ASSERT(!TEST(INSTR_RAW_BITS_ALLOCATED, instr->flags),
-                  "instruction's raw bits occupying label callback memory");
-
     if (instr_is_label(instr) && instr_get_label_callback(instr) != NULL)
         (*instr->label_cb)(dcontext, instr);
     if ((instr->flags & INSTR_RAW_BITS_ALLOCATED) != 0) {
@@ -1132,6 +1129,8 @@ instr_get_label_callback(instr_t *instr)
 {
     CLIENT_ASSERT(instr_is_label(instr),
                   "only label instructions have a callback function");
+    CLIENT_ASSERT(!TEST(INSTR_RAW_BITS_ALLOCATED, instr->flags),
+                  "instruction's raw bits occupying label callback memory");
     return instr->label_cb;
 }
 
