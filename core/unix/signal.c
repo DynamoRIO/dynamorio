DynamoRIO/dynamorio

commit 49ee692223553f642ecd2ad28e5fcee4b51135df

i#3171: fix hang running Go programs (#3174)
Go's thread creation looks at the SYS_clone stack parameter
post-syscall, which DR was not restoring after replacing it with the
dstack.  We fix that here.

That bug led to Go programs using the dstack, resulting in them
executing DR memory, where DR tried to raise an artificial SIGSEGV.
However, this ended up in Go's signal handler where SIGSEGV was
blocked, and due to various #196 checks lining up incorrectly, this
led to an infinite loop.

To fully fix the infinite loop and get the SIGSEGV to the outer lever,
I changed os_forge_exception() to set SI_KERNEL and added a forged
flag into execute_default_action() and avoid trying to call
decode_sizeof() there (just crashes b/c SIGSEGV handler removed).  I
alo cleared the app's signal mask, to avoid SIGSEGV being blocked
during cleanup, causing the supposed-to-be-fatal SYS_kill to just
return, and another hang in debug build in debug_infinite_loop.

Issue: #196, #3171
Fixes #3171

core/unix/signal.c
@@ -257,7 +257,7 @@ execute_handler_from_dispatch(dcontext_t *dcontext, int sig);
  */
 static bool
 execute_default_from_cache(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
-                           sigcontext_t *sc_orig);
+                           sigcontext_t *sc_orig, bool forged);
 
 static void
 execute_default_from_dispatch(dcontext_t *dcontext, int sig, sigframe_rt_t *frame);
@@ -650,6 +650,8 @@ create_clone_record(dcontext_t *dcontext, reg_t *app_thread_xsp)
          * Note: it's glibc who sets up the arg to the thread start function;
          * the kernel just does a fork + stack swap, so we can get away w/ our
          * own stack swap if we restore before the glibc asm code takes over.
+         * We restore this parameter to the app value in
+         * restore_clone_param_from_clone_record().
          */
         /* i#754: set stack to be XSTATE aligned for saving YMM registers */
         ASSERT(ALIGNED(XSTATE_ALIGNMENT, REGPARM_END_ALIGN));
@@ -775,6 +777,20 @@ set_app_lib_tls_base_from_clone_record(dcontext_t *dcontext, void *record)
     }
 }
 #endif
+
+void
+restore_clone_param_from_clone_record(dcontext_t *dcontext, void *record)
+{
+    ASSERT(record != NULL);
+    clone_record_t *crec = (clone_record_t *)record;
+    if (crec->clone_sysnum == SYS_clone && TEST(CLONE_VM, crec->clone_flags)) {
+        /* Restore the original stack parameter to the syscall, which we clobbered
+         * in create_clone_record().  Some apps examine it post-syscall (i#3171).
+         */
+        set_syscall_param(dcontext, SYSCALL_PARAM_CLONE_STACK,
+                          get_mcontext(dcontext)->xsp);
+    }
+}
 
 /* Initializes info's app_sigaction, restorer_valid, and we_intercept fields */
 static void
@@ -3393,7 +3409,7 @@ handle_client_action_from_cache(dcontext_t *dcontext, int sig, dr_signal_action_
         LOG(THREAD, LOG_ASYNCH, 2, "%s: executing default action\n",
             (action == DR_SIGNAL_BYPASS) ? "client forcing default"
                                          : "app signal handler is SIG_DFL");
-        if (execute_default_from_cache(dcontext, sig, our_frame, sc_orig)) {
+        if (execute_default_from_cache(dcontext, sig, our_frame, sc_orig, false)) {
             /* if we haven't terminated, restore original (untranslated) sc
              * on request.
              */
@@ -4214,16 +4230,20 @@ record_pending_signal(dcontext_t *dcontext, int sig, kernel_ucontext_t *ucxt,
 #endif
 
         /* i#196/PR 453847: avoid infinite loop of signals if try to re-execute */
-        if (blocked && !forged && !can_always_delay[sig] &&
+        if (blocked && !can_always_delay[sig] &&
             !is_sys_kill(dcontext, pc, (byte *)sc->SC_XSP, &frame->info)) {
             ASSERT(default_action[sig] == DEFAULT_TERMINATE ||
                    default_action[sig] == DEFAULT_TERMINATE_CORE);
             LOG(THREAD, LOG_ASYNCH, 1,
                 "blocked fatal signal %d cannot be delayed: terminating\n", sig);
             sc_orig = *sc;
-            translate_sigcontext(dcontext, ucxt, true /*shouldn't fail*/, NULL);
+            /* If forged we're likely couldbelinking, and we don't need to xl8. */
+            if (forged)
+                ASSERT(is_couldbelinking(dcontext));
+            else
+                translate_sigcontext(dcontext, ucxt, true /*shouldn't fail*/, NULL);
             /* the process should be terminated */
-            execute_default_from_cache(dcontext, sig, frame, &sc_orig);
+            execute_default_from_cache(dcontext, sig, frame, &sc_orig, forged);
             ASSERT_NOT_REACHED();
         }
 
@@ -5112,7 +5132,7 @@ execute_handler_from_cache(dcontext_t *dcontext, int sig, sigframe_rt_t *our_fra
     if (info->app_sigaction[sig] == NULL ||
         info->app_sigaction[sig]->handler == (handler_t)SIG_DFL) {
         LOG(THREAD, LOG_ASYNCH, 3, "\taction is SIG_DFL\n");
-        if (execute_default_from_cache(dcontext, sig, our_frame, sc_orig)) {
+        if (execute_default_from_cache(dcontext, sig, our_frame, sc_orig, false)) {
             /* if we haven't terminated, restore original (untranslated) sc
              * on request.
              * XXX i#1615: this doesn't restore SIMD regs, if client translated them!
@@ -5435,10 +5455,15 @@ execute_handler_from_dispatch(dcontext_t *dcontext, int sig)
 }
 
 /* The arg to SYS_kill, i.e., the signal number, should be in dcontext->sys_param0 */
+/* This routine unblocks signals, but the caller must set the handler to default. */
 static void
 terminate_via_kill(dcontext_t *dcontext)
 {
+    thread_sig_info_t *info = (thread_sig_info_t *)dcontext->signal_field;
     ASSERT(dcontext == get_thread_private_dcontext());
+    unblock_all_signals(NULL);
+    /* Enure signal_thread_exit() will not re-block */
+    memset(&info->app_sigblocked, 0, sizeof(info->app_sigblocked));
 
     /* FIXME PR 541760: there can be multiple thread groups and thus
      * this may not exit all threads in the address space
@@ -5538,7 +5563,7 @@ os_terminate_via_signal(dcontext_t *dcontext, terminate_flags_t flags, int sig)
 
 static bool
 execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
-                       sigcontext_t *sc_orig, bool from_dispatch)
+                       sigcontext_t *sc_orig, bool from_dispatch, bool forged)
 {
     thread_sig_info_t *info = (thread_sig_info_t *)dcontext->signal_field;
     sigcontext_t *sc = get_sigcontext_from_rt_frame(frame);
@@ -5611,7 +5636,7 @@ execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
             /* N.B.: we don't have to restore our handler because the
              * default action is for the process (entire thread group for NPTL) to die!
              */
-            if (from_dispatch || can_always_delay[sig] ||
+            if (from_dispatch || can_always_delay[sig] || forged ||
                 is_sys_kill(dcontext, pc, (byte *)sc->SC_XSP, &frame->info)) {
                 /* This must have come from SYS_kill rather than raised by
                  * a faulting instruction.  Thus we can't go re-execute the
@@ -5625,7 +5650,8 @@ execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
                  * FIXME: should have app make the syscall to get a more
                  * transparent core dump!
                  */
-                if (!from_dispatch)
+                LOG(THREAD, LOG_ASYNCH, 1, "Terminating via kill\n");
+                if (!from_dispatch && !forged)
                     KSTOP_NOT_MATCHING_NOT_PROPAGATED(fcache_default);
                 KSTOP_NOT_MATCHING_NOT_PROPAGATED(dispatch_num_exits);
                 if (is_couldbelinking(dcontext)) /* won't be for SYS_kill (i#1159) */
@@ -5638,7 +5664,8 @@ execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
                  * re-raise the fault.  We could easily be wrong:
                  * xref PR 363811 infinite loop due to memory we
                  * thought was unreadable and thus thought would raise
-                 * a signal; xref PR 368277 to improve is_sys_kill().
+                 * a signal; xref PR 368277 to improve is_sys_kill(), and the
+                 * "forged" parameter that puts us in the if() above.
                  * FIXME PR 205310: we should check whether we come out of
                  * the cache when we expected to terminate!
                  *
@@ -5649,7 +5676,7 @@ execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
                  * signal death, but we do for asynch.
                  */
                 /* i#552: cleanup and raise client exit event */
-                int instr_sz;
+                int instr_sz = 0;
                 thread_sig_info_t *info;
                 /* We are on the sigstack now, so assign it to NULL to avoid being
                  * freed during process exit cleanup
@@ -5666,6 +5693,8 @@ execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
                  * raise the signal, so we first check if the app instr is the
                  * same as instr in the cache, and raise the signal (by return).
                  * Otherwise, we kill the process instead.
+                 * XXX: if the PC is unreadable we'll just crash here...should check
+                 * for readability safely.
                  */
                 ASSERT(sc_orig != NULL);
                 instr_sz = decode_sizeof(dcontext, (byte *)sc_orig->SC_XIP,
@@ -5677,12 +5706,14 @@ execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
                     /* the app instr matches the cache instr; cleanup and raise the
                      * the signal in the app context
                      */
+                    LOG(THREAD, LOG_ASYNCH, 1, "Raising signal by re-executing\n");
                     dynamo_process_exit();
                     /* we cannot re-enter the cache, which is freed by now */
                     ASSERT(!from_dispatch);
                     return false;
                 } else {
                     /* mismatch, cleanup and terminate */
+                    LOG(THREAD, LOG_ASYNCH, 1, "Terminating via kill\n");
                     dcontext->sys_param0 = sig;
                     terminate_via_kill(dcontext);
                     ASSERT_NOT_REACHED();
@@ -5727,15 +5758,15 @@ execute_default_action(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
 
 static bool
 execute_default_from_cache(dcontext_t *dcontext, int sig, sigframe_rt_t *frame,
-                           sigcontext_t *sc_orig)
+                           sigcontext_t *sc_orig, bool forged)
 {
-    return execute_default_action(dcontext, sig, frame, sc_orig, false);
+    return execute_default_action(dcontext, sig, frame, sc_orig, false, forged);
 }
 
 static void
 execute_default_from_dispatch(dcontext_t *dcontext, int sig, sigframe_rt_t *frame)
 {
-    execute_default_action(dcontext, sig, frame, NULL, true);
+    execute_default_action(dcontext, sig, frame, NULL, true, false);
 }
 
 void
@@ -6132,11 +6163,17 @@ os_forge_exception(app_pc target_pc, dr_exception_type_t type)
 
     LOG(GLOBAL, LOG_ASYNCH, 1, "os_forge_exception sig=%d\n", sig);
 
-    /* since we always delay delivery, we always want an rt frame.  we'll convert
+    /* Since we always delay delivery, we always want an rt frame.  we'll convert
      * to a plain frame on delivery.
      */
     memset(frame, 0, sizeof(*frame));
     frame->info.si_signo = sig;
+    /* Set si_code to match what would happen natively.  We also need this to
+     * avoid the !is_sys_kill() check in record_pending_signal() to avoid an
+     * infinite loop (i#3171).
+     */
+    frame->info.si_code = SI_KERNEL;
+    frame->info.si_addr = target_pc;
 #ifdef X86_32
     frame->sig = sig;
     frame->pinfo = &frame->info;

core/arch/x86_code.c
@@ -307,6 +307,12 @@ new_thread_setup(priv_mcontext_t *mc)
 #    ifdef ARM
     dr_set_isa_mode(dcontext, get_clone_record_isa_mode(crec), NULL);
 #    endif
+
+    /* Restore the original stack parameter to the syscall, which we clobbered
+     * in create_clone_record().  Some apps examine it post-syscall (i#3171).
+     */
+    restore_clone_param_from_clone_record(dcontext, crec);
+
     thread_starting(dcontext);
 
     call_switch_stack(dcontext, dcontext->dstack, (void (*)(void *))dispatch,

core/unix/os.c
@@ -7041,7 +7041,8 @@ pre_system_call(dcontext_t *dcontext)
          * Note: This must be done after sys_param0 is set.
          */
         if (is_thread_create_syscall(dcontext)) {
-            create_clone_record(dcontext, sys_param_addr(dcontext, 1) /*newsp*/);
+            create_clone_record(dcontext,
+                                sys_param_addr(dcontext, SYSCALL_PARAM_CLONE_STACK));
             os_clone_pre(dcontext);
             os_new_thread_pre();
         } else /* This is really a fork. */

core/unix/os_exports.h
@@ -472,6 +472,9 @@ set_app_lib_tls_base_from_clone_record(dcontext_t *dcontext, void *record);
 #endif
 
 void
+restore_clone_param_from_clone_record(dcontext_t *dcontext, void *record);
+
+void
 os_clone_post(dcontext_t *dcontext);
 
 void

core/unix/os_private.h
@@ -106,6 +106,8 @@
     (CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD | CLONE_SYSVSEM | \
      CLONE_SETTLS | CLONE_PARENT_SETTID | CLONE_CHILD_CLEARTID)
 
+#define SYSCALL_PARAM_CLONE_STACK 1
+
 /* On Mac, we use the _nocancel variant to defer app-initiated thread termination */
 #ifdef MACOS
 #    define SYSNUM_NO_CANCEL(num) num##_nocancel

commit 2710c6924ab17c54b56cb29bf53894ec3d8c5d81

Fix Mac Travis breakage
Fixes Mac build breakage from 49ee6922 #196.

Fixes Mac test failures from 036900c4 #3177: an assert was added that
should be disabled on Mac where the kernel does not restore the alt
stack from the frame.

Adds ignore rules for still-broken tests for #3127.

Issue: #3127, #196, #3177

core/unix/signal.c
@@ -792,6 +792,7 @@ set_app_lib_tls_base_from_clone_record(dcontext_t *dcontext, void *record)
 void
 restore_clone_param_from_clone_record(dcontext_t *dcontext, void *record)
 {
+#ifdef LINUX
     ASSERT(record != NULL);
     clone_record_t *crec = (clone_record_t *)record;
     if (crec->clone_sysnum == SYS_clone && TEST(CLONE_VM, crec->clone_flags)) {
@@ -801,6 +802,7 @@ restore_clone_param_from_clone_record(dcontext_t *dcontext, void *record)
         set_syscall_param(dcontext, SYSCALL_PARAM_CLONE_STACK,
                           get_mcontext(dcontext)->xsp);
     }
+#endif
 }
 
 /* Initializes info's app_sigaction, restorer_valid, and we_intercept fields */
@@ -4427,7 +4429,7 @@ record_pending_signal(dcontext_t *dcontext, int sig, kernel_ucontext_t *ucxt,
 static bool
 is_sys_kill(dcontext_t *dcontext, byte *pc, byte *xsp, kernel_siginfo_t *info)
 {
-#ifndef VMX86_SERVER /* does not properly set si_code */
+#if !defined(VMX86_SERVER) && !defined(MACOS) /* does not use SI_KERNEL */
     /* i#133: use si_code to distinguish user-sent signals.
      * Even 2.2 Linux kernel supports <=0 meaning user-sent (except
      * SIGIO) so we assume we can rely on it.
@@ -5164,10 +5166,12 @@ master_signal_handler_C(byte *xsp)
     LOG(THREAD, LOG_ASYNCH, level,
         "\tmaster_signal_handler %d returning now to " PFX "\n\n", sig, sc->SC_XIP);
 
-    /* Ensure we didn't get the app's sigstack into our frame. */
-    ASSERT(dcontext == NULL || dcontext == GLOBAL_DCONTEXT ||
-           frame->uc.uc_stack.ss_sp ==
-               ((thread_sig_info_t *)dcontext->signal_field)->sigstack.ss_sp);
+    /* Ensure we didn't get the app's sigstack into our frame.  On Mac, the kernel
+     * doesn't use the frame's uc_stack, so we limit this to Linux.
+     */
+    IF_LINUX(ASSERT(dcontext == NULL || dcontext == GLOBAL_DCONTEXT ||
+                    frame->uc.uc_stack.ss_sp ==
+                    ((thread_sig_info_t *)dcontext->signal_field)->sigstack.ss_sp));
 
     /* restore protections */
     if (local)
@@ -5975,16 +5979,6 @@ handle_sigreturn(dcontext_t *dcontext, void *ucxt_param, int style)
 #    endif
         sc = get_sigcontext_from_app_frame(info, sig, (void *)frame);
         ucxt = &frame->uc;
-        /* Re-set sigstack from the value stored in the frame.  Silently ignore failure,
-         * just like the kernel does.
-         */
-        uint ignored;
-        /* The kernel checks for being on the stack *after* swapping stacks, so pass
-         * sc->SC_XSP as the current stack.
-         */
-        handle_sigaltstack(dcontext, &frame->uc.uc_stack, NULL, sc->SC_XSP, &ignored);
-        /* Restore DR's so sigreturn syscall won't change it. */
-        frame->uc.uc_stack = info->sigstack;
 #elif defined(MACOS)
         /* The initial frame fields on the stack are messed up due to
          * params to handler from tramp, so use params to syscall.
@@ -6006,6 +6000,16 @@ handle_sigreturn(dcontext_t *dcontext, void *ucxt_param, int style)
         sc = SIGCXT_FROM_UCXT(ucxt);
 #endif
         ASSERT(sig > 0 && sig <= MAX_SIGNUM && IS_RT_FOR_APP(info, sig));
+        /* Re-set sigstack from the value stored in the frame.  Silently ignore failure,
+         * just like the kernel does.
+         */
+        uint ignored;
+        /* The kernel checks for being on the stack *after* swapping stacks, so pass
+         * sc->SC_XSP as the current stack.
+         */
+        handle_sigaltstack(dcontext, &ucxt->uc_stack, NULL, sc->SC_XSP, &ignored);
+        /* Restore DR's so sigreturn syscall won't change it. */
+        ucxt->uc_stack = info->sigstack;
 
         /* FIXME: what if handler called sigaction and requested rt
          * when itself was non-rt?
@@ -6256,7 +6260,7 @@ os_forge_exception(app_pc target_pc, dr_exception_type_t type)
      * avoid the !is_sys_kill() check in record_pending_signal() to avoid an
      * infinite loop (i#3171).
      */
-    frame->info.si_code = SI_KERNEL;
+    frame->info.si_code = IF_LINUX_ELSE(SI_KERNEL, 0);
     frame->info.si_addr = target_pc;
 #ifdef X86_32
     frame->sig = sig;

suite/runsuite_wrapper.pl
@@ -159,6 +159,18 @@ for (my $i = 0; $i < $#lines; ++$i) {
             } else {
                 $issue_no = "#2417";
             }
+        } elsif ($^O eq 'darwin') {
+            %ignore_failures_32 = ('code_api|common.decode-bad' => 1, # i#3127
+                                   'code_api|linux.signal0000' => 1, # i#3127
+                                   'code_api|linux.signal0010' => 1, # i#3127
+                                   'code_api|linux.signal0100' => 1, # i#3127
+                                   'code_api|linux.signal0110' => 1, # i#3127
+                                   'code_api|linux.sigaction' => 1, # i#3127
+                                   'code_api|security-common.codemod' => 1, # i#3127
+                                   'code_api|client.crashmsg' => 1, # i#3127
+                                   'code_api|client.exception' => 1, # i#3127
+                                   'code_api|client.timer' => 1, # i#3127
+                                   'code_api|sample.signal' => 1); # i#3127
         } else {
             # FIXME i#2921: fix flaky ptsig test
             %ignore_failures_32 = ('code_api|pthreads.ptsig' => 1);