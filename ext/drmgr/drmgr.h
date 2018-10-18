ddea145c6b9095af35524f59776b5649c5883b3b
John FX Galea <john.galea@stx.ox.ac.uk>
2018-10-18 13:02:29 +0100

    Added clang format out comment

--- ext/drmgr/drmgr.h
+++ ext/drmgr/drmgr.h
@@ -1266,9 +1266,11 @@ DR_EXPORT
  * \return true if unregistration is successful and false if it is not
  * (e.g., \p func was not registered).
  */
+/* clang-format off */ /* (work around clang-format newline-after-type bug) */
 bool
 drmgr_unregister_signal_event(dr_signal_action_t (*func)(void *drcontext,
                                                          dr_siginfo_t *siginfo));
+/* clang-format on */
 
 DR_EXPORT
 /**
@@ -1278,10 +1280,12 @@ DR_EXPORT
  *
  * See also drmgr_unregister_signal_event().
  */
+/* clang-format off */ /* (work around clang-format newline-after-type bug) */
 bool
 drmgr_unregister_signal_event_user_data(dr_signal_action_t (*func)(void *drcontext,
                                                                    dr_siginfo_t *siginfo,
                                                                    void *user_data));
+/* clang-format on */
 #endif /* UNIX */
 
 #ifdef WINDOWS
