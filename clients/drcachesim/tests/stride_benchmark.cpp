321dd9a1942bb311f7e4e67ff7043028b5e45340
Mazen Kharbutli <kharbutli@google.com>
2018-11-02 13:22:10 -0700

    i#3245: Fix the cache miss analyzer tests (#3246)
    Fix the flaky benchmark test (tool.drcachesim.miss_analyzer) which is occasionally timing out. The size of the main array is halved. Also, fix the analyzer's unit test (tool.drcachesim.miss_analyzer_unit_test) which was not signaling a failure when it actually failed.
    
    Fixes #3245

--- clients/drcachesim/tests/stride_benchmark.cpp
+++ clients/drcachesim/tests/stride_benchmark.cpp
@@ -61,7 +61,7 @@ main(int argc, const char *argv[])
     const int kStride = 7;
     // Number of 1-byte elements in the array.
     // (200+ MiB to guarantee the array doesn't fit in Skylake caches)
-    const size_t kArraySize = 512 * 1024 * 1024;
+    const size_t kArraySize = 256 * 1024 * 1024;
     // Number of iterations in the main loop.
     const int kIterations = 1000000;
     // The main vector/array used for emulating pointer chasing.
