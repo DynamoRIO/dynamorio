74e2cde92c25673c6626e72280455b48ad2ab0fc
Hendrik Greving <hgreving@google.com>
2018-11-13 15:45:49 -0800

    i#1418: Remove unnecessary CMP0020 policy (#3266)
    CMP0020 is not needed for cmake > 2.8.11, neither are we importing Qt5Core.
    
    Fixes #1418

--- make/policies.cmake
+++ make/policies.cmake
@@ -44,8 +44,5 @@ cmake_policy(SET CMP0042 OLD)
 # XXX DrMem-i#1481: update to cmake 2.8.12's better handling of interface imports
 cmake_policy(SET CMP0022 OLD)
 
-# XXX i#1418: update to cmake 2.8.12's better handling of interface imports, for Qt/Win
-cmake_policy(SET CMP0020 OLD)
-
 # Recognize literals in if statements
 cmake_policy(SET CMP0012 NEW)
