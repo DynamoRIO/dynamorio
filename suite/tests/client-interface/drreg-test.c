8eab5bf14881d6635731fcd17e5807767c8aea50
Hendrik Greving <hgreving@google.com>
2018-12-17 22:07:02 -0800

    i#2941 recreate_app_state spill/restore: test restore of non-public DR slot in mangling (#3313)
    Adds test to check whether drreg ignores restoring non-public slot of DR used for mangling. DR secretly violates transparency in extreme cases if scratch reg is optimized into using app's dead reg of rip-rel instruction.
    
    Issue: #2941, #2933

--- suite/tests/client-interface/drreg-test.c
+++ suite/tests/client-interface/drreg-test.c
@@ -51,6 +51,8 @@ void
 test_asm_faultC();
 void
 test_asm_faultD();
+void
+test_asm_faultE();
 
 static SIGJMP_BUF mark;
 
@@ -88,7 +90,21 @@ handle_signal3(int signal, siginfo_t *siginfo, ucontext_t *ucxt)
     if (signal == SIGSEGV) {
         sigcontext_t *sc = SIGCXT_FROM_UCXT(ucxt);
         if (sc->SC_XAX != DRREG_TEST_9_C) {
-            print("ERROR: spilled register value was not preserved! 0x%lx\n", sc->SC_XAX);
+            print("ERROR: spilled register value was not preserved!\n");
+            exit(1);
+        }
+    }
+#        endif
+    SIGLONGJMP(mark, 1);
+}
+static void
+handle_signal4(int signal, siginfo_t *siginfo, ucontext_t *ucxt)
+{
+#        ifdef X86
+    if (signal == SIGSEGV) {
+        sigcontext_t *sc = SIGCXT_FROM_UCXT(ucxt);
+        if (sc->SC_XAX != DRREG_TEST_11_C) {
+            print("ERROR: spilled register value was not preserved!\n");
             exit(1);
         }
     }
@@ -129,8 +145,18 @@ handle_exception3(struct _EXCEPTION_POINTERS *ep)
 #        endif
     SIGLONGJMP(mark, 1);
 }
+static LONG WINAPI
+handle_exception4(struct _EXCEPTION_POINTERS *ep)
+{
+#        ifdef X86
+    if (ep->ExceptionRecord->ExceptionCode == EXCEPTION_ILLEGAL_INSTRUCTION) {
+        if (ep->ContextRecord->TEST_XAX_CXT != DRREG_TEST_11_C)
+            print("ERROR: spilled register value was not preserved!\n");
+    }
+#        endif
+    SIGLONGJMP(mark, 1);
+}
 #    endif
-
 int
 main(int argc, const char *argv[])
 {
@@ -177,6 +203,21 @@ main(int argc, const char *argv[])
      */
     if (SIGSETJMP(mark) == 0) {
         test_asm_faultD();
+    }
+
+#    if defined(UNIX)
+    intercept_signal(SIGSEGV, (handler_3_t)&handle_signal4, false);
+#    elif defined(WINDOWS)
+    SetUnhandledExceptionFilter(&handle_exception4);
+#    endif
+
+    /* Test 10: test fault restore of non-public DR slot used by mangling,
+     * when rip-rel address is forced to be in register. Making sure drreg
+     * ignores restoring this slot. Exposes transparency limitation of DR
+     * if reg is optimized to be app's dead reg.
+     */
+    if (SIGSETJMP(mark) == 0) {
+        test_asm_faultE();
     }
 
     /* XXX i#511: add more fault tests and other tricky corner cases */
@@ -449,16 +490,16 @@ GLOBAL_LABEL(FUNCNAME:)
 #define FUNCNAME test_asm_faultD
         DECLARE_FUNC_SEH(FUNCNAME)
 GLOBAL_LABEL(FUNCNAME:)
-        /* XXX i#3312: Temporarily disable test until bug has been fixed. */
 #ifdef X86
 #ifdef X64
         PUSH_CALLEE_SAVED_REGS()
         sub      REG_XSP, FRAME_PADDING /* align */
         END_PROLOG
+        /* XXX i#3312: Temporarily disable test until bug has been fixed. */
 #if 0
 
         jmp      test8
-        /* Test 8: fault test restore of non-public DR slot used by mangling.
+        /* Test 8: test fault restore of non-public DR slot used by mangling.
          * Making sure drreg ignores restoring this slot.
          */
      test8:
@@ -468,10 +509,7 @@ GLOBAL_LABEL(FUNCNAME:)
         mov      TEST_REG_ASM, DRREG_TEST_8_ASM
         nop
         mov      REG_XAX, DRREG_TEST_9_ASM
-        /* There's a good chance that this will be too far away for a
-         * signed 32-bit rip-rel offset, which is what we want, in order
-         * to get the address mangled into register REG_XAX.
-         */
+        /* The address will get mangled into register REG_XAX. */
         add      TEST_REG_ASM, PTRSZ SYMREF(-0x7fffffff) /* crash */
 
         jmp      epilog8
@@ -490,6 +528,53 @@ GLOBAL_LABEL(FUNCNAME:)
 #elif defined(AARCH64)
         /* XXX i#3289: prologue missing */
         /* Test 8: not implemented for AARCH64 */
+        ret
+#endif
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+#define FUNCNAME test_asm_faultE
+        DECLARE_FUNC_SEH(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+#ifdef X86
+#ifdef X64
+        PUSH_CALLEE_SAVED_REGS()
+        sub      REG_XSP, FRAME_PADDING /* align */
+        END_PROLOG
+
+          /* XXX i#3312: Temporarily disable test until bug has been fixed. */
+#if 0
+OB        jmp      test10
+        /* Test 10: test fault restore of non-public DR slot used by mangling,
+         * when rip-rel address is forced to be in register. Making sure drreg ignores
+         * restoring this slot.
+         */
+     test10:
+        mov      PTRSZ [REG_XSP], REG_XAX
+        sub      REG_XSP, 8
+        mov      TEST_REG_ASM, DRREG_TEST_10_ASM
+        mov      TEST_REG_ASM, DRREG_TEST_10_ASM
+        nop
+        mov      REG_XAX, DRREG_TEST_11_ASM
+        /* The address will get mangled into register REG_XAX. */
+        add      REG_XAX, PTRSZ SYMREF(-0x7fffffff) /* crash */
+
+        jmp      epilog10
+     epilog10:
+        add      REG_XSP, 8
+        mov      REG_XAX, PTRSZ [REG_XSP]
+#endif
+        add      REG_XSP, FRAME_PADDING /* make a legal SEH64 epilog */
+        POP_CALLEE_SAVED_REGS()
+#endif
+        ret
+#elif defined(ARM)
+        /* XXX i#3289: prologue missing */
+        /* Test 10: not implemented for ARM */
+        bx       lr
+#elif defined(AARCH64)
+        /* XXX i#3289: prologue missing */
+        /* Test 10: not implemented for AARCH64 */
         ret
 #endif
         END_FUNC(FUNCNAME)
