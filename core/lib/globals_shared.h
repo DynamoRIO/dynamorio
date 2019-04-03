87663b9e74b0ebf032c558eaa6844022c286054e
Hendrik Greving <hgreving@google.com>
2019-04-03 09:29:36 -0700

    i#1312 AVX-512 support: Fix API comment and doxygen links. (#3501)
    Amends commit 851a961 with doxygen links and fixes a documentation bug
    for MCXT_NUM_OPMASK_SLOTS.
    
    Issue: #1312

--- core/lib/globals_shared.h
+++ core/lib/globals_shared.h
@@ -1838,9 +1838,12 @@ typedef union _dr_zmm_t {
     reg_t reg[IF_X64_ELSE(8, 16)]; /**< Representation as 8 or 16 registers. */
 } dr_zmm_t;
 
-/* OpMask (k-)register. The register may be only 16 bits on systems w/o AVX512BW, but
- * can be up to MAX_KL = 64 bits.
+/** AVX-512 OpMask (k-)register. */
+#ifdef AVOID_API_EXPORT
+/* The register may be only 16 bits wide on systems without AVX512BW, but can be up to
+ * MAX_KL = 64 bits.
  */
+#endif
 typedef uint64 dr_opmask_t;
 
 #if defined(AARCHXX)
@@ -1878,7 +1881,10 @@ typedef union _dr_simd_t {
 #    define PRE_SIMD_PADDING                                       \
         0 /**< Bytes of padding before xmm/ymm dr_mcontext_t slots \
            */
-#    define MCXT_NUM_OPMASK_SLOTS 0 /**< No simd masks */
+#    define MCXT_NUM_OPMASK_SLOTS                                    \
+        0 /**< Number of 16-64-bit OpMask Kn slots in dr_mcontext_t, \
+           * if architecture supports.                               \
+           */
 
 #elif defined(X86)
 
@@ -1912,8 +1918,9 @@ typedef union _dr_simd_t {
 #        define PRE_XMM_PADDING \
             24 /**< Bytes of padding before xmm/ymm dr_mcontext_t slots */
 #    endif
-#    define MCXT_NUM_OPMASK_SLOTS                                   \
-        8 /**< Number of 16-64-bit OpMask Kn slots in dr_mcontext_t \
+#    define MCXT_NUM_OPMASK_SLOTS                                    \
+        8 /**< Number of 16-64-bit OpMask Kn slots in dr_mcontext_t, \
+           * if architecture supports.                               \
            */
 #else
 #    error NYI
