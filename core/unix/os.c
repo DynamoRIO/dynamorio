DynamoRIO/dynamorio

commit 99fd6ce5dae8b7ea33268e1043a518ad42e4dd80

i#1132 split vmcode: Separate vmcode from vmheap (#3554)
Splits the VMM-managed memory into two pieces: "vmcode" which is
guaranteed 32-bit-displacement-reachable, and "vmheap" which has no
such guarantees.

Adds a new option -vmheap_size to control the size of vmheap.  The
placement is left to the OS, rather than DR trying to do ASLR, as
modern OS's have their own ASLR.

Changes which_vmm_t to a bitmask and adds a flag VMM_REACHABLE so
callers can specify which region to use.  Adds the flag to all
appropriate call sites.

Implements the -reachable_heap option: when on, only vmcode is used
and there is no 2nd region.

Adds heap_reachable_{alloc,free} and use for
dr_{global,thread}_{alloc,free}.  Adds local and global reachable heap
units to implement this.  Adds storage of the type of heap inside
thread_units_t and heap_unit_t.  Places both types of units on the
same free lists, but checks for a match before re-using.  Explicitly
exercises each heap's lock at init time to avoid shutdown issues due
to the temp heapmgt copy, and preserves the process lock list.

Changes the heap used by the trace buffer, private instr encode, and
allocated instr raw bits to use reachable heap to avoid rip-relative
re-relativization problems.

Refactors heap code to work with multiple regions, either by taking in
the vm_heap_t or switching to routines that do not require it (such as
s/vmm_is_reserved_unit/is_vmm_reserved_address/).

Refactors other core code to handle multiple regions.  Adds
iterate_vmm_regions() for running code for each region; changes
memquery_emulate's probe code to query every time instead of trying to
cache one region's bounds.

Implements the API flag DR_ALLOC_CACHE_REACHABLE to actually split
into reachable and non-reachable memory.  As promised,
dr_nonheap_alloc() and dr_{global,thread}_alloc() are reachable.

Requires -reachable_heap for -heap_in_lower_4GB as otherwise there is

core/unix/os.c
@@ -8867,6 +8867,13 @@ found_vsyscall_page(memquery_iter_t *iter _IF_DEBUG(OUT const char **map_type))
 #endif
 }
 
+static void
+add_to_memcache(byte *region_start, byte *region_end, void *user_data)
+{
+    memcache_update_locked(region_start, region_end, MEMPROT_NONE, DR_MEMTYPE_DATA,
+                           false /*!exists*/);
+}
+
 int
 os_walk_address_space(memquery_iter_t *iter, bool add_modules)
 {
@@ -8892,12 +8899,7 @@ os_walk_address_space(memquery_iter_t *iter, bool add_modules)
      * not yet used memory.  FIXME: we're not marking beyond-vmheap DR regions
      * as noaccess!
      */
-    byte *our_heap_start, *our_heap_end;
-    get_vmm_heap_bounds(&our_heap_start, &our_heap_end);
-    if (our_heap_end - our_heap_start > 0) {
-        memcache_update_locked(our_heap_start, our_heap_end, MEMPROT_NONE,
-                               DR_MEMTYPE_DATA, false /*!exists*/);
-    }
+    iterate_vmm_regions(add_to_memcache, NULL);
 #endif
 
 #ifndef HAVE_MEMINFO
@@ -9060,7 +9062,8 @@ os_walk_address_space(memquery_iter_t *iter, bool add_modules)
         /* add all regions (incl. dynamo_areas and stack) to all_memory_areas */
 #    ifndef HAVE_MEMINFO_QUERY
         /* Don't add if we're using one single vmheap entry. */
-        if (iter->vm_start < our_heap_start || iter->vm_end > our_heap_end) {
+        if (!is_vmm_reserved_address(iter->vm_start, iter->vm_end - iter->vm_start, NULL,
+                                     NULL)) {
             LOG(GLOBAL, LOG_VMAREAS, 4,
                 "os_walk_address_space: adding: " PFX "-" PFX " prot=%d\n",
                 iter->vm_start, iter->vm_end, iter->prot);
@@ -9236,7 +9239,7 @@ get_memory_info(const byte *pc, byte **base_pc, size_t *size,
                 uint *prot /* OUT optional, returns MEMPROT_* value */)
 {
     dr_mem_info_t info;
-    if (is_vmm_reserved_address((byte *)pc, 1)) {
+    if (is_vmm_reserved_address((byte *)pc, 1, NULL, NULL)) {
         if (!query_memory_ex_from_os(pc, &info) || info.type == DR_MEMTYPE_FREE)
             return false;
     } else {

core/arch/arch.c
@@ -271,7 +271,8 @@ check_size_and_cache_line(dr_isa_mode_t isa_mode, generated_code_t *code, byte *
     byte *next_pc = move_to_start_of_cache_line(isa_mode, pc);
     if ((byte *)ALIGN_FORWARD(pc, PAGE_SIZE) + PAGE_SIZE > code->commit_end_pc) {
         ASSERT(code->commit_end_pc + PAGE_SIZE <= ((byte *)code) + GENCODE_RESERVE_SIZE);
-        heap_mmap_extend_commitment(code->commit_end_pc, PAGE_SIZE, VMM_SPECIAL_MMAP);
+        heap_mmap_extend_commitment(code->commit_end_pc, PAGE_SIZE,
+                                    VMM_SPECIAL_MMAP | VMM_REACHABLE);
         code->commit_end_pc += PAGE_SIZE;
     }
     return next_pc;
@@ -290,7 +291,7 @@ release_final_page(generated_code_t *code)
     ASSERT(ALIGNED(leftover, PAGE_SIZE));
     if (leftover > 0) {
         heap_mmap_retract_commitment(code->commit_end_pc - leftover, leftover,
-                                     VMM_SPECIAL_MMAP);
+                                     VMM_SPECIAL_MMAP | VMM_REACHABLE);
         code->commit_end_pc -= leftover;
     }
     LOG(THREAD_GET, LOG_EMIT, 1,
@@ -486,9 +487,9 @@ static void shared_gencode_init(IF_X86_64_ELSE(gencode_mode_t gencode_mode, void
     bool x86_to_x64_mode = false;
 #endif
 
-    gencode =
-        heap_mmap_reserve(GENCODE_RESERVE_SIZE, GENCODE_COMMIT_SIZE,
-                          MEMPROT_EXEC | MEMPROT_READ | MEMPROT_WRITE, VMM_SPECIAL_MMAP);
+    gencode = heap_mmap_reserve(GENCODE_RESERVE_SIZE, GENCODE_COMMIT_SIZE,
+                                MEMPROT_EXEC | MEMPROT_READ | MEMPROT_WRITE,
+                                VMM_SPECIAL_MMAP | VMM_REACHABLE);
     /* we would return gencode and let caller assign, but emit routines
      * that this routine calls query the shared vars so we set here
      */
@@ -864,13 +865,17 @@ void d_r_arch_exit(IF_WINDOWS_ELSE_NP(bool detach_stacked_callbacks, void))
 #endif
     /* on x64 we have syscall routines in the shared code so can't free if detaching */
     if (IF_WINDOWS(IF_X64(!detach_stacked_callbacks &&)) shared_code != NULL) {
-        heap_munmap(shared_code, GENCODE_RESERVE_SIZE, VMM_SPECIAL_MMAP);
+        heap_munmap(shared_code, GENCODE_RESERVE_SIZE, VMM_SPECIAL_MMAP | VMM_REACHABLE);
     }
 #if defined(X86) && defined(X64)
-    if (shared_code_x86 != NULL)
-        heap_munmap(shared_code_x86, GENCODE_RESERVE_SIZE, VMM_SPECIAL_MMAP);
-    if (shared_code_x86_to_x64 != NULL)
-        heap_munmap(shared_code_x86_to_x64, GENCODE_RESERVE_SIZE, VMM_SPECIAL_MMAP);
+    if (shared_code_x86 != NULL) {
+        heap_munmap(shared_code_x86, GENCODE_RESERVE_SIZE,
+                    VMM_SPECIAL_MMAP | VMM_REACHABLE);
+    }
+    if (shared_code_x86_to_x64 != NULL) {
+        heap_munmap(shared_code_x86_to_x64, GENCODE_RESERVE_SIZE,
+                    VMM_SPECIAL_MMAP | VMM_REACHABLE);
+    }
 #endif
     interp_exit();
     mangle_exit();
@@ -1167,7 +1172,7 @@ arch_thread_init(dcontext_t *dcontext)
     /* case 9474; share allocation unit w/ thread-private stack */
     code = heap_mmap_reserve_post_stack(
         dcontext, GENCODE_RESERVE_SIZE, GENCODE_COMMIT_SIZE,
-        MEMPROT_EXEC | MEMPROT_READ | MEMPROT_WRITE, VMM_SPECIAL_MMAP);
+        MEMPROT_EXEC | MEMPROT_READ | MEMPROT_WRITE, VMM_SPECIAL_MMAP | VMM_REACHABLE);
     ASSERT(code != NULL);
     /* FIXME case 6493: if we split private from shared, remove this
      * memset since we will no longer have a bunch of fields we don't use
@@ -1383,7 +1388,7 @@ arch_thread_exit(dcontext_t *dcontext _IF_WINDOWS(bool detach_stacked_callbacks)
     if (!detach_stacked_callbacks)
 #endif
         heap_munmap_post_stack(dcontext, dcontext->private_code, GENCODE_RESERVE_SIZE,
-                               VMM_SPECIAL_MMAP);
+                               VMM_SPECIAL_MMAP | VMM_REACHABLE);
 }
 
 #ifdef WINDOWS

core/arch/decodelib.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2018 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2019 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
@@ -100,8 +100,21 @@ heap_alloc(dcontext_t *dcontext, size_t size HEAPACCT(which_heap_t which))
     return malloc(size);
 }
 
+void *
+heap_reachable_alloc(dcontext_t *dcontext, size_t size HEAPACCT(which_heap_t which))
+{
+    return malloc(size);
+}
+
 void
 heap_free(dcontext_t *dcontext, void *p, size_t size HEAPACCT(which_heap_t which))
+{
+    free(p);
+}
+
+void
+heap_reachable_free(dcontext_t *dcontext, void *p,
+                    size_t size HEAPACCT(which_heap_t which))
 {
     free(p);
 }

core/arch/instr_shared.c
@@ -123,7 +123,8 @@ instr_clone(dcontext_t *dcontext, instr_t *orig)
 
     if ((orig->flags & INSTR_RAW_BITS_ALLOCATED) != 0) {
         /* instr length already set from memcpy */
-        instr->bytes = (byte *)heap_alloc(dcontext, instr->length HEAPACCT(ACCT_IR));
+        instr->bytes =
+            (byte *)heap_reachable_alloc(dcontext, instr->length HEAPACCT(ACCT_IR));
         memcpy((void *)instr->bytes, (void *)orig->bytes, instr->length);
     }
 #ifdef CUSTOM_EXIT_STUBS
@@ -168,10 +169,8 @@ instr_free(dcontext_t *dcontext, instr_t *instr)
 {
     if (instr_is_label(instr) && instr_get_label_callback(instr) != NULL)
         (*instr->label_cb)(dcontext, instr);
-    if ((instr->flags & INSTR_RAW_BITS_ALLOCATED) != 0) {
-        heap_free(dcontext, instr->bytes, instr->length HEAPACCT(ACCT_IR));
-        instr->bytes = NULL;
-        instr->flags &= ~INSTR_RAW_BITS_ALLOCATED;
+    if (TEST(INSTR_RAW_BITS_ALLOCATED, instr->flags)) {
+        instr_free_raw_bits(dcontext, instr);
     }
 #ifdef CUSTOM_EXIT_STUBS
     if ((instr->flags & INSTR_HAS_CUSTOM_STUB) != 0) {
@@ -318,10 +317,10 @@ instr_build_bits(dcontext_t *dcontext, int opcode, uint num_bytes)
 static int
 private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
 {
-    /* we cannot use a stack buffer for encoding since our stack on x64 linux
-     * can be too far to reach from our heap
+    /* We cannot use a stack buffer for encoding since our stack on x64 linux
+     * can be too far to reach from our heap.  We need reachable heap.
      */
-    byte *buf = heap_alloc(dcontext, MAX_INSTR_LENGTH HEAPACCT(ACCT_IR));
+    byte *buf = heap_reachable_alloc(dcontext, MAX_INSTR_LENGTH HEAPACCT(ACCT_IR));
     uint len;
     /* Do not cache instr opnds as they are pc-relative to final encoding location.
      * Rather than us walking all of the operands separately here, we have
@@ -339,7 +338,7 @@ private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
                                                             instr_get_isa_mode(instr)
                                                                 _IF_ARM(false))
                                         ->name);
-            heap_free(dcontext, buf, MAX_INSTR_LENGTH HEAPACCT(ACCT_IR));
+            heap_reachable_free(dcontext, buf, MAX_INSTR_LENGTH HEAPACCT(ACCT_IR));
             return 0;
         }
         /* if unreachable, we can't cache, since re-relativization won't work */
@@ -387,7 +386,7 @@ private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
         instr->bytes = tmp;
         instr_set_operands_valid(instr, valid);
     }
-    heap_free(dcontext, buf, MAX_INSTR_LENGTH HEAPACCT(ACCT_IR));
+    heap_reachable_free(dcontext, buf, MAX_INSTR_LENGTH HEAPACCT(ACCT_IR));
     return len;
 }
 
@@ -1072,7 +1071,8 @@ instr_free_raw_bits(dcontext_t *dcontext, instr_t *instr)
 {
     if ((instr->flags & INSTR_RAW_BITS_ALLOCATED) == 0)
         return;
-    heap_free(dcontext, instr->bytes, instr->length HEAPACCT(ACCT_IR));
+    heap_reachable_free(dcontext, instr->bytes, instr->length HEAPACCT(ACCT_IR));
+    instr->bytes = NULL;
     instr->flags &= ~INSTR_RAW_BITS_VALID;
     instr->flags &= ~INSTR_RAW_BITS_ALLOCATED;
 }
@@ -1088,7 +1088,9 @@ instr_allocate_raw_bits(dcontext_t *dcontext, instr_t *instr, uint num_bytes)
     if ((instr->flags & INSTR_RAW_BITS_VALID) != 0)
         original_bits = instr->bytes;
     if ((instr->flags & INSTR_RAW_BITS_ALLOCATED) == 0 || instr->length != num_bytes) {
-        byte *new_bits = (byte *)heap_alloc(dcontext, num_bytes HEAPACCT(ACCT_IR));
+        /* We need reachable heap for rip-rel re-relativization. */
+        byte *new_bits =
+            (byte *)heap_reachable_alloc(dcontext, num_bytes HEAPACCT(ACCT_IR));
         if (original_bits != NULL) {
             /* copy original bits into modified bits so can just modify
              * a few and still have all info in one place

core/fcache.c
@@ -965,8 +965,10 @@ fcache_really_free_unit(fcache_unit_t *u, bool on_dead_list, bool dealloc_unit)
      * being re-used and not showing up in in_fcache
      */
     vmvector_remove(fcache_unit_areas, u->start_pc, u->reserved_end_pc);
-    if (dealloc_unit)
-        heap_munmap((void *)u->start_pc, UNIT_RESERVED_SIZE(u), VMM_CACHE);
+    if (dealloc_unit) {
+        heap_munmap((void *)u->start_pc, UNIT_RESERVED_SIZE(u),
+                    VMM_CACHE | VMM_REACHABLE);
+    }
     /* always dealloc the metadata */
     nonpersistent_heap_free(GLOBAL_DCONTEXT, u,
                             sizeof(fcache_unit_t) HEAPACCT(ACCT_MEM_MGT));
@@ -1382,7 +1384,7 @@ fcache_create_unit(dcontext_t *dcontext, fcache_t *cache, cache_pc pc, size_t si
                 commit_size = size;
             u->start_pc = (cache_pc)heap_mmap_reserve(
                 size, commit_size, MEMPROT_EXEC | MEMPROT_READ | MEMPROT_WRITE,
-                VMM_CACHE);
+                VMM_CACHE | VMM_REACHABLE);
         }
         ASSERT(u->start_pc != NULL);
         ASSERT(proc_is_cache_aligned((void *)u->start_pc));
@@ -1891,7 +1893,7 @@ cache_extend_commitment(fcache_unit_t *unit, size_t commit_size)
 {
     ASSERT(unit != NULL);
     ASSERT(ALIGNED(commit_size, DYNAMO_OPTION(cache_commit_increment)));
-    heap_mmap_extend_commitment(unit->end_pc, commit_size, VMM_CACHE);
+    heap_mmap_extend_commitment(unit->end_pc, commit_size, VMM_CACHE | VMM_REACHABLE);
     unit->end_pc += commit_size;
     unit->size += commit_size;
     unit->cache->size += commit_size;
@@ -2028,7 +2030,7 @@ fcache_increase_size(dcontext_t *dcontext, fcache_t *cache, fcache_unit_t *unit,
         ASSERT(commit_size <= new_size);
         new_memory = (cache_pc)heap_mmap_reserve(
             new_size, commit_size, MEMPROT_EXEC | MEMPROT_READ | MEMPROT_WRITE,
-            VMM_CACHE);
+            VMM_CACHE | VMM_REACHABLE);
         STATS_FCACHE_SUB(cache, capacity, unit->size);
         STATS_FCACHE_ADD(cache, capacity, commit_size);
         STATS_FCACHE_MAX(cache, capacity_peak, capacity);
@@ -2196,7 +2198,8 @@ fcache_thread_reset_free(dcontext_t *dcontext)
          */
         vmvector_remove(fcache_unit_areas, tu->pending_unmap_pc,
                         tu->pending_unmap_pc + tu->pending_unmap_size);
-        heap_munmap(tu->pending_unmap_pc, tu->pending_unmap_size, VMM_CACHE);
+        heap_munmap(tu->pending_unmap_pc, tu->pending_unmap_size,
+                    VMM_CACHE | VMM_REACHABLE);
         tu->pending_unmap_pc = NULL;
     }
     if (tu->bb != NULL) {
@@ -3645,7 +3648,8 @@ fcache_add_fragment(dcontext_t *dcontext, fragment_t *f)
         vmvector_remove(fcache_unit_areas, tu->pending_unmap_pc,
                         tu->pending_unmap_pc + tu->pending_unmap_size);
         /* caller must dec stats since here we don't know type of cache */
-        heap_munmap(tu->pending_unmap_pc, tu->pending_unmap_size, VMM_CACHE);
+        heap_munmap(tu->pending_unmap_pc, tu->pending_unmap_size,
+                    VMM_CACHE | VMM_REACHABLE);
         tu->pending_unmap_pc = NULL;
     }
 

core/heap.h
@@ -108,13 +108,16 @@ void
 vmcode_get_reachable_region(byte **region_start OUT, byte **region_end OUT);
 #endif
 
-/* virtual heap manager */
+/* Virtual memory types.  These are bitmask values. */
 typedef enum {
-    VMM_HEAP = 0,
-    VMM_CACHE,
-    VMM_STACK,
-    VMM_SPECIAL_HEAP,
-    VMM_SPECIAL_MMAP,
+    /* Mutually-exclusive core types. */
+    VMM_HEAP = 0x0001,
+    VMM_CACHE = 0x0002,
+    VMM_STACK = 0x0004,
+    VMM_SPECIAL_HEAP = 0x0008,
+    VMM_SPECIAL_MMAP = 0x0010,
+    /* These modify the core types. */
+    VMM_REACHABLE = 0x0020,
 } which_vmm_t;
 
 void
@@ -123,12 +126,13 @@ void
 vmm_heap_exit(void);
 void
 print_vmm_heap_data(file_t outf);
-void
-get_vmm_heap_bounds(byte **heap_start /*OUT*/, byte **heap_end /*OUT*/);
 byte *
 vmcode_get_start();
 byte *
 vmcode_get_end();
+void
+iterate_vmm_regions(void (*cb)(byte *region_start, byte *region_end, void *user_data),
+                    void *user_data);
 byte *
 vmcode_unreachable_pc();
 
@@ -136,7 +140,8 @@ bool
 heap_check_option_compatibility(void);
 
 bool
-is_vmm_reserved_address(byte *pc, size_t size);
+is_vmm_reserved_address(byte *pc, size_t size, OUT byte **region_start,
+                        OUT byte **region_end);
 bool
 rel32_reachable_from_vmcode(byte *target);
 
@@ -248,6 +253,13 @@ nonpersistent_heap_alloc(dcontext_t *dcontext, size_t size HEAPACCT(which_heap_t
 void
 nonpersistent_heap_free(dcontext_t *dcontext, void *p,
                         size_t size HEAPACCT(which_heap_t which));
+
+/* Passing dcontext == GLOBAL_DCONTEXT allocates from a global pool. */
+void *
+heap_reachable_alloc(dcontext_t *dcontext, size_t size HEAPACCT(which_heap_t which));
+void
+heap_reachable_free(dcontext_t *dcontext, void *p,
+                    size_t size HEAPACCT(which_heap_t which));
 
 bool
 local_heap_protected(dcontext_t *dcontext);

core/lib/instrument.c
@@ -2839,11 +2839,6 @@ dr_get_random_seed(void)
 
 /***************************************************************************
  * MEMORY ALLOCATION
- *
- * XXX i#774: once we split vmheap from vmcode, we need to make
- * dr_thread_alloc(), dr_global_alloc(), and dr_nonheap_alloc()
- * all allocate vmcode-reachable memory.  Library-redirected
- * allocations do not need to be reachable.
  */
 
 DR_API
@@ -2854,7 +2849,8 @@ void *
 dr_thread_alloc(void *drcontext, size_t size)
 {
     dcontext_t *dcontext = (dcontext_t *)drcontext;
-    return heap_alloc(dcontext, size HEAPACCT(ACCT_CLIENT));
+    /* For back-compat this is guaranteed-reachable. */
+    return heap_reachable_alloc(dcontext, size HEAPACCT(ACCT_CLIENT));
 }
 
 DR_API
@@ -2867,7 +2863,7 @@ dr_thread_free(void *drcontext, void *mem, size_t size)
     dcontext_t *dcontext = (dcontext_t *)drcontext;
     CLIENT_ASSERT(drcontext != NULL, "dr_thread_free: drcontext cannot be NULL");
     CLIENT_ASSERT(drcontext != GLOBAL_DCONTEXT, "dr_thread_free: drcontext is invalid");
-    heap_free(dcontext, mem, size HEAPACCT(ACCT_CLIENT));
+    heap_reachable_free(dcontext, mem, size HEAPACCT(ACCT_CLIENT));
 }
 
 DR_API
@@ -2876,7 +2872,8 @@ DR_API
 void *
 dr_global_alloc(size_t size)
 {
-    return global_heap_alloc(size HEAPACCT(ACCT_CLIENT));
+    /* For back-compat this is guaranteed-reachable. */
+    return heap_reachable_alloc(GLOBAL_DCONTEXT, size HEAPACCT(ACCT_CLIENT));
 }
 
 DR_API
@@ -2886,7 +2883,7 @@ DR_API
 void
 dr_global_free(void *mem, size_t size)
 {
-    global_heap_free(mem, size HEAPACCT(ACCT_CLIENT));
+    heap_reachable_free(GLOBAL_DCONTEXT, mem, size HEAPACCT(ACCT_CLIENT));
 }
 
 DR_API
@@ -2894,14 +2891,16 @@ DR_API
 void *
 dr_nonheap_alloc(size_t size, uint prot)
 {
-    return heap_mmap_ex(size, size, prot, false /*no guard pages*/, VMM_SPECIAL_MMAP);
+    return heap_mmap_ex(size, size, prot, false /*no guard pages*/,
+                        /* For back-compat we preserve reachability. */
+                        VMM_SPECIAL_MMAP | VMM_REACHABLE);
 }
 
 DR_API
 void
 dr_nonheap_free(void *mem, size_t size)
 {
-    heap_munmap_ex(mem, size, false /*no guard pages*/, VMM_SPECIAL_MMAP);
+    heap_munmap_ex(mem, size, false /*no guard pages*/, VMM_SPECIAL_MMAP | VMM_REACHABLE);
 }
 
 static void *
@@ -3023,6 +3022,7 @@ static void *
 custom_memory_shared(bool alloc, void *drcontext, dr_alloc_flags_t flags, size_t size,
                      uint prot, void *addr, bool *free_res)
 {
+    dcontext_t *dcontext = (dcontext_t *)drcontext;
     CLIENT_ASSERT(alloc || free_res != NULL, "must ask for free_res on free");
     CLIENT_ASSERT(alloc || addr != NULL, "cannot free NULL");
     CLIENT_ASSERT(!TESTALL(DR_ALLOC_NON_DR | DR_ALLOC_CACHE_REACHABLE, flags),
@@ -3071,8 +3071,9 @@ custom_memory_shared(bool alloc, void *drcontext, dr_alloc_flags_t flags, size_t
                               addr == NULL,
                           "dr_custom_alloc: cannot ask for addr and cache-reachable");
             /* This flag is here solely so we know which version of free to call */
-            if (TEST(DR_ALLOC_FIXED_LOCATION, flags)) {
-                CLIENT_ASSERT(addr != NULL,
+            if (TEST(DR_ALLOC_FIXED_LOCATION, flags) ||
+                !TEST(DR_ALLOC_CACHE_REACHABLE, flags)) {
+                CLIENT_ASSERT(addr != NULL || !TEST(DR_ALLOC_FIXED_LOCATION, flags),
                               "dr_custom_alloc: fixed location requires an address");
                 if (alloc)
                     return raw_mem_alloc(size, prot, addr, 0);
@@ -3098,17 +3099,30 @@ custom_memory_shared(bool alloc, void *drcontext, dr_alloc_flags_t flags, size_t
                       "dr_custom_alloc: cannot ask for heap in low 2GB");
         CLIENT_ASSERT(!TEST(DR_ALLOC_NON_DR, flags),
                       "dr_custom_alloc: cannot ask for non-DR heap memory");
-        /* for now it's all cache-reachable so we ignore DR_ALLOC_CACHE_REACHABLE */
-        if (TEST(DR_ALLOC_THREAD_PRIVATE, flags)) {
-            if (alloc)
-                return dr_thread_alloc(drcontext, size);
-            else
-                dr_thread_free(drcontext, addr, size);
+        if (TEST(DR_ALLOC_CACHE_REACHABLE, flags)) {
+            if (TEST(DR_ALLOC_THREAD_PRIVATE, flags)) {
+                if (alloc)
+                    return dr_thread_alloc(drcontext, size);
+                else
+                    dr_thread_free(drcontext, addr, size);
+            } else {
+                if (alloc)
+                    return dr_global_alloc(size);
+                else
+                    dr_global_free(addr, size);
+            }
         } else {
-            if (alloc)
-                return dr_global_alloc(size);
-            else
-                dr_global_free(addr, size);
+            if (TEST(DR_ALLOC_THREAD_PRIVATE, flags)) {
+                if (alloc)
+                    return heap_alloc(dcontext, size HEAPACCT(ACCT_CLIENT));
+                else
+                    heap_free(dcontext, addr, size HEAPACCT(ACCT_CLIENT));
+            } else {
+                if (alloc)
+                    return global_heap_alloc(size HEAPACCT(ACCT_CLIENT));
+                else
+                    global_heap_free(addr, size HEAPACCT(ACCT_CLIENT));
+            }
         }
     }
     return NULL;

commit 77e0b8dfd39b1d17053902f43ef7e05b982b65b2

i#1132 split vmcode: Fix Mac build
Fixes a Mac build caused by 99fd6ce.
Enables Mac builds on pull requests.

Issue: #1132, #2764

.travis.yml
@@ -92,9 +92,6 @@ jobs:
       compiler: clang
       # We do not have 64-bit support on OSX yet (i#1979).
       env: DYNAMORIO_CROSS_AARCHXX_LINUX_ONLY=no DEPLOY=no EXTRA_ARGS=32_only
-      # XXX i#2764: Travis OSX resources are over-subscribed and it can take
-      # hours to get an OSX machine, so we skip running PR's for now.
-      if: type = push
 
 # For C/C++ there is no default install, so we set "install", not "before_install".
 install:

core/unix/os.c
@@ -8867,12 +8867,14 @@ found_vsyscall_page(memquery_iter_t *iter _IF_DEBUG(OUT const char **map_type))
 #endif
 }
 
+#ifndef HAVE_MEMINFO_QUERY
 static void
 add_to_memcache(byte *region_start, byte *region_end, void *user_data)
 {
     memcache_update_locked(region_start, region_end, MEMPROT_NONE, DR_MEMTYPE_DATA,
                            false /*!exists*/);
 }
+#endif
 
 int
 os_walk_address_space(memquery_iter_t *iter, bool add_modules)