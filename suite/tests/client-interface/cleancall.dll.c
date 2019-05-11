1ee140b3a94bec5e27891ebc8be7c58cf0a4f45d
johnfxgalea <john.galea@stx.ox.ac.uk>
2019-05-11 09:06:08 +0100

    Added additional tests

--- suite/tests/client-interface/cleancall.dll.c
+++ suite/tests/client-interface/cleancall.dll.c
@@ -52,6 +52,41 @@ static void print_error_on_fail(bool check)
         dr_fprintf(STDERR, "error\n");
 }
 
+static void set_gpr()
+{
+    void *drcontext = dr_get_current_drcontext();
+    opnd_t reg_opnd = opnd_create_reg(DR_REG_XAX);
+    opnd_size_t size_opnd = opnd_get_size(reg_opnd);
+    size_t size = opnd_size_in_bytes(size_opnd); 
+    dr_mcontext_t mcontext = { sizeof(mcontext), DR_MC_ALL, };
+    dr_get_mcontext(drcontext, &mcontext);
+    reg_get_value_ex(DR_REG_XAX, &mcontext, orig_reg_val_buf);
+    reg_get_value_ex(DR_REG_XAX, &mcontext, new_reg_val_buf);
+    new_reg_val_buf[0] = 0x75;
+    new_reg_val_buf[2] = 0x83;
+    new_reg_val_buf[3] = 0x23;
+    bool succ = reg_set_value_ex(DR_REG_XAX, &mcontext, new_reg_val_buf, size);
+    print_error_on_fail(succ == true);
+    dr_set_mcontext(drcontext, &mcontext);
+}
+
+static void check_gpr()
+{
+    void *drcontext = dr_get_current_drcontext();
+    opnd_t reg_opnd = opnd_create_reg(DR_REG_XAX);
+    opnd_size_t size_opnd = opnd_get_size(reg_opnd);
+    size_t size = opnd_size_in_bytes(size_opnd);
+    dr_mcontext_t mcontext = { sizeof(mcontext), DR_MC_ALL, };
+    dr_get_mcontext(drcontext, &mcontext);
+    reg_get_value_ex(DR_REG_XAX, &mcontext, new_reg_val_buf);
+    print_error_on_fail(new_reg_val_buf[0] == 0x75);
+    print_error_on_fail(new_reg_val_buf[2] == 0x83);
+    print_error_on_fail(new_reg_val_buf[3] == 0x23);
+    bool succ = reg_set_value_ex(DR_REG_XAX, &mcontext, orig_reg_val_buf, size);
+    print_error_on_fail(succ == true);
+    dr_set_mcontext(drcontext, &mcontext);
+}
+
 static void set_xmm()
 {
     void *drcontext = dr_get_current_drcontext();
@@ -81,40 +116,80 @@ static void check_xmm()
     dr_set_mcontext(drcontext, &mcontext);
 }
 
-static void set_gpr()
+static void set_ymm()
 {
     void *drcontext = dr_get_current_drcontext();
-    opnd_t reg_opnd = opnd_create_reg(DR_REG_XAX);
-    opnd_size_t size_opnd = opnd_get_size(reg_opnd);
-    size_t size = opnd_size_in_bytes(size_opnd);
     dr_mcontext_t mcontext = { sizeof(mcontext), DR_MC_ALL, };
     dr_get_mcontext(drcontext, &mcontext);
-    reg_get_value_ex(DR_REG_XAX, &mcontext, orig_reg_val_buf);
-    reg_get_value_ex(DR_REG_XAX, &mcontext, new_reg_val_buf);
-    new_reg_val_buf[0] = 0x75;
-    new_reg_val_buf[2] = 0x83;
-    new_reg_val_buf[3] = 0x23;
-    bool succ = reg_set_value_ex(DR_REG_XAX, &mcontext, new_reg_val_buf, size);
+    reg_get_value_ex(DR_REG_YMM0, &mcontext, orig_reg_val_buf);
+    reg_get_value_ex(DR_REG_YMM0, &mcontext, new_reg_val_buf);
+    new_reg_val_buf[0] = 0x77;
+    new_reg_val_buf[2] = 0x80;
+    new_reg_val_buf[14] = 0x25;
+    new_reg_val_buf[20] = 0x09;
+    new_reg_val_buf[25] = 0x06;
+    bool succ = reg_set_value_ex(DR_REG_YMM0, &mcontext, new_reg_val_buf, 32);
     print_error_on_fail(succ == true);
     dr_set_mcontext(drcontext, &mcontext);
 }
 
-static void check_gpr()
+static void check_ymm()
 {
     void *drcontext = dr_get_current_drcontext();
-    opnd_t reg_opnd = opnd_create_reg(DR_REG_XAX);
-    opnd_size_t size_opnd = opnd_get_size(reg_opnd);
-    size_t size = opnd_size_in_bytes(size_opnd);
     dr_mcontext_t mcontext = { sizeof(mcontext), DR_MC_ALL, };
     dr_get_mcontext(drcontext, &mcontext);
-    reg_get_value_ex(DR_REG_XAX, &mcontext, new_reg_val_buf);
-    print_error_on_fail(new_reg_val_buf[0] == 0x75);
-    print_error_on_fail(new_reg_val_buf[2] == 0x83);
-    print_error_on_fail(new_reg_val_buf[3] == 0x23);
-    bool succ = reg_set_value_ex(DR_REG_XAX, &mcontext, orig_reg_val_buf, size);
+    reg_get_value_ex(DR_REG_XMM0, &mcontext, new_reg_val_buf);
+    print_error_on_fail(new_reg_val_buf[0] == 0x77);
+    print_error_on_fail(new_reg_val_buf[2] == 0x80);
+    print_error_on_fail(new_reg_val_buf[14] == 0x25);
+    print_error_on_fail(new_reg_val_buf[20] == 0x09);
+    print_error_on_fail(new_reg_val_buf[25] == 0x06);
+    bool succ = reg_set_value_ex(DR_REG_YMM0, &mcontext, orig_reg_val_buf, 32);
     print_error_on_fail(succ == true);
     dr_set_mcontext(drcontext, &mcontext);
 }
+
+#ifdef __AVX512F__
+static void set_zmm()
+{
+    void *drcontext = dr_get_current_drcontext();
+    dr_mcontext_t mcontext = { sizeof(mcontext), DR_MC_ALL, };
+    dr_get_mcontext(drcontext, &mcontext);
+    reg_get_value_ex(DR_REG_YMM0, &mcontext, orig_reg_val_buf);
+    reg_get_value_ex(DR_REG_YMM0, &mcontext, new_reg_val_buf);
+    new_reg_val_buf[0] = 0x77;
+    new_reg_val_buf[2] = 0x80;
+    new_reg_val_buf[14] = 0x25;
+    new_reg_val_buf[20] = 0x09;
+    new_reg_val_buf[25] = 0x02;
+    new_reg_val_buf[32] = 0x16;
+    new_reg_val_buf[55] = 0x18;
+    new_reg_val_buf[60] = 0x22;
+    bool succ = reg_set_value_ex(DR_REG_YMM0, &mcontext, new_reg_val_buf, 64);
+    print_error_on_fail(succ == true);
+    dr_set_mcontext(drcontext, &mcontext);
+}
+
+static void check_zmm()
+{
+    void *drcontext = dr_get_current_drcontext();
+    dr_mcontext_t mcontext = { sizeof(mcontext), DR_MC_ALL, };
+    dr_get_mcontext(drcontext, &mcontext);
+    reg_get_value_ex(DR_REG_XMM0, &mcontext, new_reg_val_buf);
+    print_error_on_fail(new_reg_val_buf[0] == 0x77);
+    print_error_on_fail(new_reg_val_buf[2] == 0x80);
+    print_error_on_fail(new_reg_val_buf[14] == 0x25);
+    print_error_on_fail(new_reg_val_buf[20] == 0x09);
+    print_error_on_fail(new_reg_val_buf[25] == 0x02);
+    print_error_on_fail(new_reg_val_buf[32] == 0x16);
+    print_error_on_fail(new_reg_val_buf[55] == 0x18);
+    print_error_on_fail(new_reg_val_buf[60] == 0x22);
+    bool succ = reg_set_value_ex(DR_REG_YMM0, &mcontext, orig_reg_val_buf, 64);
+    print_error_on_fail(succ == true);
+    dr_set_mcontext(drcontext, &mcontext);
+}
+#endif
+
 #endif
 
 static void
@@ -224,17 +299,16 @@ bb_event(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool trans
         dr_insert_clean_call(drcontext, bb, cmp, (void *)cleancall_aflags_save, false, 0);
 
 #ifdef X86
-        /* Another unrelated test for setting register values.
-         * More specifically, this is a test for setting XMM value.
-         * cleancall to store original XMM value and set new value.
-         * cleancall to check new XMM value and restore to the original value.
-         */
+        /* Other unrelated tests for setting register values. */
+        
+        dr_insert_clean_call(drcontext, bb, instr, set_gpr, false, 0);
+        dr_insert_clean_call(drcontext, bb, instr, check_gpr, false, 0);
+
         dr_insert_clean_call(drcontext, bb, instr, set_xmm, false, 0);
         dr_insert_clean_call(drcontext, bb, instr, check_xmm, false, 0);
 
-        /* Another test, setting a gpr. */
-        dr_insert_clean_call(drcontext, bb, instr, set_gpr, false, 0);
-        dr_insert_clean_call(drcontext, bb, instr, check_gpr, false, 0);
+        dr_insert_clean_call(drcontext, bb, instr, set_ymm, false, 0);
+        dr_insert_clean_call(drcontext, bb, instr, check_ymm, false, 0);
 #endif
     }
 
