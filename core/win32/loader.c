DynamoRIO/dynamorio

commit 7bbe9eeaa03f7b85a7114202c3125cab842bed1f

i#3633: Fix Win10 1903 FLS isolation crash (#3663)
On Win10 1903, the FLS fields in the PEB are no longer used, leading to a NULL de-ref in DR.  We fix that here.

For isolation, instead of mirroring 1903 which uses ntdll global variables, we continue using the fields in the private_peb to better handle pre-1903 versions.

Adds ntdll_redir_fls_thread_exit() to unlink priv_fls_data, which makes sure that FLS is removed from internal ntdll.dll in case it ends up there on 1903.

Fixes #3633

core/win32/loader.c
@@ -412,7 +412,11 @@ os_loader_thread_init_epilogue(dcontext_t *dcontext)
 void
 os_loader_thread_exit(dcontext_t *dcontext)
 {
-    /* do nothing in Windows */
+    /* i#3633: In case of windows 1903 if priv_fls_data ends up in internal list of
+     * ntdll.dll, we have to unlink it manually. We do unlinking always on thread
+     * exit.
+     */
+    ntdll_redir_fls_thread_exit(dcontext->priv_fls_data);
 }
 
 void

core/win32/drwinapi/drwinapi.h
@@ -61,4 +61,10 @@ ntdll_redir_fls_init(PEB *app_peb, PEB *private_peb);
 void
 ntdll_redir_fls_exit(PEB *private_peb);
 
+/* i#3633: Fix Windows 1903 issue. FLS is not held inside of PEB but in private
+ * variables inside of ntdll.dll. This routine will perform unlinking to prevent
+ * crash.
+ */
+void
+ntdll_redir_fls_thread_exit(PPVOID fls_data);
 #endif /* _DRWINAPI_H_ */

core/win32/drwinapi/kernel32_proc.c
@@ -48,8 +48,11 @@ void
 kernel32_redir_init_proc(void)
 {
     PEB *peb = get_own_peb();
+    /* i#3633: Implement FLS isolation for Win10 1903+ where FLS data is no longer
+     * in the PEB.
+     */
     ASSERT(get_os_version() < WINDOWS_VERSION_2003 ||
-           peb->FlsBitmap->SizeOfBitMap == FLS_MAX_COUNT);
+           (peb->FlsBitmap == NULL || peb->FlsBitmap->SizeOfBitMap == FLS_MAX_COUNT));
 #ifdef CLIENT_INTERFACE
     /* We rely on -private_peb for FLS isolation.  Otherwise we'd have to
      * put back in place all the code to handle mixing private and app FLS

core/win32/drwinapi/ntdll_redir.c
@@ -838,15 +838,27 @@ redirect_RtlPcToFileHeader(__in PVOID PcValue, __out PVOID *BaseOfImage)
  * i#875: FLS isolation
  */
 
+#ifndef FLS_MAX_COUNT
+#    define FLS_MAX_COUNT 128
+#endif
+
 void
 ntdll_redir_fls_init(PEB *app_peb, PEB *private_peb)
 {
     /* FLS is supported in WinXP-64 or later */
     ASSERT(get_os_version() >= WINDOWS_VERSION_2003);
+
+    /* i#3633: Implement FLS isolation for Win10 1903+ where FLS data is no longer
+     * in the PEB. It is now SparePointers/Ulongs. We will use them as PEB->Fls*.
+     */
+
     /* We need a deep copy of FLS structures */
     private_peb->FlsBitmap =
         HEAP_TYPE_ALLOC(GLOBAL_DCONTEXT, RTL_BITMAP, ACCT_LIBDUP, UNPROTECTED);
-    private_peb->FlsBitmap->SizeOfBitMap = app_peb->FlsBitmap->SizeOfBitMap;
+    if (app_peb->FlsBitmap == NULL)
+        private_peb->FlsBitmap->SizeOfBitMap = FLS_MAX_COUNT;
+    else
+        private_peb->FlsBitmap->SizeOfBitMap = app_peb->FlsBitmap->SizeOfBitMap;
     private_peb->FlsBitmap->BitMapBuffer = (LPBYTE)&private_peb->FlsBitmapBits;
     memset(private_peb->FlsBitmapBits, 0, sizeof(private_peb->FlsBitmapBits));
 
@@ -876,6 +888,33 @@ ntdll_redir_fls_exit(PEB *private_peb)
                     private_peb->FlsBitmap->SizeOfBitMap, ACCT_LIBDUP, UNPROTECTED);
     HEAP_TYPE_FREE(GLOBAL_DCONTEXT, private_peb->FlsBitmap, RTL_BITMAP, ACCT_LIBDUP,
                    UNPROTECTED);
+}
+
+/* i#3633: Fix Windows 1903 issue. FLS is not held inside of PEB but in private
+ * variables inside of ntdll.dll. In case that FLS slips, and priv_fls_data ends up
+ * in ntdll.dll internal struct, we will perform unlinking to prevent crashes which
+ * can happen if priv_fls_data remains inside of ntdll.dll.
+ */
+void
+ntdll_redir_fls_thread_exit(PPVOID fls_data_ptr)
+{
+    PEB *peb = IF_CLIENT_INTERFACE_ELSE(get_private_peb(), get_peb(NT_CURRENT_PROCESS));
+    LIST_ENTRY *fls_data;
+    NTSTATUS res;
+    if (fls_data_ptr == NULL)
+        return;
+
+    res = RtlEnterCriticalSection(peb->FastPebLock);
+    if (!NT_SUCCESS(res))
+        return;
+
+    fls_data = (LIST_ENTRY *)fls_data_ptr;
+
+    fls_data->Flink->Blink = fls_data->Blink;
+    fls_data->Blink->Flink = fls_data->Flink;
+
+    RtlLeaveCriticalSection(peb->FastPebLock);
+    return;
 }
 
 NTSTATUS NTAPI
@@ -931,7 +970,7 @@ redirect_RtlFlsFree(IN DWORD index)
     if (!NT_SUCCESS(res))
         return res;
 
-    bitmap_mark_freed_sequence(peb->TlsBitmap->BitMapBuffer, peb->TlsBitmap->SizeOfBitMap,
+    bitmap_mark_freed_sequence(peb->FlsBitmap->BitMapBuffer, peb->FlsBitmap->SizeOfBitMap,
                                index, 1);
     /* Call the cb, if the slot value is non-NULL */
     if (peb->FlsCallback[index] != NULL &&

commit 7134fcc3ffd1b6ba415e1ab6479019856e48d84d

i#3633 win10-1903: Fix VPS build
Fixes the VPS build broken by 7bbe9ee.

Issue: #36333

core/win32/loader.c
@@ -412,11 +412,13 @@ os_loader_thread_init_epilogue(dcontext_t *dcontext)
 void
 os_loader_thread_exit(dcontext_t *dcontext)
 {
+#ifdef CLIENT_INTERFACE
     /* i#3633: In case of windows 1903 if priv_fls_data ends up in internal list of
      * ntdll.dll, we have to unlink it manually. We do unlinking always on thread
      * exit.
      */
     ntdll_redir_fls_thread_exit(dcontext->priv_fls_data);
+#endif
 }
 
 void