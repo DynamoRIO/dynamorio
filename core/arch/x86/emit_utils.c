DynamoRIO/dynamorio

commit 1beb5463c6cd245b730e9234670125a073aa5395

i#1312 AVX-512 support: Add code cache enter/return context switching. (#3738)
Adds support for dynamic AVX-512 context switching to/from the code cache. DynamoRIO now
saves and restores the AVX-512 context once AVX-512 has been detected in the application.
The latter is called lazy context switching in DynamoRIO. Mask registers are saved with
respect to their width dependent on the AVX-512BW feature support of the processor.

Adds the internal function proc_set_num_simd_saved() used to change the number of saved
SIMD registers that is also returned by proc_num_simd_saved().

Adds the internal function move_mm_avx512_reg_opcode() that is intentionally distinct from
move_mm_reg_opcode(), because its uses are distinct as well.

Renames FEATURE_AVX512 to FEATURE_AVX512F and adds FEATURE_AVX512BW. In the same
context, renames OPMASK_REG_SIZE to OPMASK_AVX512BW_REG_SIZE and adds
OPMASK_AVX512F_REG_SIZE. Both changes reflect the different mask register width dependent
on the AVX-512BW processor feature. DynamoRIO reserves enough space by default for the
wider variant, but different kmov[wq] instructions are used to read and write mask
registers.

For x86, saving SIMD registers during a code-cache context switch has been moved after
saving the flags, because saving SIMD now adds control flow. The same applies to restoring
the SIMD registers, respectively.

Moves the global variable d_r_avx512_code_in_use to reachable heap.

Adds an ATOMIC_1BYTE_WRITE() macro.

Adds a per-thread LOG entry when AVX-512 detection occurs.

Adds an AVX-512 test for above. The test also works in 32-bit mode.

Issue: #1312

core/arch/x86/emit_utils.c
@@ -1335,32 +1335,68 @@ append_restore_xflags(dcontext_t *dcontext, instrlist_t *ilist, bool absolute)
 void
 append_restore_simd_reg(dcontext_t *dcontext, instrlist_t *ilist, bool absolute)
 {
-    if (preserve_xmm_caller_saved()) {
-        /* PR 264138: we must preserve xmm0-5 if on a 64-bit kernel.
-         * Rather than try and optimize we save/restore on every cxt
-         * sw.  The xmm field is aligned, so we can use movdqa/movaps,
-         * though movdqu is stated to be as fast as movdqa when aligned:
-         * but if so, why have two versions?  Is it only loads and not stores
-         * for which that is true?  => PR 266305.
-         * It's not clear that movdqa is any faster (and its opcode is longer):
-         * movdqa and movaps are listed as the same latency and throughput in
-         * the AMD optimization guide.  Yet examples of fast memcpy online seem
-         * to use movdqa when sse2 is available.
-         * Note that mov[au]p[sd] and movdq[au] are functionally equivalent.
-         */
-        /* FIXME i#438: once have SandyBridge processor need to measure
-         * cost of vmovdqu and whether worth arranging 32-byte alignment
-         */
-        int i;
-        uint opcode = move_mm_reg_opcode(true /*align16*/, true /*align32*/);
-        ASSERT(proc_has_feature(FEATURE_SSE));
-        for (i = 0; i < proc_num_simd_saved(); i++) {
+    /* No processor will support AVX-512 but no SSE/AVX. */
+    ASSERT(preserve_xmm_caller_saved() || !ZMM_ENABLED());
+    if (!preserve_xmm_caller_saved())
+        return;
+    /* PR 264138: we must preserve xmm0-5 if on a 64-bit kernel.
+     * Rather than try and optimize we save/restore on every cxt
+     * sw.  The xmm field is aligned, so we can use movdqa/movaps,
+     * though movdqu is stated to be as fast as movdqa when aligned:
+     * but if so, why have two versions?  Is it only loads and not stores
+     * for which that is true?  => PR 266305.
+     * It's not clear that movdqa is any faster (and its opcode is longer):
+     * movdqa and movaps are listed as the same latency and throughput in
+     * the AMD optimization guide.  Yet examples of fast memcpy online seem
+     * to use movdqa when sse2 is available.
+     * Note that mov[au]p[sd] and movdq[au] are functionally equivalent.
+     */
+    /* FIXME i#438: once have SandyBridge processor need to measure
+     * cost of vmovdqu and whether worth arranging 32-byte alignment
+     */
+    int i;
+    uint opcode = move_mm_reg_opcode(true /*align16*/, true /*align32*/);
+    ASSERT(proc_has_feature(FEATURE_SSE));
+    instr_t *post_restore = NULL;
+    instr_t *pre_avx512_restore = NULL;
+    if (ZMM_ENABLED()) {
+        post_restore = INSTR_CREATE_label(dcontext);
+        pre_avx512_restore = INSTR_CREATE_label(dcontext);
+        APP(ilist,
+            INSTR_CREATE_cmp(dcontext, OPND_CREATE_ABSMEM(d_r_avx512_code_in_use, OPSZ_1),
+                             OPND_CREATE_INT8(0)));
+        APP(ilist,
+            INSTR_CREATE_jcc(dcontext, OP_jnz, opnd_create_instr(pre_avx512_restore)));
+    }
+    for (i = 0; i < proc_num_simd_sse_avx_saved(); i++) {
+        APP(ilist,
+            instr_create_1dst_1src(dcontext, opcode,
+                                   opnd_create_reg(REG_SAVED_XMM0 + (reg_id_t)i),
+                                   OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_XMM,
+                                                 SIMD_OFFSET + i * MCXT_SIMD_SLOT_SIZE)));
+    }
+    if (ZMM_ENABLED()) {
+        APP(ilist, INSTR_CREATE_jmp(dcontext, opnd_create_instr(post_restore)));
+        APP(ilist, pre_avx512_restore /*label*/);
+        uint opcode_avx512 = move_mm_avx512_reg_opcode(true /*align64*/);
+        for (i = 0; i < proc_num_simd_registers(); i++) {
             APP(ilist,
-                instr_create_1dst_1src(
-                    dcontext, opcode, opnd_create_reg(REG_SAVED_XMM0 + (reg_id_t)i),
-                    OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_XMM,
+                instr_create_1dst_2src(
+                    dcontext, opcode_avx512,
+                    opnd_create_reg(DR_REG_START_ZMM + (reg_id_t)i),
+                    opnd_create_reg(DR_REG_K0),
+                    OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_ZMM,
                                   SIMD_OFFSET + i * MCXT_SIMD_SLOT_SIZE)));
         }
+        for (i = 0; i < MCXT_NUM_OPMASK_SLOTS; i++) {
+            APP(ilist,
+                instr_create_1dst_1src(
+                    dcontext, proc_has_feature(FEATURE_AVX512BW) ? OP_kmovq : OP_kmovw,
+                    opnd_create_reg(DR_REG_START_OPMASK + (reg_id_t)i),
+                    OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_OPMASK,
+                                  OPMASK_OFFSET + i * OPMASK_AVX512BW_REG_SIZE)));
+        }
+        APP(ilist, post_restore /*label*/);
     }
 }
 
@@ -1558,33 +1594,68 @@ append_save_gpr(dcontext_t *dcontext, instrlist_t *ilist, bool ibl_end, bool abs
 void
 append_save_simd_reg(dcontext_t *dcontext, instrlist_t *ilist, bool absolute)
 {
-    if (preserve_xmm_caller_saved()) {
-        /* PR 264138: we must preserve xmm0-5 if on a 64-bit kernel.
-         * Rather than try and optimize we save/restore on every cxt
-         * sw.  The xmm field is aligned, so we can use movdqa/movaps,
-         * though movdqu is stated to be as fast as movdqa when aligned:
-         * but if so, why have two versions?  Is it only loads and not stores
-         * for which that is true?  => PR 266305.
-         * It's not clear that movdqa is any faster (and its opcode is longer):
-         * movdqa and movaps are listed as the same latency and throughput in
-         * the AMD optimization guide.  Yet examples of fast memcpy online seem
-         * to use movdqa when sse2 is available.
-         * Note that mov[au]p[sd] and movdq[au] are functionally equivalent.
-         */
-        /* FIXME i#438: once have SandyBridge processor need to measure
-         * cost of vmovdqu and whether worth arranging 32-byte alignment
-         */
-        int i;
-        uint opcode = move_mm_reg_opcode(true /*align16*/, true /*align32*/);
-        ASSERT(proc_has_feature(FEATURE_SSE));
-        for (i = 0; i < proc_num_simd_saved(); i++) {
+    /* No processor will support AVX-512 but no SSE/AVX. */
+    ASSERT(preserve_xmm_caller_saved() || !ZMM_ENABLED());
+    if (!preserve_xmm_caller_saved())
+        return;
+    /* PR 264138: we must preserve xmm0-5 if on a 64-bit kernel.
+     * Rather than try and optimize we save/restore on every cxt
+     * sw.  The xmm field is aligned, so we can use movdqa/movaps,
+     * though movdqu is stated to be as fast as movdqa when aligned:
+     * but if so, why have two versions?  Is it only loads and not stores
+     * for which that is true?  => PR 266305.
+     * It's not clear that movdqa is any faster (and its opcode is longer):
+     * movdqa and movaps are listed as the same latency and throughput in
+     * the AMD optimization guide.  Yet examples of fast memcpy online seem
+     * to use movdqa when sse2 is available.
+     * Note that mov[au]p[sd] and movdq[au] are functionally equivalent.
+     */
+    /* FIXME i#438: once have SandyBridge processor need to measure
+     * cost of vmovdqu and whether worth arranging 32-byte alignment
+     */
+    int i;
+    uint opcode = move_mm_reg_opcode(true /*align16*/, true /*align32*/);
+    ASSERT(proc_has_feature(FEATURE_SSE));
+    instr_t *post_save = NULL;
+    instr_t *pre_avx512_save = NULL;
+    if (ZMM_ENABLED()) {
+        post_save = INSTR_CREATE_label(dcontext);
+        pre_avx512_save = INSTR_CREATE_label(dcontext);
+        APP(ilist,
+            INSTR_CREATE_cmp(dcontext, OPND_CREATE_ABSMEM(d_r_avx512_code_in_use, OPSZ_1),
+                             OPND_CREATE_INT8(0)));
+        APP(ilist,
+            INSTR_CREATE_jcc(dcontext, OP_jnz, opnd_create_instr(pre_avx512_save)));
+    }
+    for (i = 0; i < proc_num_simd_sse_avx_saved(); i++) {
+        APP(ilist,
+            instr_create_1dst_1src(dcontext, opcode,
+                                   OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_XMM,
+                                                 SIMD_OFFSET + i * MCXT_SIMD_SLOT_SIZE),
+                                   opnd_create_reg(REG_SAVED_XMM0 + (reg_id_t)i)));
+    }
+    if (ZMM_ENABLED()) {
+        APP(ilist, INSTR_CREATE_jmp(dcontext, opnd_create_instr(post_save)));
+        APP(ilist, pre_avx512_save /*label*/);
+        uint opcode_avx512 = move_mm_avx512_reg_opcode(true /*align64*/);
+        for (i = 0; i < proc_num_simd_registers(); i++) {
+            APP(ilist,
+                instr_create_1dst_2src(
+                    dcontext, opcode_avx512,
+                    OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_ZMM,
+                                  SIMD_OFFSET + i * MCXT_SIMD_SLOT_SIZE),
+                    opnd_create_reg(DR_REG_K0),
+                    opnd_create_reg(DR_REG_START_ZMM + (reg_id_t)i)));
+        }
+        for (i = 0; i < MCXT_NUM_OPMASK_SLOTS; i++) {
             APP(ilist,
                 instr_create_1dst_1src(
-                    dcontext, opcode,
-                    OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_XMM,
-                                  SIMD_OFFSET + i * MCXT_SIMD_SLOT_SIZE),
-                    opnd_create_reg(REG_SAVED_XMM0 + (reg_id_t)i)));
+                    dcontext, proc_has_feature(FEATURE_AVX512BW) ? OP_kmovq : OP_kmovw,
+                    OPND_DC_FIELD(absolute, dcontext, OPSZ_SAVED_OPMASK,
+                                  OPMASK_OFFSET + i * OPMASK_AVX512BW_REG_SIZE),
+                    opnd_create_reg(DR_REG_START_OPMASK + (reg_id_t)i)));
         }
+        APP(ilist, post_save /*label*/);
     }
 }
 

suite/tests/CMakeLists.txt
@@ -2561,6 +2561,12 @@ endif ()
     # XXX i#1312: This is untested on Windows.
     tobuild_ci(client.avx512lazy client-interface/avx512lazy.c "" "" "")
     set_target_properties(client.avx512lazy PROPERTIES COMPILE_FLAGS "${CFLAGS_AVX512}")
+
+    if (UNIX)
+      # XXX i#1312: we do not yet support AVX-512 Windows context switching.
+      tobuild_ci(client.avx512ctx client-interface/avx512ctx.c "" "" "")
+      set_target_properties(client.avx512ctx PROPERTIES COMPILE_FLAGS "${CFLAGS_AVX512}")
+    endif (UNIX)
   endif ()
 
   if (BUILD_SAMPLES)

core/arch/aarch64/proc.c
@@ -74,6 +74,14 @@ proc_num_simd_saved(void)
     return num_simd_saved;
 }
 
+void
+proc_set_num_simd_saved(int num)
+{
+    SELF_UNPROTECT_DATASEC(DATASEC_RARELY_PROT);
+    ATOMIC_4BYTE_WRITE(&num_simd_saved, num, false);
+    SELF_PROTECT_DATASEC(DATASEC_RARELY_PROT);
+}
+
 DR_API
 int
 proc_num_simd_registers(void)

core/arch/arch.c
@@ -86,7 +86,7 @@ static bool sysenter_hook_failed = false;
 #endif
 
 #ifdef X86
-bool d_r_avx512_code_in_use = false;
+bool *d_r_avx512_code_in_use = NULL;
 #endif
 
 /* static functions forward references */
@@ -676,6 +676,10 @@ d_r_arch_init(void)
     ASSERT(DR_REG_YMM16 == DR_REG_YMM15 + 1);
     ASSERT(DR_REG_ZMM16 == DR_REG_ZMM15 + 1);
 #endif
+    /* We rely on the dr_opmask_t register type to be able to store AVX512BW wide 64-bit
+     * masks. Also priv_mcontext_t.opmask slots are AVX512BW wide.
+     */
+    IF_X86(ASSERT(sizeof(dr_opmask_t) == OPMASK_AVX512BW_REG_SIZE));
 
     /* Verify that the structures used for a register spill area and to hold IBT
      * table addresses & masks for IBL code are laid out as expected. We expect
@@ -717,6 +721,15 @@ d_r_arch_init(void)
                IF_CLIENT_INTERFACE_ELSE(5 * sizeof(reg_t), 0));
 
     interp_init();
+
+#ifdef X86
+    /* We're allocating a reachable heap variable in order to be able to use a more
+     * compact rip-rel load in SIMD restore/save gencode.
+     */
+    d_r_avx512_code_in_use = heap_reachable_alloc(
+        GLOBAL_DCONTEXT, sizeof(*d_r_avx512_code_in_use) HEAPACCT(ACCT_OTHER));
+    *d_r_avx512_code_in_use = false;
+#endif
 
 #ifdef CHECK_RETURNS_SSE2
     if (proc_has_feature(FEATURE_SSE2)) {
@@ -899,6 +912,12 @@ void d_r_arch_exit(IF_WINDOWS_ELSE_NP(bool detach_stacked_callbacks, void))
                     VMM_SPECIAL_MMAP | VMM_REACHABLE);
     }
 #endif
+
+#ifdef X86
+    heap_reachable_free(GLOBAL_DCONTEXT, d_r_avx512_code_in_use,
+                        sizeof(*d_r_avx512_code_in_use) HEAPACCT(ACCT_OTHER));
+#endif
+
     interp_exit();
     mangle_exit();
 

core/arch/arch.h
@@ -102,6 +102,7 @@ mixed_mode_enabled(void)
 #        define R15_OFFSET ((MC_OFFS) + (offsetof(priv_mcontext_t, r15)))
 #    endif /* X64 */
 #    define SIMD_OFFSET ((MC_OFFS) + (offsetof(priv_mcontext_t, simd)))
+#    define OPMASK_OFFSET ((MC_OFFS) + (offsetof(priv_mcontext_t, opmask)))
 #    define SCRATCH_REG0 DR_REG_XAX
 #    define SCRATCH_REG1 DR_REG_XBX
 #    define SCRATCH_REG2 DR_REG_XCX
@@ -208,7 +209,9 @@ int
 reg_spill_tls_offs(reg_id_t reg);
 
 #define OPSZ_SAVED_XMM (YMM_ENABLED() ? OPSZ_32 : OPSZ_16)
+#define OPSZ_SAVED_ZMM OPSZ_64
 #define REG_SAVED_XMM0 (YMM_ENABLED() ? REG_YMM0 : REG_XMM0)
+#define OPSZ_SAVED_OPMASK OPSZ_8
 
 /* Xref the partially overlapping CONTEXT_PRESERVE_XMM */
 /* This routine also determines whether ymm registers should be saved. */
@@ -228,22 +231,25 @@ preserve_xmm_caller_saved(void)
 
 #ifdef X86
 /* This is used for AVX-512 context switching and indicates whether AVX-512 has been seen
- * during decode.
+ * during decode. The variable is allocated on reachable heap during initialization.
  */
-extern bool d_r_avx512_code_in_use;
+extern bool *d_r_avx512_code_in_use;
 
 /* This routine determines whether zmm registers should be saved. */
 static inline bool
 d_r_is_avx512_code_in_use()
 {
-    return d_r_avx512_code_in_use;
+    return *d_r_avx512_code_in_use;
 }
 
 static inline void
 d_r_set_avx512_code_in_use(bool in_use)
 {
-    d_r_avx512_code_in_use = in_use;
+    SELF_UNPROTECT_DATASEC(DATASEC_RARELY_PROT);
+    ATOMIC_1BYTE_WRITE(d_r_avx512_code_in_use, in_use, false);
+    SELF_PROTECT_DATASEC(DATASEC_RARELY_PROT);
 }
+
 #endif
 
 typedef enum {
@@ -1368,9 +1374,16 @@ void
 encode_track_it_block(dcontext_t *dcontext, instr_t *instr);
 #endif
 
-/* in instr_shared.c */
+/* In instr_shared.c. */
 uint
 move_mm_reg_opcode(bool aligned16, bool aligned32);
+
+/* In instr_shared.c. We have a separate function for AVX-512, because we do not want to
+ * introduce AVX-512 code if not explicitly requested, due to DynamoRIO's lazy AVX-512
+ * context switching.
+ */
+uint
+move_mm_avx512_reg_opcode(bool aligned64);
 
 /* clean call optimization */
 /* Describes usage of a scratch slot. */

core/arch/arch_exports.h
@@ -59,13 +59,15 @@
 #    define XMM_REG_SIZE 16
 #    define YMM_REG_SIZE 32
 #    define ZMM_REG_SIZE 64
-#    define OPMASK_REG_SIZE 8
+#    define OPMASK_AVX512F_REG_SIZE 8
+#    define OPMASK_AVX512BW_REG_SIZE 8
 #    define MCXT_SIMD_SLOT_SIZE ZMM_REG_SIZE
 #    define MCXT_TOTAL_SIMD_SLOTS_SIZE (MCXT_NUM_SIMD_SLOTS * MCXT_SIMD_SLOT_SIZE)
 /* TODO i#1312: This is currently used to maintain an empty opmask context structure.
  * Actually filling the structure will be added along with a future patch.
  */
-#    define MCXT_TOTAL_OPMASK_SLOTS_SIZE (MCXT_NUM_OPMASK_SLOTS * OPMASK_REG_SIZE)
+#    define MCXT_TOTAL_OPMASK_SLOTS_SIZE \
+        (MCXT_NUM_OPMASK_SLOTS * OPMASK_AVX512BW_REG_SIZE)
 #    define MCXT_TOTAL_SIMD_SSE_AVX_SLOTS_SIZE \
         (MCXT_NUM_SIMD_SSE_AVX_SLOTS * MCXT_SIMD_SLOT_SIZE)
 /* Indicates OS support, not just processor support (xref i#1278) */
@@ -318,6 +320,14 @@ emit_detach_callback_final_jmp(dcontext_t *dcontext,
 /* note that the microsoft compiler will not enregister variables across asm
  * blocks that touch those registers, so don't need to worry about clobbering
  * eax and ebx */
+#    define ATOMIC_1BYTE_WRITE(target, value, hot_patch)                      \
+        do {                                                                  \
+            ASSERT(sizeof(value) == 1);                                       \
+            /* No alignment check necessary, hot_patch parameter provided for \
+             * consistency.                                                   \
+             */                                                               \
+            _InterlockedExchange8((volatile CHAR *)target, (CHAR)value);      \
+        } while (0)
 #    define ATOMIC_4BYTE_WRITE(target, value, hot_patch)                    \
         do {                                                                \
             ASSERT(sizeof(value) == 4);                                     \
@@ -441,6 +451,18 @@ atomic_add_exchange_int64(volatile int64 *var, int64 value)
 /* IA-32 vol 3 7.1.4: processor will internally suppress the bus lock
  * if target is within cache line.
  */
+#        define ATOMIC_1BYTE_WRITE(target, value, hot_patch)                       \
+            do {                                                                   \
+                /* allow a constant to be passed in by supplying our own lvalue */ \
+                char _myval = value;                                               \
+                ASSERT(sizeof(value) == 1);                                        \
+                /* No alignment check necessary, hot_patch parameter provided for  \
+                 * consistency.                                                    \
+                 */                                                                \
+                __asm__ __volatile__("xchgb %0, %1"                                \
+                                     : "+m"(*(char *)(target)), "+r"(_myval)       \
+                                     :);                                           \
+            } while (0)
 #        define ATOMIC_4BYTE_WRITE(target, value, hot_patch)                       \
             do {                                                                   \
                 /* allow a constant to be passed in by supplying our own lvalue */ \

core/arch/arm/proc.c
@@ -99,6 +99,14 @@ proc_num_simd_saved(void)
     return num_simd_saved;
 }
 
+void
+proc_set_num_simd_saved(int num)
+{
+    SELF_UNPROTECT_DATASEC(DATASEC_RARELY_PROT);
+    ATOMIC_4BYTE_WRITE(&num_simd_saved, num, false);
+    SELF_PROTECT_DATASEC(DATASEC_RARELY_PROT);
+}
+
 DR_API
 int
 proc_num_simd_registers(void)

commit c60e8cd823a4f9ab8325de14fb2dfc87a24254e1

i#1312 AVX-512 support: Fix reachable address to executable view.
Fixes a bug introduced in 1beb5463c6cd2, missing the translation to the executable address
view, leading to unreachable 32-bit displacements.

Adds vm_base options to an existing test in order to expose future bugs like above.

Issue: #1312

core/arch/x86/emit_utils.c
@@ -1363,8 +1363,11 @@ append_restore_simd_reg(dcontext_t *dcontext, instrlist_t *ilist, bool absolute)
         post_restore = INSTR_CREATE_label(dcontext);
         pre_avx512_restore = INSTR_CREATE_label(dcontext);
         APP(ilist,
-            INSTR_CREATE_cmp(dcontext, OPND_CREATE_ABSMEM(d_r_avx512_code_in_use, OPSZ_1),
-                             OPND_CREATE_INT8(0)));
+            INSTR_CREATE_cmp(
+                dcontext,
+                OPND_CREATE_ABSMEM(
+                    vmcode_get_executable_addr((byte *)d_r_avx512_code_in_use), OPSZ_1),
+                OPND_CREATE_INT8(0)));
         APP(ilist,
             INSTR_CREATE_jcc(dcontext, OP_jnz, opnd_create_instr(pre_avx512_restore)));
     }
@@ -1622,8 +1625,11 @@ append_save_simd_reg(dcontext_t *dcontext, instrlist_t *ilist, bool absolute)
         post_save = INSTR_CREATE_label(dcontext);
         pre_avx512_save = INSTR_CREATE_label(dcontext);
         APP(ilist,
-            INSTR_CREATE_cmp(dcontext, OPND_CREATE_ABSMEM(d_r_avx512_code_in_use, OPSZ_1),
-                             OPND_CREATE_INT8(0)));
+            INSTR_CREATE_cmp(
+                dcontext,
+                OPND_CREATE_ABSMEM(
+                    vmcode_get_executable_addr((byte *)d_r_avx512_code_in_use), OPSZ_1),
+                OPND_CREATE_INT8(0)));
         APP(ilist,
             INSTR_CREATE_jcc(dcontext, OP_jnz, opnd_create_instr(pre_avx512_save)));
     }

suite/tests/CMakeLists.txt
@@ -2537,7 +2537,10 @@ endif ()
   endif ()
 
   if (X86) # Tests x86 reachability.
-    tobuild_ci(client.reachability client-interface/reachability.c "" "" "")
+    tobuild_ci(client.reachability client-interface/reachability.c ""
+      # The vm_base options were added in order to expose bugs that fail to xl8 reachable
+      # addresses to the executable view within the scheme of satisfy_w_xor_x.
+      "-no_vm_base_near_app -vm_base 0x100000000" "")
   endif ()
 
   if (X86) # FIXME i#1551, i#1569: fix bugs on ARM and AArch64