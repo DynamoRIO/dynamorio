165a228fe1685ce74471fdb4ccd1ca737c6505e3
Hendrik Greving <hgreving@google.com>
2019-09-16 12:35:31 -0700

    Exclude check from 32-bit mode.

--- suite/tests/client-interface/drx-scattergather.dll.c
+++ suite/tests/client-interface/drx-scattergather.dll.c
@@ -73,7 +73,10 @@ event_bb_app2app(void *drcontext, void *tag, instrlist_t *bb, bool for_trace,
     /* Insert a meta instr to test drutil_expand_rep_string() handling it (i#1055) */
     instrlist_meta_preinsert(bb, instrlist_first(bb), INSTR_CREATE_label(drcontext));
     if (!drx_expand_scatter_gather(drcontext, bb, &expanded)) {
-        CHECK(false, "drx_expand_scatter_gather() failed");
+        /* XXX i#2985: The qword versions of scatter/gather are unsupported
+         * in 32-bit mode.
+         */
+        IF_X64(CHECK(false, "drx_expand_scatter_gather() failed"));
     }
     CHECK(scatter_gather_present || (!expanded && instr == NULL),
           "drutil_expand_rep_string_ex bad OUT values");
