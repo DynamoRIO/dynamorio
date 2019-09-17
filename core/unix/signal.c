bf24dbe032e884ec2e86e77e4d4a3cf8ebd02cd4
Derek Bruening <bruening@google.com>
2019-09-17 00:29:49 -0400

    i#3839: Do not block SIGSEGV and SIGBUS at exit time (#3840)
    Fixes a regression from edb2eed where all signals, including SIGSEGV and
    SIGBUS, were blocked while running exit code.  This meant that DR wouldn't
    catch and report crashes, and more importantly clients couldn't use safe_read
    and other features in their exit events.  Now we avoid blocking those two
    key signals.
    
    Tested: Dr. Memory's safe_read uses during exit no longer crash or hang.
    
    Fixes #3839

--- core/unix/signal.c
+++ core/unix/signal.c
@@ -348,8 +348,8 @@ sigprocmask_syscall(int how, kernel_sigset_t *set, kernel_sigset_t *oset,
 }
 
 void
-block_all_signals_except(kernel_sigset_t *oset, int num_signals,
-                         ... /* list of signals */)
+block_all_noncrash_signals_except(kernel_sigset_t *oset, int num_signals,
+                                  ... /* list of signals */)
 {
     kernel_sigset_t set;
     kernel_sigfillset(&set);
@@ -359,6 +359,11 @@ block_all_signals_except(kernel_sigset_t *oset, int num_signals,
         kernel_sigdelset(&set, va_arg(ap, int));
     }
     va_end(ap);
+    /* We never block SIGSEGV or SIGBUS: we need them for various safe reads and to
+     * properly report crashes.
+     */
+    kernel_sigdelset(&set, SIGSEGV);
+    kernel_sigdelset(&set, SIGBUS);
     sigprocmask_syscall(SIG_SETMASK, &set, oset, sizeof(set));
 }
 
