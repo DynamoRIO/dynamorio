d80a98c06fd1dcd9f1aacc68122bc97e794e82fb
johnfxgalea <john.galea@stx.ox.ac.uk>
2019-10-19 03:21:14 +0100

    Moved position to also include ARM

--- core/arch/opnd.h
+++ core/arch/opnd.h
@@ -848,6 +848,9 @@ enum {
     DR_REG_XSP = DR_REG_SP,
 #    endif
 
+/** Number of SIMD vector registers */
+#    define DR_NUM_SIMD_VECTOR_REGS MCXT_NUM_SIMD_SLOTS
+
 #endif /* X86/ARM */
 };
 
@@ -939,10 +942,8 @@ extern const reg_id_t dr_reg_fixer[];
         DR_REG_SPL /**< Start of 8-bit x64-only register enum values*/
 #    define DR_REG_STOP_x64_8 \
         DR_REG_DIL /**< Stop of 8-bit x64-only register enum values */
-#    define DR_REG_START_MMX DR_REG_MM0 /**< Start of mmx register enum values */
-#    define DR_REG_STOP_MMX DR_REG_MM7  /**< End of mmx register enum values */
-/** Number of SIMD vector registers */
-#    define DR_NUM_SIMD_VECTOR_REGS MCXT_NUM_SIMD_SLOTS
+#    define DR_REG_START_MMX DR_REG_MM0   /**< Start of mmx register enum values */
+#    define DR_REG_STOP_MMX DR_REG_MM7    /**< End of mmx register enum values */
 #    define DR_REG_START_XMM DR_REG_XMM0  /**< Start of sse xmm register enum values */
 #    define DR_REG_STOP_XMM DR_REG_XMM31  /**< End of sse xmm register enum values */
 #    define DR_REG_START_YMM DR_REG_YMM0  /**< Start of ymm register enum values */
