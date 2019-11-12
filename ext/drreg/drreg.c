25ec2a2321260b3bd7552b52a249f07eb830bd07
johnfxgalea <john.galea@stx.ox.ac.uk>
2019-11-12 09:54:01 +0000

    Arrange conditionals for writes

--- ext/drreg/drreg.c
+++ ext/drreg/drreg.c
@@ -552,8 +552,7 @@ determine_simd_liveness_state(void *drcontext, instr_t *inst, reg_id_t reg, void
     }
 
     if (instr_writes_to_reg(inst, zmm_reg, DR_QUERY_INCLUDE_COND_SRCS)) {
-        if (instr_writes_to_exact_reg(inst, zmm_reg, DR_QUERY_INCLUDE_COND_SRCS) &&
-            (*value < SIMD_ZMM_DEAD || *value >= SIMD_XMM_LIVE)) {
+        if (instr_writes_to_exact_reg(inst, zmm_reg, DR_QUERY_INCLUDE_COND_SRCS)) {
             *value = SIMD_ZMM_DEAD;
             return true;
         } else if (instr_writes_to_exact_reg(inst, ymm_reg, DR_QUERY_INCLUDE_COND_SRCS) &&
@@ -561,7 +560,7 @@ determine_simd_liveness_state(void *drcontext, instr_t *inst, reg_id_t reg, void
             *value = SIMD_YMM_DEAD;
             return true;
         } else if (instr_writes_to_exact_reg(inst, xmm_reg, DR_QUERY_INCLUDE_COND_SRCS) &&
-                   (*value < SIMD_XMM_DEAD || *value >= SIMD_XMM_LIVE)) {
+                   *value >= SIMD_XMM_LIVE) {
             *value = SIMD_XMM_DEAD;
             return true;
         }
