146e4dcc8ae2c9f970f3d06f97cd571787ceb203
johnfxgalea <john.galea@stx.ox.ac.uk>
2020-02-02 16:09:16 +0000

    Arrange doc

--- ext/drmgr/drmgr.h
+++ ext/drmgr/drmgr.h
@@ -493,14 +493,22 @@ DR_EXPORT
 /**
  * Returns the number of instructions (including meta-instructions) inside a basic block
  * \p bb.
+ *
+ * The function iterates over the bb in order to obtain the count. The result is not
+ * cached. Therefore, avoid using this function during the insert stage of the
+ * instrumentation process.
  */
 size_t
 drmgr_get_bb_size(instrlist_t *bb);
 
 DR_EXPORT
 /**
- * Returns the number of instructions (excluding meta-instructions) inside a basic block
- * \p bb.
+ * Returns the number of application instructions (excluding meta-instructions) inside
+ * a basic block \p bb.
+ *
+ * The function iterates over the bb in order to obtain the count. The result is not
+ * cached. Therefore, avoid using this function during the insert stage of the
+ * instrumentation process.
  */
 size_t
 drmgr_get_bb_app_size(instrlist_t *bb);
