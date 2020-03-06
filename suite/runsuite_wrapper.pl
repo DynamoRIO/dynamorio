cdcbb38e7b8084af3814e442b23f78b0e874c0dc
Derek Bruening <bruening@google.com>
2020-03-06 10:35:36 -0500

    i#2892,i#3980: Ignore known-flaky tests on A64 (#4165)
    Adds drcachesim.invariants (i#2982) and tool.histogram.offline
    (i#3980) to the tests whose failures are ignored, until we can
    reproduce and fix them.
    
    Issue: #2892, #3980

--- suite/runsuite_wrapper.pl
+++ suite/runsuite_wrapper.pl
@@ -230,7 +230,9 @@ for (my $i = 0; $i < $#lines; ++$i) {
                                    'code_api|tool.histogram.gzip' => 1);
             # FIXME i#2417: fix flaky AArch64 tests
             %ignore_failures_64 = ('code_api|linux.sigsuspend' => 1,
-                                   'code_api|pthreads.pthreads_exit' => 1);
+                                   'code_api|pthreads.pthreads_exit' => 1,
+                                   'code_api|tool.drcachesim.invariants' => 1, # i#2892
+                                   'code_api|tool.histogram.offline' => 1); # i#3980
             if ($is_32) {
                 $issue_no = "#2416";
             } else {
