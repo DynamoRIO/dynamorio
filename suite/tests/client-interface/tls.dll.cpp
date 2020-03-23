fb3bb248d8f0d91d73f08a864f02345a6620cbb6
Derek Bruening <bruening@google.com>
2020-03-23 11:38:14 -0400

    i#4215 atomics: Add load and store atomics (#4216)
    Adds 32-bit and 64-bit load and store atomics to core DR and exports
    them to the client API.
    
    Adds missing barriers to the existing store atomic macros on both ARM
    and AArch64.
    
    Adds a sanity test that at least exercises the functions.  Adds the
    test sequence to client.thread and client.tls (only client.tls is
    currently enabled on AArchXX).
    
    Uses dr_atomic_load* in drwrap in place of a previously overkill
    dr_atomic_add*.
    
    These will also help with #2502.
    
    The next step is to add gencode versions of these, either to the core
    or drx.
    
    Issue: #4215, #2502

--- suite/tests/client-interface/tls.dll.cpp
+++ suite/tests/client-interface/tls.dll.cpp
@@ -122,6 +122,30 @@ event_thread_init(void *drcontext)
 #endif
     thread_init_called = true;
     check();
+
+    /* Just a sanity check that these functions operate.  We do not take the
+     * time to set up racing threads or sthg.
+     * This is a duplicate of the test in thread.dll.c, placed here b/c that test
+     * is not yet enabled for AArchXX.
+     */
+    static int counter32;
+    int count = dr_atomic_add32_return_sum(&counter32, 1);
+    ASSERT(count > 0 && count <= counter32);
+    int local_counter;
+    dr_atomic_store32(&local_counter, 42);
+    count = dr_atomic_load32(&local_counter);
+    ASSERT(count == 42);
+    ASSERT(local_counter == 42);
+#ifdef X64
+    static int64 counter64;
+    int64 count64 = dr_atomic_add64_return_sum(&counter64, 1);
+    ASSERT(count64 > 0 && count64 <= counter64);
+    int64 local_counter64;
+    dr_atomic_store64(&local_counter64, 42);
+    count64 = dr_atomic_load64(&local_counter64);
+    ASSERT(count64 == 42);
+    ASSERT(local_counter64 == 42);
+#endif
 }
 
 static void
