9bf85f380d92c20baba430287d9a8bc7d77adfbe
johnfxgalea <john.galea@stx.ox.ac.uk>
2020-05-13 02:59:39 +0100

    Fix typos

--- ext/drmgr/drmgr_priv.h
+++ ext/drmgr/drmgr_priv.h
@@ -50,21 +50,21 @@ extern "C" {
  * For DRBBDUP
  */
 
-/* Responsible for duplicating basic blocks. Returns local_info
+/* Responsible for duplicating basic blocks. Returns local info
  * used to iterate over basic block copies.
  */
 typedef bool (*drmgr_bbdup_duplicate_bb_cb_t)(void *drcontext, void *tag, instrlist_t *bb,
                                               bool for_trace, bool translating,
                                               OUT void **local_info);
 
-/* Extracts and returns the a pending basic block copy from the main basic block. Returns
+/* Extracts and returns a pending basic block copy from the main basic block. Returns
  * NULL, if no further copies are pending.
  */
 typedef instrlist_t *(*drmgr_bbdup_extract_cb_t)(void *drcontext, void *tag,
                                                  instrlist_t *bb, bool for_trace,
                                                  bool translating, void *local_info);
 
-/* Stitches an extract basic block copy back to the main basic block. */
+/* Stitches an extracted basic block copy back to the main basic block. */
 typedef void (*drmgr_bbdup_stitch_cb_t)(void *drcontext, void *tag, instrlist_t *bb,
                                         instrlist_t *case_bb, bool for_trace,
                                         bool translating, void *local_dup_info_opaque);
