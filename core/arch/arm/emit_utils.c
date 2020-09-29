358acfb26d3884f58e7a18509c1c24b79f46867c
Abhinav Anil Sharma <sharmaabhinav@google.com>
2020-09-29 18:32:56 -0400

    Move append_fcache_enter_prologue to aarchxx/emit_utils.c

--- core/arch/arm/emit_utils.c
+++ core/arch/arm/emit_utils.c
@@ -593,68 +593,6 @@ insert_fragment_prefix(dcontext_t *dcontext, fragment_t *f)
 
 /* helper functions for emit_fcache_enter_common */
 
-#define OPND_ARG1 opnd_create_reg(DR_REG_R0)
-
-/* Load DR's TLS base to dr_reg_stolen via reg_base */
-static void
-insert_load_dr_tls_base(dcontext_t *dcontext, instrlist_t *ilist, instr_t *where,
-                        reg_id_t reg_base)
-{
-    /* load TLS base from user-read-only-thread-ID register
-     * mrc p15, 0, reg_base, c13, c0, 3
-     */
-    PRE(ilist, where,
-        INSTR_CREATE_mrc(dcontext, opnd_create_reg(reg_base),
-                         OPND_CREATE_INT(USR_TLS_COPROC_15), OPND_CREATE_INT(0),
-                         opnd_create_reg(DR_REG_CR13), opnd_create_reg(DR_REG_CR0),
-                         OPND_CREATE_INT(USR_TLS_REG_OPCODE)));
-    /* ldr dr_reg_stolen, [reg_base, DR_TLS_BASE_OFFSET] */
-    PRE(ilist, where,
-        XINST_CREATE_load(dcontext, opnd_create_reg(dr_reg_stolen),
-                          OPND_CREATE_MEMPTR(reg_base, DR_TLS_BASE_OFFSET)));
-}
-
-/* Having only one thread register (TPIDRURO) shared between app and DR,
- * we steal a register for DR's TLS base in the code cache,
- * and store DR's TLS base into an private lib's TLS slot for accessing in C code.
- * On entering the code cache (fcache_enter):
- * - grab gen routine's parameter dcontext and put it into REG_DCXT
- * - load DR's TLS base into dr_reg_stolen from privlib's TLS
- */
-void
-append_fcache_enter_prologue(dcontext_t *dcontext, instrlist_t *ilist, bool absolute)
-{
-#ifdef UNIX
-    instr_t *no_signals = INSTR_CREATE_label(dcontext);
-    /* save callee-saved reg in case we return for a signal */
-    APP(ilist,
-        XINST_CREATE_move(dcontext, opnd_create_reg(DR_REG_R1),
-                          opnd_create_reg(REG_DCXT)));
-#endif
-    ASSERT_NOT_IMPLEMENTED(!absolute &&
-                           !TEST(SELFPROT_DCONTEXT, dynamo_options.protect_mask));
-    /* grab gen routine's parameter dcontext and put it into REG_DCXT */
-    APP(ilist, XINST_CREATE_move(dcontext, opnd_create_reg(REG_DCXT), OPND_ARG1 /*r0*/));
-#ifdef UNIX
-    APP(ilist,
-        INSTR_CREATE_ldrsb(dcontext, opnd_create_reg(DR_REG_R2),
-                           OPND_DC_FIELD(absolute, dcontext, OPSZ_1, SIGPENDING_OFFSET)));
-    APP(ilist,
-        XINST_CREATE_cmp(dcontext, opnd_create_reg(DR_REG_R2), OPND_CREATE_INT8(0)));
-    APP(ilist,
-        INSTR_PRED(INSTR_CREATE_b(dcontext, opnd_create_instr(no_signals)), DR_PRED_LE));
-    /* restore callee-saved reg */
-    APP(ilist,
-        XINST_CREATE_move(dcontext, opnd_create_reg(REG_DCXT),
-                          opnd_create_reg(DR_REG_R1)));
-    APP(ilist, INSTR_CREATE_bx(dcontext, opnd_create_reg(DR_REG_LR)));
-    APP(ilist, no_signals);
-#endif
-
-    /* set up stolen reg */
-    insert_load_dr_tls_base(dcontext, ilist, NULL /*append*/, SCRATCH_REG0);
-}
-
 void
 append_call_exit_dr_hook(dcontext_t *dcontext, instrlist_t *ilist, bool absolute,
                          bool shared)
