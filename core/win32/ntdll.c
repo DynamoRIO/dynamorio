157fa8985acca98563b0ee50e80135881e778677
Derek Bruening <bruening@google.com>
2020-12-05 23:21:14 -0500

    Remove diagnostics; impl i#4587 soln: for win10-1511+ always use sysnums from wrappers

--- core/win32/ntdll.c
+++ core/win32/ntdll.c
@@ -593,52 +593,44 @@ syscalls_init()
     /* Prime use_ki_syscall_routines() */
     use_ki_syscall_routines();
 
-    if (syscalls == windows_unknown_syscalls) {
+    if (syscalls == windows_unknown_syscalls ||
+        /* There are variations within the versions we have (e.g., i#4587), so our
+         * static arrays are not foolproof.  It is simpler to just get the ground truth
+         * for any moderately recent version.
+         */
+        get_os_version() >= WINDOWS_VERSION_10_1511) {
         /* i#1598: try to work on new, unsupported Windows versions */
         int i;
         app_pc wrapper;
         ASSERT(ntdllh != NULL);
         for (i = 0; i < SYS_MAX; i++) {
-            print_file(STDERR, "%s: %d==%s 0x%x\n",
-                       __FUNCTION__, i, syscall_names[i], syscalls[i]);
-            if (syscalls[i] == SYSCALL_NOT_PRESENT) { /* presumably matches known ver */
-                print_file(STDERR, "%s: skipping %d==%s\n",
-                           __FUNCTION__, i, syscall_names[i]);
+            if (syscalls[i] == SYSCALL_NOT_PRESENT) /* presumably matches known ver */
                 continue;
-            }
             wrapper = (app_pc)d_r_get_proc_address(ntdllh, syscall_names[i]);
-            print_file(STDERR, "%s: %d==%s => %p\n",
-                       __FUNCTION__, i, syscall_names[i], wrapper);
-            for (int j = 0; j < 30; j++)
-                print_file(STDERR, "  %02x", *(wrapper+j));
-            print_file(STDERR, "\n");
             if (wrapper != NULL && !ALLOW_HOOKER(wrapper)) {
                 syscalls[i] = *((int *)((wrapper) + SYSNUM_OFFS));
-                print_file(STDERR, "%s: set %d==%s to 0x%x\n",
-                           __FUNCTION__, i, syscall_names[i], syscalls[i]);
             }
             /* We ignore TestAlert complications: we don't call it anyway */
         }
+    } else {
+        /* Quick sanity check that the syscall numbers we care about are what's
+         * in our static array.  We still do our later full-decode sanity checks.
+         * This will always be true if we went through the wrapper loop above.
+         */
+        DOCHECK(1, {
+            int i;
+            ASSERT(ntdllh != NULL);
+            for (i = 0; i < SYS_MAX; i++) {
+                if (syscalls[i] == SYSCALL_NOT_PRESENT)
+                    continue;
+                /* note that this check allows a hooker so we'll need a
+                 * better way of determining syscall numbers
+                 */
+                app_pc wrapper = (app_pc)d_r_get_proc_address(ntdllh, syscall_names[i]);
+                CHECK_SYSNUM_AT((byte *)d_r_get_proc_address(ntdllh, syscall_names[i]), i);
+            }
+        });
     }
-    /* quick sanity check that the syscall numbers we care about are what's
-     * in our static array.  we still do our later full-decode sanity checks.
-     */
-    DOCHECK(1, {
-        int i;
-        ASSERT(ntdllh != NULL);
-        for (i = 0; i < SYS_MAX; i++) {
-            if (syscalls[i] == SYSCALL_NOT_PRESENT)
-                continue;
-            /* note that this check allows a hooker so we'll need a
-             * better way of determining syscall numbers
-             */
-            app_pc wrapper = (app_pc)d_r_get_proc_address(ntdllh, syscall_names[i]);
-            print_file(STDERR, "%s: checking %d==%s 0x%x vs %p => 0x%x\n",
-                       __FUNCTION__, i, syscall_names[i], syscalls[i], wrapper,
-                       *((int *)(wrapper + SYSNUM_OFFS)));
-            CHECK_SYSNUM_AT((byte *)d_r_get_proc_address(ntdllh, syscall_names[i]), i);
-        }
-    });
     return true;
 }
 
