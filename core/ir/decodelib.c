DynamoRIO/dynamorio

commit 0d6a8d0880f12761b3cee810c3d6d8c9b0663af8

i#4567: Handle instructions with a single source operand that's an AVX-512 mask register in AT&T style disassembly (#4568)
The mask register is always the first source operand. i here is stale, the result of previous loop. The `instr_get_src` correctly uses a hardcoded zero. But instr_info_opnd_type reuses i and dies if the instruction has a single source operand (because i is always out of range). is_evex_mask_pending is not ever set for Intel style disassembly which is why this bug only affects AT&T syntax.

drdisas -syntax att 62 f2 fe 48 28 c9 will trigger this bug.

This is tested in the (currently ifdefed out) binutils AVX-512 tests.

Also changes usage errors to actually show up (on this error the message is not visible) by printing to stderr and adding a trailing \n so they are not caught up in any output buffering glibc is doing.

Fixes #4567

core/ir/decodelib.c
@@ -128,7 +128,7 @@ get_thread_private_dcontext(void)
 void
 external_error(const char *file, int line, const char *msg)
 {
-    printf("Usage error: %s (%s, line %d)", msg, file, line);
+    fprintf(stderr, "Usage error: %s (%s, line %d)\n", msg, file, line);
     abort();
 }
 

core/ir/disassemble_shared.c
@@ -966,15 +966,16 @@ instr_disassemble_opnds_noimplicit(char *buf, size_t bufsz, size_t *sofar INOUT,
         }
     }
     if (is_evex_mask_pending) {
-        opnd = instr_get_src(instr, 0);
+        int mask_index = 0;
+        opnd = instr_get_src(instr, mask_index);
         CLIENT_ASSERT(IF_X86_ELSE(true, false), "evex mask can only exist for x86.");
-        optype = instr_info_opnd_type(info, !dsts_first(), i);
+        optype = instr_info_opnd_type(info, !dsts_first(), mask_index);
         CLIENT_ASSERT(!instr_is_opmask(instr) && opnd_is_reg(opnd) &&
                           reg_is_opmask(opnd_get_reg(opnd)) && opmask_with_dsts(),
                       "evex mask must always be the first source.");
         print_to_buffer(buf, bufsz, sofar, " {");
         opnd_disassemble_noimplicit(buf, bufsz, sofar, dcontext, instr, optype, opnd,
-                                    false, multiple_encodings, dsts_first(), &i);
+                                    false, multiple_encodings, dsts_first(), &mask_index);
         print_to_buffer(buf, bufsz, sofar, "}");
     }
 }

commit 2d637411357d25396851a5d5757d143260cb3a60

i#4650: Don't reference libc's stderr in code that ends up in libdrdecode.
This fixes a regression in 0d6a8d0880f12761b3cee810c3d6d8c9b0663af8 (from #4567). Instead of using fprintf(stderr) we need to use print_file(STDERR) like other DR code.

Fixes #4650

core/ir/decodelib.c
@@ -128,7 +128,7 @@ get_thread_private_dcontext(void)
 void
 external_error(const char *file, int line, const char *msg)
 {
-    fprintf(stderr, "Usage error: %s (%s, line %d)\n", msg, file, line);
+    print_file(STDERR, "Usage error: %s (%s, line %d)\n", msg, file, line);
     abort();
 }
 