ca8217940608f60ed98804059f596b130eb31ad5
Sotiris Apostolakis <apostolakis.sotiris@gmail.com>
2021-01-31 00:50:18 +0000

    Reserve aflags before clobbering cmp inst on x86

--- clients/drcachesim/tracer/tracer.cpp
+++ clients/drcachesim/tracer/tracer.cpp
@@ -1456,6 +1456,8 @@ event_delay_app_instruction(void *drcontext, void *tag, instrlist_t *bb, instr_t
                                    &instr_count, num_instrs, DRX_COUNTER_64BIT))
         DR_ASSERT(false);
 
+    if (drreg_reserve_aflags(drcontext, bb, instr) != DRREG_SUCCESS)
+        FATAL("Fatal error: failed to reserve aflags");
     reg_id_t scratch = DR_REG_NULL;
     if (op_trace_after_instrs.get_value() < INT_MAX) {
         MINSERT(bb, instr,
@@ -1501,6 +1503,8 @@ event_delay_app_instruction(void *drcontext, void *tag, instrlist_t *bb, instr_t
     /* hit_instr_count_threshold does not always return. Restore scratch registers and
      * aflags.*/
 #        ifdef X86_64
+    drreg_statelessly_restore_app_value(drcontext, bb, DR_REG_NULL, instr, instr, NULL,
+                                        NULL);
     if (scratch != DR_REG_NULL) {
         drreg_statelessly_restore_app_value(drcontext, bb, scratch, instr, instr, NULL,
                                             NULL);
@@ -1519,6 +1523,8 @@ event_delay_app_instruction(void *drcontext, void *tag, instrlist_t *bb, instr_t
     MINSERT(bb, instr, skip_call);
 
 #        ifdef X86_64
+    if (drreg_unreserve_aflags(drcontext, bb, instr) != DRREG_SUCCESS)
+        DR_ASSERT(false);
     if (scratch != DR_REG_NULL) {
         if (drreg_unreserve_register(drcontext, bb, instr, scratch) != DRREG_SUCCESS)
             DR_ASSERT(false);
