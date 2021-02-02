a46bb63752ef115397c2c9f5d1a6a1b5f0c9dce0
Abhinav Anil Sharma <sharmaabhinav@google.com>
2021-02-01 20:38:49 -0500

    Update year in copyright notice.

--- core/arch/arm/emit_utils.c
+++ core/arch/arm/emit_utils.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2014-2020 Google, Inc.  All rights reserved.
+ * Copyright (c) 2014-2021 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
