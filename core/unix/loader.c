DynamoRIO/dynamorio

commit 6667641fe60c6d8d60af186cbf00521e4b6066bb

Avoid blind syslog during init (#4729)
Avoids printing of an internal warning during early initialization for
single-bitwidth setups regardless of -stderr_mask by moving options
init even earlier.

To avoid DR heap init messing up the app's brk setup, moves heap init
out of the options init and into the later half.  This undoes the
early heap init from PR #4726, which is worked around by switching to
a stack buffer for -arch_init.  This seems safer in any case, delaying
heap init and client lib loads until after the app's interpreter is
loaded.

Moves the 1config file deletion from d_r_config_init() to -config_heap_init(),
after any potential reload_dynamorio().

Issue: #4719

core/unix/loader.c
@@ -1935,6 +1935,11 @@ privload_early_inject(void **sp, byte *old_libdr_base, size_t old_libdr_size)
              "Failed to read app ELF headers.  Check path and "
              "architecture.");
 
+    /* Initialize DR's options to avoid syslogs in get_dynamo_library_bounds() and
+     * for the -xarch_root option below.
+     */
+    dynamorio_app_init_part_one_options();
+
     /* Find range of app */
     exe_map = module_vaddr_from_prog_header((app_pc)exe_ld.phdrs, exe_ld.ehdr->e_phnum,
                                             NULL, &exe_end);
@@ -2003,17 +2008,14 @@ privload_early_inject(void **sp, byte *old_libdr_base, size_t old_libdr_size)
     reserve_brk(exe_map + exe_ld.image_size +
                 (INTERNAL_OPTION(separate_private_bss) ? PAGE_SIZE : 0));
 
-    /* Initialize DR's options and heap for the -xarch_root option. */
-    dynamorio_app_init_part_one_options_and_heap();
-
     interp = elf_loader_find_pt_interp(&exe_ld);
     if (interp != NULL) {
-        char *buf = NULL;
         if (!IS_STRING_OPTION_EMPTY(xarch_root) && !os_file_exists(interp, false)) {
-            buf = global_heap_alloc(MAXIMUM_PATH HEAPACCT(ACCT_OTHER));
+            char buf[MAXIMUM_PATH];
             string_option_read_lock();
-            snprintf(buf, MAXIMUM_PATH, "%s/%s", DYNAMO_OPTION(xarch_root), interp);
-            buf[MAXIMUM_PATH - 1] = '\0';
+            snprintf(buf, BUFFER_SIZE_ELEMENTS(buf), "%s/%s", DYNAMO_OPTION(xarch_root),
+                     interp);
+            NULL_TERMINATE_BUFFER(buf);
             string_option_read_unlock();
             if (os_file_exists(buf, false)) {
                 LOG(GLOBAL, LOG_SYSCALLS, 2, "replacing interpreter |%s| with |%s|\n",
@@ -2024,8 +2026,6 @@ privload_early_inject(void **sp, byte *old_libdr_base, size_t old_libdr_size)
         /* Load the ELF pointed at by PT_INTERP, usually ld.so. */
         elf_loader_t interp_ld;
         success = elf_loader_read_headers(&interp_ld, interp);
-        if (buf != NULL)
-            global_heap_free(buf, MAXIMUM_PATH HEAPACCT(ACCT_OTHER));
         apicheck(success, "Failed to read ELF interpreter headers.");
         interp_map = elf_loader_map_phdrs(
             &interp_ld, false /* fixed */, os_map_file, os_unmap_file, os_set_protection,

core/config.c
@@ -624,21 +624,6 @@ d_r_config_init(void)
     config.u.v = &myvals;
     config_read(&config, NULL, 0, CFG_SFX);
     config_initialized = true;
-
-#    ifndef NOT_DYNAMORIO_CORE_PROPER
-    /* i#1271: to avoid leaving a stale 1config file behind if this process
-     * crashes w/o a clean exit, we give up on re-reading the file and delete
-     * it now.  It's an anonymous file anyway and not meant for manual updates.
-     * The user could override the dynamic_options by re-specifying in
-     * the option string, if desired, and re-create the 1config manually.
-     */
-    if (config.has_1config) {
-        INFO(2, "deleting config file %s", config.fname_app);
-        os_delete_file(config.fname_app);
-        dynamo_options.dynamic_options = false;
-    }
-    /* we ignore otherarch having 1config */
-#    endif
 }
 
 #    ifndef NOT_DYNAMORIO_CORE_PROPER
@@ -652,6 +637,21 @@ config_heap_init(void)
                                                                 HEAPACCT(ACCT_OTHER));
     config_reread_vals = (config_vals_t *)global_heap_alloc(sizeof(*config_reread_vals)
                                                                 HEAPACCT(ACCT_OTHER));
+
+    /* i#1271: to avoid leaving a stale 1config file behind if this process
+     * crashes w/o a clean exit, we give up on re-reading the file and delete
+     * it now.  It's an anonymous file anyway and not meant for manual updates.
+     * The user could override the dynamic_options by re-specifying in
+     * the option string, if desired, and re-create the 1config manually.
+     * We do this here and not in d_r_config_init() so we can re-read it
+     * after reload_dynamorio() in privload_early_inject().
+     */
+    if (config.has_1config) {
+        INFO(2, "deleting config file %s", config.fname_app);
+        os_delete_file(config.fname_app);
+        dynamo_options.dynamic_options = false;
+    }
+    /* we ignore otherarch having 1config */
 }
 
 void

core/dynamo.c
@@ -96,6 +96,7 @@ portable and to avoid frequency scaling."
 
 /* global thread-shared variables */
 bool dynamo_initialized = false;
+static bool dynamo_options_initialized = false;
 bool dynamo_heap_initialized = false;
 bool dynamo_started = false;
 bool automatic_startup = false;
@@ -389,14 +390,14 @@ get_dr_stats(void)
 DYNAMORIO_EXPORT int
 dynamorio_app_init(void)
 {
-    dynamorio_app_init_part_one_options_and_heap();
+    dynamorio_app_init_part_one_options();
     return dynamorio_app_init_part_two_finalize();
 }
 
 void
-dynamorio_app_init_part_one_options_and_heap(void)
+dynamorio_app_init_part_one_options(void)
 {
-    if (dynamo_initialized || dynamo_heap_initialized) {
+    if (dynamo_initialized || dynamo_options_initialized) {
         if (standalone_library) {
             REPORT_FATAL_ERROR_AND_EXIT(STANDALONE_ALREADY, 2, get_application_name(),
                                         get_application_pid());
@@ -497,6 +498,26 @@ dynamorio_app_init_part_one_options_and_heap(void)
 #endif
         statistics_init();
 
+        dynamo_options_initialized = true;
+    }
+}
+
+int
+dynamorio_app_init_part_two_finalize(void)
+{
+    if (!dynamo_options_initialized) {
+        /* Part one was never called. */
+        return FAILURE;
+    } else if (dynamo_initialized) {
+        if (standalone_library) {
+            REPORT_FATAL_ERROR_AND_EXIT(STANDALONE_ALREADY, 2, get_application_name(),
+                                        get_application_pid());
+        }
+        /* Nop. */
+    } else if (INTERNAL_OPTION(nullcalls)) {
+        print_file(main_logfile, "** nullcalls is set, NOT taking over execution **\n\n");
+        return SUCCESS;
+    } else {
 #ifdef VMX86_SERVER
         /* Must be before {vmm,d_r}_heap_init() */
         vmk_init_lib();
@@ -513,25 +534,7 @@ dynamorio_app_init_part_one_options_and_heap(void)
 #endif
         d_r_heap_init();
         dynamo_heap_initialized = true;
-    }
-}
 
-int
-dynamorio_app_init_part_two_finalize(void)
-{
-    if (!dynamo_heap_initialized) {
-        /* Part one was never called. */
-        return FAILURE;
-    } else if (dynamo_initialized) {
-        if (standalone_library) {
-            REPORT_FATAL_ERROR_AND_EXIT(STANDALONE_ALREADY, 2, get_application_name(),
-                                        get_application_pid());
-        }
-        /* Nop. */
-    } else if (INTERNAL_OPTION(nullcalls)) {
-        print_file(main_logfile, "** nullcalls is set, NOT taking over execution **\n\n");
-        return SUCCESS;
-    } else {
         /* The process start event should be done after d_r_os_init() but before
          * process_control_int() because the former initializes event logging
          * and the latter can kill the process if a violation occurs.
@@ -1013,6 +1016,7 @@ standalone_exit(void)
     doing_detach = false;
     standalone_library = false;
     dynamo_initialized = false;
+    dynamo_options_initialized = false;
     dynamo_heap_initialized = false;
 }
 #endif
@@ -1646,6 +1650,7 @@ dynamo_exit_post_detach(void)
     do_once_generation++; /* Increment the generation in case we re-attach */
 
     dynamo_initialized = false;
+    dynamo_options_initialized = false;
     dynamo_heap_initialized = false;
     automatic_startup = false;
     control_all_threads = false;

core/globals.h
@@ -614,7 +614,7 @@ DYNAMORIO_EXPORT int
 dynamorio_app_init(void);
 /* dynamorio_app_init() can be called in two parts: */
 void
-dynamorio_app_init_part_one_options_and_heap();
+dynamorio_app_init_part_one_options();
 int
 dynamorio_app_init_part_two_finalize();
 int

commit ebf44729766d49110be50665f4cf3c062e004ba7

i#4719 qemu: Fix regression in interp rewrite
Fixes a bug introduced by PR #4729 which swapped a heap buffer for a
stack buffer but placed the buffer in a too-deep scope.

Manually tested via:
$ qemu-aarch64 -L /usr/aarch64-linux-gnu bin64/drrun -xarch_root /usr/aarch64-linux-gnu -- suite/tests/bin/simple_app
$ qemu-arm -L /usr/arm-linux-gnueabihf bin32/drrun -xarch_root /usr/arm-linux-gnueabihf -- suite/tests/bin/simple_app

Forthcoming test suite support for running under qemu will add CI
tests that will avoid such regressions in the future.

Issue: #4719

core/unix/loader.c
@@ -2010,8 +2010,8 @@ privload_early_inject(void **sp, byte *old_libdr_base, size_t old_libdr_size)
 
     interp = elf_loader_find_pt_interp(&exe_ld);
     if (interp != NULL) {
+        char buf[MAXIMUM_PATH];
         if (!IS_STRING_OPTION_EMPTY(xarch_root) && !os_file_exists(interp, false)) {
-            char buf[MAXIMUM_PATH];
             string_option_read_lock();
             snprintf(buf, BUFFER_SIZE_ELEMENTS(buf), "%s/%s", DYNAMO_OPTION(xarch_root),
                      interp);