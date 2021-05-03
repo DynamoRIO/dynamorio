5dcb84d605c1aec7aa1f65d53d5c2efc8a163727
Abhinav Anil Sharma <sharmaabhinav@google.com>
2021-05-03 10:20:44 -0400

    Remove raw bytes check completely.
    This check didn't work well before this PR (it missed the kmovw case). As the decoded instr should
    be available anyway (full_decode is set when clients process bbs), it is more straight-forward
    to just check the destinations directly.

--- core/ir/x86/instr.c
+++ core/ir/x86/instr.c
@@ -1346,16 +1346,6 @@ instr_may_write_zmm_or_opmask_register(instr_t *instr)
 {
     if (instr_get_prefix_flag(instr, PREFIX_EVEX))
         return true;
-    if (instr_raw_bits_valid(instr) &&
-        /* At least 4 for evex bytes + 1 opcode byte. */
-        instr_length(NULL, instr) >= 5 &&
-        instr_get_raw_byte(instr, 0) == EVEX_PREFIX_OPCODE &&
-        (instr_get_isa_mode(instr) == DR_ISA_AMD64 ||
-         /* If evex, P[3:2] must be 0 and P[10] must be 1. */
-         TESTALL(MODRM_BYTE(3, 0, 0), instr_get_raw_byte(instr, 1))) &&
-        !TEST(0xC, instr_get_raw_byte(instr, 1)) &&
-        TEST(0x04, instr_get_raw_byte(instr, 2)))
-        return true;
 
     for (int i = 0; i < instr_num_dsts(instr); ++i) {
         opnd_t dst = instr_get_dst(instr, i);
