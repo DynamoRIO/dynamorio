6d84fea04a036038db5a3af2e979e77d2cd356c0
Derek Bruening <bruening@google.com>
2021-05-25 22:49:43 -0400

    i#4865: Add emulation instr-only and convenience features (#4920)
    Adds a convenience routine drmgr_in_emulation_region() and flag
    DR_EMULATE_FIRST_INSTR to make it easier to query emulation markers.
    
    Adds a new flag DR_EMULATE_INSTR_ONLY for expansions where the
    sequence being executed should be examined for data references
    and only instruction observation should look at the original
    instruction.  Sets the flag for drutil_expand_rep_string*() and
    drx_expand_scatter_gather().
    
    Clarifies that the flags field added in PR #4877 is a source
    compatibility break if source code was not zeroing the full struct.
    
    Ports the emulation_api_simple test to x86_64.  Augments the test to
    perform a partial DR_EMULATE_INSTR_ONLY emulation as well for both
    platforms.  Adds checks for various invariants in the test.
    
    Future work will add emulation awareness to tracing clients.
    
    Issue: #4866, #4865
    Fixes #4865

--- ext/drmgr/drmgr.h
+++ ext/drmgr/drmgr.h
@@ -815,6 +815,30 @@ typedef enum {
      * block in any case.
      */
     DR_EMULATE_REST_OF_BLOCK = 0x0001,
+    /**
+     * When used with drmgr_in_emulation_region(), indicates that the current
+     * instruction is the first instruction of the emulation region.  This allows
+     * a client to act on the original instruction just once, despite multiple
+     * emulation instructions.
+     */
+    DR_EMULATE_FIRST_INSTR = 0x0002,
+    /**
+     * Indicates that only the instruction fetch is being emulated differently.
+     * The operation of the instruction remains the same.
+     * Observational instrumentation should examine the original instruction
+     * (in #emulated_instr_t.instr) for instruction fetch purposes, but should
+     * examine the emulation sequence for data accesses (e.g., loads and stores for
+     * a memory address tracing tool).  If this flag is not set, instrumentation
+     * should instrument the original instruction in every way and ignore the
+     * emulation sequence.
+     *
+     * This flag is used for instruction refactorings that simplify instrumentation:
+     * e.g., drutil_expand_rep_string() and drx_expand_scatter_gather().  It is not
+     * used for true emulation of an instruction, for example, for replacing an
+     * instruction that is not supported by the current hardware with
+     * an alternative sequence of instructions.
+     */
+    DR_EMULATE_INSTR_ONLY = 0x0004,
 } dr_emulate_options_t;
 
 /**
@@ -909,6 +933,39 @@ drmgr_is_emulation_end(instr_t *instr);
 DR_EXPORT
 bool
 drmgr_get_emulated_instr_data(instr_t *instr, OUT emulated_instr_t *emulated);
+
+/**
+ * Must be called during drmgr's insertion phase.  Returns whether the current
+ * instruction in the phase is inside an emulation region.  If it returns true,
+ * \p emulation_info is written with a pointer to information about the emulation.
+ * The pointed-at information's lifetime is the full range of the emulation region.
+ *
+ * This is a convenience routine that records the state for the
+ * drmgr_is_emulation_start() and drmgr_is_emulation_end() labels and the
+ * #DR_EMULATE_REST_OF_BLOCK flag to prevent the client having to store state.
+ *
+ * This is the recommended usage for observational clients in the insertion event,
+ * highlighting the different paths for instruction versus data instrumentation:
+ *
+ *    const emulated_instr_t *emulation;
+ *    if (drmgr_in_emulation_region(drcontext, &emulation)) {
+ *        if (TEST(DR_EMULATE_FIRST_INSTR, emulation->flags)) {
+ *            record_instr_fetch(emulation->instr);
+ *            if (!TEST(DR_EMULATE_INSTR_ONLY, emulation->flags))
+ *                record_data_addresses(emulation->instr);
+ *        } // Else skip further instr fetches until outside emulation region.
+ *        if (instr_is_app(exec_instr) && TEST(DR_EMULATE_INSTR_ONLY, emulation->flags))
+ *            record_data_addresses(exec_instr);
+ *    } else if (instr_is_app(exec_instr)) {
+ *        record_instr_fetch(exec_instr);
+ *        record_data_addresses(exec_instr);
+ *    }
+ *
+ * \return false if the caller's \p emulated_instr_t is not compatible, true otherwise.
+ */
+DR_EXPORT
+bool
+drmgr_in_emulation_region(void *drcontext, OUT const emulated_instr_t **emulation_info);
 
 /***************************************************************************
  * UTILITIES
