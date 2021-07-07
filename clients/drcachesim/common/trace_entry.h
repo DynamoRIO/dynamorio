5ffacec738f182a29d20ec642a7df63712766644
Derek Bruening <bruening@google.com>
2021-07-07 16:55:14 -0400

    Augment docs for new rseq entry; add ld-2.31.so binary to new test; add other-arch failure conditions

--- clients/drcachesim/common/trace_entry.h
+++ clients/drcachesim/common/trace_entry.h
@@ -321,7 +321,8 @@ typedef enum {
 
     /**
      * Serves to further identify #TRACE_MARKER_TYPE_KERNEL_EVENT as a
-     * restartable sequence abort handler.
+     * restartable sequence abort handler.  This will always be immediately followed
+     * by #TRACE_MARKER_TYPE_KERNEL_EVENT.
      */
     TRACE_MARKER_TYPE_RSEQ_ABORT,
 
