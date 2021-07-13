a0ac11672a59276d87d609d5b599fa89ddd3a32e
JerryYouxin <youxin2015@outlook.com>
2021-07-13 11:49:57 +0800

    use IF_DEBUG instead

--- ext/drx/drx.c
+++ ext/drx/drx.c
@@ -220,8 +220,9 @@ bool
 drx_aflags_are_dead(instr_t *where)
 {
     bool dead = false;
-    drreg_status_t res = drreg_are_aflags_dead(dr_get_current_drcontext(), where, &dead);
-    DR_ASSERT_MSG(res == DRREG_SUCCESS, "drreg_are_aflags_dead failed!");
+    IF_DEBUG(drreg_status_t res =)
+    drreg_are_aflags_dead(dr_get_current_drcontext(), where, &dead);
+    ASSERT(res == DRREG_SUCCESS, "drreg_are_aflags_dead failed!");
     return dead;
 }
 
