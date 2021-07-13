8f85344c737c841138b07920df775bab2e747fe1
Nofix <16479266+N0fix@users.noreply.github.com>
2021-07-13 21:36:06 +0200

    i#4990: Fix support for inject_x64 inside WOW64 processes (#4989)
    Fixes issues around the -inject_x64 prototype option added by PR #4653 for #803 to enable injecting a 64-bit DR into a WOW64 (32-bit) child ("mixed mode").
    
    Xref discussion at https://groups.google.com/g/dynamorio-users/c/rhEpslerwf8
    
    Adds a new option -vmheap_size_wow64 since the default x64 size will not fit in a WOW64 process.
    Saves eax register that holds routine address for RtlUserThreadStart before mode switch, and restores it on mode switch.
    Fixes far jmp to switch to x64 mode on injection.
    Fixes env variable argument propagation.
    
    Example command line that works :
    
      $ bin64\drrun.exe -inject_x64 -c .\clientdll.dll -- bin64\create_process.exe .\helloworld32.exe
    
    We still need to add proper support on drrun64 to inject natively without having to use create_process.exe.
    
    Issue: #49, #4990

--- core/win32/syscall.c
+++ core/win32/syscall.c
@@ -1744,7 +1744,10 @@ propagate_options_via_env_vars(dcontext_t *dcontext, HANDLE process_handle,
      * model without requiring setting up config files for children.
      */
     uint64 peb;
-    bool peb_is_32 = is_32bit_process(process_handle);
+    bool peb_is_32 = is_32bit_process(process_handle)
+        // If x64 client targeting WOW64 we need to
+        // target x64 PEB.
+        IF_X64(&&!DYNAMO_OPTION(inject_x64));
     size_t sz_read;
     union {
         uint64 ptr_64;
@@ -2961,7 +2964,12 @@ pre_system_call(dcontext_t *dcontext)
     reg_t *param_base = pre_system_call_param_base(mc);
     dr_where_am_i_t old_whereami = dcontext->whereami;
     dcontext->whereami = DR_WHERE_SYSCALL_HANDLER;
-    IF_X64(ASSERT_TRUNCATE(sysnum, int, mc->xax));
+    /* XXX i#49: mc->rax's top bits are non-zero in 32-bit mode for
+     * reasons we do not yet understand.
+     * For now we disable the assert for mixed-mode.
+     */
+    IF_X64(
+        ASSERT(is_wow64_process(NT_CURRENT_PROCESS) || CHECK_TRUNCATE_TYPE_int(mc->xax)));
     DODEBUG(dcontext->expect_last_syscall_to_fail = false;);
 
     KSTART(pre_syscall);
