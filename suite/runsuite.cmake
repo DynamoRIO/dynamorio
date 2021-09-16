df8d4887976428c52e67695661d92237787e13ca
Derek Bruening <bruening@google.com>
2021-09-15 23:27:47 -0400

    Add new DYNAMORIO_CLANG env var to replace the old CC=clang* for disabling clang tests

--- suite/runsuite.cmake
+++ suite/runsuite.cmake
@@ -82,7 +82,7 @@ set(build_tests "BUILD_TESTS:BOOL=ON")
 if (arg_automated_ci)
   # XXX i#1801, i#1962: under clang we have several failing tests.  Until those are
   # fixed, our CI clang suite only builds and does not run tests.
-  if (UNIX AND NOT APPLE AND "$ENV{CC}" MATCHES "clang")
+  if (UNIX AND NOT APPLE AND "$ENV{DYNAMORIO_CLANG}" MATCHES "yes")
     set(run_tests OFF)
     message("Detected a CI clang suite: disabling running of tests")
   endif ()
