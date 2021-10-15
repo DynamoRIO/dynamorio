a88dd81fe116bbd10a6e4ee8c5b982e3f1a35e62
Felix Moessbauer <felix.moessbauer@siemens.com>
2021-10-15 18:07:51 +0200

    RFC: drrun: add support to inject from a gnu-dir-layout DR install
    This patch extends the DynamoRIO frontend and drdeploy to
    support injection from a system-wide install with GNU layout.
    
    To activate the support, pass "/" as DynamoRIO root.
    This should not conflict with traditional DR roots, as a traditonal
    install into the system's root folder is not likely or expected.
    
    The new injection logic works (at least on x86) targets for both
    release / debug and i386 / x64 versions.
    
    Further, the drrun from a GNU layout install can be used to execute
    from a traditional DynamoRIO root by just specifying the path
    to the directory.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- libutil/dr_config.c
+++ libutil/dr_config.c
@@ -1,6 +1,7 @@
 /* **********************************************************
  * Copyright (c) 2011-2021 Google, Inc.  All rights reserved.
  * Copyright (c) 2008-2010 VMware, Inc.  All rights reserved.
+ * Copyright (c) 2021 Siemens AG
  * **********************************************************/
 
 /*
@@ -81,6 +82,18 @@ d_r_snprintf_wide(wchar_t *s, size_t max, const wchar_t *fmt, ...);
 #    define DEBUG32_DLL "/lib32/debug/libdynamorio.so"
 #    define RELEASE64_DLL "/lib64/release/libdynamorio.so"
 #    define DEBUG64_DLL "/lib64/debug/libdynamorio.so"
+#    define GNU_DEBUG32_DLL                            \
+        "usr/lib/i386-linux-gnu/DynamoRIO-" STRINGIFY( \
+            SOVERSION) "/debug/libdynamorio.so." STRINGIFY(SOVERSION)
+#    define GNU_RELEASE32_DLL                          \
+        "usr/lib/i386-linux-gnu/DynamoRIO-" STRINGIFY( \
+            SOVERSION) "/release/libdynamorio.so." STRINGIFY(SOVERSION)
+#    define GNU_DEBUG64_DLL                              \
+        "usr/lib/x86_64-linux-gnu/DynamoRIO-" STRINGIFY( \
+            SOVERSION) "/debug/libdynamorio.so." STRINGIFY(SOVERSION)
+#    define GNU_RELEASE64_DLL                            \
+        "usr/lib/x86_64-linux-gnu/DynamoRIO-" STRINGIFY( \
+            SOVERSION) "/release/libdynamorio.so." STRINGIFY(SOVERSION)
 #    define LOG_SUBDIR "/logs"
 #    define LIB32_SUBDIR "/lib32/"
 #    undef _sntprintf
@@ -1154,18 +1167,43 @@ dr_register_process(const char *process_name, process_id_t pid, bool global,
     if (status != DR_SUCCESS)
         return status;
 
-    /* set the autoinject string (i.e., path to dynamorio.dll */
-    if (debug) {
-        if (!platform_is_64bit(dr_platform))
-            _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) DEBUG32_DLL, dr_root_dir);
-        else
-            _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) DEBUG64_DLL, dr_root_dir);
+        /* set the autoinject string (i.e., path to dynamorio.dll */
+        /* TODO: cleanup and simplify */
+#ifndef WINDOWS
+    if (strcmp(dr_root_dir, "/") == 0) {
+        if (debug) {
+            if (!platform_is_64bit(dr_platform))
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) GNU_DEBUG32_DLL,
+                           dr_root_dir);
+            else
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) GNU_DEBUG64_DLL,
+                           dr_root_dir);
+        } else {
+            if (!platform_is_64bit(dr_platform))
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) GNU_RELEASE32_DLL,
+                           dr_root_dir);
+            else
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) GNU_RELEASE64_DLL,
+                           dr_root_dir);
+        }
     } else {
-        if (!platform_is_64bit(dr_platform))
-            _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) RELEASE32_DLL, dr_root_dir);
-        else
-            _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) RELEASE64_DLL, dr_root_dir);
+#endif
+        if (debug) {
+            if (!platform_is_64bit(dr_platform))
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) DEBUG32_DLL, dr_root_dir);
+            else
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) DEBUG64_DLL, dr_root_dir);
+        } else {
+            if (!platform_is_64bit(dr_platform))
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) RELEASE32_DLL,
+                           dr_root_dir);
+            else
+                _sntprintf(wbuf, MAXIMUM_PATH, _TEXT(TSTR_FMT) RELEASE64_DLL,
+                           dr_root_dir);
+        }
+#ifndef WINDOWS
     }
+#endif
     NULL_TERMINATE_BUFFER(wbuf);
     status = write_config_param(IF_REG_ELSE(proc_policy, f),
                                 PARAM_STR(DYNAMORIO_VAR_AUTOINJECT), wbuf);
