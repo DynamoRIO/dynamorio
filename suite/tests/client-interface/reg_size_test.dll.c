47bdeb564bdb5f369174689dcb6f10186e756822
JerryYouxin <youxin2015@outlook.com>
2021-11-15 09:03:15 +0000

    avoid x86_32 test error

--- suite/tests/client-interface/reg_size_test.dll.c
+++ suite/tests/client-interface/reg_size_test.dll.c
@@ -20,6 +20,8 @@ dr_client_main(client_id_t id, int argc, const char *argv[])
 #if defined(X86) && !defined(X64)
         if (i >= REG_START_64 && i <= REG_STOP_64)
             continue;
+        if (i >= REG_START_x64_8 && i <= REG_STOP_x64_8)
+            continue;
 #endif
         res = reg_get_size(i);
     }
