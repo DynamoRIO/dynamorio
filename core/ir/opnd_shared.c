ad30befeeebf9700b3888cbacf146292d36571a6
JerryYouxin <youxin2015@outlook.com>
2021-11-17 14:30:59 +0800

    Fix missing reg encoding info for aarch64 (#5206)
    The register information (including reg_get_size() and register name reg_names) is incomplete for AArch64, missing DR_REG_CNTVCT_EL0, DR_REG_NZCV, DR_REG_FPCR, and DR_REG_FPSR. This lack of information will cause client failure as follows (e.g., client tool called reg_get_size(DR_REG_CNTVCT_EL0)):
    
    ...
    <Application /usr/bin/ls (60103) DynamoRIO usage error : reg_get_size: invalid register>
    <Usage error: reg_get_size: invalid register (/home/yx/dynamorio/core/ir/opnd_shared.c, line 2538)
    ...
    
    I have added this information via this PR and added a CI test to early-detect similar information lacks for possible future extensions. These CI tests are listed as code_api|client.reg_size_test.*

--- core/ir/opnd_shared.c
+++ core/ir/opnd_shared.c
@@ -2531,10 +2531,16 @@ reg_get_size(reg_id_t reg)
         return OPSZ_SCALABLE;
     if (reg >= DR_REG_P0 && reg <= DR_REG_P15)
         return OPSZ_SCALABLE_PRED;
+    if (reg == DR_REG_CNTVCT_EL0)
+        return OPSZ_8;
+    if (reg >= DR_REG_NZCV && reg <= DR_REG_FPSR)
+        return OPSZ_8;
 #    endif
     if (reg == DR_REG_TPIDRURW || reg == DR_REG_TPIDRURO)
         return OPSZ_PTR;
 #endif
+    LOG(GLOBAL, LOG_ANNOTATIONS, 2, "reg=%d, %s, last reg=%d\n", reg,
+        get_register_name(reg), DR_REG_LAST_ENUM);
     CLIENT_ASSERT(false, "reg_get_size: invalid register");
     return OPSZ_NA;
 }
