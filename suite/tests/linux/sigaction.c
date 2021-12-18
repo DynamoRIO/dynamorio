fd490142f1cf3f8ce87670714455d93f81c007dd
Abhinav Anil Sharma <sharmaabhinav@google.com>
2021-12-18 15:54:11 -0500

    Block SIGBUS only.

--- suite/tests/linux/sigaction.c
+++ suite/tests/linux/sigaction.c
@@ -138,14 +138,14 @@ static void
 test_sigprocmask()
 {
 #if defined(MACOS)
-    sigset_t new = 0xf00d, old, original;
+    sigset_t new, old, original;
     /* We explicitly add SIGSEGV to the blocked set as it is one of the
      * signals that DR intercepts.
      */
+    sigemptyset(&new);
     sigaddset(&new, SIGBUS);
-    sigdelset(&new, SIGSEGV);
 #else
-    uint64 new = (0xf00d | (1<<SIGBUS)) & ~(1<<SIGSEGV), old, original;
+    uint64 new = (1 << SIGBUS), old, original;
 #endif
     /* Save original sigprocmask. Both return the current sigprocmask. */
     assert(make_sigprocmask(SIG_SETMASK, NULL, &original,
