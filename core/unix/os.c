a3504dc0a4b0bbbef5adae105924905df534b72a
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-17 09:04:57 -0500

    include os_private.h to ensure definitions match

--- core/unix/os.c
+++ core/unix/os.c
@@ -64,6 +64,7 @@
 #include "../globals.h"
 #include "../hashtable.h"
 #include "../native_exec.h"
+#include "os_private.h"
 #include <unistd.h> /* for write and usleep and _exit */
 #include <limits.h>
 
