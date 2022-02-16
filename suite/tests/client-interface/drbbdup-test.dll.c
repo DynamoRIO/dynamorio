d16f93ef6c8702dfa7709f9ee192bcb4058ce8aa
Derek Bruening <bruening@google.com>
2022-02-16 21:15:20 +0000

    i#5356 bbdup-wrap: Add extended drbbdup instrumentation callbacks (#5361)
    Adds new fields to the drbbdup options that point at a
    case analysis handler and an instrumentation handler
    that both take in the "for_trace" and "translating" parameters
    and return dr_emit_flags_t.  For each, only one of the
    original or the new extended version must be set.
    
    Adds a simple sanity check of the parameters.  Testing the return
    value is difficult, unfortunately.
    
    Issue: #5356, #3995, #4134

--- suite/tests/client-interface/drbbdup-test.dll.c
+++ suite/tests/client-interface/drbbdup-test.dll.c
@@ -64,6 +64,8 @@ static bool enable_dups_flag = false;
 /* Counters to test statistics provided by drbbdup. */
 static unsigned long no_dup_count = 0;
 static unsigned long no_dynamic_handling_count = 0;
+static unsigned long count_for_trace = 0;
+static unsigned long count_analyze_for_trace = 0;
 
 static uintptr_t
 set_up_bb_dups(void *drbbdup_ctx, void *drcontext, void *tag, instrlist_t *bb,
@@ -109,12 +111,16 @@ destroy_orig_analysis(void *drcontext, void *user_data, void *orig_analysis_data
     orig_analysis_destroy_called = true;
 }
 
-static void
-analyse_bb(void *drcontext, void *tag, instrlist_t *bb, uintptr_t encoding,
-           void *user_data, void *orig_analysis_data, void **analysis_data)
+static dr_emit_flags_t
+analyse_bb(void *drcontext, void *tag, instrlist_t *bb, bool for_trace, bool translating,
+           uintptr_t encoding, void *user_data, void *orig_analysis_data,
+           void **analysis_data)
 {
     CHECK(user_data == USER_DATA_VAL, "user data does not match");
     CHECK(orig_analysis_data == ORIG_ANALYSIS_VAL, "orig analysis data does not match");
+
+    if (for_trace)
+        ++count_analyze_for_trace;
 
     switch (encoding) {
     case 0:
@@ -131,6 +137,8 @@ analyse_bb(void *drcontext, void *tag, instrlist_t *bb, uintptr_t encoding,
         break;
     default: CHECK(false, "invalid encoding");
     }
+
+    return DR_EMIT_DEFAULT;
 }
 
 static void
@@ -177,16 +185,19 @@ print_case(uintptr_t case_val)
     dr_fprintf(STDERR, "case %u\n", case_val);
 }
 
-static void
+static dr_emit_flags_t
 instrument_instr(void *drcontext, void *tag, instrlist_t *bb, instr_t *instr,
-                 instr_t *where, uintptr_t encoding, void *user_data,
-                 void *orig_analysis_data, void *analysis_data)
+                 instr_t *where, bool for_trace, bool translating, uintptr_t encoding,
+                 void *user_data, void *orig_analysis_data, void *analysis_data)
 {
     bool is_first, is_first_nonlabel;
     drbbdup_status_t res;
 
     CHECK(user_data == USER_DATA_VAL, "user data does not match");
     CHECK(orig_analysis_data == ORIG_ANALYSIS_VAL, "orig analysis data does not match");
+
+    if (for_trace)
+        ++count_for_trace;
 
     switch (encoding) {
     case 0:
@@ -215,6 +226,7 @@ instrument_instr(void *drcontext, void *tag, instrlist_t *bb, instr_t *instr,
         dr_insert_clean_call(drcontext, bb, where, print_case, false, 1,
                              OPND_CREATE_INTPTR(encoding));
     }
+    return DR_EMIT_DEFAULT;
 }
 
 static void
@@ -246,6 +258,14 @@ event_exit(void)
 
     CHECK(instrum_called, "instrumentation was not inserted");
 
+    /* Sanity check that the _ex parameters are passed.
+     * We'd like to test the dr_emit_flags_t return value too but it's not
+     * easy to do that.
+     * XXX i#1668,i#2974: x86-only because traces are not yet implemented on aarchxx.
+     */
+    IF_X86(CHECK(count_analyze_for_trace > 0, "for_trace was never passed"));
+    IF_X86(CHECK(count_for_trace > 0, "for_trace was never passed"));
+
     drmgr_exit();
 }
 
@@ -260,9 +280,9 @@ dr_init(client_id_t id)
     opts.insert_encode = insert_encode;
     opts.analyze_orig = orig_analyse_bb;
     opts.destroy_orig_analysis = destroy_orig_analysis;
-    opts.analyze_case = analyse_bb;
+    opts.analyze_case_ex = analyse_bb;
     opts.destroy_case_analysis = destroy_analysis;
-    opts.instrument_instr = instrument_instr;
+    opts.instrument_instr_ex = instrument_instr;
     opts.runtime_case_opnd = OPND_CREATE_ABSMEM(&encode_val, OPSZ_PTR);
     /* Though single-threaded, we sanity-check the atomic load feature. */
     opts.atomic_load_encoding = true;
