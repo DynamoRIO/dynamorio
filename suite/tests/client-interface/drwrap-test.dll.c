DynamoRIO/dynamorio

commit 5da8df6d9f80997d81323cb218b25b75cbdd0a0d

i#2502 locks: Add acquires to bare loads in lock routines (#5370)
Adds load-acquire semantics to a number of bare loads (i.e., not
inside a mutex) in DR's various lock routines.

This includes a key missing load-acquire in the spinlock loop in
d_r_mutex_lock_app() which might help explain some performance issues
such as #4279.

Since some of these are in the utils.h header, and we have ordering
dependencies between utils.h and arch_exports.h, this required
splitting the atomic defines out of arch/arch_exports.h into a new
file arch/atomic_exports.h.  Since those atomic defines use ASSERT
which references dynamo_options, this further required splitting the
option struct out of options.h into options_struct.h.

Issue: #2502, #4928, #4279

core/utils.c
@@ -866,7 +866,7 @@ d_r_mutex_lock_app(mutex_t *lock, priv_mcontext_t *mc)
                when the lock->lock_requests > 0 (which means that at least one thread is
                already blocked).  And of course, we also break if it is LOCK_FREE_STATE.
             */
-            if (lock->lock_requests != LOCK_SET_STATE) {
+            if (atomic_aligned_read_int(&lock->lock_requests) != LOCK_SET_STATE) {
 #ifdef DEADLOCK_AVOIDANCE
                 lock->count_times_spin_only++;
 #endif
@@ -988,6 +988,7 @@ d_r_mutex_mark_as_app(mutex_t *lock)
 static inline void
 own_recursive_lock(recursive_lock_t *lock)
 {
+    ASSERT(OWN_MUTEX(&lock->lock));
     ASSERT(lock->owner == INVALID_THREAD_ID);
     ASSERT(lock->count == 0);
     lock->owner = d_r_get_thread_id();
@@ -1002,8 +1003,7 @@ acquire_recursive_app_lock(recursive_lock_t *lock, priv_mcontext_t *mc)
        busy try_lock
     */
 
-    /* ASSUMPTION: reading owner field is atomic */
-    if (lock->owner == d_r_get_thread_id()) {
+    if (ATOMIC_READ_THREAD_ID(&lock->owner) == d_r_get_thread_id()) {
         lock->count++;
     } else {
         d_r_mutex_lock_app(&lock->lock, mc);
@@ -1021,8 +1021,7 @@ acquire_recursive_lock(recursive_lock_t *lock)
 bool
 try_recursive_lock(recursive_lock_t *lock)
 {
-    /* ASSUMPTION: reading owner field is atomic */
-    if (lock->owner == d_r_get_thread_id()) {
+    if (ATOMIC_READ_THREAD_ID(&lock->owner) == d_r_get_thread_id()) {
         lock->count++;
     } else {
         if (!d_r_mutex_trylock(&lock->lock))
@@ -1035,6 +1034,7 @@ try_recursive_lock(recursive_lock_t *lock)
 void
 release_recursive_lock(recursive_lock_t *lock)
 {
+    ASSERT(OWN_MUTEX(&lock->lock));
     ASSERT(lock->owner == d_r_get_thread_id());
     ASSERT(lock->count > 0);
     lock->count--;
@@ -1047,8 +1047,7 @@ release_recursive_lock(recursive_lock_t *lock)
 bool
 self_owns_recursive_lock(recursive_lock_t *lock)
 {
-    /* ASSUMPTION: reading owner field is atomic */
-    return (lock->owner == d_r_get_thread_id());
+    return (ATOMIC_READ_THREAD_ID(&lock->owner) == d_r_get_thread_id());
 }
 
 /* Read write locks */
@@ -1121,6 +1120,7 @@ d_r_read_lock(read_write_lock_t *rw)
                  * FIXME: we could also reorganize this check so that it is done only once
                  * instead of in the loop body but it doesn't seem wortwhile
                  */
+                /* We have the lock so we do not need a load-acquire. */
                 if (rw->writer == d_r_get_thread_id()) {
                     /* we would share the code below but we do not want
                      * the deadlock avoidance to consider this an acquire
@@ -1155,6 +1155,7 @@ d_r_read_lock(read_write_lock_t *rw)
              * Update: linux d_r_get_thread_id() now calls get_tls_thread_id()
              * and avoids the syscall (xref PR 473640).
              */
+            /* We have the lock so we do not need a load-acquire. */
             if (rw->writer == d_r_get_thread_id()) {
                 /* we would share the code below but we do not want
                  * the deadlock avoidance to consider this an acquire
@@ -1224,6 +1225,7 @@ d_r_write_lock(read_write_lock_t *rw)
      */
     if (INTERNAL_OPTION(spin_yield_rwlock)) {
         d_r_mutex_lock(&rw->lock);
+        /* We have the lock so we do not need a load-acquire. */
         while (rw->num_readers > 0) {
             /* contended write */
             DEADLOCK_AVOIDANCE_LOCK(&rw->lock, false, LOCK_NOT_OWNABLE);
@@ -1251,6 +1253,7 @@ d_r_write_trylock(read_write_lock_t *rw)
 {
     if (d_r_mutex_trylock(&rw->lock)) {
         ASSERT_NOT_TESTED();
+        /* We have the lock so we do not need a load-acquire. */
         if (rw->num_readers == 0) {
             rw->writer = d_r_get_thread_id();
             return true;
@@ -1260,7 +1263,7 @@ d_r_write_trylock(read_write_lock_t *rw)
                that one may already be waiting on the broadcast event */
             d_r_mutex_unlock(&rw->lock);
             /* check whether any reader is currently waiting */
-            if (rw->num_pending_readers > 0) {
+            if (atomic_aligned_read_int(&rw->num_pending_readers) > 0) {
                 /* after we've released the write lock, pending
                  * readers will no longer wait
                  */
@@ -1291,6 +1294,7 @@ d_r_read_unlock(read_write_lock_t *rw)
         /* if the writer is waiting it definitely needs to hold the mutex */
         if (mutex_testlock(&rw->lock)) {
             /* test that it was not this thread owning both write and read lock */
+            /* We have the lock so we do not need a load-acquire. */
             if (rw->writer != d_r_get_thread_id()) {
                 /* we're assuming the writer has been forced to wait,
                    but since we can't tell whether it did indeed wait this
@@ -1332,7 +1336,7 @@ d_r_write_unlock(read_write_lock_t *rw)
     */
     d_r_mutex_unlock(&rw->lock);
     /* check whether any reader is currently waiting */
-    if (rw->num_pending_readers > 0) {
+    if (atomic_aligned_read_int(&rw->num_pending_readers) > 0) {
         /* after we've released the write lock, pending readers will no longer wait */
         rwlock_notify_readers(rw);
     }
@@ -1341,8 +1345,7 @@ d_r_write_unlock(read_write_lock_t *rw)
 bool
 self_owns_write_lock(read_write_lock_t *rw)
 {
-    /* ASSUMPTION: reading owner field is atomic */
-    return (rw->writer == d_r_get_thread_id());
+    return (ATOMIC_READ_THREAD_ID(&rw->writer) == d_r_get_thread_id());
 }
 
 /****************************************************************************/

core/globals.h
@@ -298,6 +298,7 @@ typedef struct _thread_record_t {
 #endif
 
 #include "heap.h"
+#include "options_struct.h"
 #include "utils.h"
 #include "options.h"
 #include "os_exports.h"

core/options.h
@@ -47,188 +47,7 @@
 extern "C" {
 #endif
 
-/* Security policy flags.  Note that if the off flag is not explicitly
- * used in an option default value definition, then the option doesn't
- * support that flag.
- */
-typedef enum {
-    /* security mechanism needed for detection can be completely disabled */
-    OPTION_ENABLED = 0x1,  /* if set, security mechanism is on */
-    OPTION_DISABLED = 0x0, /* if not set, security mechanism is off  */
-
-    /* security policy can be built on top.  For stateless policies
-     * all flags can be dynamic options.  For stateful security mechanisms as
-     * long as OPTION_ENABLED stays on for gathering information, the
-     * security policies OPTION_BLOCK|OPTION_REPORT can be dynamically
-     * set.  If no detection action is set, a detection check may be skipped.
-     */
-
-    /* associated security policy can be either blocking or not */
-    /* If set, app action (i.e., bad behavior) is disallowed and remediation
-     * action (kill thread, kill process, throw exception) is performed.
-     * Note: detect_mode will override this flag.
-     * FIXME: app_thread_policy_helper seems to be the one place where
-     * detect_mode doesn't override this; case 9088 tracks this; xref case 8451
-     * for an explanation of why this seemed to leave as such.
-     */
-    OPTION_BLOCK = 0x2,
-    OPTION_NO_BLOCK = 0x0,
-
-    /* policies that lend themselves to standard attack handling may use this flag */
-    OPTION_HANDLING = 0x4,    /* if set, overrides default attack handling */
-    OPTION_NO_HANDLING = 0x0, /* when not set, default attack handling is used */
-
-    /* report or stay silent */
-    OPTION_REPORT = 0x8,    /* if set, report action is being taken */
-    OPTION_NO_REPORT = 0x0, /* if not set, action is taken silently */
-
-    /* Block ignoring detect_mode; to handle case 10610. */
-    OPTION_BLOCK_IGNORE_DETECT = 0x20,
-
-    /* modifications in security policy or detection mechanism are controlled with */
-    OPTION_CUSTOM = 0x100,  /* alternative policy bit - custom meaning per option */
-    OPTION_NO_CUSTOM = 0x0, /* alternative policy bit - custom meaning per option */
-} security_option_t;
-
-/* values taken by the option hook_conflict */
-enum {
-    /* these are mutually exclusive */
-    HOOKED_TRAMPOLINE_DIE = 0,         /* throw a fatal error if chained */
-    HOOKED_TRAMPOLINE_SQUASH = 1,      /* modify any existing chains with a good guess */
-    HOOKED_TRAMPOLINE_CHAIN = 2,       /* rerelativize and mangle to support chaining */
-    HOOKED_TRAMPOLINE_HOOK_DEEPER = 3, /* move our hook deeper into the function */
-    HOOKED_TRAMPOLINE_NO_HOOK = 4,     /* give up on adding our hook */
-    HOOKED_TRAMPOLINE_MAX = 4,
-};
-
-/* for options.appfault_mask */
-enum {
-    /* XXX: we don't raise on handled signals b/c nobody would want notification
-     * on timer signals.  Should we raise on other handled signals?
-     */
-    APPFAULT_FAULT = 0x0001, /* Unhandled signal, or any Windows exception */
-    APPFAULT_CRASH = 0x0002, /* Unhandled signal or exception (NYI on Windows) */
-};
-
-/* for all option uses */
-#define uint_size ptr_uint_t
-#define uint_time uint
-/* So far all addr_t are external so we don't have a 64-bit problem */
-#define uint_addr ptr_uint_t
-/* XXX: For signed integer options, we'll need to correctly sign-extend in
- * dr_get_integer_option.
- */
-
-/* to dispatch on string default values, kept in struct not enum */
-#define ISSTRING_bool 0
-#define ISSTRING_uint 0
-#define ISSTRING_uint_size 0
-#define ISSTRING_uint_time 0
-#define ISSTRING_ptr_uint_t 0
-#define ISSTRING_pathstring_t 1
-#define ISSTRING_liststring_t 1
-
-/* Does this option affect persistent cache formation? */
-typedef enum {
-    OP_PCACHE_NOP = 0,    /* No effect on pcaches */
-    OP_PCACHE_LOCAL = 1,  /* Can only relax (not tighten), and when it relaxes any
-                           * module that module is marked via
-                           * os_module_set_flag(MODULE_WAS_EXEMPTED).
-                           */
-    OP_PCACHE_GLOBAL = 2, /* Affects pcaches but not called out as local. */
-} op_pcache_t;
-
-/* We use an enum for default values of non-string options so that the compiler
- * will fold them away (cl, at least, won't do it even for struct fields
- * declared const), and also to determine whether options are internal or take
- * string values or affect peristent caches.
- * In order for this to work optionsx.h strings must either be EMPTY_STRING or
- * a string in the OPTION_STRING() macro.
- * N.B.: for 64-bit presumably every enum value is 64-bit-wide b/c some
- * of them are.  FIXME: could split up the enums to save space: or are we
- * never actually storing these values?
- */
-#define OPTION_STRING(x) 0 /* no string in enum */
-#define EMPTY_STRING 0     /* no string in enum */
-#define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
-                       description, flag, pcache)                                 \
-    OPTION_IS_INTERNAL_##name = false, OPTION_IS_STRING_##name = ISSTRING_##type, \
-    OPTION_AFFECTS_PCACHE_##name = pcache,
-#define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option,  \
-                                statement, description, flag, pcache)            \
-    OPTION_IS_INTERNAL_##name = true, OPTION_IS_STRING_##name = ISSTRING_##type, \
-    OPTION_AFFECTS_PCACHE_##name = pcache,
-enum option_is_internal {
-#include "optionsx.h"
-};
-/* We have to make the default values not part of the enum since MSVC uses
- * "long" as the enum type and we'd have truncation of large values.
- * Instead we have to declare constant variables.
- * Hopefully the compiler will treat as constants and optimize away any
- * memory references.
- */
-#undef OPTION_COMMAND
-#undef OPTION_COMMAND_INTERNAL
-#define liststring_t int
-#define pathstring_t int
-#define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
-                       description, flag, pcache)                                 \
-    extern const type OPTION_DEFAULT_VALUE_##name;
-#define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
-                                statement, description, flag, pcache)           \
-    extern const type OPTION_DEFAULT_VALUE_##name;
-#include "optionsx.h"
-#undef liststring_t
-#undef pathstring_t
-#undef OPTION_COMMAND
-#undef OPTION_COMMAND_INTERNAL
-#undef OPTION_STRING
-#undef EMPTY_STRING
-
-/* for all other option uses */
-#define OPTION_STRING(x) x
-#define EMPTY_STRING \
-    {                \
-        0            \
-    } /* fills the constant char structure with zeroes */
-
-/* the Option struct typedef */
-#ifdef EXPOSE_INTERNAL_OPTIONS
-#    define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
-                                    statement, description, flag, pcache)           \
-        type name;
-#else
-#    define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
-                                    statement, description, flag, pcache) /* nothing */
-#endif
-
-#define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
-                       description, flag, pcache)                                 \
-    type name;
-typedef struct _options_t {
-#include "optionsx.h"
-} options_t;
-
-#undef OPTION_COMMAND
-#undef OPTION_COMMAND_INTERNAL
-
-#ifndef EXPOSE_INTERNAL_OPTIONS
-/* special struct for internal option default values */
-#    define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
-                           description, flag, pcache) /* nothing */
-#    define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
-                                    statement, description, flag, pcache)           \
-        const type name;
-typedef struct _internal_options_t {
-#    include "optionsx.h"
-} internal_options_t;
-#    undef OPTION_COMMAND
-#    undef OPTION_COMMAND_INTERNAL
-#endif
-
-#undef uint_size
-#undef uint_time
-#undef uint_addr
+#include "options_struct.h"
 
 /* For default integer values we use an enum, while for default string values we
  * use the default_options struct instance that is already being used
@@ -382,7 +201,6 @@ set_dynamo_options(options_t *options, const char *optstr);
 #    endif
 
 extern char d_r_option_string[];
-extern options_t dynamo_options;
 extern read_write_lock_t options_lock;
 
 /* check for empty is considered safe w/o the read lock

core/options_struct.h
@@ -1,0 +1,236 @@
+/* **********************************************************
+ * Copyright (c) 2012-2022 Google, Inc.  All rights reserved.
+ * Copyright (c) 2003-2009 VMware, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of VMware, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/* Copyright (c) 2003-2007 Determina Corp. */
+
+/* options_struct.h: options struct.  This is separated out from options.h as it
+ * is required for arch/atomic_exports.h to use ASSERT which references dynamo_options.
+ */
+
+#ifndef _OPTIONS_STRUCT_H_
+#define _OPTIONS_STRUCT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Security policy flags.  Note that if the off flag is not explicitly
+ * used in an option default value definition, then the option doesn't
+ * support that flag.
+ */
+typedef enum {
+    /* security mechanism needed for detection can be completely disabled */
+    OPTION_ENABLED = 0x1,  /* if set, security mechanism is on */
+    OPTION_DISABLED = 0x0, /* if not set, security mechanism is off  */
+
+    /* security policy can be built on top.  For stateless policies
+     * all flags can be dynamic options.  For stateful security mechanisms as
+     * long as OPTION_ENABLED stays on for gathering information, the
+     * security policies OPTION_BLOCK|OPTION_REPORT can be dynamically
+     * set.  If no detection action is set, a detection check may be skipped.
+     */
+
+    /* associated security policy can be either blocking or not */
+    /* If set, app action (i.e., bad behavior) is disallowed and remediation
+     * action (kill thread, kill process, throw exception) is performed.
+     * Note: detect_mode will override this flag.
+     * FIXME: app_thread_policy_helper seems to be the one place where
+     * detect_mode doesn't override this; case 9088 tracks this; xref case 8451
+     * for an explanation of why this seemed to leave as such.
+     */
+    OPTION_BLOCK = 0x2,
+    OPTION_NO_BLOCK = 0x0,
+
+    /* policies that lend themselves to standard attack handling may use this flag */
+    OPTION_HANDLING = 0x4,    /* if set, overrides default attack handling */
+    OPTION_NO_HANDLING = 0x0, /* when not set, default attack handling is used */
+
+    /* report or stay silent */
+    OPTION_REPORT = 0x8,    /* if set, report action is being taken */
+    OPTION_NO_REPORT = 0x0, /* if not set, action is taken silently */
+
+    /* Block ignoring detect_mode; to handle case 10610. */
+    OPTION_BLOCK_IGNORE_DETECT = 0x20,
+
+    /* modifications in security policy or detection mechanism are controlled with */
+    OPTION_CUSTOM = 0x100,  /* alternative policy bit - custom meaning per option */
+    OPTION_NO_CUSTOM = 0x0, /* alternative policy bit - custom meaning per option */
+} security_option_t;
+
+/* values taken by the option hook_conflict */
+enum {
+    /* these are mutually exclusive */
+    HOOKED_TRAMPOLINE_DIE = 0,         /* throw a fatal error if chained */
+    HOOKED_TRAMPOLINE_SQUASH = 1,      /* modify any existing chains with a good guess */
+    HOOKED_TRAMPOLINE_CHAIN = 2,       /* rerelativize and mangle to support chaining */
+    HOOKED_TRAMPOLINE_HOOK_DEEPER = 3, /* move our hook deeper into the function */
+    HOOKED_TRAMPOLINE_NO_HOOK = 4,     /* give up on adding our hook */
+    HOOKED_TRAMPOLINE_MAX = 4,
+};
+
+/* for options.appfault_mask */
+enum {
+    /* XXX: we don't raise on handled signals b/c nobody would want notification
+     * on timer signals.  Should we raise on other handled signals?
+     */
+    APPFAULT_FAULT = 0x0001, /* Unhandled signal, or any Windows exception */
+    APPFAULT_CRASH = 0x0002, /* Unhandled signal or exception (NYI on Windows) */
+};
+
+/* for all option uses */
+#define uint_size ptr_uint_t
+#define uint_time uint
+/* So far all addr_t are external so we don't have a 64-bit problem */
+#define uint_addr ptr_uint_t
+/* XXX: For signed integer options, we'll need to correctly sign-extend in
+ * dr_get_integer_option.
+ */
+
+/* to dispatch on string default values, kept in struct not enum */
+#define ISSTRING_bool 0
+#define ISSTRING_uint 0
+#define ISSTRING_uint_size 0
+#define ISSTRING_uint_time 0
+#define ISSTRING_ptr_uint_t 0
+#define ISSTRING_pathstring_t 1
+#define ISSTRING_liststring_t 1
+
+/* Does this option affect persistent cache formation? */
+typedef enum {
+    OP_PCACHE_NOP = 0,    /* No effect on pcaches */
+    OP_PCACHE_LOCAL = 1,  /* Can only relax (not tighten), and when it relaxes any
+                           * module that module is marked via
+                           * os_module_set_flag(MODULE_WAS_EXEMPTED).
+                           */
+    OP_PCACHE_GLOBAL = 2, /* Affects pcaches but not called out as local. */
+} op_pcache_t;
+
+/* We use an enum for default values of non-string options so that the compiler
+ * will fold them away (cl, at least, won't do it even for struct fields
+ * declared const), and also to determine whether options are internal or take
+ * string values or affect peristent caches.
+ * In order for this to work optionsx.h strings must either be EMPTY_STRING or
+ * a string in the OPTION_STRING() macro.
+ * N.B.: for 64-bit presumably every enum value is 64-bit-wide b/c some
+ * of them are.  FIXME: could split up the enums to save space: or are we
+ * never actually storing these values?
+ */
+#define OPTION_STRING(x) 0 /* no string in enum */
+#define EMPTY_STRING 0     /* no string in enum */
+#define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
+                       description, flag, pcache)                                 \
+    OPTION_IS_INTERNAL_##name = false, OPTION_IS_STRING_##name = ISSTRING_##type, \
+    OPTION_AFFECTS_PCACHE_##name = pcache,
+#define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option,  \
+                                statement, description, flag, pcache)            \
+    OPTION_IS_INTERNAL_##name = true, OPTION_IS_STRING_##name = ISSTRING_##type, \
+    OPTION_AFFECTS_PCACHE_##name = pcache,
+enum option_is_internal {
+#include "optionsx.h"
+};
+/* We have to make the default values not part of the enum since MSVC uses
+ * "long" as the enum type and we'd have truncation of large values.
+ * Instead we have to declare constant variables.
+ * Hopefully the compiler will treat as constants and optimize away any
+ * memory references.
+ */
+#undef OPTION_COMMAND
+#undef OPTION_COMMAND_INTERNAL
+#define liststring_t int
+#define pathstring_t int
+#define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
+                       description, flag, pcache)                                 \
+    extern const type OPTION_DEFAULT_VALUE_##name;
+#define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
+                                statement, description, flag, pcache)           \
+    extern const type OPTION_DEFAULT_VALUE_##name;
+#include "optionsx.h"
+#undef liststring_t
+#undef pathstring_t
+#undef OPTION_COMMAND
+#undef OPTION_COMMAND_INTERNAL
+#undef OPTION_STRING
+#undef EMPTY_STRING
+
+/* for all other option uses */
+#define OPTION_STRING(x) x
+#define EMPTY_STRING \
+    {                \
+        0            \
+    } /* fills the constant char structure with zeroes */
+
+/* the Option struct typedef */
+#ifdef EXPOSE_INTERNAL_OPTIONS
+#    define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
+                                    statement, description, flag, pcache)           \
+        type name;
+#else
+#    define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
+                                    statement, description, flag, pcache) /* nothing */
+#endif
+
+#define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
+                       description, flag, pcache)                                 \
+    type name;
+typedef struct _options_t {
+#include "optionsx.h"
+} options_t;
+
+#undef OPTION_COMMAND
+#undef OPTION_COMMAND_INTERNAL
+
+#ifndef EXPOSE_INTERNAL_OPTIONS
+/* special struct for internal option default values */
+#    define OPTION_COMMAND(type, name, default_value, command_line_option, statement, \
+                           description, flag, pcache) /* nothing */
+#    define OPTION_COMMAND_INTERNAL(type, name, default_value, command_line_option, \
+                                    statement, description, flag, pcache)           \
+        const type name;
+typedef struct _internal_options_t {
+#    include "optionsx.h"
+} internal_options_t;
+#    undef OPTION_COMMAND
+#    undef OPTION_COMMAND_INTERNAL
+#endif
+
+#undef uint_size
+#undef uint_time
+#undef uint_addr
+
+extern options_t dynamo_options;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _OPTIONS_STRUCT_H_ */

commit 9b861fd788227cc1b6fac347b3f52adec382c75a

i#5432: Relax asserts for marked-app locks
Fixes a regression from PR #5370 which added asserts to various DR
lock routines which are not valid for client locks marked as the app.

Adds sanity tests for each of the 3 types of locks that can be marked
as app locks.  These hit the asserts without this fix.

Fixes #5432

core/utils.c
@@ -988,7 +988,9 @@ d_r_mutex_mark_as_app(mutex_t *lock)
 static inline void
 own_recursive_lock(recursive_lock_t *lock)
 {
-    ASSERT(OWN_MUTEX(&lock->lock));
+#ifdef DEADLOCK_AVOIDANCE
+    ASSERT(!mutex_ownable(&lock->lock) || OWN_MUTEX(&lock->lock));
+#endif
     ASSERT(lock->owner == INVALID_THREAD_ID);
     ASSERT(lock->count == 0);
     lock->owner = d_r_get_thread_id();
@@ -1034,7 +1036,9 @@ try_recursive_lock(recursive_lock_t *lock)
 void
 release_recursive_lock(recursive_lock_t *lock)
 {
-    ASSERT(OWN_MUTEX(&lock->lock));
+#ifdef DEADLOCK_AVOIDANCE
+    ASSERT(!mutex_ownable(&lock->lock) || OWN_MUTEX(&lock->lock));
+#endif
     ASSERT(lock->owner == d_r_get_thread_id());
     ASSERT(lock->count > 0);
     lock->count--;
@@ -1319,7 +1323,7 @@ void
 d_r_write_unlock(read_write_lock_t *rw)
 {
 #ifdef DEADLOCK_AVOIDANCE
-    ASSERT(rw->writer == rw->lock.owner);
+    ASSERT(!mutex_ownable(&rw->lock) || rw->writer == rw->lock.owner);
 #endif
     rw->writer = INVALID_THREAD_ID;
     if (INTERNAL_OPTION(spin_yield_rwlock)) {

suite/tests/client-interface/drwrap-test.dll.c
@@ -69,6 +69,9 @@ static ptr_uint_t repeat_xsp;
 #ifdef ARM
 static ptr_uint_t repeat_link;
 #endif
+static void *mutex_lock;
+static void *rw_lock;
+static void *recur_lock;
 
 static int tls_idx;
 
@@ -339,11 +342,20 @@ dr_init(client_id_t id)
     drmgr_register_module_unload_event(module_unload_event);
     tls_idx = drmgr_register_tls_field();
     CHECK(tls_idx > -1, "unable to reserve TLS field");
+    mutex_lock = dr_mutex_create();
+    dr_mutex_mark_as_app(mutex_lock);
+    rw_lock = dr_rwlock_create();
+    dr_rwlock_mark_as_app(rw_lock);
+    recur_lock = dr_recurlock_create();
+    dr_recurlock_mark_as_app(recur_lock);
 }
 
 static void
 event_exit(void)
 {
+    dr_mutex_destroy(mutex_lock);
+    dr_rwlock_destroy(rw_lock);
+    dr_recurlock_destroy(recur_lock);
     drwrap_stats_t stats = {
         sizeof(stats),
     };
@@ -366,6 +378,16 @@ replacewith(int *x)
 static int
 on_clean_stack(int i, int j, int k, int l, int m, int n, int o, int p)
 {
+    /* Test lock/unlock of marked-app lock. */
+    dr_mutex_lock(mutex_lock);
+    dr_mutex_unlock(mutex_lock);
+    dr_rwlock_read_lock(rw_lock);
+    dr_rwlock_read_unlock(rw_lock);
+    dr_rwlock_write_lock(rw_lock);
+    dr_rwlock_write_unlock(rw_lock);
+    dr_recurlock_lock(recur_lock);
+    dr_recurlock_unlock(recur_lock);
+
     return i + j + k + l + m + n + o + p;
 }
 