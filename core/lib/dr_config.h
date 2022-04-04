30da7c1edd19fac69ef61334a90fb5a4df0d17c9
Derek Bruening <bruening@google.com>
2022-04-04 23:12:23 +0000

    i#5250: Add alt-bitwidth DR lib path config param (#5443)
    Adds a new config parameter DYNAMORIO_ALTINJECT for specifying the
    path to the alternate-bitwidth DR library for cross-bitwidth execve.
    
    If the parameter is set, DR uses it; otherwise it falls back to the
    current hardcoded path assumptions.
    
    drrun and friends automatically set the parameter if the other
    bitwidth library is found.
    
    Adds an automated test by augmenting the "linux.execve{32,64}" test
    from a 64-bit build which does a test-and-build.  The renamed
    "linux.xarch" test now does one build (for efficiency) and runs a
    script which executes both (32<->64) directions for each of the
    existing assumed-layout strategy and the new
    config-points-at-alt-bitwidth strategy.
    
    Also tested manually:
    $ bin64/drrun -v -use_alt_dll ~/dr/releases/DynamoRIO-Linux-9.0.0/lib32/debug/libdynamorio.so -trace_threshold 51 -- suite/tests/bin/linux.execve64 ~/dr/git/build_x86_dbg_tests/suite/tests/bin/linux.execve-sub32
    
    Fixes #5250

--- core/lib/dr_config.h
+++ core/lib/dr_config.h
@@ -303,6 +303,10 @@ DR_EXPORT
  * \param[in]   dr_root_dir     A NULL-terminated string specifying the full
  *                              path to a valid DynamoRIO root directory.
  *                              The string length cannot exceed #MAXIMUM_PATH.
+ *                              The path to the DynamoRIO library to be used will
+ *                              be constructed from this path assuming a default
+ *                              layout.  To precisely specify the path, call
+ *                               dr_register_inject_paths() afterward.
  *
  * \param[in]   dr_mode         Specifies the mode under which DynamoRIO should
  *                              operate.  See dr_operation_mode_t.
@@ -375,6 +379,67 @@ DR_EXPORT
 dr_config_status_t
 dr_unregister_process(const char *process_name, process_id_t pid, bool global,
                       dr_platform_t dr_platform);
+
+DR_EXPORT
+/**
+ * Sets the paths to the DynamoRIO library and the alternate-bitwidth DynamoRIO
+ * library for a process that has already been registered via dr_register_process().
+ * If this routine is not called, default library paths will be used which assume
+ * a default path layout within the \p dr_root_dir passed to dr_register_process().
+ *
+ * \param[in]   process_name    A NULL-terminated string specifying the name
+ *                              of the target process.  The string should
+ *                              identify the base name of the process, not the
+ *                              full path of the executable (e.g., calc.exe).
+ *
+ * \param[in]   pid             A process id of a target process, typically just
+ *                              created and suspended via dr_inject_process_exit().
+ *                              If pid != 0, the one-time configuration for that pid
+ *                              will be modified.  If pid == 0, the general
+ *                              configuration for process_name will be modified.
+ *
+ * \param[in]   global          Whether to use global or user-local config
+ *                              files.  On Windows, global config files are
+ *                              stored in a dir pointed at by the DYNAMORIO_HOME
+ *                              registry key.  On Linux, they are in
+ *                              /etc/dynamorio.  Administrative privileges may
+ *                              be needed if global is true.  Note that
+ *                              DynamoRIO gives local config files precedence
+ *                              when both exist.  The caller must separately
+ *                              create the global directory.
+ *
+ * \param[in]   dr_platform     Configurations are kept separate
+ *                              for 32-bit processes and 64-bit processes.
+ *                              This parameter allows selecting which of those
+ *                              configurations to check.
+ *
+ * \param[in]   dr_lib_path     A NULL-terminated string specifying the full
+ *                              path to a valid DynamoRIO library of the same
+ *                              bitwidth as \p dr_platform.
+ *                              The string length cannot exceed #MAXIMUM_PATH.
+ *
+ * \param[in]   dr_alt_lib_path A NULL-terminated string specifying the full
+ *                              path to a valid DynamoRIO library of the alternative
+ *                              bitwidth to \p dr_platform.
+ *                              The string length cannot exceed #MAXIMUM_PATH.
+ *
+ * \return      A dr_config_status_t code indicating the result of
+ *              registration.  Note that registration fails if the requested
+ *              process is already registered.  To modify a process's
+ *              registration, first call dr_unregister_process() to remove an
+ *              existing registration.
+ *
+ * \remarks
+ * After registration, a process will run under DynamoRIO when launched by the
+ * drinject tool or using drinjectlib.  Note that some processes may require a
+ * system reboot to restart.  Process registration that is not specific to one
+ * pid (i.e., if \p pid == 0) persists across reboots until explicitly
+ * unregistered.
+ */
+dr_config_status_t
+dr_register_inject_paths(const char *process_name, process_id_t pid, bool global,
+                         dr_platform_t dr_platform, const char *dr_lib_path,
+                         const char *dr_alt_lib_path);
 
 #ifdef WINDOWS
 
