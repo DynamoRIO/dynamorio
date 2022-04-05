DynamoRIO/dynamorio

commit 30da7c1edd19fac69ef61334a90fb5a4df0d17c9

i#5250: Add alt-bitwidth DR lib path config param (#5443)
Adds a new config parameter DYNAMORIO_ALTINJECT for specifying the
path to the alternate-bitwidth DR library for cross-bitwidth execve.

If the parameter is set, DR uses it; otherwise it falls back to the
current hardcoded path assumptions.

drrun and friends automatically set the parameter if the other
bitwidth library is found.

Adds an automated test by augmenting the "linux.execve{32,64}" test
from a 64-bit build which does a test-and-build.  The renamed
"linux.xarch" test now does one build (for efficiency) and runs a
script which executes both (32<->64) directions for each of the
existing assumed-layout strategy and the new
config-points-at-alt-bitwidth strategy.

Also tested manually:
$ bin64/drrun -v -use_alt_dll ~/dr/releases/DynamoRIO-Linux-9.0.0/lib32/debug/libdynamorio.so -trace_threshold 51 -- suite/tests/bin/linux.execve64 ~/dr/git/build_x86_dbg_tests/suite/tests/bin/linux.execve-sub32

Fixes #5250

tools/drdeploy.c
@@ -74,7 +74,6 @@ typedef enum _action_t {
 
 static bool verbose;
 static bool quiet;
-static bool DR_dll_not_needed = false;
 static bool nocheck;
 
 #define die() exit(1)
@@ -326,6 +325,7 @@ const char *options_list_str =
     "                          however, the attach process will take longer.\n"
 #    endif
     "       -use_dll <dll>     Inject given dll instead of configured DR dll.\n"
+    "       -use_alt_dll <dll> Use the given dll as the alternate-bitwidth DR dll.\n"
     "       -force             Inject regardless of configuration.\n"
     "       -exit0             Return a 0 exit code instead of the app's exit code.\n"
     "\n"
@@ -475,11 +475,13 @@ unregister_proc(const char *process, process_id_t pid, bool global,
 }
 
 /* Check if the provided root directory actually has the files we
- * expect.  Returns whether a fatal problem.
+ * expect.  Returns whether a fatal problem.  On success,
+ * fills in dr_lib_path and dr_alt_lib_path if they are non-NULL.
  */
 static bool
-check_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool preinject,
-              bool report)
+expand_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool preinject,
+               bool report, OUT char *dr_lib_path, size_t dr_lib_path_sz,
+               OUT char *dr_alt_lib_path, size_t dr_alt_lib_path_sz)
 {
     int i;
     char buf[MAXIMUM_PATH];
@@ -487,33 +489,41 @@ check_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool p
     /* FIXME i#1569: port DynamoRIO to AArch64 so we can enable the check warning */
     bool nowarn = IF_X86_ELSE(false, true);
 
-    const char *checked_files[] = {
+    typedef struct _file_entry_t {
+        const char *suffix;
+        bool is_core;
+        bool is_debug;
+        bool is_preinject;
+        dr_platform_t platform;
+    } file_entry_t;
+    const file_entry_t checked_files[] = {
 #ifdef WINDOWS
-        "lib32\\drpreinject.dll",        "lib32\\release\\dynamorio.dll",
-        "lib32\\debug\\dynamorio.dll",   "lib64\\drpreinject.dll",
-        "lib64\\release\\dynamorio.dll", "lib64\\debug\\dynamorio.dll"
+        { "lib32\\drpreinject.dll", false, false, true, DR_PLATFORM_32BIT },
+        { "lib32\\release\\dynamorio.dll", true, false, false, DR_PLATFORM_32BIT },
+        { "lib32\\debug\\dynamorio.dll", true, true, false, DR_PLATFORM_32BIT },
+        { "lib64\\drpreinject.dll", false, false, true, DR_PLATFORM_64BIT },
+        { "lib64\\release\\dynamorio.dll", true, false, false, DR_PLATFORM_64BIT },
+        { "lib64\\debug\\dynamorio.dll", true, true, false, DR_PLATFORM_64BIT },
 #elif defined(MACOS)
-        "lib32/debug/libdrpreload.dylib",   "lib32/debug/libdynamorio.dylib",
-        "lib32/release/libdrpreload.dylib", "lib32/release/libdynamorio.dylib",
-        "lib64/debug/libdrpreload.dylib",   "lib64/debug/libdynamorio.dylib",
-        "lib64/release/libdrpreload.dylib", "lib64/release/libdynamorio.dylib"
+        { "lib32/debug/libdrpreload.dylib", false, true, true, DR_PLATFORM_32BIT },
+        { "lib32/debug/libdynamorio.dylib", true, true, false, DR_PLATFORM_32BIT },
+        { "lib32/release/libdrpreload.dylib", false, false, true, DR_PLATFORM_32BIT },
+        { "lib32/release/libdynamorio.dylib", true, false, false, DR_PLATFORM_32BIT },
+        { "lib64/debug/libdrpreload.dylib", true, false, true, DR_PLATFORM_64BIT },
+        { "lib64/debug/libdynamorio.dylib", true, true, false, DR_PLATFORM_64BIT },
+        { "lib64/release/libdrpreload.dylib", false, false, true, DR_PLATFORM_64BIT },
+        { "lib64/release/libdynamorio.dylib", true, false, false, DR_PLATFORM_64BIT },
 #else /* LINUX */
         /* With early injection the default, we don't require preload to exist. */
-        "lib32/debug/libdynamorio.so", "lib32/release/libdynamorio.so",
-        "lib64/debug/libdynamorio.so", "lib64/release/libdynamorio.so"
+        { "lib32/debug/libdynamorio.so", true, true, false, DR_PLATFORM_32BIT },
+        { "lib32/release/libdynamorio.so", true, false, false, DR_PLATFORM_32BIT },
+        { "lib64/debug/libdynamorio.so", true, true, false, DR_PLATFORM_64BIT },
+        { "lib64/release/libdynamorio.so", true, false, false, DR_PLATFORM_64BIT },
 #endif
     };
 
-    const char *arch = IF_X64_ELSE("lib64", "lib32");
-    if (dr_platform == DR_PLATFORM_32BIT)
-        arch = "lib32";
-    else if (dr_platform == DR_PLATFORM_64BIT)
-        arch = "lib64";
-
-    if (DR_dll_not_needed) {
-        /* assume user knows what he's doing */
-        return true;
-    }
+    if (dr_platform == DR_PLATFORM_DEFAULT)
+        dr_platform = IF_X64_ELSE(DR_PLATFORM_64BIT, DR_PLATFORM_32BIT);
 
     /* don't warn if running from a build dir (i#458) which we attempt to detect
      * by looking for CMakeCache.txt in the root dir
@@ -523,15 +533,36 @@ check_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool p
     if (does_file_exist(buf))
         nowarn = true;
 
+    bool found_lib = false;
+    if (dr_alt_lib_path != NULL)
+        dr_alt_lib_path[0] = '\0';
     for (i = 0; i < BUFFER_SIZE_ELEMENTS(checked_files); i++) {
-        _snprintf(buf, BUFFER_SIZE_ELEMENTS(buf), "%s/%s", dr_root, checked_files[i]);
-        if (!does_file_exist(buf)) {
+        _snprintf(buf, BUFFER_SIZE_ELEMENTS(buf), "%s/%s", dr_root,
+                  checked_files[i].suffix);
+        if (does_file_exist(buf)) {
+            if (checked_files[i].is_core &&
+                ((debug && checked_files[i].is_debug) ||
+                 (!debug && !checked_files[i].is_debug))) {
+                if (checked_files[i].platform == dr_platform) {
+                    found_lib = true;
+                    if (dr_lib_path != NULL) {
+                        _snprintf(dr_lib_path, dr_lib_path_sz, "%s", buf);
+                        dr_lib_path[dr_lib_path_sz - 1] = '\0';
+                    }
+                } else {
+                    if (dr_alt_lib_path != NULL) {
+                        _snprintf(dr_alt_lib_path, dr_alt_lib_path_sz, "%s", buf);
+                        dr_alt_lib_path[dr_alt_lib_path_sz - 1] = '\0';
+                    }
+                }
+            }
+        } else {
             ok = false;
             if (!nocheck &&
-                ((preinject && strstr(checked_files[i], "drpreinject")) ||
-                 (!preinject && debug && strstr(checked_files[i], "debug") != NULL) ||
-                 (!preinject && !debug && strstr(checked_files[i], "release") != NULL)) &&
-                strstr(checked_files[i], arch) != NULL) {
+                ((preinject && checked_files[i].is_preinject) ||
+                 (!preinject && debug && checked_files[i].is_debug) ||
+                 (!preinject && !debug && !checked_files[i].is_debug)) &&
+                checked_files[i].platform == dr_platform) {
                 /* We don't want to create a .1config file that won't be freed
                  * b/c the core is never injected
                  */
@@ -542,7 +573,7 @@ check_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool p
                 }
                 return false;
             } else {
-                if (strstr(checked_files[i], arch) == NULL) {
+                if (checked_files[i].platform == DR_PLATFORM_DEFAULT) {
                     /* Support a single-bitwidth package. */
                     ok = true;
                 } else if (!nowarn)
@@ -550,16 +581,28 @@ check_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool p
             }
         }
     }
+    assert(found_lib);
     if (!ok && !nowarn)
         warn("%s does not appear to be a valid DynamoRIO root", dr_root);
     return true;
+}
+
+/* Check if the provided root directory actually has the files we
+ * expect.  Returns whether a fatal problem.
+ */
+static bool
+check_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool preinject,
+              bool report)
+{
+    return expand_dr_root(dr_root, debug, dr_platform, preinject, report, NULL, 0, NULL,
+                          0);
 }
 
 /* Register a process to run under DR */
 bool
 register_proc(const char *process, process_id_t pid, bool global, const char *dr_root,
               const dr_operation_mode_t dr_mode, bool debug, dr_platform_t dr_platform,
-              const char *extra_ops)
+              const char *extra_ops, const char *custom_dll, const char *custom_alt_dll)
 {
     dr_config_status_t status;
 
@@ -573,12 +616,28 @@ register_proc(const char *process, process_id_t pid, bool global, const char *dr
         return false;
     }
 
-    /* warn if the DR root directory doesn't look right, unless -norun,
-     * in which case don't bother
-     */
-    if (dr_mode != DR_MODE_DO_NOT_RUN &&
-        !check_dr_root(dr_root, debug, dr_platform, false /*!pre*/, true /*report*/))
-        return false;
+    char dr_lib_path[MAXIMUM_PATH];
+    char dr_alt_lib_path[MAXIMUM_PATH];
+    if (custom_dll != NULL) {
+        strncpy(dr_lib_path, custom_dll, BUFFER_SIZE_ELEMENTS(dr_lib_path));
+        NULL_TERMINATE_BUFFER(dr_lib_path);
+    } else {
+        bool check_ok =
+            expand_dr_root(dr_root, debug, dr_platform, false /*!pre*/,
+                           dr_mode != DR_MODE_DO_NOT_RUN /*report*/, dr_lib_path,
+                           BUFFER_SIZE_ELEMENTS(dr_lib_path), dr_alt_lib_path,
+                           BUFFER_SIZE_ELEMENTS(dr_alt_lib_path));
+        /* Warn if the DR root directory doesn't look right, unless -norun,
+         * in which case don't bother.
+         */
+        if (dr_mode != DR_MODE_DO_NOT_RUN && !check_ok)
+            return false;
+    }
+    if (custom_alt_dll != NULL) {
+        /* Overwrite what expand_dr_root found if an alt is specified. */
+        strncpy(dr_alt_lib_path, custom_alt_dll, BUFFER_SIZE_ELEMENTS(dr_alt_lib_path));
+        NULL_TERMINATE_BUFFER(dr_alt_lib_path);
+    }
 
     if (dr_process_is_registered(process, pid, global, dr_platform, NULL, NULL, NULL,
                                  NULL)) {
@@ -619,6 +678,12 @@ register_proc(const char *process, process_id_t pid, bool global, const char *dr
 #endif
         return false;
     }
+    status = dr_register_inject_paths(
+        process, pid, global, dr_platform, dr_lib_path[0] == '\0' ? NULL : dr_lib_path,
+        dr_alt_lib_path[0] == '\0' ? NULL : dr_alt_lib_path);
+    if (status != DR_SUCCESS)
+        warn("failed to specify DynamoRIO library paths: falling back to defaults");
+
     return true;
 }
 
@@ -1120,7 +1185,6 @@ _tmain(int argc, TCHAR *targv[])
     bool force_injection = false;
     bool inject = true;
     int limit = 0; /* in seconds */
-    char *drlib_path = NULL;
 #    ifdef WINDOWS
     time_t start_time, end_time;
 #    else
@@ -1132,12 +1196,15 @@ _tmain(int argc, TCHAR *targv[])
     char *app_name = NULL;
     char full_app_name[MAXIMUM_PATH];
     const char **app_argv;
-    char custom_dll[MAXIMUM_PATH];
     int errcode;
     void *inject_data;
     bool success;
     bool exit0 = false;
 #endif
+    char *drlib_path = NULL;
+    char *drlib_alt_path = NULL;
+    char custom_dll[MAXIMUM_PATH];
+    char custom_alt_dll[MAXIMUM_PATH];
     int i;
 #ifndef DRINJECT
     size_t j;
@@ -1256,7 +1323,6 @@ _tmain(int argc, TCHAR *targv[])
         } else if (strcmp(argv[i], "-no_inject") == 0 ||
                    /* support old drinjectx param name */
                    strcmp(argv[i], "-noinject") == 0 || strcmp(argv[i], "-static") == 0) {
-            DR_dll_not_needed = true;
             inject = false;
             continue;
         } else if (strcmp(argv[i], "-force") == 0) {
@@ -1453,15 +1519,20 @@ _tmain(int argc, TCHAR *targv[])
                              "%s", argv[++i]);
         }
 #endif
-#if defined(DRRUN) || defined(DRINJECT)
-        else if (strcmp(argv[i], "-pidfile") == 0) {
-            pidfile = argv[++i];
-        } else if (strcmp(argv[i], "-use_dll") == 0) {
-            DR_dll_not_needed = true;
+        else if (strcmp(argv[i], "-use_dll") == 0) {
             /* Support relative path: very useful! */
             get_absolute_path(argv[++i], custom_dll, BUFFER_SIZE_ELEMENTS(custom_dll));
             NULL_TERMINATE_BUFFER(custom_dll);
             drlib_path = custom_dll;
+        } else if (strcmp(argv[i], "-use_alt_dll") == 0) {
+            get_absolute_path(argv[++i], custom_alt_dll,
+                              BUFFER_SIZE_ELEMENTS(custom_alt_dll));
+            NULL_TERMINATE_BUFFER(custom_alt_dll);
+            drlib_alt_path = custom_alt_dll;
+        }
+#if defined(DRRUN) || defined(DRINJECT)
+        else if (strcmp(argv[i], "-pidfile") == 0) {
+            pidfile = argv[++i];
         } else if (strcmp(argv[i], "-s") == 0) {
             limit = atoi(argv[++i]);
             if (limit <= 0)
@@ -1691,7 +1762,7 @@ done_with_options:
     }
     if (action == action_register) {
         if (!register_proc(process, 0, global, dr_root, dr_mode, use_debug, dr_platform,
-                           extra_ops))
+                           extra_ops, drlib_path, drlib_alt_path))
             die();
         for (j = 0; j < num_clients; j++) {
             if (!register_client(process, 0, global, dr_platform, client_ids[j],
@@ -1885,7 +1956,8 @@ done_with_options:
     if (configure) {
         process = dr_inject_get_image_name(inject_data);
         if (!register_proc(process, dr_inject_get_process_id(inject_data), global,
-                           dr_root, dr_mode, use_debug, dr_platform, extra_ops))
+                           dr_root, dr_mode, use_debug, dr_platform, extra_ops,
+                           drlib_path, drlib_alt_path))
             goto error;
         for (j = 0; j < num_clients; j++) {
             if (!register_client(process, dr_inject_get_process_id(inject_data), global,

api/docs/release.dox
@@ -153,6 +153,9 @@ Further non-compatibility-affecting changes include:
  - Added droption_parser_t::clear_values() for re-setting accumulating option
    values on re-attach for statically linked clients.
  - Added the count of cache exits to #dr_stats_t.
+ - Added dr_register_inject_paths() and a corresponding -use_alt_dll drrun/drconfig
+   parameter for control over DynamoRIO library paths, in particular the other
+   bitwidth path, when configuring target processes.
 
 The changes between version 9.0.0 and 8.0.0 include the following compatibility
 changes:

core/config.c
@@ -125,17 +125,17 @@ DECLARE_NEVERPROT_VAR(static int infolevel, VERBOSE);
 
 /* we store values for each of these vars: */
 static const char *const config_var[] = {
-    DYNAMORIO_VAR_HOME,       DYNAMORIO_VAR_LOGDIR,       DYNAMORIO_VAR_OPTIONS,
-    DYNAMORIO_VAR_AUTOINJECT, DYNAMORIO_VAR_UNSUPPORTED,  DYNAMORIO_VAR_RUNUNDER,
-    DYNAMORIO_VAR_CMDLINE,    DYNAMORIO_VAR_ONCRASH,      DYNAMORIO_VAR_SAFEMARKER,
-    DYNAMORIO_VAR_CACHE_ROOT, DYNAMORIO_VAR_CACHE_SHARED,
+    DYNAMORIO_VAR_HOME,       DYNAMORIO_VAR_LOGDIR,     DYNAMORIO_VAR_OPTIONS,
+    DYNAMORIO_VAR_AUTOINJECT, DYNAMORIO_VAR_ALTINJECT,  DYNAMORIO_VAR_UNSUPPORTED,
+    DYNAMORIO_VAR_RUNUNDER,   DYNAMORIO_VAR_CMDLINE,    DYNAMORIO_VAR_ONCRASH,
+    DYNAMORIO_VAR_SAFEMARKER, DYNAMORIO_VAR_CACHE_ROOT, DYNAMORIO_VAR_CACHE_SHARED,
 };
 #    ifdef WINDOWS
 static const wchar_t *const w_config_var[] = {
-    L_DYNAMORIO_VAR_HOME,       L_DYNAMORIO_VAR_LOGDIR,       L_DYNAMORIO_VAR_OPTIONS,
-    L_DYNAMORIO_VAR_AUTOINJECT, L_DYNAMORIO_VAR_UNSUPPORTED,  L_DYNAMORIO_VAR_RUNUNDER,
-    L_DYNAMORIO_VAR_CMDLINE,    L_DYNAMORIO_VAR_ONCRASH,      L_DYNAMORIO_VAR_SAFEMARKER,
-    L_DYNAMORIO_VAR_CACHE_ROOT, L_DYNAMORIO_VAR_CACHE_SHARED,
+    L_DYNAMORIO_VAR_HOME,       L_DYNAMORIO_VAR_LOGDIR,     L_DYNAMORIO_VAR_OPTIONS,
+    L_DYNAMORIO_VAR_AUTOINJECT, L_DYNAMORIO_VAR_ALTINJECT,  L_DYNAMORIO_VAR_UNSUPPORTED,
+    L_DYNAMORIO_VAR_RUNUNDER,   L_DYNAMORIO_VAR_CMDLINE,    L_DYNAMORIO_VAR_ONCRASH,
+    L_DYNAMORIO_VAR_SAFEMARKER, L_DYNAMORIO_VAR_CACHE_ROOT, L_DYNAMORIO_VAR_CACHE_SHARED,
 };
 #    endif
 #    define NUM_CONFIG_VAR (sizeof(config_var) / sizeof(config_var[0]))
@@ -624,6 +624,12 @@ d_r_config_init(void)
     config.u.v = &myvals;
     config_read(&config, NULL, 0, CFG_SFX);
     config_initialized = true;
+}
+
+bool
+d_r_config_initialized(void)
+{
+    return config_initialized;
 }
 
 #    ifndef NOT_DYNAMORIO_CORE_PROPER

core/config.h
@@ -46,6 +46,9 @@ d_r_config_init(void);
 void
 d_r_config_exit(void);
 
+bool
+d_r_config_initialized(void);
+
 void
 config_heap_init(void);
 

core/lib/dr_config.h
@@ -303,6 +303,10 @@ DR_EXPORT
  * \param[in]   dr_root_dir     A NULL-terminated string specifying the full
  *                              path to a valid DynamoRIO root directory.
  *                              The string length cannot exceed #MAXIMUM_PATH.
+ *                              The path to the DynamoRIO library to be used will
+ *                              be constructed from this path assuming a default
+ *                              layout.  To precisely specify the path, call
+ *                               dr_register_inject_paths() afterward.
  *
  * \param[in]   dr_mode         Specifies the mode under which DynamoRIO should
  *                              operate.  See dr_operation_mode_t.
@@ -375,6 +379,67 @@ DR_EXPORT
 dr_config_status_t
 dr_unregister_process(const char *process_name, process_id_t pid, bool global,
                       dr_platform_t dr_platform);
+
+DR_EXPORT
+/**
+ * Sets the paths to the DynamoRIO library and the alternate-bitwidth DynamoRIO
+ * library for a process that has already been registered via dr_register_process().
+ * If this routine is not called, default library paths will be used which assume
+ * a default path layout within the \p dr_root_dir passed to dr_register_process().
+ *
+ * \param[in]   process_name    A NULL-terminated string specifying the name
+ *                              of the target process.  The string should
+ *                              identify the base name of the process, not the
+ *                              full path of the executable (e.g., calc.exe).
+ *
+ * \param[in]   pid             A process id of a target process, typically just
+ *                              created and suspended via dr_inject_process_exit().
+ *                              If pid != 0, the one-time configuration for that pid
+ *                              will be modified.  If pid == 0, the general
+ *                              configuration for process_name will be modified.
+ *
+ * \param[in]   global          Whether to use global or user-local config
+ *                              files.  On Windows, global config files are
+ *                              stored in a dir pointed at by the DYNAMORIO_HOME
+ *                              registry key.  On Linux, they are in
+ *                              /etc/dynamorio.  Administrative privileges may
+ *                              be needed if global is true.  Note that
+ *                              DynamoRIO gives local config files precedence
+ *                              when both exist.  The caller must separately
+ *                              create the global directory.
+ *
+ * \param[in]   dr_platform     Configurations are kept separate
+ *                              for 32-bit processes and 64-bit processes.
+ *                              This parameter allows selecting which of those
+ *                              configurations to check.
+ *
+ * \param[in]   dr_lib_path     A NULL-terminated string specifying the full
+ *                              path to a valid DynamoRIO library of the same
+ *                              bitwidth as \p dr_platform.
+ *                              The string length cannot exceed #MAXIMUM_PATH.
+ *
+ * \param[in]   dr_alt_lib_path A NULL-terminated string specifying the full
+ *                              path to a valid DynamoRIO library of the alternative
+ *                              bitwidth to \p dr_platform.
+ *                              The string length cannot exceed #MAXIMUM_PATH.
+ *
+ * \return      A dr_config_status_t code indicating the result of
+ *              registration.  Note that registration fails if the requested
+ *              process is already registered.  To modify a process's
+ *              registration, first call dr_unregister_process() to remove an
+ *              existing registration.
+ *
+ * \remarks
+ * After registration, a process will run under DynamoRIO when launched by the
+ * drinject tool or using drinjectlib.  Note that some processes may require a
+ * system reboot to restart.  Process registration that is not specific to one
+ * pid (i.e., if \p pid == 0) persists across reboots until explicitly
+ * unregistered.
+ */
+dr_config_status_t
+dr_register_inject_paths(const char *process_name, process_id_t pid, bool global,
+                         dr_platform_t dr_platform, const char *dr_lib_path,
+                         const char *dr_alt_lib_path);
 
 #ifdef WINDOWS
 

core/lib/globals_shared.h
@@ -535,6 +535,7 @@ typedef char liststring_t[MAX_LIST_OPTION_LENGTH];
 #define DYNAMORIO_VAR_LOGDIR_ID DYNAMORIO_LOGDIR
 #define DYNAMORIO_VAR_OPTIONS_ID DYNAMORIO_OPTIONS
 #define DYNAMORIO_VAR_AUTOINJECT_ID DYNAMORIO_AUTOINJECT
+#define DYNAMORIO_VAR_ALTINJECT_ID DYNAMORIO_ALTINJECT
 #define DYNAMORIO_VAR_UNSUPPORTED_ID DYNAMORIO_UNSUPPORTED
 #define DYNAMORIO_VAR_RUNUNDER_ID DYNAMORIO_RUNUNDER
 #define DYNAMORIO_VAR_CMDLINE_ID DYNAMORIO_CMDLINE
@@ -580,6 +581,7 @@ typedef char liststring_t[MAX_LIST_OPTION_LENGTH];
 #define DYNAMORIO_VAR_LOGDIR STRINGIFY(DYNAMORIO_VAR_LOGDIR_ID)
 #define DYNAMORIO_VAR_OPTIONS STRINGIFY(DYNAMORIO_VAR_OPTIONS_ID)
 #define DYNAMORIO_VAR_AUTOINJECT STRINGIFY(DYNAMORIO_VAR_AUTOINJECT_ID)
+#define DYNAMORIO_VAR_ALTINJECT STRINGIFY(DYNAMORIO_VAR_ALTINJECT_ID)
 #define DYNAMORIO_VAR_UNSUPPORTED STRINGIFY(DYNAMORIO_VAR_UNSUPPORTED_ID)
 #define DYNAMORIO_VAR_RUNUNDER STRINGIFY(DYNAMORIO_VAR_RUNUNDER_ID)
 #define DYNAMORIO_VAR_CMDLINE STRINGIFY(DYNAMORIO_VAR_CMDLINE_ID)
@@ -626,6 +628,7 @@ typedef char liststring_t[MAX_LIST_OPTION_LENGTH];
 #    define L_DYNAMORIO_VAR_LOGDIR L_EXPAND_LEVEL(DYNAMORIO_VAR_LOGDIR)
 #    define L_DYNAMORIO_VAR_OPTIONS L_EXPAND_LEVEL(DYNAMORIO_VAR_OPTIONS)
 #    define L_DYNAMORIO_VAR_AUTOINJECT L_EXPAND_LEVEL(DYNAMORIO_VAR_AUTOINJECT)
+#    define L_DYNAMORIO_VAR_ALTINJECT L_EXPAND_LEVEL(DYNAMORIO_VAR_ALTINJECT)
 #    define L_DYNAMORIO_VAR_UNSUPPORTED L_EXPAND_LEVEL(DYNAMORIO_VAR_UNSUPPORTED)
 #    define L_DYNAMORIO_VAR_RUNUNDER L_EXPAND_LEVEL(DYNAMORIO_VAR_RUNUNDER)
 #    define L_DYNAMORIO_VAR_CMDLINE L_EXPAND_LEVEL(DYNAMORIO_VAR_CMDLINE)

commit b8e5d7818bc07807a1e1778cd7250e4de507ee8f

i#5250 alt-bitwidth: Fix regression in drrun warning
Fixes a regression from PR #5443 where warnings about an incomplete
multi-bitwidth installation that used to be suppressed when -use_dll
is passed were instead enabled.

Issue: #5250

tools/drdeploy.c
@@ -74,6 +74,7 @@ typedef enum _action_t {
 
 static bool verbose;
 static bool quiet;
+static bool DR_dll_not_needed = false;
 static bool nocheck;
 
 #define die() exit(1)
@@ -525,6 +526,10 @@ expand_dr_root(const char *dr_root, bool debug, dr_platform_t dr_platform, bool 
     if (dr_platform == DR_PLATFORM_DEFAULT)
         dr_platform = IF_X64_ELSE(DR_PLATFORM_64BIT, DR_PLATFORM_32BIT);
 
+    if (DR_dll_not_needed) {
+        /* An explicit path was passed so don't require a regular installation. */
+        nowarn = true;
+    }
     /* don't warn if running from a build dir (i#458) which we attempt to detect
      * by looking for CMakeCache.txt in the root dir
      * (warnings can also be suppressed via -quiet)
@@ -1323,6 +1328,7 @@ _tmain(int argc, TCHAR *targv[])
         } else if (strcmp(argv[i], "-no_inject") == 0 ||
                    /* support old drinjectx param name */
                    strcmp(argv[i], "-noinject") == 0 || strcmp(argv[i], "-static") == 0) {
+            DR_dll_not_needed = true;
             inject = false;
             continue;
         } else if (strcmp(argv[i], "-force") == 0) {
@@ -1520,6 +1526,7 @@ _tmain(int argc, TCHAR *targv[])
         }
 #endif
         else if (strcmp(argv[i], "-use_dll") == 0) {
+            DR_dll_not_needed = true;
             /* Support relative path: very useful! */
             get_absolute_path(argv[++i], custom_dll, BUFFER_SIZE_ELEMENTS(custom_dll));
             NULL_TERMINATE_BUFFER(custom_dll);