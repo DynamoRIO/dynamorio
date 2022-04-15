3e7136d39433dd2cd0e844d4f1bdb4f3f27b3652
Nahome <nbete@google.com>
2022-04-15 16:20:36 +0000

    Fix clang-format error

--- clients/drcachesim/tests/cache_replacement_policy_unit_test.cpp
+++ clients/drcachesim/tests/cache_replacement_policy_unit_test.cpp
@@ -102,7 +102,7 @@ public:
         ref.data.addr = addr;
         request(ref);
         assert(get_next_way_to_replace(get_block_index(addr)) ==
-                                       expected_replacement_way_after_access);
+               expected_replacement_way_after_access);
     }
 };
 
