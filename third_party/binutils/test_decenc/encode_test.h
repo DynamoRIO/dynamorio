d82eb8f55873b386e8ddf519cef859340d9d05ad
Prasun Ratn <prasun3@gmail.com>
2022-05-10 17:26:37 +0530

    i5483: clang format fix and added comment
    Issue: #5483

--- third_party/binutils/test_decenc/encode_test.h
+++ third_party/binutils/test_decenc/encode_test.h
@@ -5,35 +5,35 @@ enum {
     ENCODE_FLAG_Z = 2,
 };
 
-#        define ASSERT(x)                                                       \
-            ((void)((!(x)) ? (dr_fprintf(STDERR, "ASSERT FAILURE: %s:%d: %s\n", \
-                                         __FILE__, __LINE__, #x),               \
-                              dr_abort(), 0)                                    \
-                           : 0))
+#    define ASSERT(x)                                                                 \
+        ((void)((!(x)) ? (dr_fprintf(STDERR, "ASSERT FAILURE: %s:%d: %s\n", __FILE__, \
+                                     __LINE__, #x),                                   \
+                          dr_abort(), 0)                                              \
+                       : 0))
 
-#        define HANDLE_FLAGS(instr, flags)                       \
-            do {                                                 \
-                if (flags & ENCODE_FLAG_Z) {                     \
-                    instr_set_prefix_flag(instr, PREFIX_EVEX_z); \
-                }                                                \
-                if (flags & ENCODE_FLAG_SET_DST_SIZE_HALF) {     \
-                    opnd_t dst = instr_get_dst(instr, 0);        \
-                    if (opnd_get_size(dst) == OPSZ_64)           \
-                        opnd_set_size(&dst, OPSZ_32);            \
-                    else if (opnd_get_size(dst) == OPSZ_32)      \
-                        opnd_set_size(&dst, OPSZ_16);            \
-                    else if (opnd_get_size(dst) == OPSZ_16)      \
-                        opnd_set_size(&dst, OPSZ_8);             \
-                    instr_set_dst(instr, 0, dst);                \
-                }                                                \
-            } while (0)
+#    define HANDLE_FLAGS(instr, flags)                       \
+        do {                                                 \
+            if (flags & ENCODE_FLAG_Z) {                     \
+                instr_set_prefix_flag(instr, PREFIX_EVEX_z); \
+            }                                                \
+            if (flags & ENCODE_FLAG_SET_DST_SIZE_HALF) {     \
+                opnd_t dst = instr_get_dst(instr, 0);        \
+                if (opnd_get_size(dst) == OPSZ_64)           \
+                    opnd_set_size(&dst, OPSZ_32);            \
+                else if (opnd_get_size(dst) == OPSZ_32)      \
+                    opnd_set_size(&dst, OPSZ_16);            \
+                else if (opnd_get_size(dst) == OPSZ_16)      \
+                    opnd_set_size(&dst, OPSZ_8);             \
+                instr_set_dst(instr, 0, dst);                \
+            }                                                \
+        } while (0)
 
 //
-#        if VERBOSE
-#            define PRINT_TEST_NAME(name) // add fprintf
-#        else
-#            define PRINT_TEST_NAME(x)
-#        endif
+#    if VERBOSE
+#        define PRINT_TEST_NAME(name) // add fprintf
+#    else
+#        define PRINT_TEST_NAME(x)
+#    endif
 
 #    define ENCODE_TEST_3args(name, opc, flags, arg1, arg2, arg3)      \
         do {                                                           \
