6f96cfbf3e5d69ed70187ba2e2072cca21b70b85
Stanislaw Kardach <kda@semihalf.com>
2022-07-15 11:59:23 +0200

    Mark SEG_TLS as unused
    For now it doesn't seem we'll need it, so set it to DR_REG_INVALID.
    
    Signed-off-by: Stanislaw Kardach <kda@semihalf.com>

--- core/unix/os_exports.h
+++ core/unix/os_exports.h
@@ -106,8 +106,8 @@
 #        define LIB_SEG_TLS DR_REG_TPIDRURO /* libc+loader tls */
 #    endif                                  /* 64/32-bit */
 #elif defined(RISCV64)
-/* FIXME i#3544: Check if this is preserved by kernel and safe to use? */
-#    define SEG_TLS DR_REG_S11
+/* FIXME i#3544: Not used on RISC-V, so set to invalid. Check if this is true. */
+#    define SEG_TLS DR_REG_INVALID
 #    define LIB_SEG_TLS DR_REG_TP
 #endif /* X86/ARM */
 
