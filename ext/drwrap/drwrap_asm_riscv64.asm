8092f319fe7ea7b728dc968a25c771e80a52f912
Stanislaw Kardach <kda@semihalf.com>
2022-08-11 11:43:30 +0200

    i#3544 RISC-V, part 6: Add extension stubs (#5577)
    Add definitions required for extensions to compile for RISC-V.
    Most important, add is_aflags_spill/restore functions to replace
    X86/ARM/RISC-V ifdef ladders in the drreg and remove all
    handling of dr_mcontext::xflags from RISC-V paths in extensions.
    The field itself will be removed in other pull requests.
    With this the RISC-V logic can be cleaned up as it really should never
    spill/restore condition flags, simply because this architecture has
    none.
    
    NOTE:
      This code is not validated and mostly contains stubs as the main point
      was to achieve compilation and estimate the effort required for the
      port. Some of the trivial logic has been implemented though.
    
    Issue: #3544
    
    Signed-off-by: Stanislaw Kardach <kda@semihalf.com>

---
+++ ext/drwrap/drwrap_asm_riscv64.asm
@@ -0,0 +1,78 @@
+/* **********************************************************
+ * Copyright (c) 2022 Rivos, Inc.  All rights reserved.
+ * **********************************************************/
+
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Rivos, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL RIVOS, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+
+/***************************************************************************
+ * assembly utilities for which there are no intrinsics
+ */
+
+#include "cpp2asm_defines.h"
+
+START_FILE
+
+#define FUNCNAME replace_native_xfer
+        DECLARE_FUNC(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        ret
+        /* never reached */
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+DECLARE_GLOBAL(replace_native_ret_imms)
+DECLARE_GLOBAL(replace_native_ret_imms_end)
+#define FUNCNAME replace_native_rets
+        DECLARE_FUNC(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        ret
+ADDRTAKEN_LABEL(replace_native_ret_imms:)
+ADDRTAKEN_LABEL(replace_native_ret_imms_end:)
+        nop
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+#define FUNCNAME get_cur_xsp
+        DECLARE_FUNC(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        mv      a0, sp
+        ret
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+/* We just need a sentinel block that does not cause DR to complain about
+ * non-executable code or illegal instrutions, for DRWRAP_REPLACE_RETADDR.
+ */
+#define FUNCNAME replace_retaddr_sentinel
+        DECLARE_FUNC(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        ret
+        END_FUNC(FUNCNAME)
+
+END_FILE
