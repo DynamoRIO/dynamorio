be10db7fc189c1f8bcbe62f4266abf9c2c8e8238
Derek Bruening <bruening@google.com>
2022-11-14 10:47:58 -0500

    TO REVERT AFTER REVIEW: disable tab check for libelftc .patch file

--- suite/runsuite.cmake
+++ suite/runsuite.cmake
@@ -283,7 +283,7 @@ string(REGEX REPLACE "\n(---|\\+\\+\\+)[^\n]*\t" "" diff_notabs "${diff_contents
 string(REGEX MATCH "\t" match "${diff_notabs}")
 if (NOT "${match}" STREQUAL "")
   string(REGEX MATCH "\n[^\n]*\t[^\n]*" match "${diff_notabs}")
-  message(FATAL_ERROR "ERROR: diff contains tabs: ${match}")
+#NOCHECKIN disable for .patch: message(FATAL_ERROR "ERROR: diff contains tabs: ${match}")
 endif ()
 
 # Check for NOCHECKIN
