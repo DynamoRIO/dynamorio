8a5f39ae24137a9bfcb1d80493054885676e2484
Kuai Wei <kuaiwei.kw@alibaba-inc.com>
2022-12-13 18:55:45 +0800

    fix again

--- core/dispatch.c
+++ core/dispatch.c
@@ -948,7 +948,7 @@ dispatch_enter_dynamorio(dcontext_t *dcontext)
             } else {
                 LOG(THREAD, LOG_DISPATCH, 2,
                     "WARNING: skip selfmod region from %llx to %llx, size %d\n", begin,
-                    end, end-begin);
+                    end, end - begin);
             }
         }
 #endif
