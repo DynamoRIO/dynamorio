4b9a2c3dc0caf5e3e0a4233c1824ff605df7a924
Derek Bruening <bruening@google.com>
2022-12-19 22:56:51 -0500

    Write real xstate_bv into signal frame when setting the xstate context to avoid lazy restore problems

--- core/unix/signal_linux_x86.c
+++ core/unix/signal_linux_x86.c
@@ -265,6 +265,8 @@ save_xmm(dcontext_t *dcontext, sigframe_rt_t *frame)
          */
         uint bv_high, bv_low;
         dr_xgetbv(&bv_high, &bv_low);
+        LOG(THREAD, LOG_ASYNCH, 3, "setting xstate_bv from 0x%016lx to 0x%08x%08x\n",
+            xstate->xstate_hdr.xstate_bv, bv_high, bv_low);
         xstate->xstate_hdr.xstate_bv = (((uint64)bv_high) << 32) | bv_low;
     }
     for (i = 0; i < proc_num_simd_sse_avx_saved(); i++) {
@@ -594,6 +596,18 @@ mcontext_to_sigcontext_simd(sig_full_cxt_t *sc_full, priv_mcontext_t *mc)
                     memcpy(&xstate->ymmh.ymmh_space[i * 4], &mc->simd[i].u32[4],
                            YMMH_REG_SIZE);
                 }
+            }
+            /* Lazy signal state can stop us from setting the full state.
+             * Switch the signal to full state if necessary.
+             */
+            uint bv_high, bv_low;
+            dr_xgetbv(&bv_high, &bv_low);
+            uint64 real_val = (((uint64)bv_high) << 32) | bv_low;
+            if (xstate->xstate_hdr.xstate_bv != real_val) {
+                LOG(THREAD_GET, LOG_ASYNCH, 3,
+                    "%s: setting xstate_bv from 0x%016lx to 0x%016lx\n", __FUNCTION__,
+                    xstate->xstate_hdr.xstate_bv, real_val);
+                xstate->xstate_hdr.xstate_bv = real_val;
             }
         }
 #ifdef X64
