c84f972ffc40d1430220d1862378b46eb4f877b9
Sahil <guptsah@google.com>
2023-02-08 20:06:24 +0000

    Revert changes to invariant_checker.

--- clients/drcachesim/tools/invariant_checker.cpp
+++ clients/drcachesim/tools/invariant_checker.cpp
@@ -454,10 +454,7 @@ invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &mem
                   << " type " << memref.instr.type << "\n";
     }
     if (memref.marker.type == TRACE_TYPE_MARKER &&
-        memref.marker.marker_type == TRACE_MARKER_TYPE_TIMESTAMP &&
-        // Timestamps at the start of a chunk should be invisible to analysis tools. These
-        // are duplicate timestamps used to faciliate skipping.
-        shard->prev_entry_.marker.marker_type != TRACE_MARKER_TYPE_CHUNK_FOOTER) {
+        memref.marker.marker_type == TRACE_MARKER_TYPE_TIMESTAMP) {
         shard->last_timestamp_ = memref.marker.marker_value;
         shard->saw_timestamp_but_no_instr_ = true;
         if (knob_verbose_ >= 3) {
