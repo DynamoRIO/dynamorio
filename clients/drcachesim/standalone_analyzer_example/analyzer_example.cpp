903308e5da43bcc7cae7301fde174e8b8b959c9e
prasun3 <prasun3@gmail.com>
2023-02-17 14:58:13 +0530

    i5879: clang-format fix
    Issue: #5879

--- clients/drcachesim/standalone_analyzer_example/analyzer_example.cpp
+++ clients/drcachesim/standalone_analyzer_example/analyzer_example.cpp
@@ -6,36 +6,43 @@
 #include "drmemtrace/analysis_tool.h"
 #include "drmemtrace/analyzer.h"
 
-#define FATAL_ERROR(msg, ...) do { \
-    fprintf(stderr, "ERROR: " msg "\n", ##__VA_ARGS__);    \
-    fflush(stderr); \
-    exit(1); \
-} while (0)
+#define FATAL_ERROR(msg, ...)                               \
+    do {                                                    \
+        fprintf(stderr, "ERROR: " msg "\n", ##__VA_ARGS__); \
+        fflush(stderr);                                     \
+        exit(1);                                            \
+    } while (0)
 
-static droption_t<std::string> op_trace
-(DROPTION_SCOPE_FRONTEND, "trace", "", "[Required] Trace input file",
- "Specifies the file containing the trace to be analyzed.");
+static droption_t<std::string>
+    op_trace(DROPTION_SCOPE_FRONTEND, "trace", "", "[Required] Trace input file",
+             "Specifies the file containing the trace to be analyzed.");
 
 class analyzer_example_t : public analysis_tool_t {
 public:
-    analyzer_example_t(const std::string &module_file_path) {}
-    bool
-    process_memref(const memref_t &memref) override {
-      if (type_is_instr(memref.instr.type)) {
-        num_instrs++;
-      }
-      return true;
+    analyzer_example_t(const std::string &module_file_path)
+    {
     }
     bool
-    print_results() override {
-      std::cerr << "Found " << num_instrs << " instructions\n";
-      return true;
+    process_memref(const memref_t &memref) override
+    {
+        if (type_is_instr(memref.instr.type)) {
+            num_instrs++;
+        }
+        return true;
     }
+    bool
+    print_results() override
+    {
+        std::cerr << "Found " << num_instrs << " instructions\n";
+        return true;
+    }
+
 protected:
     int num_instrs = 0;
 };
 
-int _tmain(int argc, const TCHAR *targv[])
+int
+_tmain(int argc, const TCHAR *targv[])
 {
     // Convert to UTF-8 if necessary
     char **argv;
@@ -47,17 +54,17 @@ int _tmain(int argc, const TCHAR *targv[])
     if (!droption_parser_t::parse_argv(DROPTION_SCOPE_FRONTEND, argc, (const char **)argv,
                                        &parse_err, NULL) ||
         op_trace.get_value().empty()) {
-        if (parse_err.find ("help") != std::string::npos) {
+        if (parse_err.find("help") != std::string::npos) {
             FATAL_ERROR("Usage error: %s\nUsage:\n%s", parse_err.c_str(),
                         droption_parser_t::usage_long(DROPTION_SCOPE_ALL).c_str());
         } else {
             FATAL_ERROR("Usage error: %s\nUsage:\n%s", parse_err.c_str(),
-                    droption_parser_t::usage_short(DROPTION_SCOPE_ALL).c_str());
+                        droption_parser_t::usage_short(DROPTION_SCOPE_ALL).c_str());
         }
     }
 
-    analysis_tool_t * d = new analyzer_example_t("");
-    std::vector<analysis_tool_t*> tools;
+    analysis_tool_t *d = new analyzer_example_t("");
+    std::vector<analysis_tool_t *> tools;
     tools.push_back(d);
     analyzer_t analyzer(op_trace.get_value(), &tools[0], (int)tools.size());
     if (!analyzer)
@@ -70,7 +77,6 @@ int _tmain(int argc, const TCHAR *targv[])
     sc = drfront_cleanup_args(argv, argc);
     if (sc != DRFRONT_SUCCESS)
         FATAL_ERROR("drfront_cleanup_args failed: %d\n", sc);
-
 
     return 0;
 }
