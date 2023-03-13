8cfea5a99a655bbc61e422051ae2df5ed5e27ce6
Derek Bruening <bruening@google.com>
2023-03-13 19:59:19 -0400

    Add another check of drdecode use with an explicit dr_standalone_init() inside instr_init() to avoid the complexity of calling dr_standalone_init() in invariant_checker and to support the documented use of drdecode when full DR is linked in (xref i#2499).  A simple one-src-no-dest decode does not hit the existing heap alloc check.

--- core/ir/instr_shared.c
+++ core/ir/instr_shared.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2022 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2023 Google, Inc.  All rights reserved.
  * Copyright (c) 2000-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -176,6 +176,16 @@ instr_init(void *drcontext, instr_t *instr)
      * an uninitialized instruction */
     memset((void *)instr, 0, sizeof(instr_t));
     instr_set_isa_mode(instr, dr_get_isa_mode(dcontext));
+#ifndef NOT_DYNAMORIO_CORE_PROPER
+    /* Just like in global_heap_alloc() we pay the cost of this check to support
+     * drdecode use even with full DR linked in (i#2499).  Decoding of simple
+     * single-source-no-dest instrs never hits the heap code so we check here too.
+     */
+    if (dcontext == GLOBAL_DCONTEXT && !dynamo_initialized) {
+        /* XXX: We have no control point to call standalone_exit(). */
+        standalone_init();
+    }
+#endif
 }
 
 /* zeroes out the fields of instr */
