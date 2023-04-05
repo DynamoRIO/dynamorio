40bb7f885b8123b25c399f419f66e9ce77a98574
Sahil <guptsah@google.com>
2023-04-05 17:21:51 +0000

    Rename conditions.

--- clients/drcachesim/tools/invariant_checker.cpp
+++ clients/drcachesim/tools/invariant_checker.cpp
@@ -411,14 +411,14 @@ invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &mem
         }
         if (shard->prev_instr_.instr.addr != 0 /*first*/) {
 
-            bool branch_condition =
+            bool branch_target_condition =
                 (type_is_instr_branch(shard->prev_instr_.instr.type) &&
                  !type_is_instr_direct_branch(shard->prev_instr_.instr.type)) ||
                 // Conditional fall-through hits the regular case above.
                 (type_is_instr_direct_branch(shard->prev_instr_.instr.type) &&
                  (!have_cond_branch_target || memref.instr.addr == cond_branch_target));
 
-            bool condition =
+            bool control_flow_condition =
                 TESTANY(OFFLINE_FILE_TYPE_FILTERED | OFFLINE_FILE_TYPE_IFILTERED,
                         shard->file_type_) ||
                 // Regular fall-through.
@@ -443,13 +443,13 @@ invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &mem
                 shard->prev_instr_.instr.type == TRACE_TYPE_INSTR_SYSENTER;
 
             // TODO(sahil): Clean up this logic.
-            if (!condition) {
-                if (!branch_condition) {
-                    report_if_false(shard, branch_condition,
+            if (!control_flow_condition) {
+                if (!branch_target_condition) {
+                    report_if_false(shard, branch_target_condition,
                                     "Direct branch target PC discontinuity");
                 } else {
                     report_if_false(shard, // Filtered.
-                                    condition || branch_condition,
+                                    control_flow_condition || branch_target_condition,
                                     "Non-explicit control flow has no marker");
                 }
             }
