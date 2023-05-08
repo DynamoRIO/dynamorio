eab6dcb3761d888767faf5bfe8d3006e782f69ee
brettcoon <brett.coon@gmail.com>
2023-05-08 15:21:27 -0700

    Reuse Distance: Add data access histogram to collected stats.
    Adds a separate histogram for non-instruction access histograms.
    This primary histogram continues to include all accesses.  The
    final output reports instruction and data reference counts, and
    print_histogram() includes both total and data-only accesses.

--- clients/drcachesim/tools/reuse_distance.h
+++ clients/drcachesim/tools/reuse_distance.h
@@ -90,6 +90,9 @@ public:
     // different verbosities.
     static unsigned int knob_verbose;
 
+    using distance_histogram_t = std::unordered_map<int_least64_t, int_least64_t>;
+    using distance_map_pair_t = std::pair<int_least64_t, int_least64_t>;
+
 protected:
     // We assume that the shard unit is the unit over which we should measure
     // distance.  By default this is a traced thread.  For serial operation we look
@@ -101,10 +104,22 @@ protected:
                      unsigned int distance_limit, bool verify);
         std::unordered_map<addr_t, line_ref_t *> cache_map;
         std::unordered_set<addr_t> pruned_addresses;
-        // This is our reuse distance histogram.
-        std::unordered_map<int_least64_t, int_least64_t> dist_map;
+        // These are our reuse distance histograms: one for all accesses and one
+        // only for data references.  An instruction histogram can be computed by
+        // subtracting data references from the full histogram.  The final histogram
+        // statistics need a full histogram to sort, and in most traces the majority of
+        // accesss are instruction references, so the histograms are split this way to
+        // provide the full histogram we need with the smallest secondary histogram.
+        // Furthermore, during analysis each reference is added to only one histogram
+        // to minimize the performance impact of dual histogram collection, with the data
+        // references added into the primary histogram during final result aggregation.
+        // This means dist_map is effectively instruction-only until aggregation.
+        distance_histogram_t dist_map;
+        distance_histogram_t dist_map_data;
+        bool dist_map_needs_merge = true;
         std::unique_ptr<line_ref_list_t> ref_list;
         int_least64_t total_refs = 0;
+        int_least64_t data_refs = 0; // Non-instruction reference count.
         // Ideally the shard index would be the tid when shard==thread but that's
         // not the case today so we store the tid.
         memref_tid_t tid;
@@ -119,7 +134,8 @@ protected:
 
     void
     print_histogram(std::ostream &out, int_least64_t total_count,
-                    const std::vector<std::pair<int_least64_t, int_least64_t>> &sorted);
+                    const std::vector<distance_map_pair_t> &sorted,
+                    const distance_histogram_t &dist_map_data);
 
     void
     print_shard_results(const shard_data_t *shard);
