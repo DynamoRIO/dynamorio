2a9a6206de0652921be2793617ef9d2818a709f6
Ze Yang <yang.ze@utexas.edu>
2023-05-09 10:41:35 -0500

    update format

--- clients/drcachesim/tools/opcode_mix.h
+++ clients/drcachesim/tools/opcode_mix.h
@@ -76,7 +76,7 @@ protected:
         std::unordered_map<app_pc, int> opcode_cache;
     };
 
-    enum branch_t { cbr, ubr, mbr, ret, dircall, indcall};
+    enum branch_t { cbr, ubr, mbr, ret, dircall, indcall };
 
     struct shard_data_t {
         shard_data_t()
@@ -99,7 +99,8 @@ protected:
         int_least64_t branch_instr_count;
         std::unordered_map<int, int_least64_t> opcode_counts;
         std::unordered_map<int, std::unordered_map<branch_t, int_least64_t>>
-            opcode_branch_counts; // TODO: refactor to define new struct after design finalized.
+            opcode_branch_counts; // TODO: refactor to define new struct after design
+                                  // finalized.
         std::string error;
         app_pc last_trace_module_start;
         size_t last_trace_module_size;
