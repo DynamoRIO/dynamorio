DynamoRIO/dynamorio

commit a6f95720a86c0b6a624df7daad7aa3aafe96d8c9

i#6020: Add trace interval analysis support (#6022)
Adds support for performing trace interval analysis.

Adds a new analyzer framework option -interval_microseconds that specifies the
length of each trace interval, which is measured using the
TRACE_MARKER_TYPE_TIMESTAMP marker values in the trace.

Adds new analysis_tool_t APIs: generate_interval_snapshot() (for serial analysis) and
generate_shard_interval_snapshot() (for parallel analysis), which notify the analysis
tool when a trace interval completes so that it can generate a snapshot of its
internal state. These snapshots are passed to the tool at the end in the
print_interval_results() API where the tool can compute and print per-interval results
as needed.

Adds the combine_interval_snapshots() analysis_tool_t API that allows the analyzer
to combine shard-local interval state snapshots from all trace shards, for each
whole-trace interval. This is to support whole-trace interval results in the parallel
mode of the analyzer.

Adds trace_interval_analysis_unit_tests that verify the invocation of the new
analysis tool APIs by using test versions of analyzer_t and stream_t. Also verifies
that mixing tools that do and do not support interval analysis works properly.

Adds an example usage of the new APIs in the existing basic counts tool, and
also adds a regex test on basic counts output.

Issue: #6020

clients/drcachesim/analyzer.h
@@ -45,6 +45,7 @@
 
 #include <iterator>
 #include <memory>
+#include <queue>
 #include <string>
 #include <vector>
 #include "analysis_tool.h"
@@ -103,7 +104,8 @@ public:
      */
     analyzer_tmpl_t(const std::string &trace_path,
                     analysis_tool_tmpl_t<RecordType> **tools, int num_tools,
-                    int worker_count = 0, uint64_t skip_instrs = 0, int verbosity = 0);
+                    int worker_count = 0, uint64_t skip_instrs = 0,
+                    uint64_t interval_microseconds = 0, int verbosity = 0);
     /** Launches the analysis process. */
     virtual bool
     run();
@@ -113,6 +115,41 @@ public:
 
 protected:
     typedef scheduler_tmpl_t<RecordType, ReaderType> sched_type_t;
+
+    // Tool data for one shard.
+    struct analyzer_tool_shard_data_t {
+        analyzer_tool_shard_data_t()
+            : shard_data(nullptr)
+        {
+        }
+        analyzer_tool_shard_data_t(analyzer_tool_shard_data_t &&src)
+        {
+            shard_data = src.shard_data;
+            interval_snapshot_data = std::move(interval_snapshot_data);
+        }
+
+        void *shard_data;
+        // This is a queue as merge_shard_interval_results processes the intervals in a
+        // FIFO manner. Using a queue also makes code a bit simpler.
+        std::queue<typename analysis_tool_tmpl_t<RecordType>::interval_state_snapshot_t *>
+            interval_snapshot_data;
+    };
+
+    // Data for one trace shard.
+    struct analyzer_shard_data_t {
+        analyzer_shard_data_t()
+            : cur_interval_index(0)
+        {
+        }
+        analyzer_shard_data_t(analyzer_shard_data_t &&src)
+        {
+            cur_interval_index = src.cur_interval_index;
+            tool_data = std::move(tool_data);
+        }
+
+        uint64_t cur_interval_index;
+        std::vector<analyzer_tool_shard_data_t> tool_data;
+    };
 
     // Data for one worker thread.  Our concurrency model has each input shard
     // analyzed by a single worker thread, eliminating the need for locks.
@@ -126,12 +163,14 @@ protected:
         {
             index = src.index;
             stream = src.stream;
+            shard_data = std::move(src.shard_data);
             error = std::move(src.error);
         }
 
         int index;
         typename scheduler_tmpl_t<RecordType, ReaderType>::stream_t *stream;
         std::string error;
+        std::unordered_map<int, analyzer_shard_data_t> shard_data;
 
     private:
         analyzer_worker_data_t(const analyzer_worker_data_t &) = delete;
@@ -165,6 +204,46 @@ protected:
     bool
     record_is_thread_final(RecordType record);
 
+    bool
+    record_is_timestamp(const RecordType &record);
+
+    // Invoked when the given interval finishes during serial or parallel
+    // analysis of the trace. For parallel analysis, the shard_id
+    // parameter should be set to the shard_id for which the interval
+    // finished. For serial analysis, it should remain the default value.
+    bool
+    process_interval(uint64_t interval_id, analyzer_worker_data_t *worker, bool parallel,
+                     int shard_id = 0);
+
+    // Possibly advances the current interval id stored in the worker data, based
+    // on the most recent seen timestamp in the trace stream. Returns whether the
+    // current interval id was updated, and if so also sets the previous interval index
+    // in prev_interval_index.
+    bool
+    advance_interval_id(
+        typename scheduler_tmpl_t<RecordType, ReaderType>::stream_t *stream,
+        analyzer_shard_data_t *shard, uint64_t &prev_interval_index);
+
+    // Collects interval results for all shards from the workers, and then merges
+    // the shard-local intervals to form the whole-trace interval results using
+    // merge_shard_interval_results().
+    bool
+    collect_and_merge_shard_interval_results();
+
+    // Computes and stores the interval results in merged_interval_snapshots_. For
+    // serial analysis where we already have only a single shard, this involves
+    // simply copying interval_state_snapshot_t* from the input. For parallel
+    // analysis, this involves merging results from multiple shards for intervals
+    // that map to the same final whole-trace interval.
+    virtual bool
+    merge_shard_interval_results(
+        std::vector<std::queue<
+            typename analysis_tool_tmpl_t<RecordType>::interval_state_snapshot_t *>>
+            &intervals,
+        std::vector<typename analysis_tool_tmpl_t<RecordType>::interval_state_snapshot_t
+                        *> &merged_intervals,
+        int tool_idx);
+
     bool success_;
     scheduler_tmpl_t<RecordType, ReaderType> scheduler_;
     std::string error_string_;
@@ -173,10 +252,19 @@ protected:
     std::vector<analyzer_worker_data_t> worker_data_;
     int num_tools_;
     analysis_tool_tmpl_t<RecordType> **tools_;
+    // Stores the interval state snapshots for the whole trace, which for the parallel
+    // mode are the resulting interval state snapshots after merging from all shards
+    // in merge_shard_interval_results.
+    // merged_interval_snapshots_[tool_idx] is a vector of the interval snapshots
+    // (in order of the intervals) for that tool.
+    std::vector<std::vector<
+        typename analysis_tool_tmpl_t<RecordType>::interval_state_snapshot_t *>>
+        merged_interval_snapshots_;
     bool parallel_;
     int worker_count_;
     const char *output_prefix_ = "[analyzer]";
     uint64_t skip_instrs_ = 0;
+    uint64_t interval_microseconds_ = 0;
     int verbosity_ = 0;
 
 private:

api/docs/release.dox
@@ -166,6 +166,19 @@ changes:
    INSTR_CREATE_eor_sve_pred(), INSTR_CREATE_and_sve_pred() and
    INSTR_CREATE_bic_sve_pred() to use the new vector element registers and to
    correctly encode the predicate mode.
+ - Added a new drmemtrace analyzer option \p -interval_microseconds and various new
+   #analysis_tool_t APIs for producing per-interval results. The analyzer framework
+   invokes the generate_interval_snapshot() and generate_shard_interval_snapshot()
+   analysis tool APIs periodically every \p -interval_microseconds of the trace as
+   measured by the #TRACE_MARKER_TYPE_TIMESTAMP marker values. In these callbacks, the
+   tool creates and returns a snapshot of their internal state that is required to
+   produce and print per-interval results in a later print_interval_results() call.
+   The tool's internal state is a struct derived from the
+   #analysis_tool_tmpl_t::interval_state_snapshot_t base struct. Additionally, the tool
+   implements combine_interval_snapshot() to combine two interval snapshot structs,
+   which is required for producing whole-trace results in the parallel mode of analyzer
+   operation, and also release_interval_snapshot() which is used to release the
+   analyzer framework's claim to the interval snapshot objects.
 
 Further non-compatibility-affecting changes include:
  - Added AArchXX support for attaching to a running process.

clients/drcachesim/CMakeLists.txt
@@ -719,6 +719,14 @@ if (BUILD_TESTS)
                      ${trace_dir} --tmp_output_dir ${tmp_output_dir})
   endif ()
 
+  add_executable(tool.drcacheoff.trace_interval_analysis_unit_tests
+                 tests/trace_interval_analysis_unit_tests.cpp)
+  add_win32_flags(tool.drcacheoff.trace_interval_analysis_unit_tests)
+  target_link_libraries(tool.drcacheoff.trace_interval_analysis_unit_tests
+    drmemtrace_analyzer drdecode)
+  add_test(NAME tool.drcacheoff.trace_interval_analysis_unit_tests
+           COMMAND tool.drcacheoff.trace_interval_analysis_unit_tests)
+
   if (DR_HOST_AARCH64 AND NOT APPLE) # i#1997: static DR on Mac NYI.
     add_executable(tool.drcacheoff.burst_aarch64_sys tests/burst_aarch64_sys.cpp)
     configure_DynamoRIO_static(tool.drcacheoff.burst_aarch64_sys)

clients/drcachesim/analysis_tool.h
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2016-2022 Google, Inc.  All rights reserved.
+ * Copyright (c) 2016-2023 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
@@ -47,6 +47,7 @@
 #include "memtrace_stream.h"
 #include "trace_entry.h"
 #include <string>
+#include <vector>
 
 /**
  * The base class for a tool that analyzes a trace.  A new tool should subclass this
@@ -158,6 +159,124 @@ public:
     print_results() = 0;
 
     /**
+     * Struct that stores details of a tool's state snapshot at an interval. This is
+     * useful for computing and combining interval results. Tools should inherit from
+     * this struct to define their own state snapshot structs. Tools do not need to
+     * supply any values to construct this base struct; they can simply use the
+     * default constructor. The members of this base class will be set by the
+     * framework automatically.
+     */
+    struct interval_state_snapshot_t {
+        interval_state_snapshot_t(uint64_t interval_id, uint64_t interval_end_timestamp)
+            : interval_id(interval_id)
+            , interval_end_timestamp(interval_end_timestamp)
+        {
+        }
+        interval_state_snapshot_t()
+            : interval_id(0)
+            , interval_end_timestamp(0)
+        {
+        }
+
+        uint64_t interval_id;
+        // Stores the timestamp (exclusive) when the above interval ends. Note
+        // that this is not the last timestamp actually seen in the trace interval,
+        // but simply the abstract boundary of the interval. This will be aligned
+        // to the specified -interval_microseconds.
+        uint64_t interval_end_timestamp;
+
+        virtual ~interval_state_snapshot_t() = default;
+    };
+    /**
+     * Notifies the analysis tool that the given trace \p interval_id has ended so
+     * that it can generate a snapshot of its internal state in a struct derived
+     * from \p interval_state_snapshot_t, and return a pointer to it. The returned
+     * pointer will be provided to the tool in later combine_interval_snapshots()
+     * and print_interval_result() calls.
+     *
+     * \p interval_id is the ordinal of the trace interval that just ended. Trace
+     * intervals have a length equal to the \p -interval_microseconds specified
+     * to the framework. Trace intervals are measured using the value of the
+     * #TRACE_MARKER_TYPE_TIMESTAMP markers. The provided \p interval_id
+     * values will be monotonically increasing but may not be continuous,
+     * i.e. the tool may not see some \p interval_id if the trace did not have
+     * any activity in that interval.
+     *
+     * The returned \p interval_state_snapshot_t* will be passed to the
+     * combine_interval_snapshots() API which is invoked by the framework to merge
+     * multiple \p interval_state_snapshot_t from different shards in the parallel
+     * mode of the analyzer.
+     *
+     * Finally, the print_interval_result() API is invoked with a list of
+     * \p interval_state_snapshot_t* representing interval snapshots for the
+     * whole trace.
+     *
+     * The tool must not de-allocate the state snapshot until
+     * release_interval_snapshot() is invoked by the framework.
+     *
+     * An example use case of this API is to create a time series of some output
+     * metric over the whole trace.
+     */
+    virtual interval_state_snapshot_t *
+    generate_interval_snapshot(uint64_t interval_id)
+    {
+        return nullptr;
+    }
+    /**
+     * Invoked by the framework to combine the shard-local \p interval_state_snapshot_t
+     * objects pointed at by \p latest_shard_snapshots, to create the combined
+     * \p interval_state_snapshot_t for the whole-trace interval that ends at
+     * \p interval_end_timestamp. This is useful in the parallel mode of the analyzer,
+     * where each trace shard is processed in parallel. In this mode, the framework
+     * creates the \p interval_state_snapshot_t for each whole-trace interval, one by one.
+     * For each interval, it invokes combine_interval_snapshots() with the latest seen
+     * interval snapshots from each shard (in \p latest_shard_snapshots) as of the
+     * interval ending at \p interval_end_timestamp. \p latest_shard_snapshots will
+     * contain at least one non-null element, and at least one of them will have its \p
+     * interval_state_snapshot_t::interval_end_timestamp set to \p interval_end_timestamp.
+     * Tools must not return a nullptr. Tools must not modify the provided snapshots.
+     *
+     * \p interval_end_timestamp helps the tool to determine which of the provided
+     * \p latest_shard_snapshots are relevant to it.
+     * - for cumulative metrics (e.g. total instructions till an interval), the tool
+     *   may want to combine latest interval snapshots from all shards to get the accurate
+     *   cumulative count.
+     * - for delta metrics (e.g. incremental instructions in an interval), the tool may
+     *   want to combine interval snapshots from only the shards that were observed in the
+     *   current interval (with \p latest_shard_snapshots [i]->interval_end_timestamp ==
+     *   \p interval_end_timestamp)
+     * - or if the tool mixes cumulative and delta metrics: some field-specific logic that
+     *   combines the above two strategies.
+     */
+    virtual interval_state_snapshot_t *
+    combine_interval_snapshots(
+        const std::vector<const interval_state_snapshot_t *> latest_shard_snapshots,
+        uint64_t interval_end_timestamp)
+    {
+        return nullptr;
+    }
+    /**
+     * Prints the interval results for the whole trace. The state snapshots
+     * for intervals can be found in the given \p interval_snapshots.
+     */
+    virtual bool
+    print_interval_results(
+        const std::vector<interval_state_snapshot_t *> &interval_snapshots)
+    {
+        return true;
+    }
+    /**
+     * Notifies the tool that the \p interval_state_snapshot_t object pointed to
+     * by \p interval_snapshot is no longer needed by the framework. The tool may
+     * de-allocate it right away or later, as it needs. Returns whether it was
+     * successful.
+     */
+    virtual bool
+    release_interval_snapshot(interval_state_snapshot_t *interval_snapshot)
+    {
+        return true;
+    }
+    /**
      * Returns whether this tool supports analyzing trace shards concurrently, or
      * whether it needs to see a single thread-interleaved stream of traced
      * events.  This may be called prior to initialize().
@@ -252,6 +371,51 @@ public:
     parallel_shard_error(void *shard_data)
     {
         return "";
+    }
+    /**
+     * Notifies the analysis tool that the given trace \p interval_id in the shard
+     * represented by the given \p shard_data has ended, so that it can generate a
+     * snapshot of its internal state in a struct derived from \p
+     * interval_state_snapshot_t, and return a pointer to it. The returned pointer will
+     * be provided to the tool in later combine_interval_snapshots() and
+     * print_interval_result() calls.
+     *
+     * Note that the provided \p interval_id is local to the shard that is
+     * represented by the given \p shard_data, and not the whole-trace interval. The
+     * framework will automatically create an \p interval_state_snapshot_t for each
+     * whole-trace interval by using combine_interval_snapshots() to combine one or more
+     * shard-local \p interval_state_snapshot_t corresponding to that whole-trace
+     * interval.
+     *
+     * \p interval_id is the ordinal of the trace interval that just ended. Trace
+     * intervals have a length equal to the \p -interval_microseconds specified
+     * to the framework. Trace intervals are measured using the value of the
+     * #TRACE_MARKER_TYPE_TIMESTAMP markers. The provided \p interval_id
+     * values will be monotonically increasing but may not be continuous,
+     * i.e. the tool may not see some \p interval_id if the trace shard did not
+     * have any activity in that interval.
+     *
+     * The returned \p interval_state_snapshot_t* will be passed to the
+     * combine_interval_snapshot() API which is invoked by the framework to merge
+     * multiple \p interval_state_snapshot_t from different shards in the parallel
+     * mode of the analyzer.
+     *
+     * Finally, the print_interval_result() API is invoked with a list of
+     * \p interval_state_snapshot_t* representing interval snapshots for the
+     * whole trace. In the parallel mode of the analyzer, this list is computed by
+     * combining the shard-local \p interval_state_snapshot_t using the tool's
+     * combine_interval_snapshot() API.
+     *
+     * The tool must not de-allocate the state snapshot until
+     * release_interval_snapshot() is invoked by the framework.
+     *
+     * An example use case of this API is to create a time series of some output
+     * metric over the whole trace.
+     */
+    virtual interval_state_snapshot_t *
+    generate_shard_interval_snapshot(void *shard_data, uint64_t interval_id)
+    {
+        return nullptr;
     }
 
 protected:

clients/drcachesim/analyzer_multi.cpp
@@ -53,6 +53,7 @@ analyzer_multi_t::analyzer_multi_t()
 {
     worker_count_ = op_jobs.get_value();
     skip_instrs_ = op_skip_instrs.get_value();
+    interval_microseconds_ = op_interval_microseconds.get_value();
     // Initial measurements show it's sometimes faster to keep the parallel model
     // of using single-file readers but use them sequentially, as opposed to
     // the every-file interleaving reader, but the user can specify -jobs 1, so

clients/drcachesim/common/memtrace_stream.h
@@ -84,10 +84,16 @@ public:
     get_stream_name() const = 0;
 
     /**
-     * Returns the value of the last seen #TRACE_MARKER_TYPE_TIMESTAMP marker.
+     * Returns the value of the most recently seen #TRACE_MARKER_TYPE_TIMESTAMP marker.
      */
     virtual uint64_t
     get_last_timestamp() const = 0;
+
+    /**
+     * Returns the value of the first seen #TRACE_MARKER_TYPE_TIMESTAMP marker.
+     */
+    virtual uint64_t
+    get_first_timestamp() const = 0;
 
     /**
      * Returns the #trace_version_t value from the #TRACE_MARKER_TYPE_VERSION record
@@ -174,6 +180,11 @@ public:
     }
     uint64_t
     get_last_timestamp() const override
+    {
+        return 0;
+    }
+    uint64_t
+    get_first_timestamp() const override
     {
         return 0;
     }

clients/drcachesim/common/options.cpp
@@ -487,6 +487,13 @@ droption_t<std::string> op_tracer_ops(
     "(For internal use: sweeps up tracer options)",
     "This is an internal option that sweeps up other options to pass to the tracer.");
 
+droption_t<bytesize_t> op_interval_microseconds(
+    DROPTION_SCOPE_FRONTEND, "interval_microseconds", 0,
+    "Enable periodic heartbeats for intervals of given microseconds in the trace.",
+    "Desired length of each trace interval, defined in microseconds of trace time. "
+    "Trace intervals are measured using the TRACE_MARKER_TYPE_TIMESTAMP marker values. "
+    "If set, analysis tools receive a callback at the end of each interval.");
+
 droption_t<int>
     op_only_thread(DROPTION_SCOPE_FRONTEND, "only_thread", 0,
                    "Only analyze this thread (0 means all)",

commit a134503fb98262fb22a59816e97ab1cf8ed362ca

i#6020: Fix incorrect args to std::move.
Fixes incorrect args to std::move. This was another bug introduced in PR #6022.

Issue: #6020

clients/drcachesim/analyzer.h
@@ -125,7 +125,7 @@ protected:
         analyzer_tool_shard_data_t(analyzer_tool_shard_data_t &&src)
         {
             shard_data = src.shard_data;
-            interval_snapshot_data = std::move(interval_snapshot_data);
+            interval_snapshot_data = std::move(src.interval_snapshot_data);
         }
 
         void *shard_data;
@@ -144,7 +144,7 @@ protected:
         analyzer_shard_data_t(analyzer_shard_data_t &&src)
         {
             cur_interval_index = src.cur_interval_index;
-            tool_data = std::move(tool_data);
+            tool_data = std::move(src.tool_data);
         }
 
         uint64_t cur_interval_index;