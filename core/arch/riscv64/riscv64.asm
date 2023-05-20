87af19efb207d54d9b619f936afbdf70687af1d7
Yang Liu <liuyang22@iscas.ac.cn>
2023-05-20 11:58:53 +0800

    i#3544 RV64: Implemented more global assembly functions (#6064)
    This patch implemented the following global assembly functions for RV64:
    1. `dr_app_start`
    2. `dr_app_take_over`
    3. `dr_app_running_under_dynamorio`
    4. `dynamorio_app_take_over`
    
    Issue: #3544

--- core/arch/riscv64/riscv64.asm
+++ core/arch/riscv64/riscv64.asm
@@ -40,6 +40,17 @@ START_FILE
 # error Non-Linux is not supported
 #endif
 
+/* sizeof(priv_mcontext_t) rounded up to a multiple of 16 */
+/* The reserved space for SIMD is also included. */
+#define PRIV_MCONTEXT_SIZE 0x290
+
+/* offset of priv_mcontext_t in dr_mcontext_t */
+#define PRIV_MCONTEXT_OFFSET 16
+
+#if PRIV_MCONTEXT_OFFSET < 16 || PRIV_MCONTEXT_OFFSET % 16 != 0
+# error PRIV_MCONTEXT_OFFSET
+#endif
+
 /* offsetof(dcontext_t, dstack) */
 #define dstack_OFFSET     0x458
 /* offsetof(dcontext_t, is_exiting) */
@@ -144,21 +155,114 @@ GLOBAL_LABEL(dr_call_on_clean_stack:)
 
 #ifdef DR_APP_EXPORTS
 
+/* Save priv_mcontext_t, except for x0(zero), x1(ra), x2(sp), x3(scratch),
+ * x10(arg1) and pc, to the address in ARG1.
+ * Typically the caller will save those five registers itself before calling this.
+ */
+save_priv_mcontext_helper:
+        sd       x4,   4*ARG_SZ(ARG1)
+        sd       x5,   5*ARG_SZ(ARG1)
+        sd       x6,   6*ARG_SZ(ARG1)
+        sd       x7,   7*ARG_SZ(ARG1)
+        sd       x8,   8*ARG_SZ(ARG1)
+        sd       x9,   9*ARG_SZ(ARG1)
+        /* a0 (10*ARG_SZ) is already saved. */
+        sd       x11, 11*ARG_SZ(ARG1)
+        sd       x12, 12*ARG_SZ(ARG1)
+        sd       x13, 13*ARG_SZ(ARG1)
+        sd       x14, 14*ARG_SZ(ARG1)
+        sd       x15, 15*ARG_SZ(ARG1)
+        sd       x16, 16*ARG_SZ(ARG1)
+        sd       x17, 17*ARG_SZ(ARG1)
+        sd       x18, 18*ARG_SZ(ARG1)
+        sd       x19, 19*ARG_SZ(ARG1)
+        sd       x20, 20*ARG_SZ(ARG1)
+        sd       x21, 21*ARG_SZ(ARG1)
+        sd       x22, 22*ARG_SZ(ARG1)
+        sd       x23, 23*ARG_SZ(ARG1)
+        sd       x24, 24*ARG_SZ(ARG1)
+        sd       x25, 25*ARG_SZ(ARG1)
+        sd       x26, 26*ARG_SZ(ARG1)
+        sd       x27, 27*ARG_SZ(ARG1)
+        sd       x28, 28*ARG_SZ(ARG1)
+        sd       x29, 29*ARG_SZ(ARG1)
+        sd       x30, 30*ARG_SZ(ARG1)
+        sd       x31, 31*ARG_SZ(ARG1)
+        /* pc (32*ARG_SZ) is already saved. */
+        fsd      f0,  33*ARG_SZ(ARG1)
+        fsd      f1,  34*ARG_SZ(ARG1)
+        fsd      f2,  35*ARG_SZ(ARG1)
+        fsd      f3,  36*ARG_SZ(ARG1)
+        fsd      f4,  37*ARG_SZ(ARG1)
+        fsd      f5,  38*ARG_SZ(ARG1)
+        fsd      f6,  39*ARG_SZ(ARG1)
+        fsd      f7,  40*ARG_SZ(ARG1)
+        fsd      f8,  41*ARG_SZ(ARG1)
+        fsd      f9,  42*ARG_SZ(ARG1)
+        fsd      f10, 43*ARG_SZ(ARG1)
+        fsd      f11, 44*ARG_SZ(ARG1)
+        fsd      f12, 45*ARG_SZ(ARG1)
+        fsd      f13, 46*ARG_SZ(ARG1)
+        fsd      f14, 47*ARG_SZ(ARG1)
+        fsd      f15, 48*ARG_SZ(ARG1)
+        fsd      f16, 49*ARG_SZ(ARG1)
+        fsd      f17, 50*ARG_SZ(ARG1)
+        fsd      f18, 51*ARG_SZ(ARG1)
+        fsd      f19, 52*ARG_SZ(ARG1)
+        fsd      f20, 53*ARG_SZ(ARG1)
+        fsd      f21, 54*ARG_SZ(ARG1)
+        fsd      f22, 55*ARG_SZ(ARG1)
+        fsd      f23, 56*ARG_SZ(ARG1)
+        fsd      f24, 57*ARG_SZ(ARG1)
+        fsd      f25, 58*ARG_SZ(ARG1)
+        fsd      f26, 59*ARG_SZ(ARG1)
+        fsd      f27, 60*ARG_SZ(ARG1)
+        fsd      f28, 61*ARG_SZ(ARG1)
+        fsd      f29, 62*ARG_SZ(ARG1)
+        fsd      f30, 63*ARG_SZ(ARG1)
+        fsd      f31, 64*ARG_SZ(ARG1)
+        frcsr    x3
+        sd       x3,  65*ARG_SZ(ARG1)
+        /* No need to save simd registers, at least for now. */
+        ret
+
         DECLARE_EXPORTED_FUNC(dr_app_start)
 GLOBAL_LABEL(dr_app_start:)
-/* FIXME i#3544: Not implemented */
+        /* Save link register for the case that DR is not taking over. */
+        addi     sp, sp, -16
+        sd       ra, 0(sp)
+
+        /* Preverse stack space. */
+        addi     sp, sp, -PRIV_MCONTEXT_SIZE
+        /* Push x3 on to stack to use it as a scratch. */
+        sd       x3, 3*ARG_SZ(sp)
+        /* Compute original sp. */
+        addi     x3, sp, PRIV_MCONTEXT_SIZE+16
+        /* Save original sp on to stack. */
+        sd       x3, 2*ARG_SZ(sp)
+        /* Save link register on to stack. */
+        sd       ra, 1*ARG_SZ(sp)
+        /* Save ra as pc */
+        sd       ra, 32*ARG_SZ(sp)
+        /* Save arg1 */
+        sd       ARG1, 10*ARG_SZ(sp)
+        CALLC1(save_priv_mcontext_helper, sp)
+        CALLC1(GLOBAL_REF(dr_app_start_helper), sp)
+        /* If we get here, DR is not taking over. */
+        addi     sp, sp, PRIV_MCONTEXT_SIZE
+        ld       ra, 0(sp)
+        addi     sp, sp, 16
         ret
         END_FUNC(dr_app_start)
 
         DECLARE_EXPORTED_FUNC(dr_app_take_over)
 GLOBAL_LABEL(dr_app_take_over:)
-/* FIXME i#3544: Not implemented */
         j        GLOBAL_REF(dynamorio_app_take_over)
         END_FUNC(dr_app_take_over)
 
         DECLARE_EXPORTED_FUNC(dr_app_running_under_dynamorio)
 GLOBAL_LABEL(dr_app_running_under_dynamorio:)
-/* FIXME i#3544: Not implemented */
+        li       ARG1, 0 /* This instruction is manged by mangle_pre_client. */
         ret
         END_FUNC(dr_app_running_under_dynamorio)
 
@@ -166,7 +270,30 @@ GLOBAL_LABEL(dr_app_running_under_dynamorio:)
 
         DECLARE_EXPORTED_FUNC(dynamorio_app_take_over)
 GLOBAL_LABEL(dynamorio_app_take_over:)
-/* FIXME i#3544: Not implemented */
+        /* Save link register for the case that DR is not taking over. */
+        addi     sp, sp, -16
+        sd       ra, 0(sp)
+
+        /* Preverse stack space. */
+        addi     sp, sp, -PRIV_MCONTEXT_SIZE
+        /* Push x3 on to stack to use it as a scratch. */
+        sd       x3, 3*ARG_SZ(sp)
+        /* Compute original sp. */
+        addi     x3, sp, PRIV_MCONTEXT_SIZE+16
+        /* Save original sp on to stack. */
+        sd       x3, 2*ARG_SZ(sp)
+        /* Save link register on to stack. */
+        sd       ra, 1*ARG_SZ(sp)
+        /* Save ra as pc */
+        sd       ra, 32*ARG_SZ(sp)
+        /* Save arg1 */
+        sd       ARG1, 10*ARG_SZ(sp)
+        CALLC1(save_priv_mcontext_helper, sp)
+        CALLC1(GLOBAL_REF(dynamorio_app_take_over_helper), sp)
+        /* If we get here, DR is not taking over. */
+        addi     sp, sp, PRIV_MCONTEXT_SIZE
+        ld       ra, 0(sp)
+        addi     sp, sp, 16
         ret
         END_FUNC(dynamorio_app_take_over)
 
