7def0bd5485d38d994ad5f4048071655397b1851
Nahome Bete <nbete@google.com>
2023-07-29 06:52:40 +0000

    Try to decrease number of windowed traces created for test app

--- clients/drcachesim/tests/window_test.cpp
+++ clients/drcachesim/tests/window_test.cpp
@@ -120,7 +120,7 @@ gather_trace()
 {
     std::string dr_ops(
         "-stderr_mask 0xc -client_lib ';;-offline -offline -trace_after_instrs 10000 "
-        "-trace_for_instrs 2000 -retrace_every_instrs 1000");
+        "-trace_for_instrs 1500 -retrace_every_instrs 2000");
     if (!my_setenv("DYNAMORIO_OPTIONS", dr_ops.c_str()))
         std::cerr << "failed to set env var!\n";
     dr_app_setup();
