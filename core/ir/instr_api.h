183023d6a03b4c9632a553a0aec580d18dd52c5d
Ukhanov Kirill <kirill.g.uhanov@gmail.com>
2023-08-10 15:17:56 +0300

    Fix formatting

--- core/ir/instr_api.h
+++ core/ir/instr_api.h
@@ -1879,14 +1879,14 @@ instr_is_rep_string_op(instr_t *instr);
  * Indicates which category the instruction corresponds to.
  */
 typedef enum {
-    DR_INSTR_CATEGORY_UNCATEGORIZED = 0x0,  /**< Uncategorised. */
-    DR_INSTR_CATEGORY_INT_MATH      = 0x1,  /**< Integer arithmetic operations */
-    DR_INSTR_CATEGORY_FP_MATH       = 0x2,  /**< Floating-Point arithmetic operations */
-    DR_INSTR_CATEGORY_LOAD          = 0x4,  /**< Loads */
-    DR_INSTR_CATEGORY_STORE         = 0x8,  /**< Stores */
-    DR_INSTR_CATEGORY_BRANCH        = 0x10, /**< Branches */
-    DR_INSTR_CATEGORY_SIMD          = 0x20, /**< Operations with vector registers (SIMD) */
-    DR_INSTR_CATEGORY_OTHER         = 0x40  /**< Other types of instructions */
+    DR_INSTR_CATEGORY_UNCATEGORIZED = 0x0, /**< Uncategorised. */
+    DR_INSTR_CATEGORY_INT_MATH = 0x1,      /**< Integer arithmetic operations */
+    DR_INSTR_CATEGORY_FP_MATH = 0x2,       /**< Floating-Point arithmetic operations */
+    DR_INSTR_CATEGORY_LOAD = 0x4,          /**< Loads */
+    DR_INSTR_CATEGORY_STORE = 0x8,         /**< Stores */
+    DR_INSTR_CATEGORY_BRANCH = 0x10,       /**< Branches */
+    DR_INSTR_CATEGORY_SIMD = 0x20,         /**< Operations with vector registers (SIMD) */
+    DR_INSTR_CATEGORY_OTHER = 0x40         /**< Other types of instructions */
 } dr_instr_category_type_t;
 
 /**
