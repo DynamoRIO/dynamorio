aa05a6d9df51e42a054657ccb86c5dde8f4a8884
Sahil <guptsah@google.com>
2023-08-17 20:24:00 +0000

    Change to unique_ptr.

--- clients/drcachesim/tools/invariant_checker.h
+++ clients/drcachesim/tools/invariant_checker.h
@@ -217,7 +217,7 @@ protected:
     check_for_pc_discontinuity(
         per_shard_t *shard, const memref_t &memref,
         const per_shard_t::instr_info_t &prev_instr_info, addr_t cur_pc,
-        const std::shared_ptr<instr_autoclean_t> &cur_instr_decoded, bool expect_encoding,
+        const std::unique_ptr<instr_autoclean_t> &cur_instr_decoded, bool expect_encoding,
         bool at_kernel_event);
 
     // The keys here are int for parallel, tid for serial.
