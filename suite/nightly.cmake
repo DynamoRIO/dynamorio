36c2f49cea551e8b3c92771711f3ed4d91e95a00
Yang Liu <liuyang22@iscas.ac.cn>
2023-09-02 05:50:52 +0000

    Bump CMake to 3.17

--- suite/nightly.cmake
+++ suite/nightly.cmake
@@ -29,7 +29,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 # DAMAGE.
 
-cmake_minimum_required(VERSION 3.7)
+cmake_minimum_required(VERSION 3.17)
 
 # Instructions to set up a machine to run nightly regression tests:
 #
