af42b1108e8b60e67d9d6b38185dee9b1727bba7
Derek Bruening <bruening@google.com>
2023-11-08 14:46:40 -0500

    Revert "DO NOT CHECK IN: tmate debugging of burst_traceopts on win64"
    This reverts commit 08b96200cdb9fd4d39a4c89e2aa9eafed92027f4.

--- suite/runsuite.cmake
+++ suite/runsuite.cmake
@@ -93,7 +93,6 @@ if (UNIX AND NOT APPLE AND NOT ANDROID)
     set(arg_debug_only ON)
   endif ()
 endif ()
-set(extra_ctest_args INCLUDE_LABEL TMATE_DEBUG)#NOCHECK
 
 set(build_tests "BUILD_TESTS:BOOL=ON")
 
