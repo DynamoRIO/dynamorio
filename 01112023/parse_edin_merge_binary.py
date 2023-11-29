5c2322be044a3f995841d9870770c237f3f50bed
Edin Jelacic <edin.jelacic@mdu.se>
2023-11-29 12:07:50 +0100

    Testing with large merged file

--- 01112023/parse_edin_merge_binary.py
+++ 01112023/parse_edin_merge_binary.py
@@ -25,20 +25,24 @@ def merge_binary_csvs(
             if df_indices[-1] != 100:  # add hundred just in case it has been forgotten
                 df_indices.insert(-1, len(df_temp))
 
-            for i in range(0, len(df_indices)-1, 2):
-                df_total.append(df_temp.loc[df_indices[i]:df_indices[i+1]])
+            for i in range(0, len(df_indices) - 1, 2):
+                sub_df = df_temp.loc[df_indices[i] : df_indices[i + 1]]
+                df_total = pd.concat([df_total, sub_df], ignore_index=True)
             
+            print(f"Finished with the CSV with cache {cache_size}k and matrix size {matrix_size}")
+
     # Save dataframe as CSV
-    df_total.to_csv(f"{path_to_csvs_folder}/merged_binary_all.csv")
-    
+    df_total.to_csv(f"{path_to_csvs_folder}/merged_binary_all.csv", index=False)
+
     # Return it if it is needed later
     return df_total
-    
 
 
 cache_sizes = [1, 2, 8, 128, 256, 512]
 matrix_sizes = [10, 20, 50, 80, 100]
 
-csv_folder_path = "./parsed-traces"
+csv_folder_path = "./parsed_traces"
 
-merge_binary_csvs(csv_folder_path, cache_sizes, matrix_sizes, [0,10, 30,40, 60,70, 90, 100])
+merge_binary_csvs(
+    csv_folder_path, cache_sizes, matrix_sizes, [0, 10, 30, 40, 60, 70, 90, 100]
+)
