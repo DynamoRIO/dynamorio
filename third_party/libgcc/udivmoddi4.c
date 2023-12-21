5bc728cb8121f96e3e8070d4402b9e0b975e4460
Doug Evans <dje@google.com>
2023-12-21 15:47:30 -0800

    Reformat udivmoddi4.c with clang-format

--- third_party/libgcc/udivmoddi4.c
+++ third_party/libgcc/udivmoddi4.c
@@ -29,94 +29,93 @@ typedef int DWtype;
 typedef unsigned int UWtype;
 typedef unsigned long long UDWtype;
 #if __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
-struct DWstruct {Wtype high, low;};
+struct DWstruct {
+    Wtype high, low;
+};
 #else
-struct DWstruct {Wtype low, high;};
+struct DWstruct {
+    Wtype low, high;
+};
 #endif
 typedef union {
-  struct DWstruct s;
-  DWtype ll;
+    struct DWstruct s;
+    DWtype ll;
 } DWunion;
 
 UDWtype
-__udivmoddi4 (UDWtype n, UDWtype d, UDWtype *rp)
+__udivmoddi4(UDWtype n, UDWtype d, UDWtype *rp)
 {
-  UDWtype q = 0, r = n, y = d;
-  UWtype lz1, lz2, i, k;
+    UDWtype q = 0, r = n, y = d;
+    UWtype lz1, lz2, i, k;
 
-  /* Implements align divisor shift dividend method. This algorithm
-     aligns the divisor under the dividend and then perform number of
-     test-subtract iterations which shift the dividend left. Number of
-     iterations is k + 1 where k is the number of bit positions the
-     divisor must be shifted left  to align it under the dividend.
-     quotient bits can be saved in the rightmost positions of the dividend
-     as it shifts left on each test-subtract iteration. */
+    /* Implements align divisor shift dividend method. This algorithm
+       aligns the divisor under the dividend and then perform number of
+       test-subtract iterations which shift the dividend left. Number of
+       iterations is k + 1 where k is the number of bit positions the
+       divisor must be shifted left  to align it under the dividend.
+       quotient bits can be saved in the rightmost positions of the dividend
+       as it shifts left on each test-subtract iteration. */
 
-  if (y <= r)
-    {
-      lz1 = __builtin_clzll (d);
-      lz2 = __builtin_clzll (n);
+    if (y <= r) {
+        lz1 = __builtin_clzll(d);
+        lz2 = __builtin_clzll(n);
 
-      k = lz1 - lz2;
-      y = (y << k);
+        k = lz1 - lz2;
+        y = (y << k);
 
-      /* Dividend can exceed 2 ^ (width − 1) − 1 but still be less than the
-         aligned divisor. Normal iteration can drops the high order bit
-         of the dividend. Therefore, first test-subtract iteration is a
-         special case, saving its quotient bit in a separate location and
-         not shifting the dividend. */
-      if (r >= y)
-        {
-          r = r - y;
-          q =  (1ULL << k);
+        /* Dividend can exceed 2 ^ (width − 1) − 1 but still be less than the
+           aligned divisor. Normal iteration can drops the high order bit
+           of the dividend. Therefore, first test-subtract iteration is a
+           special case, saving its quotient bit in a separate location and
+           not shifting the dividend. */
+        if (r >= y) {
+            r = r - y;
+            q = (1ULL << k);
         }
 
-      if (k > 0)
-        {
-          y = y >> 1;
+        if (k > 0) {
+            y = y >> 1;
 
-          /* k additional iterations where k regular test subtract shift
-            dividend iterations are done.  */
-          i = k;
-          do
-            {
-              if (r >= y)
-                r = ((r - y) << 1) + 1;
-              else
-                r =  (r << 1);
-              i = i - 1;
+            /* k additional iterations where k regular test subtract shift
+              dividend iterations are done.  */
+            i = k;
+            do {
+                if (r >= y)
+                    r = ((r - y) << 1) + 1;
+                else
+                    r = (r << 1);
+                i = i - 1;
             } while (i != 0);
 
-          /* First quotient bit is combined with the quotient bits resulting
-             from the k regular iterations.  */
-          q = q + r;
-          r = r >> k;
-          q = q - (r << k);
+            /* First quotient bit is combined with the quotient bits resulting
+               from the k regular iterations.  */
+            q = q + r;
+            r = r >> k;
+            q = q - (r << k);
         }
     }
 
-  if (rp)
-    *rp = r;
-  return q;
+    if (rp)
+        *rp = r;
+    return q;
 }
 
 DWtype
-__moddi3 (DWtype u, DWtype v)
+__moddi3(DWtype u, DWtype v)
 {
-  Wtype c = 0;
-  DWunion uu = {.ll = u};
-  DWunion vv = {.ll = v};
-  DWtype w;
+    Wtype c = 0;
+    DWunion uu = { .ll = u };
+    DWunion vv = { .ll = v };
+    DWtype w;
 
-  if (uu.s.high < 0)
-    c = ~c,
-    uu.ll = -uu.ll;
-  if (vv.s.high < 0)
-    vv.ll = -vv.ll;
+    if (uu.s.high < 0)
+        c = ~c, uu.ll = -uu.ll;
+    if (vv.s.high < 0)
+        vv.ll = -vv.ll;
 
-  (void) __udivmoddi4 (uu.ll, vv.ll, (UDWtype*)&w);
-  if (c)
-    w = -w;
+    (void)__udivmoddi4(uu.ll, vv.ll, (UDWtype *)&w);
+    if (c)
+        w = -w;
 
-  return w;
+    return w;
 }
