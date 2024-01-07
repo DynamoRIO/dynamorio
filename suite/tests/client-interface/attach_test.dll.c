1f0bf0f074c596ce1ef5cf5817ef495004c0e223
Wenlong Mu <muwl182@163.com>
2024-01-07 20:16:56 +0800

    print "done" in attach_test.dll.c

--- suite/tests/client-interface/attach_test.dll.c
+++ suite/tests/client-interface/attach_test.dll.c
@@ -46,16 +46,8 @@ dr_exit(void)
 {
     if (!saw_attach_event)
         dr_fprintf(STDERR, "Error: never saw attach event!\n");
-#ifdef WINDOWS
     dr_fprintf(STDERR, "done\n");
-#else
-        /* The app prints 'done' for us. */
-#endif
     // dr_fprintf(STDERR, "done\n");
-    //#ifdef WINDOWS
-    //#else
-    //        /* The app prints 'done' for us. */
-    //#endif
 }
 
 static void
