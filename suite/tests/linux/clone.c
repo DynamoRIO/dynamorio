bdc75a12e67e7cf33cdac703c1382f30da9c2f5e
Doug Evans <xdje42@gmail.com>
2024-02-01 16:18:55 -0800

    Remove unnecessary diff

--- suite/tests/linux/clone.c
+++ suite/tests/linux/clone.c
@@ -109,9 +109,8 @@ test_thread(bool share_sighand, bool clone_vm, bool use_clone3)
      * the output is the same.
      */
     pid_t (*create_thread_func)(void (*fcn)(void), void **stack, bool share_sighand,
-                                bool clone_vm) = create_thread;
-    if (use_clone3 && clone3_available)
-        create_thread_func = create_thread_clone3;
+                                bool clone_vm) =
+        (use_clone3 && clone3_available) ? create_thread_clone3 : create_thread;
 
     child = create_thread_func(run_with_exit, &stack, share_sighand, clone_vm);
 
