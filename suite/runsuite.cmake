18fe20d0d5f702464be3be7f4e8a1038a521b902
Doug Evans <xdje42@gmail.com>
2024-02-23 15:09:33 -0800

    i#6599: Mark tool.record_filter test as flaky on win32
    Mark the tool.record_filter test as flaky on win32
    
    Issue: #6599

--- suite/runsuite.cmake
+++ suite/runsuite.cmake
@@ -86,16 +86,11 @@ if (UNIX AND NOT APPLE AND NOT ANDROID AND NOT cross_riscv64_linux_only)
     RESULT_VARIABLE ldd_result ERROR_VARIABLE ldd_err OUTPUT_VARIABLE ldd_out)
   if (ldd_result OR ldd_err)
     # Failed; just move on.
-  elseif (ldd_out MATCHES "GLIBC 2.3[5-9]")
+  elseif (ldd_out MATCHES "xyzGLIBC 2.3[5-9]")
     # XXX i#5437, i#5431: While we work through Ubuntu22 issues we run
     # just a few tests.
     set(extra_ctest_args INCLUDE_LABEL UBUNTU_22)
     set(arg_debug_only ON)
-  elseif (arg_32_only AND NOT cross_aarchxx_linux_only AND NOT cross_android_only)
-    # TODO i#6417: The switch to AMD VM's for GA CI has broken many of our tests.
-    # This includes timeouts which increases suite length.
-    # Until we get ths x86-32 job back green, we drop back to a small set of tests.
-    set(extra_ctest_args INCLUDE_LABEL UBUNTU_22)
   endif ()
 endif ()
 
