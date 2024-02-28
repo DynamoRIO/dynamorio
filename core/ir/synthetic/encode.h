4b70a9d24f44bf2ae082a29da5c5e58f4d9ad92f
edeiana <edeiana@google.com>
2024-02-28 01:39:55 -0800

    Forgot to include encode header for synthetic ISA.

---
+++ core/ir/synthetic/encode.h
@@ -0,0 +1,9 @@
+#ifndef _SYNTHETIC_ENCODE_H_
+#define _SYNTHETIC_ENCODE_H_ 1
+
+#include "../globals.h"
+
+void
+encode_to_synth(dcontext_t *dcontext, instr_t *instr, byte *encoded_instr);
+
+#endif /* _SYNTHETIC_ENCODE_H_ */
