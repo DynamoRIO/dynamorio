f0c9a5d22f83c93f44dba26f180b417157b214dc
edeiana <edeiana@google.com>
2024-03-11 15:46:05 -0700

    ARM and future arches set instr_t isa_mode from global dcontext.

--- core/ir/instr_shared.c
+++ core/ir/instr_shared.c
@@ -91,6 +91,10 @@ instr_create(void *drcontext)
     memset((void *)instr, 0, sizeof(instr_t));
 #if defined(X86) && defined(X64)
     instr_set_isa_mode(instr, X64_CACHE_MODE_DC(dcontext) ? DR_ISA_AMD64 : DR_ISA_IA32);
+#elif defined(AARCH64)
+    instr_set_isa_mode(instr, DR_ISA_ARM_A64);
+#elif defined(RISCV64)
+    instr_set_isa_mode(instr, DR_ISA_RV64IMAFDC);
 #else
     instr_set_isa_mode(instr, dr_get_isa_mode(dcontext));
 #endif
