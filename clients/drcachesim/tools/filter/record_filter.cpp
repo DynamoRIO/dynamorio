a8c13938a622be4e54b4e13761b7cfa1dbbe39a5
Derek Bruening <bruening@google.com>
2024-03-27 10:26:17 -0400

    Fix 32-bit Windows warning

--- clients/drcachesim/tools/filter/record_filter.cpp
+++ clients/drcachesim/tools/filter/record_filter.cpp
@@ -213,7 +213,7 @@ record_filter_t::initialize_shard_output(per_shard_t *per_shard,
                    shard_stream->get_shard_index(), output_ext_.c_str(), version_,
                    filetype_);
             per_shard->output_path += output_ext_;
-            per_shard->filetype = filetype_;
+            per_shard->filetype = static_cast<addr_t>(filetype_);
             lock.unlock();
         } else if (!input_name.empty()) {
             size_t last_dot = input_name.rfind('.');
@@ -233,7 +233,7 @@ record_filter_t::initialize_shard_output(per_shard_t *per_shard,
                    shard_stream->get_shard_index(), output_ext_.c_str(), version_,
                    filetype_);
             per_shard->output_path += output_ext_;
-            per_shard->filetype = filetype_;
+            per_shard->filetype = static_cast<addr_t>(filetype_);
             lock.unlock();
             input_info_cond_var_.notify_all();
         } else {
@@ -244,7 +244,7 @@ record_filter_t::initialize_shard_output(per_shard_t *per_shard,
                    shard_stream->get_shard_index(), output_ext_.c_str(), version_,
                    filetype_);
             per_shard->output_path += output_ext_;
-            per_shard->filetype = filetype_;
+            per_shard->filetype = static_cast<addr_t>(filetype_);
             lock.unlock();
         }
     } else {
