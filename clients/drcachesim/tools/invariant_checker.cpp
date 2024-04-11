f4c31f91bfe0c3296d4c83e73f45787804addc38
Abhinav Anil Sharma <sharmaabhinav@google.com>
2024-04-10 21:07:50 -0400

    Reviewer suggested edits.

--- clients/drcachesim/tools/invariant_checker.cpp
+++ clients/drcachesim/tools/invariant_checker.cpp
@@ -199,6 +199,59 @@ invariant_checker_t::is_a_unit_test(per_shard_t *shard)
 }
 
 bool
+invariant_checker_t::relax_expected_write_count_check_for_kernel(per_shard_t *shard)
+{
+    if (!shard->between_kernel_syscall_trace_markers_ &&
+        !shard->between_kernel_context_switch_markers_)
+        return false;
+    bool relax =
+        // Xsave does multiple writes. Write count cannot be
+        // determined using the decoder. System call trace
+        // templates collected on QEMU would show all writes.
+        // XXX: Perhaps we can query the expected size using
+        // cpuid and add it as a marker in the system call
+        // trace template, and then adjust it according to the
+        // cpuid value for the trace being injected into.
+        shard->prev_instr_.decoding.is_xsave;
+    return relax;
+}
+
+bool
+invariant_checker_t::relax_expected_read_count_check_for_kernel(per_shard_t *shard)
+{
+    if (!shard->between_kernel_syscall_trace_markers_ &&
+        !shard->between_kernel_context_switch_markers_)
+        return false;
+    bool relax =
+        // iret pops bunch of data from the stack at interrupt
+        // returns. System call trace templates collected on
+        // QEMU would show all reads.
+        // TODO i#6742: iret has different behavior in user vs
+        // protected mode. This difference in iret behavior can
+        // be detected in the decoder and perhaps be accounted
+        // for here in a way better than relying on
+        // between_kernel_syscall_trace_markers_.
+        shard->prev_instr_.decoding.opcode == OP_iret
+
+        // Xrstor does multiple reads. Read count cannot be
+        // determined using the decoder. System call trace
+        // templates collected on QEMU would show all reads.
+        // XXX: Perhaps we can query the expected size using
+        // cpuid and add it as a marker in the system call
+        // trace template, and then adjust it according to the
+        // cpuid value for the trace being injected into.
+        || shard->prev_instr_.decoding.is_xrstor
+
+        // xsave variants also read some fields from the xsave
+        // header (https://www.felixcloutier.com/x86/xsaveopt).
+        // XXX, i#6769: Same as above, can we store any metadata
+        // in the trace to allow us to adapt the decoder to expect
+        // this?
+        || shard->prev_instr_.decoding.is_xsave;
+    return relax;
+}
+
+bool
 invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &memref)
 {
     per_shard_t *shard = reinterpret_cast<per_shard_t *>(shard_data);
@@ -1100,33 +1153,7 @@ invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &mem
                 report_if_false(
                     shard,
                     shard->expected_read_records_ >
-                        0 IF_X86(
-                            ||
-                            (shard->between_kernel_syscall_trace_markers_ &&
-                             (
-                                 // iret pops bunch of data from the stack at interrupt
-                                 // returns. System call trace templates collected on
-                                 // QEMU would show all reads.
-                                 // TODO i#6742: iret has different behavior in user vs
-                                 // protected mode. This difference in iret behavior can
-                                 // be detected in the decoder and perhaps be accounted
-                                 // for here in a way better than relying on
-                                 // between_kernel_syscall_trace_markers_.
-                                 shard->prev_instr_.decoding.opcode == OP_iret
-                                 // Xrstor does multiple reads. Read count cannot be
-                                 // determined using the decoder. System call trace
-                                 // templates collected on QEMU would show all reads.
-                                 // XXX: Perhaps we can query the expected size using
-                                 // cpuid and add it as a marker in the system call
-                                 // trace template, and then adjust it according to the
-                                 // cpuid value for the trace being injected into.
-                                 || shard->prev_instr_.decoding.is_xrstor
-                                 // xsave variants also reads some fields from the xsave
-                                 // header (https://www.felixcloutier.com/x86/xsaveopt).
-                                 // XXX: Same as above, can we store any metadata in the
-                                 // trace to allow us to adapt the decoder to expect
-                                 // this?
-                                 || shard->prev_instr_.decoding.is_xsave))),
+                        0 IF_X86(|| relax_expected_read_count_check_for_kernel(shard)),
                     "Too many read records");
                 if (shard->expected_read_records_ > 0) {
                     shard->expected_read_records_--;
@@ -1136,16 +1163,7 @@ invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &mem
                 report_if_false(
                     shard,
                     shard->expected_write_records_ >
-                        0 IF_X86(
-                            // Xsave does multiple writes. Write count cannot be
-                            // determined using the decoder. System call trace
-                            // templates collected on QEMU would show all writes.
-                            // XXX: Perhaps we can query the expected size using
-                            // cpuid and add it as a marker in the system call
-                            // trace template, and then adjust it according to the
-                            // cpuid value for the trace being injected into.
-                            || (shard->between_kernel_syscall_trace_markers_ &&
-                                shard->prev_instr_.decoding.is_xsave)),
+                        0 IF_X86(|| relax_expected_write_count_check_for_kernel(shard)),
                     "Too many write records");
                 if (shard->expected_write_records_ > 0) {
                     shard->expected_write_records_--;
