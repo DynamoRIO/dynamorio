0ca1bf61ac9afa7e9911073f74507ebb9533301e
edeiana <edeiana@google.com>
2024-05-07 15:40:34 -0700

    Disabling clang-format for expected output. Spacing is important there, cannot be changed.

--- clients/drcachesim/tests/view_test.cpp
+++ clients/drcachesim/tests/view_test.cpp
@@ -612,6 +612,7 @@ run_regdeps_test(void *drcontext)
         { TRACE_TYPE_INSTR, 3, { PC } },
         { TRACE_TYPE_FOOTER, 0, { 0 } },
     } };
+    /* clang-format off */
     const char *expect = R"DELIM(           1           0:           3 <marker: version 3>
            2           0:           3 <marker: filetype 0x20e00>
            3           0:           3 <marker: cache line size 64>
@@ -621,6 +622,7 @@ run_regdeps_test(void *drcontext)
            7           1:           3 ifetch       3 byte(s) @ 0x00007f6fdd3ec360 11 00 01 00 06 09 06 move   %rv4[8byte] -> %rv7[8byte]
            7           1:           3                                             00
 )DELIM";
+    /* clang-format on */
     instrlist_t *ilist_unused = nullptr;
     view_nomod_test_t view(drcontext, *ilist_unused, 0, 0);
     std::string res = run_serial_test_helper(view, entries, tids);
