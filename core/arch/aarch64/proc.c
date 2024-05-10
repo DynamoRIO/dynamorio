85d00aef0100a29e7e53ef33d097f745d64be761
Joshua Warburton <joshua.warburton@arm.com>
2024-05-10 12:04:34 +0100

    disable vector length detection for testing
    Change-Id: I30c15b8a834c122e623153be34684bd69f330269

--- core/arch/aarch64/proc.c
+++ core/arch/aarch64/proc.c
@@ -113,7 +113,7 @@ get_processor_specific_info(void)
      * - Release or development test build.
      */
 #        if !defined(DR_HOST_NOT_TARGET)
-    if (proc_has_feature(FEATURE_SVE)) {
+    if (proc_has_feature(FEATURE_SVE) && false) {
         uint64 vl;
         /* This RDVL instruction is inserted as raw hex because we don't build
          * with SVE enabled: i.e. not -march=armv8-a+sve, so that we can run a
