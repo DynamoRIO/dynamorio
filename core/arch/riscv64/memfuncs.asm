2a8b8b5f502b7fff579db55a752e6a1aa12ea346
chenhy0106 <1002138131@qq.com>
2024-05-10 03:33:51 -0400

    i#3544: Add more commnets to private memcpy and memset.

--- core/arch/riscv64/memfuncs.asm
+++ core/arch/riscv64/memfuncs.asm
@@ -40,14 +40,21 @@
 START_FILE
 #ifdef UNIX
 
+
 /* Private memcpy.
+ * Optimize private memcpy by using loop unrolling.
  */
         DECLARE_FUNC(memcpy)
 GLOBAL_LABEL(memcpy:)
-        addi     a5, x0, 32
-        mv       t0, ARG2 /* save for return */
-copy32_:
-        blt      ARG3, a5, copy0_32
+        addi     t6, x0, 32
+        mv       t0, ARG2 /* Save dst for return */
+copy32_: 
+        /* When size is greater than 32, we use 4 ld/sd pairs 
+         * to copy 4*8=32 bytes in each iteration. 
+         * When size is less than 32, we jump to copy_remain and 
+         * use other optimized copy methods.
+         */
+        blt      ARG3, t6, copy_remain
         ld       t1, 0(ARG2)
         ld       t2, 8(ARG2)
         ld       t3, 16(ARG2)
@@ -60,16 +67,25 @@ copy32_:
         addi     ARG1, ARG1, 32
         addi     ARG2, ARG2, 32
         j        copy32_
-copy0_32:
-        add      a6, ARG2, ARG3 /* a6 = src_end */
-        add      a7, ARG1, ARG3 /* a7 = dst_end */
-        addi     a5, x0, 8
-        bge      ARG3, a5, copy8_32
-        addi     a5, x0, 4
-        bge      ARG3, a5, copy4_8
+
+copy_remain:
+        add      a6, ARG2, ARG3 /* a6 = src + size */
+        add      a7, ARG1, ARG3 /* a7 = dst + size */
+        addi     t6, x0, 8
+        bge      ARG3, t6, copy8_32
+        addi     t6, x0, 4
+        bge      ARG3, t6, copy4_8
         bgtz     ARG3, copy0_4
         j        copyexit
+
 copy0_4:
+        /* 0 < size < 4:
+         * Using a trick to avoid branches.
+         * t2 and t3 may be redundant.
+         * size = 1: t1 = src[0], t2 = src[0], t3 = src[0]
+         * size = 2: t1 = src[0], t2 = src[1], t3 = src[1]
+         * size = 3: t1 = src[0], t2 = src[1], t3 = src[2]
+         */
         srli     t4, ARG3, 1
         add      t5, t4, ARG1
         add      t4, t4, ARG2
@@ -80,35 +96,57 @@ copy0_4:
         sb       t2, -1(a7)
         sb       t3, 0(t5)
         j        copyexit
+
 copy4_8:
+        /* 4 < size < 8:
+         * Step1: Use lwu/sw pair to copy src[0]~src[3] to dst[0]~dst[3].
+         * Step2: Copy src[size-4]~src[size-1] to dst[size-4]~dst[size-1].
+         * Some overlap here, but the overhead is negligible.
+         */
         lwu      t1, 0(ARG2)
         lwu      t2, -4(a6)
         sw       t1, 0(ARG1)
         sw       t2, -4(a7)
         j        copyexit
+        
 copy8_32:
+        /* 8 < size < 32: */
+
+        /* Copy src[0]~src[7] and src[size-8]~size[size-1].
+         * Some overlap here, but the overhead is negligible.
+         */
         ld       t1, 0(ARG2)
         ld       t2, -8(a6)
         sd       t1, 0(ARG1)
         sd       t2, -8(a7)
-        addi     a5, x0, 16
-        ble      ARG3, a5, copyexit
+
+        /* If size > 16, src[8]~src[size-9] has not been copied. 
+         * Copy src[8]~src[15] with ld/sd pair.
+         */
+        addi     t6, x0, 16
+        ble      ARG3, t6, copyexit
         ld       t1, 8(ARG2)
         sd       t1, 8(ARG1)
-        addi     a5, x0, 24
-        ble      ARG3, a5, copyexit
+
+        /* If size > 24, src[16]~src[size-9] has not been copied. 
+         * Copy src[16]~src[23] with ld/sd pair.
+         */
+        addi     t6, x0, 24
+        ble      ARG3, t6, copyexit
         ld       t1, 16(ARG2)
         sd       t1, 16(ARG1)
 copyexit:
-        mv       a0, t0
+        mv       a0, t0 /* Restore original dst as return value */
         ret
         END_FUNC(memcpy)
 
 /* Private memset.
+ * Optimize private memset by using loop unrolling.
+ * Optimization method is same as memcpy.
  */
         DECLARE_FUNC(memset)
 GLOBAL_LABEL(memset:)
-        addi     a5, x0, 32
+        addi     t6, x0, 32
         mv       t0, ARG1 /* save for return */
 
         /* duplicate byte into whole register */
@@ -129,7 +167,7 @@ GLOBAL_LABEL(memset:)
         slli     ARG2, ARG2, 8
         or       t1, t1, ARG2
 set32_:
-        blt      ARG3, a5, set0_32
+        blt      ARG3, t6, set0_32
         sd       t1, 0(ARG1)
         sd       t1, 8(ARG1)
         sd       t1, 16(ARG1)
@@ -139,10 +177,10 @@ set32_:
         j        set32_
 set0_32:
         add      a6, ARG1, ARG3
-        addi     a5, x0, 8
-        bge      ARG3, a5, set8_32
-        addi     a5, x0, 4
-        bge      ARG3, a5, set4_8
+        addi     t6, x0, 8
+        bge      ARG3, t6, set8_32
+        addi     t6, x0, 4
+        bge      ARG3, t6, set4_8
         bgtz     ARG3, set0_4
         j        setexit
 set0_4:
@@ -159,14 +197,14 @@ set4_8:
 set8_32:
         sd       t1, 0(ARG1)
         sd       t1, -8(a6)
-        addi     a5, x0, 16
-        ble      ARG3, a5, setexit
+        addi     t6, x0, 16
+        ble      ARG3, t6, setexit
         sd       t1, 8(ARG1)
-        addi     a5, x0, 24
-        ble      ARG3, a5, setexit
+        addi     t6, x0, 24
+        ble      ARG3, t6, setexit
         sd       t1, 16(ARG1)
 setexit:
-        mv       a0, t0
+        mv       a0, t0 /* Restore original dst as return value */
         ret
         END_FUNC(memset)
 
