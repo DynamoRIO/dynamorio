753124e3724c1e122a5321d425233291e05c545f
Phil Ramsey <phil.ramsey@arm.com>
2024-05-13 11:47:37 +0000

    i#5623: AARCH64 feature FEAT_PAUTH2 is now recognized
    Enhance the AARCH64 feature interface so that we can correctly
    identify features identifed by more than one register nibble.
    
    Update the identification of FEATURE_PAUTH so that all six
    identification nibbles are checked.
    
    Remove the automatic assumption that if the value of the nibble in
    the register is greater than the expected value then the feature is
    implemented.
    
    Issue: #5623

--- suite/tests/client-interface/features.dll.c
+++ suite/tests/client-interface/features.dll.c
@@ -46,7 +46,9 @@
 /* These features are supported by almost all base v8.0 h/w, i.e. at least one
  * of them will appear in /proc/cpuinfo's 'Features' string.
  */
-const char *test_features[] = { "aes", "pmull", "sha1", "sha2", "crc32", "" };
+const char *test_features[] = { "aes",    "pmull",   "sha1", "sha2",  "crc32", "sve",
+                                "sha512", "atomics", "bf16", "jscvt", "lrcpc", "sm3",
+                                "sm4",    "i8mm",    "rng",  "fphp",  "" };
 
 int
 read_hw_features(char *feat_str)
@@ -71,11 +73,35 @@ read_hw_features(char *feat_str)
     return 1;
 }
 
+/*
+ * FEATURE_PAUTH is identified by six different nibbles. We initially check the API nibble
+ * but on Neoverse V1 hardware this is 0. Running this on a Neoverse V1 machine verifies
+ * that the other nibbles are being checked correctly.
+ */
+void
+check_for_pauth()
+{
+    uint64 id_aa64isar1_el1 = 0;
+
+    asm("mrs %0, ID_AA64ISAR1_EL1" : "=r"(id_aa64isar1_el1));
+
+    ushort gpi = (id_aa64isar1_el1 >> 28) & 0xF;
+    ushort gpa = (id_aa64isar1_el1 >> 24) & 0xF;
+    ushort api = (id_aa64isar1_el1 >> 8) & 0xF;
+    ushort apa = (id_aa64isar1_el1 >> 4) & 0xF;
+
+    if (apa >= 1 || api >= 1 || gpa == 1 || gpa == 1) {
+        ASSERT(proc_has_feature(FEATURE_PAUTH));
+    }
+}
+
 DR_EXPORT void
 dr_init(client_id_t client_id)
 {
     char feat_str[MAX_FEATURES_LEN];
     char *feat;
+
+    check_for_pauth();
 
     if (read_hw_features(feat_str) == -1)
         dr_fprintf(STDERR, "Error retrieving 'Features' string from /proc/cpuinfo\n");
@@ -94,6 +120,28 @@ dr_init(client_id_t client_id)
                 ASSERT(proc_has_feature(FEATURE_SHA256));
             if (strcmp(feat, "crc32") == 0)
                 ASSERT(proc_has_feature(FEATURE_CRC32));
+            if (strcmp(feat, "sve") == 0)
+                ASSERT(proc_has_feature(FEATURE_SVE));
+            if (strcmp(feat, "sha512") == 0)
+                ASSERT(proc_has_feature(FEATURE_SHA512));
+            if (strcmp(feat, "atomics") == 0)
+                ASSERT(proc_has_feature(FEATURE_LSE));
+            if (strcmp(feat, "bf16") == 0)
+                ASSERT(proc_has_feature(FEATURE_BF16));
+            if (strcmp(feat, "jscvt") == 0)
+                ASSERT(proc_has_feature(FEATURE_JSCVT));
+            if (strcmp(feat, "jscvt") == 0)
+                ASSERT(proc_has_feature(FEATURE_LRCPC));
+            if (strcmp(feat, "sm3") == 0)
+                ASSERT(proc_has_feature(FEATURE_SM3));
+            if (strcmp(feat, "sm4") == 0)
+                ASSERT(proc_has_feature(FEATURE_SM4));
+            if (strcmp(feat, "i8mm") == 0)
+                ASSERT(proc_has_feature(FEATURE_I8MM));
+            if (strcmp(feat, "rng") == 0)
+                ASSERT(proc_has_feature(FEATURE_RNG));
+            if (strcmp(feat, "fphp") == 0)
+                ASSERT(proc_has_feature(FEATURE_FP16));
         }
         feat = strtok(NULL, " ");
     }
