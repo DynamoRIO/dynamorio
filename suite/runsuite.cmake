20f652d78ccf2e722a31477548451a564cc84d24
Abhinav Anil Sharma <sharmaabhinav@google.com>
2024-05-22 12:04:16 -0400

    Revert "Run full test suite"
    This reverts commit 0fdb026940720e48be4915974bd1db62df84fed4.

--- suite/runsuite.cmake
+++ suite/runsuite.cmake
@@ -101,7 +101,7 @@ if (UNIX AND NOT APPLE AND NOT ANDROID AND NOT cross_riscv64_linux_only)
     set(extra_ctest_args EXCLUDE_LABEL AMD_X32_DENYLIST)
   endif ()
 endif ()
-
+set(extra_ctest_args INCLUDE_LABEL TMATE_DEBUG)
 set(build_tests "BUILD_TESTS:BOOL=ON")
 
 if (arg_automated_ci)
