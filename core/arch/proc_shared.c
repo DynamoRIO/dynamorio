3fb94852fa7b411c6e9083053b42c915ea0c8a05
Yang Liu <liuyang22@iscas.ac.cn>
2024-06-03 20:25:28 +0800

    fix compilation

--- core/arch/proc_shared.c
+++ core/arch/proc_shared.c
@@ -85,7 +85,11 @@ cpu_info_t cpu_info = {
     CACHE_SIZE_UNKNOWN,
     CACHE_SIZE_UNKNOWN,
     CACHE_SIZE_UNKNOWN,
+#if defined(AARCHXX) || defined(RISCV64)
     {},
+#else
+    { 0 },
+#endif
     { 0x6e6b6e75, 0x006e776f }
 };
 
