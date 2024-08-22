9a1d8839fd9d1ca4e040875b2ff884d006c9ba4e
Abhinav Anil Sharma <sharmaabhinav@google.com>
2024-08-22 15:11:39 -0400

    Justify invariant error change.

--- clients/drcachesim/tools/invariant_checker.cpp
+++ clients/drcachesim/tools/invariant_checker.cpp
@@ -574,6 +574,11 @@ invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &mem
                         "Nested kernel syscall traces are not expected");
         if (!TESTANY(OFFLINE_FILE_TYPE_KERNEL_SYSCALL_TRACE_TEMPLATES,
                      shard->file_type_)) {
+            // XXX: PT kernel syscall traces are inserted at the
+            // TRACE_MARKER_TYPE_SYSCALL_IDX marker. The marker is deliberately added
+            // to the trace in the post-syscall callback to ensure it is emitted
+            // together with the actual PT trace and not before. This may cause it to
+            // separate from the TRACE_MARKER_TYPE_SYSCALL marker.
             report_if_false(shard,
                             prev_was_syscall_marker_saved ||
                                 TESTANY(OFFLINE_FILE_TYPE_KERNEL_SYSCALL_INSTR_ONLY,
