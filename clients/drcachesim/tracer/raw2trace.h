DynamoRIO/dynamorio

commit 77b7b2b6e434623566a7200dcdd732c86be9c102

i#7050: Remove preempted and faulting instruction from the drmemtraces. (#7058)
Change the implementation to remove preempted and faulting instructions
from drmemtraces. The objective of the change is to remove non-retired
instructions and the corresponding memrefs from drmemtraces.

When an instruction is removed, a new marker
TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION will be added. Its value is
the encoding of the removed instruction up to the size of a pointer.

Renamed handle_kernel_interrupt_and_markers() to
handle_rseq_abort_marker() since the function has been modified to
handle rseq abort.

In oder to remove preempted instructions and memrefs, a new function
preempted_by_kernel_event() is added to look for KERNEL EVENT marker
which may be preceded by memrefs. If a KERNEL EVENT marker is found with
the same PC, the instruction and any following memrefs are removed.

Add unit tests to cover instruction and memref removed caused by a
KERNEL EVENT.

Update offline-legacy-int-offs.templatex,
offline-burst_aarch64_sys.templatex and signal_invariants.c to account
for removed instructions.

Fixes #7050

clients/drcachesim/tracer/raw2trace.h
@@ -1488,19 +1488,18 @@ private:
     analyze_elidable_addresses(raw2trace_thread_data_t *tdata, uint64 modidx,
                                uint64 modoffs, app_pc start_pc, uint instr_count);
 
+    // Returns true if a kernel interrupt happened at cur_pc. The check skips
+    // memrefs to search for a kernel event marker. If the kernel event marker has
+    // the value of cur_pc, it returns true.
     bool
-    process_memref(raw2trace_thread_data_t *tdata, trace_entry_t **buf_in,
-                   const instr_summary_t *instr, instr_summary_t::memref_summary_t memref,
-                   bool write, std::unordered_map<reg_id_t, addr_t> &reg_vals,
-                   uint64_t cur_pc, uint64_t cur_offs, bool instrs_are_separate,
-                   DR_PARAM_OUT bool *reached_end_of_memrefs,
-                   DR_PARAM_OUT bool *interrupted);
+    interrupted_by_kernel_event(raw2trace_thread_data_t *tdata, uint64_t cur_pc,
+                                uint64_t cur_offs);
 
     bool
     append_bb_entries(raw2trace_thread_data_t *tdata, const offline_entry_t *in_entry,
                       DR_PARAM_OUT bool *handled);
 
-    // Returns true if a kernel interrupt happened at cur_pc.
+    // Returns true if an rseq abort happened at cur_pc.
     // Outputs a kernel interrupt if this is the right location.
     // Outputs any other markers observed if !instrs_are_separate, since they
     // are part of this block and need to be inserted now. Inserts all
@@ -1509,11 +1508,9 @@ private:
     // handled at a higher level (process_offline_entry()) and are never
     // inserted here.
     bool
-    handle_kernel_interrupt_and_markers(raw2trace_thread_data_t *tdata,
-                                        DR_PARAM_INOUT trace_entry_t **buf_in,
-                                        uint64_t cur_pc, uint64_t cur_offs,
-                                        int instr_length, bool instrs_are_separate,
-                                        DR_PARAM_OUT bool *interrupted);
+    handle_rseq_abort_marker(raw2trace_thread_data_t *tdata,
+                             DR_PARAM_INOUT trace_entry_t **buf_in, uint64_t cur_pc,
+                             uint64_t cur_offs, DR_PARAM_OUT bool *interrupted);
 
     bool
     get_marker_value(raw2trace_thread_data_t *tdata,

api/docs/release.dox
@@ -126,11 +126,18 @@ clients.
 
 The changes between version \DR_VERSION and 11.0.0 include the following compatibility
 changes:
- - Added X64 Linux support to dr_create_memory_dump(). This API has the same
-   restriction as dr_suspend_all_other_threads_ex().
+ - Removed uncompleted instructions and the corresponding memrefs from drmemtraces.
+   Instructions which are fetched but not completed due to asynchronous signal or
+   fault are removed. A new marker
+   #dynamorio::drmemtrace::TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION was added
+   to indicate an uncompleted instruction was removed. The value of the marker
+   is the encoding of the removed instruction up to a pointer's length. Added
+   #OFFLINE_FILE_VERSION_RETIRED_INSTRUCTIONS_ONLY to increase the trace version
+   for drmemtraces with uncompleted instructions removed.
 
 Further non-compatibility-affecting changes include:
- - No changes yet.
+ - Added X64 Linux support to dr_create_memory_dump(). This API has the same
+   restriction as dr_suspend_all_other_threads_ex().
 
 **************************************************
 <hr>

clients/drcachesim/common/trace_entry.h
@@ -692,6 +692,21 @@ typedef enum {
      */
     TRACE_MARKER_TYPE_SIGNAL_NUMBER,
 
+    /**
+     * This marker is used to indicate an instruction started to execute but
+     * didn't retire. The instruction was either preempted by an asynchronous
+     * signal or caused a fault. The instruction and corresponding memrefs
+     * are removed from the trace.
+     *
+     * The marker value is the raw encoding bytes of the instruction up to the
+     * length of a pointer. The encoding will be incomplete for instructions
+     * with long encodings. It is best-effort to help understand the sequence of
+     * generated code where encodings are not available offline. The PC of this
+     * instruction is available in a subsequent
+     * #dynamorio::drmemtrace::TRACE_MARKER_TYPE_KERNEL_EVENT marker.
+     */
+    TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION,
+
     // ...
     // These values are reserved for future built-in marker types.
     // ...
@@ -946,8 +961,11 @@ typedef enum {
 #define OFFLINE_FILE_VERSION_KERNEL_INT_PC 4
 #define OFFLINE_FILE_VERSION_HEADER_FIELDS_SWAP 5
 #define OFFLINE_FILE_VERSION_ENCODINGS 6
-#define OFFLINE_FILE_VERSION_XFER_ABS_PC 7
-#define OFFLINE_FILE_VERSION OFFLINE_FILE_VERSION_XFER_ABS_PC
+#define OFFLINE_FILE_VERSION_XFER_ABS_PC \
+    7 /**< Use the absolute PC for kernel interruption PC for 64-bit mode.*/
+#define OFFLINE_FILE_VERSION_RETIRED_INSTRUCTIONS_ONLY \
+    8 /**< Trace version which has only retired instructions in drmemtraces.*/
+#define OFFLINE_FILE_VERSION OFFLINE_FILE_VERSION_RETIRED_INSTRUCTIONS_ONLY
 
 /**
  * Bitfields used to describe the high-level characteristics of both an

clients/drcachesim/docs/drcachesim.dox.in
@@ -193,6 +193,8 @@ Some of the more important markers are:
 
 - #dynamorio::drmemtrace::TRACE_MARKER_TYPE_CORE_WAIT - This is inserted by the trace scheduler (see \ref sec_drcachesim_sched) during replay of a previously recorded schedule when one core gets too far ahead of another according to the recorded timestamps.  This is an artificial wait to keep the replay on track, as opposed to the natural idle time of #dynamorio::drmemtrace::TRACE_MARKER_TYPE_CORE_IDLE.
 
+- #dynamorio::drmemtrace::TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION - This is inserted when an uncompleted instruction and its corresponding memory records are removed. The marker value contains the encoding of the removed instruction up to the length of a pointer. The encoding will be incomplete for instructions with long encodings. It is best-effort to help understand the sequence for generated code where encodings are not available offline. The PC of this instruction is available in a subsequent #dynamorio::drmemtrace::TRACE_MARKER_TYPE_KERNEL_EVENT marker.
+
 The full set of markers is listed under the enum #dynamorio::drmemtrace::trace_marker_type_t.
 
 ****************************************************************************
@@ -790,6 +792,46 @@ untaken conditional branch:
       801375      601842:     1159769 <marker: timestamp 13335923552684029>
       801376      601842:     1159769 <marker: tid 1159769 on core 7>
       801377      601843:     1159769 ifetch       4 byte(s) @ 0x00007fc2c3aa5c72 48 83 c4 48          add    $0x48, %rsp
+\endcode
+
+Here is an illustration of what a trace would look like when an uncompleted
+instruction (mov) is interrupted by an asynchronous signal and the instruction
+is not removed (The following trace is for demonstration purpose only):
+
+\code
+
+    46914793    33950158:     3767811 ifetch       5 byte(s) @ 0x000055f52911b75e c4 42 f8 f5 e8       bzhi   %r8, %rax, %r13
+    46914794    33950159:     3767811 ifetch       5 byte(s) @ 0x000055f52911b763 c4 62 f8 f5 c3       bzhi   %rbx, %rax, %r8
+    46914795    33950160:     3767811 ifetch       4 byte(s) @ 0x000055f52911b768 48 c1 eb 33          shr    $0x33, %rbx
+    46914796    33950161:     3767811 ifetch       3 byte(s) @ 0x000055f52911b76c 48 01 f3             add    %rsi, %rbx
+    46914797    33950162:     3767811 ifetch       5 byte(s) @ 0x000055f52911b76f b8 04 00 00 00       mov    $0x00000004, %eax
+    (The line below is for demonstration purpose only and will not appear in a real drmemtrace.)
+    46914798    33950163:     3767811 ifetch       6 byte(s) @ 0x000055f52911b774 89 85 68 ff ff ff    mov    %eax, -0x98(%rbp)
+    46914799    33950163:     3767811 <marker: kernel xfer from 0x55f52911b774 to handler>
+    46914800    33950163:     3767811 <marker: signal #27>
+    46914801    33950163:     3767811 <marker: timestamp 13373506292215933>
+    46914802    33950163:     3767811 <marker: tid 3767811 on core 4139>
+    46914803    33950164:     3767811 ifetch       1 byte(s) @ 0x000055f5298f7500 55                   push   %rbp
+    46914804    33950164:     3767811 write        8 byte(s) @ 0x00007fd98bd2f210 by PC 0x000055f5298f7500
+\endcode
+
+When the uncompleted mov instruction is removed, a
+#dynamorio::drmemtrace::TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION marker is placed to indicate an
+instruction has been removed:
+
+\code
+    46914793    33950158:     3767811 ifetch       5 byte(s) @ 0x000055f52911b75e c4 42 f8 f5 e8       bzhi   %r8, %rax, %r13
+    46914794    33950159:     3767811 ifetch       5 byte(s) @ 0x000055f52911b763 c4 62 f8 f5 c3       bzhi   %rbx, %rax, %r8
+    46914795    33950160:     3767811 ifetch       4 byte(s) @ 0x000055f52911b768 48 c1 eb 33          shr    $0x33, %rbx
+    46914796    33950161:     3767811 ifetch       3 byte(s) @ 0x000055f52911b76c 48 01 f3             add    %rsi, %rbx
+    46914797    33950162:     3767811 ifetch       5 byte(s) @ 0x000055f52911b76f b8 04 00 00 00       mov    $0x00000004, %eax
+    46914798    33950163:     3767811 <marker: uncompleted instruction, encoding 0x898568ff>
+    46914799    33950163:     3767811 <marker: kernel xfer from 0x55f52911b774 to handler>
+    46914800    33950163:     3767811 <marker: signal #27>
+    46914801    33950163:     3767811 <marker: timestamp 13373506292215933>
+    46914802    33950163:     3767811 <marker: tid 3767811 on core 4139>
+    46914803    33950164:     3767811 ifetch       1 byte(s) @ 0x000055f5298f7500 55                   push   %rbp
+    46914804    33950164:     3767811 write        8 byte(s) @ 0x00007fd98bd2f210 by PC 0x000055f5298f7500
 \endcode
 
 \section sec_tool_func_view View Function Calls

clients/drcachesim/tests/offline-burst_aarch64_sys.templatex
@@ -13,7 +13,7 @@ Total counts:
      .* total data loads
      .* total data stores
            1 total icache flushes
-           4 total dcache flushes
+           3 total dcache flushes
            1 total threads
      .* total timestamp \+ cpuid markers
 .*
@@ -25,6 +25,6 @@ Thread .* counts:
      .* data loads
      .* data stores
            1 icache flushes
-           4 dcache flushes
+           3 dcache flushes
      .* timestamp \+ cpuid markers
 .*

clients/drcachesim/tests/offline-legacy-int-offs.templatex
@@ -3,12 +3,12 @@ ERROR: failed to initialize analyzer: Directory setup failed: Failed sanity chec
 #elif defined(X86) && defined(X64)
 Basic counts tool results:
 Total counts:
-      109205 total \(fetched\) instructions
-        6768 total unique \(fetched\) instructions
+      109201 total \(fetched\) instructions
+        6765 total unique \(fetched\) instructions
           93 total non-fetched instructions
           11 total prefetches
-       23974 total data loads
-        5544 total data stores
+       23972 total data loads
+        5543 total data stores
            0 total icache flushes
            0 total dcache flushes
            3 total threads
@@ -24,15 +24,15 @@ Total counts:
            0 total physical address unavailable markers
            0 total system call number markers
            0 total blocking system call markers
-          12 total other markers
-        8429 total encodings
+          16 total other markers
+        8426 total encodings
 Thread 552306 counts:
-      101049 \(fetched\) instructions
-        6393 unique \(fetched\) instructions
+      101045 \(fetched\) instructions
+        6390 unique \(fetched\) instructions
           93 non-fetched instructions
           11 prefetches
-       21712 data loads
-        4442 data stores
+       21710 data loads
+        4441 data stores
            0 icache flushes
            0 dcache flushes
          110 timestamp \+ cpuid markers
@@ -47,8 +47,8 @@ Thread 552306 counts:
            0 physical address unavailable markers
            0 system call number markers
            0 blocking system call markers
-           4 other markers
-        6393 encodings
+           8 other markers
+        6390 encodings
 Thread 552323 counts:
         4674 \(fetched\) instructions
         1028 unique \(fetched\) instructions

clients/drcachesim/tests/raw2trace_unit_tests.cpp
@@ -3156,6 +3156,249 @@ test_ifiltered(void *drcontext)
 #endif
 }
 
+/* Tests asynchronous signal handling (i#7050). */
+bool
+test_asynchronous_signal(void *drcontext)
+{
+    bool res = true;
+    {
+        std::cerr << "\n===============\nTesting instr removal by async signal\n";
+        instrlist_t *ilist = instrlist_create(drcontext);
+        // raw2trace doesn't like offsets of 0 so we shift with a nop.
+        instr_t *nop = XINST_CREATE_nop(drcontext);
+        instr_t *move1 =
+            XINST_CREATE_move(drcontext, opnd_create_reg(REG1), opnd_create_reg(REG2));
+        instr_t *move2 =
+            XINST_CREATE_move(drcontext, opnd_create_reg(REG2), opnd_create_reg(REG1));
+        instrlist_append(ilist, nop);
+        instrlist_append(ilist, move1);
+        instrlist_append(ilist, move2);
+        size_t offs_nop = 0;
+        size_t offs_move1 = offs_nop + instr_length(drcontext, nop);
+
+        std::vector<offline_entry_t> raw;
+        raw.push_back(make_header());
+        raw.push_back(make_tid());
+        raw.push_back(make_pid());
+        raw.push_back(make_line_size());
+        raw.push_back(make_timestamp());
+        raw.push_back(make_core());
+        raw.push_back(make_block(offs_move1, 2));
+        raw.push_back(make_marker(TRACE_MARKER_TYPE_KERNEL_EVENT, offs_move1));
+        raw.push_back(make_exit());
+
+        std::vector<uint64_t> stats;
+        std::vector<trace_entry_t> entries;
+        if (!run_raw2trace(drcontext, raw, ilist, entries, &stats))
+            return false;
+        int idx = 0;
+        res = check_entry(entries, idx, TRACE_TYPE_HEADER, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_VERSION) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_FILETYPE) &&
+            check_entry(entries, idx, TRACE_TYPE_THREAD, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_PID, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_CACHE_LINE_SIZE) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_CHUNK_INSTR_COUNT) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_TIMESTAMP) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_CPU_ID) &&
+            // The move1 instruction is removed because of the asynchronous signal.
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_KERNEL_EVENT,
+                        offs_move1) &&
+            check_entry(entries, idx, TRACE_TYPE_THREAD_EXIT, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_FOOTER, -1);
+    }
+    {
+        std::cerr
+            << "\n===============\nTesting instr and memref removal by async signal\n";
+        instrlist_t *ilist = instrlist_create(drcontext);
+        // raw2trace doesn't like offsets of 0 so we shift with a nop.
+        instr_t *nop = XINST_CREATE_nop(drcontext);
+        instr_t *load = XINST_CREATE_load(drcontext, opnd_create_reg(REG1),
+                                          OPND_CREATE_MEMPTR(REG2, 0));
+        instr_t *store = XINST_CREATE_store(drcontext, OPND_CREATE_MEMPTR(REG2, 0),
+                                            opnd_create_reg(REG1));
+        instr_t *move =
+            XINST_CREATE_move(drcontext, opnd_create_reg(REG2), opnd_create_reg(REG1));
+        instrlist_append(ilist, nop);
+        instrlist_append(ilist, load);
+        instrlist_append(ilist, store);
+        instrlist_append(ilist, move);
+        size_t offs_nop = 0;
+        size_t offs_load = offs_nop + instr_length(drcontext, nop);
+        size_t offs_store = offs_load + instr_length(drcontext, load);
+
+        std::vector<offline_entry_t> raw;
+        raw.push_back(make_header());
+        raw.push_back(make_tid());
+        raw.push_back(make_pid());
+        raw.push_back(make_line_size());
+        raw.push_back(make_timestamp());
+        raw.push_back(make_core());
+        raw.push_back(make_block(offs_load, 3));
+        raw.push_back(make_memref(42));
+        raw.push_back(make_marker(TRACE_MARKER_TYPE_KERNEL_EVENT, offs_store));
+        raw.push_back(make_exit());
+
+        std::vector<uint64_t> stats;
+        std::vector<trace_entry_t> entries;
+        if (!run_raw2trace(drcontext, raw, ilist, entries, &stats))
+            return false;
+        int idx = 0;
+        res &=
+            (check_entry(entries, idx, TRACE_TYPE_HEADER, -1) &&
+             check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_VERSION) &&
+             check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_FILETYPE) &&
+             check_entry(entries, idx, TRACE_TYPE_THREAD, -1) &&
+             check_entry(entries, idx, TRACE_TYPE_PID, -1) &&
+             check_entry(entries, idx, TRACE_TYPE_MARKER,
+                         TRACE_MARKER_TYPE_CACHE_LINE_SIZE) &&
+             check_entry(entries, idx, TRACE_TYPE_MARKER,
+                         TRACE_MARKER_TYPE_CHUNK_INSTR_COUNT) &&
+             check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_TIMESTAMP) &&
+             check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_CPU_ID) &&
+             // The load instruction.
+             check_entry(entries, idx, TRACE_TYPE_ENCODING, -1) &&
+             check_entry(entries, idx, TRACE_TYPE_INSTR, -1, offs_load) &&
+             check_entry(entries, idx, TRACE_TYPE_READ, -1) &&
+             // The store instruction and the memref are removed.
+             check_entry(entries, idx, TRACE_TYPE_MARKER,
+                         TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION) &&
+             check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_KERNEL_EVENT,
+                         offs_store) &&
+             check_entry(entries, idx, TRACE_TYPE_THREAD_EXIT, -1) &&
+             check_entry(entries, idx, TRACE_TYPE_FOOTER, -1));
+    }
+#ifdef X86
+    {
+        std::cerr << "\n===============\nTesting rep string instr without async signal\n";
+        instrlist_t *ilist = instrlist_create(drcontext);
+        // raw2trace doesn't like offsets of 0 so we shift with a nop.
+        instr_t *nop = XINST_CREATE_nop(drcontext);
+        instr_t *rep_stos = INSTR_CREATE_rep_stos_4(drcontext);
+        instr_t *move =
+            XINST_CREATE_move(drcontext, opnd_create_reg(REG2), opnd_create_reg(REG1));
+        instrlist_append(ilist, nop);
+        instrlist_append(ilist, rep_stos);
+        instrlist_append(ilist, move);
+        size_t offs_nop = 0;
+        size_t offs_rep_stos = offs_nop + instr_length(drcontext, nop);
+        size_t offs_move = offs_rep_stos + instr_length(drcontext, rep_stos);
+
+        std::vector<offline_entry_t> raw;
+        raw.push_back(make_header());
+        raw.push_back(make_tid());
+        raw.push_back(make_pid());
+        raw.push_back(make_line_size());
+        raw.push_back(make_timestamp());
+        raw.push_back(make_core());
+        raw.push_back(make_block(offs_rep_stos, 1));
+        raw.push_back(make_memref(42));
+        raw.push_back(make_block(offs_rep_stos, 1));
+        raw.push_back(make_memref(46));
+        raw.push_back(make_block(offs_rep_stos, 1));
+        raw.push_back(make_memref(50));
+        raw.push_back(make_block(offs_move, 1));
+        raw.push_back(make_exit());
+
+        std::vector<uint64_t> stats;
+        std::vector<trace_entry_t> entries;
+        if (!run_raw2trace(drcontext, raw, ilist, entries, &stats))
+            return false;
+        int idx = 0;
+        res &= check_entry(entries, idx, TRACE_TYPE_HEADER, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_VERSION) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_FILETYPE) &&
+            check_entry(entries, idx, TRACE_TYPE_THREAD, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_PID, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_CACHE_LINE_SIZE) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_CHUNK_INSTR_COUNT) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_TIMESTAMP) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_CPU_ID) &&
+            // The rep_stos instruction.
+            check_entry(entries, idx, TRACE_TYPE_ENCODING, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_INSTR, -1, offs_rep_stos) &&
+            check_entry(entries, idx, TRACE_TYPE_WRITE, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_INSTR_NO_FETCH, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_WRITE, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_INSTR_NO_FETCH, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_WRITE, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_ENCODING, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_INSTR, -1, offs_move) &&
+            check_entry(entries, idx, TRACE_TYPE_THREAD_EXIT, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_FOOTER, -1);
+    }
+    {
+        std::cerr
+            << "\n===============\nTesting rep string instr removal by async signal\n";
+        instrlist_t *ilist = instrlist_create(drcontext);
+        // raw2trace doesn't like offsets of 0 so we shift with a nop.
+        instr_t *nop = XINST_CREATE_nop(drcontext);
+        instr_t *rep_stos = INSTR_CREATE_rep_stos_4(drcontext);
+        instr_t *move =
+            XINST_CREATE_move(drcontext, opnd_create_reg(REG2), opnd_create_reg(REG1));
+        instrlist_append(ilist, nop);
+        instrlist_append(ilist, rep_stos);
+        instrlist_append(ilist, move);
+        size_t offs_nop = 0;
+        size_t offs_rep_stos = offs_nop + instr_length(drcontext, nop);
+
+        std::vector<offline_entry_t> raw;
+        raw.push_back(make_header());
+        raw.push_back(make_tid());
+        raw.push_back(make_pid());
+        raw.push_back(make_line_size());
+        raw.push_back(make_timestamp());
+        raw.push_back(make_core());
+        raw.push_back(make_block(offs_rep_stos, 1));
+        raw.push_back(make_memref(42));
+        raw.push_back(make_block(offs_rep_stos, 1));
+        raw.push_back(make_memref(46));
+        raw.push_back(make_block(offs_rep_stos, 1));
+        raw.push_back(make_memref(50));
+        raw.push_back(make_marker(TRACE_MARKER_TYPE_KERNEL_EVENT, offs_rep_stos));
+        raw.push_back(make_exit());
+
+        std::vector<uint64_t> stats;
+        std::vector<trace_entry_t> entries;
+        if (!run_raw2trace(drcontext, raw, ilist, entries, &stats))
+            return false;
+        int idx = 0;
+        res &= check_entry(entries, idx, TRACE_TYPE_HEADER, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_VERSION) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_FILETYPE) &&
+            check_entry(entries, idx, TRACE_TYPE_THREAD, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_PID, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_CACHE_LINE_SIZE) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_CHUNK_INSTR_COUNT) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_TIMESTAMP) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_CPU_ID) &&
+            // The rep_stos instruction.
+            check_entry(entries, idx, TRACE_TYPE_ENCODING, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_INSTR, -1, offs_rep_stos) &&
+            check_entry(entries, idx, TRACE_TYPE_WRITE, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_INSTR_NO_FETCH, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_WRITE, -1) &&
+            // The last rep_stos instruction and the write record are removed because of
+            // the asynchronous signal.
+            check_entry(entries, idx, TRACE_TYPE_MARKER,
+                        TRACE_MARKER_TYPE_UNCOMPLETED_INSTRUCTION) &&
+            check_entry(entries, idx, TRACE_TYPE_MARKER, TRACE_MARKER_TYPE_KERNEL_EVENT,
+                        offs_rep_stos) &&
+            check_entry(entries, idx, TRACE_TYPE_THREAD_EXIT, -1) &&
+            check_entry(entries, idx, TRACE_TYPE_FOOTER, -1);
+    }
+#endif
+    return res;
+}
+
 int
 test_main(int argc, const char *argv[])
 {
@@ -3177,7 +3420,8 @@ test_main(int argc, const char *argv[])
         !test_xfer_modoffs(drcontext) || !test_xfer_absolute(drcontext) ||
         !test_branch_decoration(drcontext) ||
         !test_stats_timestamp_instr_count(drcontext) ||
-        !test_is_maybe_blocking_syscall(drcontext) || !test_ifiltered(drcontext))
+        !test_is_maybe_blocking_syscall(drcontext) || !test_ifiltered(drcontext) ||
+        !test_asynchronous_signal(drcontext))
         return 1;
     return 0;
 }

clients/drcachesim/tests/signal_invariants.c
@@ -190,8 +190,11 @@ GLOBAL_LABEL(FUNCNAME:)
 #define FUNCNAME test_signal_midbb
         DECLARE_FUNC(FUNCNAME)
 GLOBAL_LABEL(FUNCNAME:)
-        /* prefetcht2's address is the instr count until a signal */
-        prefetcht2 [3]
+        /* prefetcht2's address is the instr count until a signal not counting
+         * the faulting instr because the faulting instr is removed from the
+         * trace.
+         */
+        prefetcht2 [2]
         nop
         nop
         ud2
@@ -205,8 +208,11 @@ GLOBAL_LABEL(FUNCNAME:)
 #define FUNCNAME test_signal_startbb
         DECLARE_FUNC(FUNCNAME)
 GLOBAL_LABEL(FUNCNAME:)
-        /* prefetcht2's address is the instr count until a signal */
-        prefetcht2 [2]
+        /* prefetcht2's address is the instr count until a signal not counting
+         * the faulting instr because the faulting instr is removed from the
+         * trace.
+         */
+        prefetcht2 [1]
         jmp      new_bb
     new_bb:
         ud2
@@ -221,10 +227,15 @@ GLOBAL_LABEL(FUNCNAME:)
          * XXX i#3958: Today the 2nd movs memref is incorrectly included *before*
          * the fault.
          */
-        /* prefetcht2's address is the instr count until a signal */
-        prefetcht2 [5]
-        /* prefetcht1's address is the memref count until a signal */
-        prefetcht1 [3]
+        /* prefetcht2's address is the instr count until a signal not counting
+         * the faulting instr because the faulting instr is removed from the
+         * trace.
+         */
+        prefetcht2 [4]
+        /* prefetcht1's address is the memref count until a signal not counting
+         * the faulting memref because the faulting memref is removed from the trace.
+         */
+        prefetcht1 [1]
         mov      REG_XSI, HEX(42)
         mov      REG_XDI, REG_XSP
         push     REG_XAX

commit 7491c9cd3d01ae7a468aa00903680e8c4e34509e

i#7084: Change handle_rseq_abort_marker parameter name interrupted to rseq_aborted. (#7086)
raw2trace_t::handle_rseq_abort_marker() uses different names of the last
parameter. It's interrupted in raw2trace.h
but it is rseq_aborted in raw2trace.cpp.

Change the name from interrupted to rseq_aborted to be consistent with
raw2trace.cpp.

The inconsistency was introduced by PR #7058.

Fixes: https://github.com/DynamoRIO/dynamorio/issues/7084

clients/drcachesim/tracer/raw2trace.h
@@ -1510,7 +1510,7 @@ private:
     bool
     handle_rseq_abort_marker(raw2trace_thread_data_t *tdata,
                              DR_PARAM_INOUT trace_entry_t **buf_in, uint64_t cur_pc,
-                             uint64_t cur_offs, DR_PARAM_OUT bool *interrupted);
+                             uint64_t cur_offs, DR_PARAM_OUT bool *rseq_aborted);
 
     bool
     get_marker_value(raw2trace_thread_data_t *tdata,