e8ab1e49c3e4c116ff57e2a148050bf0e78dd440
edeiana <edeiana@google.com>
2025-01-06 15:25:01 -0800

    Fix comment.

--- clients/drcachesim/tests/v2p_reader_unit_test.cpp
+++ clients/drcachesim/tests/v2p_reader_unit_test.cpp
@@ -45,7 +45,7 @@ namespace drmemtrace {
 static void
 check_v2p_info(const v2p_info_t &v2p_info)
 {
-    // Change the number of entries if v2p_map_example.textproto is updated.
+    // Change NUM_ENTRIES if clients/drcachesim/tests/v2p_example.textproto is updated.
     // Must be equal to the number of "address_mapping {...}" blocks in the textproto.
     constexpr size_t NUM_ENTRIES = 4;
     if (v2p_info.v2p_map.size() != NUM_ENTRIES) {
@@ -54,7 +54,8 @@ check_v2p_info(const v2p_info_t &v2p_info)
         exit(1);
     }
 
-    // Virtual and physical addresses must be aligned with v2p_example.textproto.
+    // Virtual and physical addresses must be aligned with
+    // clients/drcachesim/tests/v2p_example.textproto.
     const std::vector<addr_t> virtual_addresses = { 0x123, 0x456, 0x0, 0x789 };
     const std::vector<addr_t> physical_addresses = { 0x3, 0x4, 0x6, 0x5 };
     for (int i = 0; i < virtual_addresses.size(); ++i) {
