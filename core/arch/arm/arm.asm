a53fcb8dc2102aa1e881688dcf42d7e979fb553d
Edmund Grimley Evans <edmund.grimley-evans@arm.com>
2025-01-16 20:25:38 +0000

    arm.asm: Add Arm copyright.
    Change-Id: Ic69b5335d277ec3ff6508ae02cc8698e333e51b1

--- core/arch/arm/arm.asm
+++ core/arch/arm/arm.asm
@@ -1,5 +1,6 @@
 /* **********************************************************
  * Copyright (c) 2014-2022 Google, Inc.  All rights reserved.
+ * Copyright (c) 2025 Arm Limited. All rights reserved.
  * ********************************************************** */
 
 /*
