256e016422677039fc7b0fffe21fd6cf3b8fb0d0
flandini <flanders.michaelk@gmail.com>
2025-02-12 22:28:11 -0600

    Switch to dr_snprintf in func_trace.cpp

--- clients/drcachesim/tracer/func_trace.cpp
+++ clients/drcachesim/tracer/func_trace.cpp
@@ -101,13 +101,11 @@ static func_metadata_t *
 create_func_metadata(const char *name, int id, int arg_num, bool noret)
 {
     func_metadata_t *f = (func_metadata_t *)dr_global_alloc(sizeof(func_metadata_t));
-    size_t name_len = strlen(name);
-    // Ensure there is room for the string and a null terminator (+ 1).
-    // Other code assumes that this is a null terminated string.
-    DR_ASSERT(name_len + 1 && name_len + 1 <= BUFFER_SIZE_ELEMENTS(f->name) &&
-              "Func metadata name is too large and would be truncated");
-    memcpy(f->name, name, name_len);
-    f->name[name_len] = '\0';
+    int potentially_written = dr_snprintf(f->name, BUFFER_SIZE_ELEMENTS(f->name), "%s", name);
+    NULL_TERMINATE_BUFFER(f->name);
+    if (potentially_written < 0 || (size_t)potentially_written >= BUFFER_SIZE_ELEMENTS(f->name)) {
+        NOTIFY(0, "Func metadata name is too long and was truncated: %s!\n", name);
+    }
     f->id = id;
     f->arg_num = arg_num;
     f->noret = noret;
