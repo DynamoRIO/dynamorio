5d67e0b8a0cd9d99474cf18eab9c2dccdc532553
flandini <flanders.michaelk@gmail.com>
2025-02-13 08:00:17 -0600

    Formatting

--- clients/drcachesim/tracer/func_trace.cpp
+++ clients/drcachesim/tracer/func_trace.cpp
@@ -101,8 +101,7 @@ static func_metadata_t *
 create_func_metadata(const char *name, int id, int arg_num, bool noret)
 {
     func_metadata_t *f = (func_metadata_t *)dr_global_alloc(sizeof(func_metadata_t));
-    int bytes_written =
-        dr_snprintf(f->name, BUFFER_SIZE_ELEMENTS(f->name), "%s", name);
+    int bytes_written = dr_snprintf(f->name, BUFFER_SIZE_ELEMENTS(f->name), "%s", name);
     NULL_TERMINATE_BUFFER(f->name);
     if (bytes_written < 0 || bytes_written == BUFFER_SIZE_ELEMENTS(f->name)) {
         NOTIFY(0, "Func metadata name is too long and was truncated: %s\n", name);
