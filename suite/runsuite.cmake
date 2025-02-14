c75f228f5435a72142526e1644fb0ad3b6ac7b70
Abhinav Anil Sharma <sharmaabhinav@google.com>
2025-02-14 11:42:03 -0500

    i#7270 Ubuntu 22: Update Linux OS in workflows
    Also removes restrictions on the Ubuntu 22 test suite.
    
    For now, this PR is only to allow us to find out the extent of work required to upgrade to Ubuntu 22.04 in all our workflows.
    
    Issue: #7270

--- suite/runsuite.cmake
+++ suite/runsuite.cmake
@@ -84,24 +84,6 @@ foreach (arg ${CTEST_SCRIPT_ARG})
   endif ()
 endforeach (arg)
 
-if (UNIX AND NOT APPLE AND NOT ANDROID AND NOT cross_riscv64_linux_only)
-  execute_process(COMMAND ldd --version
-    RESULT_VARIABLE ldd_result ERROR_VARIABLE ldd_err OUTPUT_VARIABLE ldd_out)
-  if (ldd_result OR ldd_err)
-    # Failed; just move on.
-  elseif (ldd_out MATCHES "GLIBC 2.3[5-9]")
-    # XXX i#5437, i#5431: While we work through Ubuntu22 issues we run
-    # just a few tests.
-    set(extra_ctest_args INCLUDE_LABEL UBUNTU_22)
-    set(arg_debug_only ON)
-  elseif (arg_32_only AND NOT cross_aarchxx_linux_only AND NOT cross_android_only)
-    # TODO i#6417: The switch to AMD VM's for GA CI has broken many of our tests.
-    # This includes timeouts which increases suite length.
-    # Until we get ths x86-32 job back green, we drop back to a small set of tests.
-    set(extra_ctest_args EXCLUDE_LABEL AMD_X32_DENYLIST)
-  endif ()
-endif ()
-
 set(build_tests "BUILD_TESTS:BOOL=ON")
 
 if (arg_automated_ci)
