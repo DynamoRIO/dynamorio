a677c8b4f786a4c3cea4c410f1d668f0bf9c71eb
Kaiyeung <kyluk@google.com>
2025-03-05 10:24:31 -0800

    Cleanup drsyscall/CMakeLists.txt to remove unnecessary statements.

--- core/lib/dr_api.h
+++ core/lib/dr_api.h
@@ -132,7 +132,7 @@ dr_init(client_id_t id);
 /* Version checking */
 /* This equals major*100 + minor */
 /* clang-format off */
-DR_EXPORT LINK_ONCE int _USES_DR_VERSION_ = 12345;
+DR_EXPORT LINK_ONCE int _USES_DR_VERSION_ =  ${VERSION_NUMBER_INTEGER};
 #else
 /* We provide the version as a define but we don't want an actual symbol to avoid
  * problems when standalone-using libraries are combined with clients.
