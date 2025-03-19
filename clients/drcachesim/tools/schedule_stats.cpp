1f05366f03195e55486ac47c8c9238aa43a27950
Derek Bruening <bruening@google.com>
2025-03-19 16:17:03 -0400

    Use local var to simplify code

--- clients/drcachesim/tools/schedule_stats.cpp
+++ clients/drcachesim/tools/schedule_stats.cpp
@@ -268,19 +268,16 @@ schedule_stats_t::parallel_shard_memref(void *shard_data, const memref_t &memref
     int64_t input_id = shard->stream->get_input_id();
     if (knob_verbose_ >= 4) {
         std::ostringstream line;
+        memtrace_stream_t *input_stream = shard->stream->get_input_interface();
         line << "Core #" << std::setw(2) << shard->core << " @" << std::setw(9)
              << shard->stream->get_record_ordinal() << " refs, " << std::setw(9)
              << shard->stream->get_instruction_ordinal() << " instrs: input "
              << std::setw(4) << input_id << " @"
              << std::setw(9)
              // The interface is null when we see idle records.
-             << (shard->stream->get_input_interface() == nullptr
-                     ? -1
-                     : shard->stream->get_input_interface()->get_record_ordinal())
+             << (input_stream == nullptr ? -1 : input_stream->get_record_ordinal())
              << " refs, " << std::setw(9)
-             << (shard->stream->get_input_interface() == nullptr
-                     ? -1
-                     : shard->stream->get_input_interface()->get_instruction_ordinal())
+             << (input_stream == nullptr ? -1 : input_stream->get_instruction_ordinal())
              << " instrs: " << std::setw(16) << trace_type_names[memref.marker.type];
         if (type_is_instr(memref.instr.type))
             line << " pc=" << std::hex << memref.instr.addr << std::dec;
