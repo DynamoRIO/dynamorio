39f75c0c9e6a6f62e4418e0d93e76afe3e14457e
Kaiyeung Luk <kyluk@google.com>
2025-03-21 13:47:48 -0700

    Restore non-C/C++ files.

--- ext/drmf/common/asm_utils_arm.asm
+++ ext/drmf/common/asm_utils_arm.asm
@@ -50,6 +50,7 @@
  * DAMAGE.
  */
 
+
 /***************************************************************************
  * assembly utilities
  */
@@ -60,41 +61,48 @@ START_FILE
 
 /* void get_stack_registers(reg_t *sp OUT, reg_t *fp OUT) */
 #define FUNCNAME get_stack_registers
-DECLARE_FUNC(FUNCNAME)
+        DECLARE_FUNC(FUNCNAME)
 GLOBAL_LABEL(FUNCNAME:)
-str sp, [REG_R0] str fp,
-    [REG_R1] bx lr
-    END_FUNC(FUNCNAME)
+        str      sp, [REG_R0]
+        str      fp, [REG_R1]
+        bx       lr
+        END_FUNC(FUNCNAME)
 #undef FUNCNAME
+
 
 /* void get_unwind_registers(reg_t *sp OUT, reg_t *fp OUT, app_pc *pc OUT) */
 #define FUNCNAME get_unwind_registers
-        DECLARE_FUNC(FUNCNAME) GLOBAL_LABEL(FUNCNAME:) str sp,
-    [REG_R0] str fp, [REG_R1] str lr,
-    [REG_R2] bx lr END_FUNC(FUNCNAME)
+        DECLARE_FUNC(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        str      sp, [REG_R0]
+        str      fp, [REG_R1]
+        str      lr, [REG_R2]
+        bx       lr
+        END_FUNC(FUNCNAME)
 #undef FUNCNAME
 
+
 #ifdef LINUX
-    /* Straight from DynamoRIO.
-     * Signature: raw_syscall(sysnum, num_args, arg1, arg2, ...)
-     * XXX: if we remove this and use an export from DR instead, we can remove
-     * the DR license statement from the top of this file.
-     */
-    DECLARE_FUNC(raw_syscall) GLOBAL_LABEL(raw_syscall:) push { REG_R4 - REG_R8 }
-/* shift r7 pointing to the call args */
-add REG_R8,
-    sp,
-    #20 /* size for {r4-r8} */
-    mov REG_R7,
-    ARG1 /* sysnum */
-    mov REG_R0,
-    ARG3 /* syscall arg1 */
-    mov REG_R1,
-    ARG4 /* syscall arg2 */
-    ldmfd REG_R8,
-    { REG_R2 - REG_R6 } /* syscall arg3..arg7 */
-svc #0 pop { REG_R4 - REG_R8 } bx lr END_FUNC(raw_syscall)
+/* Straight from DynamoRIO.
+ * Signature: raw_syscall(sysnum, num_args, arg1, arg2, ...)
+ * XXX: if we remove this and use an export from DR instead, we can remove
+ * the DR license statement from the top of this file.
+ */
+        DECLARE_FUNC(raw_syscall)
+GLOBAL_LABEL(raw_syscall:)
+        push     {REG_R4-REG_R8}
+        /* shift r7 pointing to the call args */
+        add      REG_R8, sp, #20         /* size for {r4-r8} */
+        mov      REG_R7, ARG1            /* sysnum */
+        mov      REG_R0, ARG3            /* syscall arg1 */
+        mov      REG_R1, ARG4            /* syscall arg2 */
+        ldmfd    REG_R8, {REG_R2-REG_R6} /* syscall arg3..arg7 */
+        svc      #0
+        pop      {REG_R4-REG_R8}
+        bx       lr
+        END_FUNC(raw_syscall)
 #endif /* LINUX */
+
 
 /* void zero_pointers_on_stack(size_t count);
  * assuming count must be multiple of ARG_SZ
@@ -105,18 +113,24 @@ svc #0 pop { REG_R4 - REG_R8 } bx lr END_FUNC(raw_syscall)
  * from C as we can easily clobber our own local variables.
  */
 #define FUNCNAME zero_pointers_on_stack
-    DECLARE_FUNC_SEH(FUNCNAME) GLOBAL_LABEL(FUNCNAME:) rsb REG_R0,
-    REG_R0, #0 sub REG_R0, REG_R0, #ARG_SZ mov REG_R2,
-    #0 1 :
-    /* we assume no pointer pointing to address below 0x10000 */
-    ldr REG_R1,
-    [ sp, REG_R0 ] cmp REG_R1,
-    HEX(10000) bls 2f /* skip store if not a pointer */
-    str REG_R2,
-    [ sp, REG_R0 ] 2 : add REG_R0,
-                       #ARG_SZ cmp REG_R0,
-                       # -
-                           ARG_SZ bne 1b bx lr END_FUNC(FUNCNAME)
+        DECLARE_FUNC_SEH(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        rsb      REG_R0, REG_R0, #0
+        sub      REG_R0, REG_R0, #ARG_SZ
+        mov      REG_R2, #0
+1:
+        /* we assume no pointer pointing to address below 0x10000 */
+        ldr      REG_R1, [sp, REG_R0]
+        cmp      REG_R1, HEX(10000)
+        bls      2f /* skip store if not a pointer */
+        str      REG_R2, [sp, REG_R0]
+2:
+        add      REG_R0, #ARG_SZ
+        cmp      REG_R0, #-ARG_SZ
+        bne      1b
+        bx       lr
+        END_FUNC(FUNCNAME)
 #undef FUNCNAME
 
-                               END_FILE
+
+END_FILE
--- ext/drmf/common/asm_utils_x86.asm
+++ ext/drmf/common/asm_utils_x86.asm
@@ -50,6 +50,7 @@
  * DAMAGE.
  */
 
+
 /***************************************************************************
  * assembly utilities for which there are no intrinsics
  */
@@ -65,124 +66,146 @@ START_FILE
  * then would have to assume no FPO to get xsp.
  */
 #define FUNCNAME get_stack_registers
-DECLARE_FUNC(FUNCNAME)
+        DECLARE_FUNC(FUNCNAME)
 GLOBAL_LABEL(FUNCNAME:)
-/* we don't bother to be SEH64 compliant */
-mov REG_XAX, ARG1 mov REG_XCX, ARG2 mov PTRSZ[REG_XCX], REG_XBP mov REG_XCX,
-    REG_XSP
-/* The caller may not use push to call us: it may allocate its
- * max frame up front.  However, all current uses are from functions
- * with large frames themselves, so our removals here are not a problem.
- */
+        /* we don't bother to be SEH64 compliant */
+        mov      REG_XAX, ARG1
+        mov      REG_XCX, ARG2
+        mov      PTRSZ [REG_XCX], REG_XBP
+        mov      REG_XCX, REG_XSP
+        /* The caller may not use push to call us: it may allocate its
+         * max frame up front.  However, all current uses are from functions
+         * with large frames themselves, so our removals here are not a problem.
+         */
 #if defined(X64) && defined(WINDOWS)
-        add REG_XCX,
-    32 +
-    ARG_SZ /* remove frame space + retaddr */
+        add      REG_XCX, 32 + ARG_SZ   /* remove frame space + retaddr */
 #elif !defined(X64)
-        add REG_XCX,
-    3 *
-    ARG_SZ /* remove args + retaddr */
+        add      REG_XCX, 3 * ARG_SZ    /* remove args + retaddr */
 
 #endif
-        mov PTRSZ[REG_XAX],
-    REG_XCX ret
-    END_FUNC(FUNCNAME)
+        mov      PTRSZ [REG_XAX], REG_XCX
+        ret
+        END_FUNC(FUNCNAME)
 #undef FUNCNAME
+
 
 /* void get_unwind_registers(reg_t *xsp OUT, reg_t *xbp OUT, app_pc *xip OUT);
  *
  * Returns the caller's values of xsp and xbp and xip.
  */
 #define FUNCNAME get_unwind_registers
-        DECLARE_FUNC(FUNCNAME) GLOBAL_LABEL(FUNCNAME:)
-    /* we don't bother to be SEH64 compliant */
-    mov REG_XAX,
-    ARG1 mov REG_XCX, ARG2 mov PTRSZ[REG_XCX], REG_XBP mov REG_XCX, ARG3 mov REG_XDX,
-    PTRSZ[REG_XSP] mov PTRSZ[REG_XCX],
-    REG_XDX
-/* The caller may not use push to call us: it may allocate its
- * max frame up front.  However, all current uses are from functions
- * with large frames themselves, so our removals here are not a problem.
- */
+        DECLARE_FUNC(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        /* we don't bother to be SEH64 compliant */
+        mov      REG_XAX, ARG1
+        mov      REG_XCX, ARG2
+        mov      PTRSZ [REG_XCX], REG_XBP
+        mov      REG_XCX, ARG3
+        mov      REG_XDX, PTRSZ [REG_XSP]
+        mov      PTRSZ [REG_XCX], REG_XDX
+        /* The caller may not use push to call us: it may allocate its
+         * max frame up front.  However, all current uses are from functions
+         * with large frames themselves, so our removals here are not a problem.
+         */
 #if defined(X64) && defined(WINDOWS)
-    add REG_XCX,
-    32 +
-    ARG_SZ /* remove frame space + retaddr */
+        add      REG_XCX, 32 + ARG_SZ   /* remove frame space + retaddr */
 #elif !defined(X64)
-    add REG_XCX,
-    3 *
-    ARG_SZ /* remove args + retaddr */
+        add      REG_XCX, 3 * ARG_SZ    /* remove args + retaddr */
 
 #endif
-    mov PTRSZ[REG_XAX],
-    REG_XCX ret END_FUNC(FUNCNAME)
+        mov      PTRSZ [REG_XAX], REG_XCX
+        ret
+        END_FUNC(FUNCNAME)
 #undef FUNCNAME
 
+
 #ifdef LINUX
-    /* Straight from DynamoRIO as a faster fix than continually tweaking the
-     * C routines raw_syscall_N_args() since i#199 has complex issues and is
-     * non-trivial to put in place.
-     * XXX: if we remove this and use an export from DR instead, we can remove
-     * the DR license statement from the top of this file.
-     */
-    /* to avoid libc wrappers we roll our own syscall here
-     * hardcoded to use int 0x80 for 32-bit -- FIXME: use something like do_syscall
-     * and syscall for 64-bit.
-     * signature: raw_syscall(sysnum, num_args, arg1, arg2, ...)
-     */
-    DECLARE_FUNC(raw_syscall) GLOBAL_LABEL(raw_syscall:)
-    /* x64 kernel doesn't clobber all the callee-saved registers */
-    push REG_XBX
-#    ifdef X64
-    /* reverse order so we don't clobber earlier args */
-    mov REG_XBX,
-    ARG2 /* put num_args where we can reference it longer */
-    mov rax,
-    ARG1 /* sysnum: only need eax, but need rax to use ARG1 (or movzx) */
-    cmp REG_XBX,
-    0 je syscall_ready mov ARG1, ARG3 cmp REG_XBX,
-    1 je syscall_ready mov ARG2, ARG4 cmp REG_XBX,
-    2 je syscall_ready mov ARG3, ARG5 cmp REG_XBX,
-    3 je syscall_ready mov ARG4, ARG6 cmp REG_XBX, 4 je syscall_ready mov ARG5,
-    [2 * ARG_SZ + REG_XSP] /* arg7: above xbx and retaddr */
-    cmp REG_XBX,
-    5 je syscall_ready mov ARG6,
-    [3 * ARG_SZ + REG_XSP] /* arg8: above arg7, xbx, retaddr */
-    syscall_ready : mov r10,
-                    rcx syscall
-#    else
-    push REG_XBP push REG_XSI push REG_XDI
-    /* add 16 to skip the 4 pushes
-     * FIXME: rather than this dispatch, could have separate routines
-     * for each #args, or could just blindly read upward on the stack.
-     * for dispatch, if assume size of mov instr can do single ind jmp */
-    mov ecx,
-    [16 + 8 + esp] /* num_args */
-    cmp ecx,
-    0 je syscall_0args cmp ecx, 1 je syscall_1args cmp ecx, 2 je syscall_2args cmp ecx,
-    3 je syscall_3args cmp ecx, 4 je syscall_4args cmp ecx, 5 je syscall_5args mov ebp,
-    [16 + 32 + esp] /* arg6 */
-    syscall_5args : mov edi,
-                    [16 + 28 + esp] /* arg5 */
-                    syscall_4args : mov esi,
-                                    [16 + 24 + esp] /* arg4 */
-                                    syscall_3args : mov edx,
-                                                    [16 + 20 + esp] /* arg3 */
-                                                    syscall_2args
-    : mov ecx,
-      [16 + 16 + esp] /* arg2 */
-      syscall_1args : mov ebx,
-                      [16 + 12 + esp] /* arg1 */
-                      syscall_0args
-    : mov eax,
-      [16 + 4 + esp] /* sysnum */
-      /* PR 254280: we assume int$80 is ok even for LOL64 */
-      int HEX(80) pop REG_XDI pop REG_XSI pop REG_XBP
-#    endif /* X64 */
-                    pop REG_XBX
-                    /* return val is in eax for us */
-                    ret END_FUNC(raw_syscall)
+/* Straight from DynamoRIO as a faster fix than continually tweaking the
+ * C routines raw_syscall_N_args() since i#199 has complex issues and is
+ * non-trivial to put in place.
+ * XXX: if we remove this and use an export from DR instead, we can remove
+ * the DR license statement from the top of this file.
+ */
+/* to avoid libc wrappers we roll our own syscall here
+ * hardcoded to use int 0x80 for 32-bit -- FIXME: use something like do_syscall
+ * and syscall for 64-bit.
+ * signature: raw_syscall(sysnum, num_args, arg1, arg2, ...)
+ */
+        DECLARE_FUNC(raw_syscall)
+GLOBAL_LABEL(raw_syscall:)
+        /* x64 kernel doesn't clobber all the callee-saved registers */
+        push     REG_XBX
+# ifdef X64
+        /* reverse order so we don't clobber earlier args */
+        mov      REG_XBX, ARG2 /* put num_args where we can reference it longer */
+        mov      rax, ARG1 /* sysnum: only need eax, but need rax to use ARG1 (or movzx) */
+        cmp      REG_XBX, 0
+        je       syscall_ready
+        mov      ARG1, ARG3
+        cmp      REG_XBX, 1
+        je       syscall_ready
+        mov      ARG2, ARG4
+        cmp      REG_XBX, 2
+        je       syscall_ready
+        mov      ARG3, ARG5
+        cmp      REG_XBX, 3
+        je       syscall_ready
+        mov      ARG4, ARG6
+        cmp      REG_XBX, 4
+        je       syscall_ready
+        mov      ARG5, [2*ARG_SZ + REG_XSP] /* arg7: above xbx and retaddr */
+        cmp      REG_XBX, 5
+        je       syscall_ready
+        mov      ARG6, [3*ARG_SZ + REG_XSP] /* arg8: above arg7, xbx, retaddr */
+syscall_ready:
+        mov      r10, rcx
+        syscall
+# else
+        push     REG_XBP
+        push     REG_XSI
+        push     REG_XDI
+        /* add 16 to skip the 4 pushes
+         * FIXME: rather than this dispatch, could have separate routines
+         * for each #args, or could just blindly read upward on the stack.
+         * for dispatch, if assume size of mov instr can do single ind jmp */
+        mov      ecx, [16+ 8 + esp] /* num_args */
+        cmp      ecx, 0
+        je       syscall_0args
+        cmp      ecx, 1
+        je       syscall_1args
+        cmp      ecx, 2
+        je       syscall_2args
+        cmp      ecx, 3
+        je       syscall_3args
+        cmp      ecx, 4
+        je       syscall_4args
+        cmp      ecx, 5
+        je       syscall_5args
+        mov      ebp, [16+32 + esp] /* arg6 */
+syscall_5args:
+        mov      edi, [16+28 + esp] /* arg5 */
+syscall_4args:
+        mov      esi, [16+24 + esp] /* arg4 */
+syscall_3args:
+        mov      edx, [16+20 + esp] /* arg3 */
+syscall_2args:
+        mov      ecx, [16+16 + esp] /* arg2 */
+syscall_1args:
+        mov      ebx, [16+12 + esp] /* arg1 */
+syscall_0args:
+        mov      eax, [16+ 4 + esp] /* sysnum */
+        /* PR 254280: we assume int$80 is ok even for LOL64 */
+        int      HEX(80)
+        pop      REG_XDI
+        pop      REG_XSI
+        pop      REG_XBP
+# endif /* X64 */
+        pop      REG_XBX
+        /* return val is in eax for us */
+        ret
+        END_FUNC(raw_syscall)
 #endif /* LINUX */
+
 
 /* void zero_pointers_on_stack(size_t count);
  * assuming count must be multiple of ARG_SZ
@@ -193,23 +216,28 @@ mov REG_XAX, ARG1 mov REG_XCX, ARG2 mov PTRSZ[REG_XCX], REG_XBP mov REG_XCX,
  * from C as we can easily clobber our own local variables.
  */
 #define FUNCNAME zero_pointers_on_stack
-                        DECLARE_FUNC_SEH(FUNCNAME) GLOBAL_LABEL(FUNCNAME:)
-                    /* we don't bother to be SEH64 compliant */
-                    mov REG_XCX,
-                    ARG1 END_PROLOG neg REG_XCX
-                    /* i#1284: we used "rep stosd" before, which is slower than
-                     * the check-before-write optimization we have below.
-                     * The tight loop with fewer stores has better performance.
-                     */
-                    check_stack :
-    /* we assume no pointer pointing to address below 0x10000 */
-    cmp PTRSZ[REG_XSP + REG_XCX - ARG_SZ],
-    HEX(10000) jbe update_xcx /* skip store if not a pointer */
-    mov PTRSZ[REG_XSP + REG_XCX - ARG_SZ],
-    0 update_xcx : add REG_XCX,
-                   ARG_SZ jne check_stack add REG_XSP,
-                   0 /* make a legal SEH64 epilog */
-                   ret END_FUNC(FUNCNAME)
+        DECLARE_FUNC_SEH(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        /* we don't bother to be SEH64 compliant */
+        mov      REG_XCX, ARG1
+        END_PROLOG
+        neg      REG_XCX
+        /* i#1284: we used "rep stosd" before, which is slower than
+         * the check-before-write optimization we have below.
+         * The tight loop with fewer stores has better performance.
+         */
+check_stack:
+        /* we assume no pointer pointing to address below 0x10000 */
+        cmp      PTRSZ [REG_XSP+REG_XCX-ARG_SZ], HEX(10000)
+        jbe      update_xcx /* skip store if not a pointer */
+        mov      PTRSZ [REG_XSP+REG_XCX-ARG_SZ], 0
+update_xcx:
+        add      REG_XCX, ARG_SZ
+        jne      check_stack
+        add      REG_XSP, 0 /* make a legal SEH64 epilog */
+        ret
+        END_FUNC(FUNCNAME)
 #undef FUNCNAME
 
-                       END_FILE
+
+END_FILE
