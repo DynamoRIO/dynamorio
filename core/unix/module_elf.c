e41122c426cdf15096ed3da916fc4e4acd93ec1f
Yao Zi <ziyao@disroot.org>
2025-03-23 10:06:22 +0000

    i#3544 RV64: Pass correct arguments to ifunc resolvers (#7374)
    Our private loader invokes ifunc resolvers with no arguments. Although
    it's correct on x86-32 and x86-64, this doesn't match glibc loader's
    behaviour on AArchxx and riscv64.
    
    On riscv64, resolvers will take the garbage in register. With a newer
    glibc version where a pointer to `__riscv_hwprobe()` was introduced to
    riscv64 ifunc ABI, the resolver may invoke the garbage as the hwprobe
    function, breaking clients depending on libc.
    
    Let's mimic the bahaviour of glibc's dynamic loader to keep our private
    loader compatible with newer glibc on riscv64.
    
    Issue: #3544, #7392

--- core/unix/module_elf.c
+++ core/unix/module_elf.c
@@ -38,6 +38,7 @@
 #include "module_private.h"
 #include "../utils.h"
 #include "instrument.h"
+#include "include/syscall.h"
 #include <stddef.h> /* offsetof */
 #include <link.h>   /* Elf_Symndx */
 
@@ -1449,6 +1450,45 @@ tlsdesc_resolver(struct tlsdesc_t *arg)
 
 #endif /* !ANDROID */
 
+#ifndef ANDROID
+
+#    ifdef RISCV64
+static int
+glibc_riscv_hwprobe(void *pairs, uint64_t pair_count, uint64_t cpu_count, uint64_t *cpus,
+                    uint32_t flags)
+{
+    return -dynamorio_syscall(SYS_riscv_hwprobe, 5, pairs, pair_count, cpu_count, cpus,
+                              flags);
+}
+#    endif
+
+/* TODO i#7392: dynamic loaders from different libc versions may provide
+ * different arguments for the ifunc resolver. We may mimic the exact behavior
+ * with its version obtained from the dynamic loader.
+ */
+static ELF_ADDR
+resolve_ifunc(app_pc resolver_pc)
+{
+    ELF_ADDR addr;
+
+    /* Refer to glibc/sysdeps/ARCH/dl-irel.h for prototype of resolver */
+#    ifdef RISCV64
+    typedef ELF_ADDR (*ifunc_resolver)(uint64_t hwcap, void *hwprobe, void *reserved);
+    /* TODO i#3544: RISC-V doesn't define any hwcap bits, thus it's fine to
+     * pass zero. Revisit when there're facilities for handling misc
+     * auxvector/hwcap bits. */
+    addr = ((ifunc_resolver)resolver_pc)(0, glibc_riscv_hwprobe, NULL);
+#    else
+    /* TODO i#7392: glibc 2.41 passes hwcap to ifunc resolvers on AArch32, and
+     * hwcap and __ifunc_arg_t structure to ifunc resolvers on AArch64.
+     */
+    addr = ((ELF_ADDR(*)(void))resolver_pc)();
+#    endif
+
+    return addr;
+}
+#endif /* !ANDROID */
+
 /* This routine is duplicated in privload_relocate_symbol for relocating
  * dynamorio symbols in a bootstrap stage. Any update here should be also
  * updated in privload_relocate_symbol.
@@ -1548,7 +1588,7 @@ module_relocate_symbol(ELF_REL_TYPE *rel, os_privmod_data_t *pd, bool is_rela)
 #    endif
     case ELF_R_IRELATIVE:
         res = (byte *)pd->load_delta + (is_rela ? addend : *r_addr);
-        *r_addr = ((ELF_ADDR(*)(void))res)();
+        *r_addr = resolve_ifunc(res);
         LOG(GLOBAL, LOG_LOADER, 4, "privmod ifunc reloc %s => " PFX "\n", name, *r_addr);
         break;
 #endif /* ANDROID */
