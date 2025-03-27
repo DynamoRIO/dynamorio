0b6ceb4d51b11e0c8c4eb0ba673aeba0cf115142
Kaiyeung Luk <kyluk@google.com>
2025-03-27 10:30:34 -0700

    Restore suite/tests/client-interface/strace.c and rename framework/samples/strace.c to strace-sample.c.

--- suite/tests/client-interface/strace.c
+++ suite/tests/client-interface/strace.c
@@ -1,85 +1,66 @@
 /* **********************************************************
- * Copyright (c) 2014-2020 Google, Inc.  All rights reserved.
+ * Copyright (c) 2014 Google, Inc.  All rights reserved.
+ * Copyright (c) 2008 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
-/* Dr. Memory: the memory debugger
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation;
- * version 2.1 of the License, and no later version.
-
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Library General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of VMware, Inc. nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
  */
 
-/* DRMF sample: strace.c
- *
- * Uses drsyscall to print the name and result of of every system call executed.
- */
+#include <stdio.h>
+#ifndef WINDOWS
+#    include <signal.h>
+#    include <errno.h>
+#endif
 
-#include "dr_api.h"
-#include "drmgr.h"
-#include "drsyscall.h"
-
-static bool
-event_filter_syscall(void *drcontext, int sysnum)
+int
+main()
 {
-    return true; /* we want to see all syscalls */
-}
-
-static bool
-event_pre_syscall(void *drcontext, int sysnum)
-{
-    drsys_syscall_t *syscall;
-    const char *name = "<unknown>";
-    if (drsys_cur_syscall(drcontext, &syscall) == DRMF_SUCCESS)
-        drsys_syscall_name(syscall, &name);
-    dr_fprintf(STDERR, "[%s ", name, sysnum);
-    /* We can also get the # of args and the type of each arg.
-     * See the drstrace tool for an example of how to do that.
-     */
-    return true; /* execute normally */
-}
-
-static void
-event_post_syscall(void *drcontext, int sysnum)
-{
-    bool success;
-    uint64 result;
-    uint error;
-    if (drsys_cur_syscall_result(drcontext, &success, &result, &error) == DRMF_SUCCESS) {
-        dr_fprintf(STDERR, "=> 0x"HEX64_FORMAT_STRING" ("SZFMT"), error=%d%s]\n",
-                   result, (ptr_int_t)result, error, success ? "" : " (failed)");
-    }
-}
-
-static void
-event_exit(void)
-{
-    if (drsys_exit() != DRMF_SUCCESS)
-        DR_ASSERT(false);
-    drmgr_exit();
-}
-
-DR_EXPORT void
-dr_init(client_id_t id)
-{
-    drsys_options_t ops = { sizeof(ops), 0, };
-    drmgr_init();
-    dr_register_filter_syscall_event(event_filter_syscall);
-    drmgr_register_pre_syscall_event(event_pre_syscall);
-    drmgr_register_post_syscall_event(event_post_syscall);
-    if (drsys_init(id, &ops) != DRMF_SUCCESS)
-        DR_ASSERT(false);
-    dr_register_exit_event(event_exit);
 #ifdef WINDOWS
-    dr_enable_console_printing(); /* ensure output shows up in cmd */
+    /* Though stderr and stdout go to the same place, we want two
+     * separate calls here.
+     */
+    fprintf(stderr, "Hello world!\n");
+    fprintf(stdout, "Goodbye world!\n");
+#else
+    fprintf(stdout, "Hello stdout!\n");
+    fprintf(stderr, "Hello stderr!\n");
+    /* I'm having trouble with deterministic ordering, w/ stdout
+     * redirected to stderr: must be buffering in libc.
+     */
+    fflush(stdout);
+    fflush(stderr);
+    /* Test dr_syscall_{get,set}_result_ex() */
+    sigset_t mask;
+    sigfillset(&mask);
+    int res = sigprocmask(SIG_BLOCK, &mask, NULL);
+    if (res == -1)
+        perror("sigprocmask failed");
+    else
+        fprintf(stderr, "sigprocmask succeeded\n");
 #endif
 }
