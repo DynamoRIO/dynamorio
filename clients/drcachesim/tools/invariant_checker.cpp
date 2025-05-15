42594591d3f9ce9749c78eab7615a60eec5320a4
Abhinav Anil Sharma <sharmaabhinav@google.com>
2025-05-15 14:57:24 -0400

    i#7157 dyn inject: Fix chunk instr count invariant (#7485)
    Fixes chunk instr count invariant check in the drmemtrace invariant
    checker, for the case when dynamic trace injection is enabled.
    
    Issue: #7157

--- clients/drcachesim/tools/invariant_checker.cpp
+++ clients/drcachesim/tools/invariant_checker.cpp
@@ -603,11 +603,14 @@ invariant_checker_t::parallel_shard_memref(void *shard_data, const memref_t &mem
     }
     if (memref.marker.type == TRACE_TYPE_MARKER &&
         memref.marker.marker_type == TRACE_MARKER_TYPE_CHUNK_FOOTER) {
-        report_if_false(shard,
-                        shard->skipped_instrs_ ||
-                            (shard->chunk_instr_count_ != 0 &&
-                             shard->instr_count_ % shard->chunk_instr_count_ == 0),
-                        "Chunk instruction counts are inconsistent");
+        report_if_false(
+            shard,
+            shard->skipped_instrs_ ||
+                (shard->chunk_instr_count_ != 0 &&
+                 (shard->instr_count_ - shard->dyn_injected_syscall_instr_count_) %
+                         shard->chunk_instr_count_ ==
+                     0),
+            "Chunk instruction counts are inconsistent");
     }
 
     // Invariant: a function marker should not appear between an instruction and its
