87f7d6c5faa7e663dd519d48a26846a07d075c9d
Derek Bruening <bruening@google.com>
2025-06-27 15:22:56 -0400

    i#7394 callstack failure: Add to ignore list (#7519)
    Adds the sample.callstack 32-bit x86 failure to the ignore list as it is
    blocking progress.
    
    Issue: #7394

--- suite/runsuite_wrapper.pl
+++ suite/runsuite_wrapper.pl
@@ -473,6 +473,7 @@ for (my $i = 0; $i <= $#lines; ++$i) {
                 'code_api|linux.thread-reset' => 1, # i#4604
                 'code_api|linux.clone-reset' => 1, # i#4604
                 'code_api|client.detach_test' => 1, # i#6764
+                'code_api|sample.callstack' => 1, # i#7394
                 # These are from the long suite.
                 'common.decode-stress' => 1, # i#1807 Ignored for all options.
                 'code_api,opt_speed|common.fib' => 1, # i#1807: Undiagnosed timeout.
