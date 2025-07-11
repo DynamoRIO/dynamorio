44ed7e590713b47cc0fa13893aaf597278aee406
Abhinav Anil Sharma <sharmaabhinav@google.com>
2025-07-11 00:21:57 -0400

    Revert extraneous clang-format change

--- clients/drcachesim/tools/view.h
+++ clients/drcachesim/tools/view.h
@@ -31,23 +31,23 @@
  */
 
 #ifndef _VIEW_H_
-#    define _VIEW_H_ 1
+#define _VIEW_H_ 1
 
-#    include <stdint.h>
+#include <stdint.h>
 
-#    include <iomanip>
-#    include <iostream>
-#    include <memory>
-#    include <string>
-#    include <unordered_map>
-#    include <unordered_set>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 
-#    include "dr_api.h" // Must be before trace_entry.h from analysis_tool.h.
-#    include "analysis_tool.h"
-#    include "decode_cache.h"
-#    include "memref.h"
-#    include "memtrace_stream.h"
-#    include "raw2trace.h"
+#include "dr_api.h" // Must be before trace_entry.h from analysis_tool.h.
+#include "analysis_tool.h"
+#include "decode_cache.h"
+#include "memref.h"
+#include "memtrace_stream.h"
+#include "raw2trace.h"
 
 namespace dynamorio {
 namespace drmemtrace {
