DynamoRIO/dynamorio

commit 5e8fbbd27dd5d1d822a2c2279af9e1a2f4f6ce01

i#7535: Properly set sysparam 1 on AMD 32-bit (#7539)
System call parameter 1 for AMD 32-bit needs to use the actual vsyscall
kernel input register and not the ABI register in order for a
modification to be seen by the kernel.

This lets us remove client.strace from the AMD_X32_DENYLIST.

Fixes #7535

core/unix/os.c
@@ -5512,9 +5512,11 @@ sys_param_addr(dcontext_t *dcontext, int num)
      */
     return ((reg_t *)mc->esp) + 1 /*retaddr*/ + num;
 #    endif
-    /* even for vsyscall where ecx (syscall) or esp (sysenter) are saved into
+    /* Even for vsyscall where ecx (syscall) or esp (sysenter) are saved into
      * ebp, the original parameter registers are not yet changed pre-syscall,
-     * except for ebp, which is pushed on the stack:
+     * except for ebp, which is pushed on the stack. But since we also modify
+     * the parameter through sys_param_addr() we need to use the actual location
+     * passed to the kernel.
      *     0xffffe400  55                   push   %ebp %esp -> %esp (%esp)
      *     0xffffe401  89 cd                mov    %ecx -> %ebp
      *     0xffffe403  0f 05                syscall -> %ecx
@@ -5527,7 +5529,9 @@ sys_param_addr(dcontext_t *dcontext, int num)
      */
     switch (num) {
     case 0: return &mc->IF_X86_ELSE(xbx, IF_RISCV64_ELSE(a0, r0));
-    case 1: return &mc->IF_X86_ELSE(xcx, IF_RISCV64_ELSE(a1, r1));
+    case 1:
+        return IF_X86_ELSE((dcontext->sys_was_int ? &mc->xcx : &mc->xbp),
+                           &mc->IF_RISCV64_ELSE(a1, r1));
     case 2: return &mc->IF_X86_ELSE(xdx, IF_RISCV64_ELSE(a2, r2));
     case 3: return &mc->IF_X86_ELSE(xsi, IF_RISCV64_ELSE(a3, r3));
     case 4: return &mc->IF_X86_ELSE(xdi, IF_RISCV64_ELSE(a4, r4));

suite/tests/CMakeLists.txt
@@ -6773,7 +6773,6 @@ if (UNIX AND X86 AND CPU_AMD AND NOT DR_HOST_X64)
     code_api|api.startstop
     code_api|api.static_signal
     code_api|client.flush
-    code_api|client.strace
     code_api|linux.eintr
     code_api|linux.eintr-noinline
     code_api|tool.basic_counts

commit 2b816136ded96b10402a5ccc1acea5606934d6ef

i#7566: Fix 32-bit Intel sysparam regression
Fixes a regression from PR #7539 where sysparam 1 on Intel 32-bit
should remain ecx.

Tested by running `ctest -j 12` on an Intel 32-bit debug build.
Without the fix:
```
64% tests passed, 166 tests failed out of 461
```
With this fix:
```
95% tests passed, 23 tests failed out of 461
```

The vmareas assert from invalid mmap size reported in the issue is
also confirmed to be gone.

Fixes #7566

core/unix/os.c
@@ -5530,8 +5530,11 @@ sys_param_addr(dcontext_t *dcontext, int num)
     switch (num) {
     case 0: return &mc->IF_X86_ELSE(xbx, IF_RISCV64_ELSE(a0, r0));
     case 1:
-        return IF_X86_ELSE((dcontext->sys_was_int ? &mc->xcx : &mc->xbp),
-                           &mc->IF_RISCV64_ELSE(a1, r1));
+        return IF_X86_ELSE(
+            ((dcontext->sys_was_int || get_syscall_method() == SYSCALL_METHOD_SYSENTER)
+                 ? &mc->xcx
+                 : &mc->xbp),
+            &mc->IF_RISCV64_ELSE(a1, r1));
     case 2: return &mc->IF_X86_ELSE(xdx, IF_RISCV64_ELSE(a2, r2));
     case 3: return &mc->IF_X86_ELSE(xsi, IF_RISCV64_ELSE(a3, r3));
     case 4: return &mc->IF_X86_ELSE(xdi, IF_RISCV64_ELSE(a4, r4));