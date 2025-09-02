2fcfd5de91880493db32335d83bd85b4a53d630e
Abhinav Anil Sharma <sharmaabhinav@google.com>
2025-09-02 13:56:45 -0400

    Enable Doxygen

--- clients/drcachesim/simulator/caching_device_stats.h
+++ clients/drcachesim/simulator/caching_device_stats.h
@@ -57,19 +57,27 @@
 namespace dynamorio {
 namespace drmemtrace {
 
-// Enumerates the different types of cache line invalidation events that
-// may be seen by a cache.
+/**
+ * Enumerates the different types of cache line invalidation events that
+ *  may be seen by a cache.
+ */
 enum invalidation_type_t {
-    // Invalidation performed to maintain inclusivity of the parent cache,
-    // which evicted a cache line due to its replacement policy, or to
-    // maintain its own parent's inclusivity. Inclusive caches also
-    // propagate this events to their children.
+    /**
+     * Invalidation performed to maintain inclusivity of the parent cache,
+     * which evicted a cache line due to its replacement policy, or to
+     * maintain its own parent's inclusivity. Inclusive caches also
+     * propagate this events to their children.
+     */
     INVALIDATION_INCLUSIVE,
-    // Invalidation performed to honor cache coherence protocols. This
-    // is also propagated to the cache's children.
+    /**
+     * Invalidation performed to honor cache coherence protocols. This
+     * is also propagated to the cache's children.
+     */
     INVALIDATION_COHERENCE,
-    // Invalidation performed when a line is moved to a child cache of
-    // an exclusive cache.
+    /**
+     * Invalidation performed when a line is moved to a child cache of
+     * an exclusive cache.
+     */
     INVALIDATION_EXCLUSIVE,
 };
 
