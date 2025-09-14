515c22b0a6464c96949e6c1876496d9ebd1ad561
Abhinav Anil Sharma <sharmaabhinav@google.com>
2025-09-13 21:43:48 -0400

    Also refactor some other reader mocks

--- clients/drcachesim/tests/mock_reader.h
+++ clients/drcachesim/tests/mock_reader.h
@@ -50,9 +50,9 @@ class mock_reader_t : public reader_t {
 public:
     mock_reader_t() = default;
     explicit mock_reader_t(const std::vector<trace_entry_t> &trace)
-        : trace_(trace)
+        : reader_t(/*online=*/false, /*verbosity=*/3, "mock_reader_t")
+        , trace_(trace)
     {
-        verbosity_ = 3;
     }
     bool
     init() override
