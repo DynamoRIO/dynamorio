05e4db552e8377fc88b3e16e1aa8dae625ec99c8
Abhinav Anil Sharma <sharmaabhinav@google.com>
2025-09-16 08:56:35 -0400

    Remove extraneous diff

--- clients/drcachesim/reader/record_file_reader.h
+++ clients/drcachesim/reader/record_file_reader.h
@@ -235,7 +235,8 @@ public:
     {
         return in_kernel_trace_;
     }
-    uint64_t get_next_trace_pc() const override
+    uint64_t
+    get_next_trace_pc() const override
     {
         return next_trace_pc_;
     }
