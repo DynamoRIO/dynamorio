d86e037fb814a02bbb80c660e65dd0ad78df2279
Abhinav Anil Sharma <sharmaabhinav@google.com>
2025-09-18 17:57:16 -0400

    Build warning on windows

--- clients/drcachesim/scheduler/scheduler_impl.cpp
+++ clients/drcachesim/scheduler/scheduler_impl.cpp
@@ -1987,7 +1987,7 @@ scheduler_impl_tmpl_t<RecordType, ReaderType>::inject_kernel_sequence(
     } else {
         next_trace_pc = input->reader->get_next_trace_pc();
     }
-    const addr_t next_input_trace_pc = reinterpret_cast<addr_t>(next_trace_pc);
+    const addr_t next_input_trace_pc = static_cast<addr_t>(next_trace_pc);
     for (int i = static_cast<int>(sequence.records.size()) - 1; i >= 0; --i) {
         RecordType record = sequence.records[i];
         // TODO i#7495: Add invariant checks that ensure these are equal to the
