a23b22188afe1632630bba0a96698f1ed1fe04b7
edeiana <edeiana@google.com>
2025-12-03 15:27:48 -0800

    Add counters inline optimization for AARCH64.

--- clients/drpoints/drpoints.cpp
+++ clients/drpoints/drpoints.cpp
@@ -333,8 +333,9 @@ event_app_instruction(void *drcontext, void *tag, instrlist_t *bb, instr_t *inst
     size_t bb_size = drx_instrlist_app_size(bb);
 
 #ifdef INLINE_COUNTER_UPDATE
-#    ifdef X86_64
+#    if defined(X86_64) || defined(AARCH64)
     instr_t *skip_call = INSTR_CREATE_label(drcontext);
+#        if defined(X86_64)
     // Increment the BB execution count by BB size in #instructions.
     drx_insert_counter_update(drcontext, bb, inst,
                               // We're using drmgr, so these slots
@@ -358,13 +359,43 @@ event_app_instruction(void *drcontext, void *tag, instrlist_t *bb, instr_t *inst
     // the BB and continue.
     MINSERT(bb, inst, INSTR_CREATE_jcc(drcontext, OP_jg, opnd_create_instr(skip_call)));
 
-    // Insert call to instrumentation function that saves the current BBV.
+#        elif defined(AARCH64)
+    reg_id_t scratch1, scratch2 = DR_REG_NULL;
+    drx_insert_counter_update(
+        drcontext, bb, inst, static_cast<dr_spill_slot_t>(SPILL_SLOT_MAX + 1),
+        static_cast<dr_spill_slot_t>(SPILL_SLOT_MAX + 1), &instr_count,
+        -(static_cast<int>(bb_size)), DRX_COUNTER_64BIT | DRX_COUNTER_REL_ACQ);
+
+    if (drreg_reserve_register(drcontext, bb, inst, NULL, &scratch1) != DRREG_SUCCESS)
+        FATAL("ERROR: failed to reserve scratch register 1");
+    if (drreg_reserve_register(drcontext, bb, inst, NULL, &scratch2) != DRREG_SUCCESS)
+        FATAL("ERROR: failed to reserve scratch register 2");
+
+    instrlist_insert_mov_immed_ptrsz(drcontext, static_cast<ptr_int_t>(&instr_count),
+                                     opnd_create_reg(scratch1), bb, inst, NULL, NULL);
+    MINSERT(bb, inst,
+            XINST_CREATE_load(drcontext, opnd_create_reg(scratch2),
+                              OPND_CREATE_MEMPTR(scratch1, 0)));
+    MINSERT(bb, inst,
+            INSTR_CREATE_tbz(drcontext, opnd_create_instr(skip_call),
+                             /* If the top bit is still zero, skip the call. */
+                             opnd_create_reg(scratch2), OPND_CREATE_INTPTR(63)));
+#        endif
+    //  Insert call to instrumentation function that saves the current BBV.
     dr_insert_clean_call(drcontext, bb, inst, reinterpret_cast<void *>(save_bbv),
                          /*save_fpstate=*/false, 0);
     MINSERT(bb, inst, skip_call);
 
+#        if defined(X86_64)
     if (drreg_unreserve_aflags(drcontext, bb, inst) != DRREG_SUCCESS)
         DR_ASSERT(false);
+#        elif defined(AARCH64)
+    if (drreg_unreserve_register(drcontext, bb, inst, scratch1) != DRREG_SUCCESS ||
+        (scratch2 != DR_REG_NULL &&
+         drreg_unreserve_register(drcontext, bb, inst, scratch2) != DRREG_SUCCESS)) {
+        DR_ASSERT(false);
+    }
+#        endif
 #    else
 #        error NYI
 #    endif
