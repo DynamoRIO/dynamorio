e5a5d105a185cfe4be6435f58f687de7281058e5
Moonseop Kim <moonseopp.kim@gmail.com>
2026-01-21 23:47:13 +0000

    Revert allasm_repstr code

--- clients/drcachesim/tests/allasm_repstr.asm
+++ clients/drcachesim/tests/allasm_repstr.asm
@@ -1,6 +1,7 @@
-/* **********************************************************
+ /* **********************************************************
  * Copyright (c) 2021-2025 Google, Inc.  All rights reserved.
  * **********************************************************/
+
 /*
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -28,6 +29,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
  */
+
 /* This is a statically-linked app. */
 .text
 .globl _start
