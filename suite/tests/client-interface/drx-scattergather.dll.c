4dab2d050a16ccb0b025fe9233e1d3c30e6411f7
Phil Ramsey <phil.ramsey@arm.com>
2026-01-26 10:18:21 +0000

    i#5365 Scatter/gather tests: add issue number to comment (#7783)
    Reference issue #1672 in the comments about not using annotations in the
    scatter/gather tests.
    
    Issue: #5365

--- suite/tests/client-interface/drx-scattergather.dll.c
+++ suite/tests/client-interface/drx-scattergather.dll.c
@@ -86,7 +86,7 @@ event_app_instruction(void *drcontext, void *tag, instrlist_t *bb, instr_t *inst
     return DR_EMIT_DEFAULT;
 }
 
-/* XXX : we need a way to only test the scatter/gather instructions in our
+/* i#1672: we need a way to only test the scatter/gather instructions in our
  * test application, and ignore scatter/gather instructions from OS libraries.
  * As AArch64 does not have annotations yet the best way to do this seems to be
  * to use the module interface to check that the current module is the exe.
