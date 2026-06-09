820fe172b2122c1a23fe436129ba3e3a4d7ee7a9
Bin Wang <binwang@umass.edu>
2026-06-09 19:51:19 -0400

    Update copyright banner

--- core/unix/coredump.c
+++ core/unix/coredump.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2024 Google, Inc.  All rights reserved.
+ * Copyright (c) 2024-2026 Google, Inc.  All rights reserved.
  * **********************************************************/
 
 /*
