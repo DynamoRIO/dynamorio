4aea5037bce34c9138b22c826fdd443a8fcf7707
Abhinav Anil Sharma <sharmaabhinav@google.com>
2026-06-18 10:57:02 -0400

    Fix comments

--- core/unix/coredump.c
+++ core/unix/coredump.c
@@ -60,9 +60,8 @@
 #define SECTION_HEADER_TABLE ".shstrtab"
 /* The [vvar] and [vvar_vclock] sections are special kernel-mapped pages. While they
  * are marked as readable in /proc/self/maps, some pages within them may not be
- * physically backed and will trigger a SIGBUS if we try to read them during core
- * dump generation. Thus, we include their program headers in the core dump (with
- * file size 0) but skip writing their actual content.
+ * physically backed and will trigger errors if we try to read them during core
+ * dump generation.
  */
 #define VVAR_SECTION "[vvar]"
 #define VVAR_VCLOCK_SECTION "[vvar_vclock]"
@@ -607,7 +606,7 @@ os_dump_core_internal(dcontext_t *dcontext, const char *output_directory DR_PARA
         }
         const ELF_WORD size = section_header_info[section_index].vm_end -
             section_header_info[section_index].vm_start;
-        // The vvar section is not readable. The file size is set to zero to
+        // The vvar sections are not readable. The file size is set to zero to
         // initialize the memory region to 0.
         if (!write_program_header(
                 elf_file, PT_LOAD, flags,
@@ -658,7 +657,7 @@ os_dump_core_internal(dcontext_t *dcontext, const char *output_directory DR_PARA
     }
     // Write memory content to the core dump file.
     for (int section_index = 0; section_index < section_count - 1; ++section_index) {
-        // The vvar section is not readable. The program header has the file
+        // The vvar sections are not readable. The program header has the file
         // size set to zero to indicate the content is not recorded in the
         // memory dump file.
         if (section_header_info[section_index].is_vvar) {
