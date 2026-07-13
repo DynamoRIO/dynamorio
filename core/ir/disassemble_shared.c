e7b3e7e87e2c2714f8fb9967933203569ec27315
Bin Wang <biwa@google.com>
2026-07-13 15:02:20 -0700

    Update disassemble_shared.c

--- core/ir/disassemble_shared.c
+++ core/ir/disassemble_shared.c
@@ -741,8 +741,10 @@ internal_opnd_disassemble(char *buf, size_t bufsz, size_t *sofar DR_PARAM_INOUT,
         break;
     case BASE_DISP_kind: opnd_base_disp_disassemble(buf, bufsz, sofar, opnd); break;
 #if defined(X64) || defined(ARM)
-    case REL_ADDR_kind: print_to_buffer(buf, bufsz, sofar, "<rel> "); DR_FALLTHROUGH;
+    case REL_ADDR_kind:
+        print_to_buffer(buf, bufsz, sofar, "<rel> ");
 #    ifdef X64
+        DR_FALLTHROUGH;
     case ABS_ADDR_kind:
 #    endif
         opnd_mem_disassemble_prefix(buf, bufsz, sofar, opnd);
