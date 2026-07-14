48cd010f947d07ae1853e8bd693ffc2053347832
Bin Wang <biwa@google.com>
2026-07-13 17:14:33 -0700

    Update stacktest.c

--- suite/tests/security-linux/stacktest.c
+++ suite/tests/security-linux/stacktest.c
@@ -109,7 +109,7 @@ main(int argc, char **argv)
     switch (argv[1][1]) {
     case 't': do_trampoline(); break;
 
-    case 'b': do_trampoline();
+    case 'b': do_trampoline(); __attribute__((__fallthrough__));
 
     case 'e': do_exploit(); break;
 
