003904e08799b7a4f3e705ea5ed65f4908e4deaa
Bin Wang <biwa@google.com>
2026-07-14 18:21:30 +0000

    Update opnd_shared.c

--- core/ir/opnd_shared.c
+++ core/ir/opnd_shared.c
@@ -988,7 +988,7 @@ uint
 opnd_size_to_shift_amount(opnd_size_t size)
 {
     switch (size) {
-    default: ASSERT(false); DR_FALLTHROUGH;
+    default: ASSERT(false); return 0;
     case OPSZ_1: return 0;
     case OPSZ_2: return 1;
     case OPSZ_4: return 2;
