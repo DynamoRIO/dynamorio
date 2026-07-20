cdca647039fa865e36673f33e2faae2f1ea10f53
Bin Wang <biwa@google.com>
2026-07-20 15:25:16 -0700

    i#8014: Rename local variables named fallthrough (#8015)
    Renamed local variables named fallthrough in two places:
    1. `mangle_trace()` in `core/arch/interp.c`: `fallthrough` ->
    `fallthrough_pc`
    2. `dr_insert_cbr_instrumentation_help()` in `core/lib/instrument.c`:
    `fallthrough` -> `fallthrough_addr`
    
    Fixes #8014

--- core/arch/interp.c
+++ core/arch/interp.c
@@ -6906,7 +6906,7 @@ mangle_trace(dcontext_t *dcontext, instrlist_t *ilist, monitor_data_t *md)
 {
     instr_t *inst, *next_inst, *start_instr, *jmp;
     uint blk, num_exits_deleted;
-    app_pc fallthrough = NULL;
+    app_pc fallthrough_pc = NULL;
     bool found_syscall = false, found_int = false;
 
     /* We don't assert that mangle_trace_at_end() is true b/c the client
@@ -6974,7 +6974,7 @@ mangle_trace(dcontext_t *dcontext, instrlist_t *ilist, monitor_data_t *md)
             /* Do not call instr_length() on this inst: use length
              * of translation! (i#509)
              */
-            fallthrough = decode_next_pc(dcontext, xl8);
+            fallthrough_pc = decode_next_pc(dcontext, xl8);
         }
 
         /* PR 299808: identify bb boundaries.  We can't go by translations alone, as
@@ -7056,7 +7056,7 @@ mangle_trace(dcontext_t *dcontext, instrlist_t *ilist, monitor_data_t *md)
             return false;
         }
         /* must have been no final exit cti: add final fall-through jmp */
-        jmp = create_exit_jmp(dcontext, fallthrough, fallthrough, 0);
+        jmp = create_exit_jmp(dcontext, fallthrough_pc, fallthrough_pc, 0);
         /* XXX PR 307284: support client modifying, replacing, or adding
          * syscalls and ints: need to re-analyze.  Then we wouldn't
          * need the md->final_exit_flags field anymore.
