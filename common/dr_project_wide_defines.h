49682a1f30a5df80e423ea345e96fc7b86e2c3db
Derek Bruening <bruening@google.com>
2026-07-28 19:46:37 -0400

    Fix DRview build

--- common/dr_project_wide_defines.h
+++ common/dr_project_wide_defines.h
@@ -68,6 +68,7 @@
 #define BUFFER_SIZE_ELEMENTS(buf) (BUFFER_SIZE_BYTES(buf) / sizeof(buf[0]))
 #define BUFFER_LAST_ELEMENT(buf) buf[BUFFER_SIZE_ELEMENTS(buf) - 1]
 #define NULL_TERMINATE_BUFFER(buf) BUFFER_LAST_ELEMENT(buf) = 0
+#define NULL_TERMINATE_SIZED_BUFFER(buf, size) (buf)[(size) - 1] = 0
 #define BUFFER_ROOM_LEFT_W(wbuf) (BUFFER_SIZE_ELEMENTS(wbuf) - wcslen(wbuf) - 1)
 #define BUFFER_ROOM_LEFT(abuf) (BUFFER_SIZE_ELEMENTS(abuf) - strlen(abuf) - 1)
 
