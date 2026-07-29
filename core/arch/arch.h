11aa40fe619b736ea479230a6731e85250f7b4cd
Bin Wang <biwa@google.com>
2026-07-29 21:17:59 +0000

    Change fragment.h back to relative pathing

--- core/arch/arch.h
+++ core/arch/arch.h
@@ -48,7 +48,7 @@
 #include "instr.h"          /* for reg_id_t */
 #include "decode.h"         /* for X64_CACHE_MODE_DC */
 #include "arch_exports.h"   /* for FRAG_IS_32 and FRAG_IS_X86_TO_X64 */
-#include "fragment.h"       /* IS_IBL_TARGET */
+#include "../fragment.h"    /* IS_IBL_TARGET */
 #include "ir_utils.h"
 
 #if defined(X86) && defined(X64)
