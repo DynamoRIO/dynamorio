838ba4cdac22e2cf53e3e4692f1444e2efe380e0
Bin Wang <biwa@google.com>
2026-09-09 19:52:55 +0000

    Parse options during module init

--- core/kernel_linux/dynamorio_module_main.c
+++ core/kernel_linux/dynamorio_module_main.c
@@ -56,6 +56,8 @@ MODULE_PARM_DESC(
     options,
     "DynamoRIO runtime options string (read-only), e.g., \"-loglevel 2 -log_to_stderr\"");
 
+void dynamorio_app_init_part_one_options(void);
+
 static int __init
 dynamorio_module_init(void)
 {
@@ -68,6 +70,8 @@ dynamorio_module_init(void)
     if (ret != 0) {
         goto fail;
     }
+
+    dynamorio_app_init_part_one_options();
 
     pr_info("Module started\n");
     return 0;
