1998ebaab6478e88a488a411d156f14e6534703c
Bin Wang <biwa@google.com>
2026-09-18 16:04:26 -0700

    Add temporary kernel initialization scaffolding

--- core/kernel_linux/dynamorio_module_main.c
+++ core/kernel_linux/dynamorio_module_main.c
@@ -82,6 +82,7 @@ dynamorio_module_init(void)
      */
     preempt_disable();
     dynamorio_app_init_part_one_options();
+    kernel_heap_init_scaffolding();
     preempt_enable();
 
     pr_info("Module started\n");
