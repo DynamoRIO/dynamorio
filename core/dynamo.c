16a201d58897393178ebb88220de68b1acfb3472
Bin Wang <biwa@google.com>
2026-09-25 12:26:42 -0700

    Advance module initialization past  loader_init_prologue()

--- core/dynamo.c
+++ core/dynamo.c
@@ -559,12 +559,6 @@ dynamorio_app_init_part_two_finalize(void)
         proc_init();
         modules_init(); /* before vm_areas_init() */
         d_r_os_init();
-#ifdef LINUX_KERNEL
-        /* TODO i#8021: Remove this early return as the remainder of part two is ported.
-         * Move it further down for each incremental step.
-         */
-        return SUCCESS;
-#endif
         config_heap_init(); /* after heap_init */
 
         /* Setup for handling faults in loader_init() */
@@ -597,6 +591,12 @@ dynamorio_app_init_part_two_finalize(void)
          * code, such as the private PEB on Windows.
          */
         loader_init_prologue();
+#ifdef LINUX_KERNEL
+        /* TODO i#8021: Remove this early return as the remainder of part two is ported.
+         * Move it further down for each incremental step.
+         */
+        return SUCCESS;
+#endif
 
         d_r_arch_init();
         synch_init();
